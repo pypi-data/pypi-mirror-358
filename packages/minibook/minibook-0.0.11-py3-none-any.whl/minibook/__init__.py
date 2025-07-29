09dd89b31d580eb65ec522d94ff11673b7c53248
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:06:06 +0000

    Add package minibook/0.0.11/minibook-0.0.11-py3-none-any.whl

---
+++ packages/minibook/minibook-0.0.11-py3-none-any.whl/minibook/__init__.py
@@ -0,0 +1,8 @@
+"""MiniBook - A tool to create a minibook from a list of links.
+
+Supports both MkDocs and Jinja2/HTML generation.
+"""
+
+from .main import entrypoint, generate_html
+
+__all__ = ["entrypoint", "generate_html"]
