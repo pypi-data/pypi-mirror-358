bc53bf766759ce9699694371bfbae1dbc132629e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:45 +0000

    Add package topasio/0.2.0/topasio-0.2.0-py3-none-any.whl

---
+++ packages/topasio/topasio-0.2.0-py3-none-any.whl/topasio/__init__.py
@@ -0,0 +1,21 @@
+
+from geometry import Ge, GElement, TsBox, TsCylinder, TsJaws, G4Trd, G4Cons, Group, VizComponent
+from scorer import Scorer, PhaseSpaceScorer
+from source import Beam, PhaseSpaceSource
+
+
+__all__ = [
+    "Ge",
+    "GElement",
+    "TsBox",
+    "TsCylinder",
+    "TsJaws",
+    "G4Trd",
+    "G4Cons",
+    "Group",
+    "VizComponent",
+    "Scorer",
+    "PhaseSpaceScorer",
+    "Beam",
+    "PhaseSpaceSource",
+]
---
+++ packages/topasio/topasio-0.2.0-py3-none-any.whl/topasio/spaces/Is.py
@@ -0,0 +1,4 @@
+from src.topasio import Space
+
+Is = Space()
+
