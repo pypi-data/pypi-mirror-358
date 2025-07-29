3f742f7fdd65f2573f8f0f2bc0ed1326ea400b5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:52 +0000

    Add package pyunilogger/0.0.3/pyunilogger-0.0.3.tar.gz

---
+++ packages/pyunilogger/pyunilogger-0.0.3.tar.gz/pyunilogger-0.0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pyunilogger",
+    version="0.0.3",
+    packages=["pyunilogger"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
