2509191103ba3579f809e3977874a20e0153d74f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:41 +0000

    Add package fastddm/0.3.14/fastddm-0.3.14-cp312-cp312-win_amd64.whl

---
+++ packages/fastddm/fastddm-0.3.14-cp312-cp312-win_amd64.whl/fastddm/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+import sys
+
+if sys.version_info < (3, 9):
+    sys.exit("Error, Python < 3.9 is not supported.")
+
+setup(
+    name="fastddm",
+    use_scm_version={
+        "root": "D:/a/fastddm/fastddm",
+        "fallback_version": "0.3.14",
+    },
+    setup_requires=["setuptools_scm"],
+    packages=["fastddm"],
+    package_dir={"fastddm": "D:/a/fastddm/fastddm/build/temp.win-amd64-cpython-312/Release/src/python/fastddm"},
+    package_data={"fastddm": ["_core.*", "_core_cuda.*"]},
+)
