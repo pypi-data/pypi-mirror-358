4eb500fe5f9aa3a4aa093b452c003261f6723866
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:06:32 +0000

    Add package ansible-collections/0.0.1/ansible-collections-0.0.1.tar.gz

---
+++ packages/ansible-collections/ansible-collections-0.0.1.tar.gz/ansible-collections-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ansible-collections",
+    version="0.0.1",
+    packages=["ansible-collections"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
