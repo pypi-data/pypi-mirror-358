b3ff86943e8f88872b67c8cdc22ed5a8291bffb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:30 +0000

    Add package sitector-access/0.4.1/sitector-access-0.4.1.tar.gz

---
+++ packages/sitector-access/sitector-access-0.4.1.tar.gz/sitector-access-0.4.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="sitector-access",
+    version="0.4.1",
+    packages=["sitector-access"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
