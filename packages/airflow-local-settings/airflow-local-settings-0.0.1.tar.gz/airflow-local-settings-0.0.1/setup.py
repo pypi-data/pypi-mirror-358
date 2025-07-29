b868ef3037ea7a1d0949208a246064d5b32e778c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:06:10 +0000

    Add package airflow-local-settings/0.0.1/airflow-local-settings-0.0.1.tar.gz

---
+++ packages/airflow-local-settings/airflow-local-settings-0.0.1.tar.gz/airflow-local-settings-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="airflow-local-settings",
+    version="0.0.1",
+    packages=["airflow-local-settings"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
