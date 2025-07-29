5307409befa7f00e6648bc8282f3300b871b63c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:35 +0000

    Add package openstack-dashboard/0.0.1/openstack-dashboard-0.0.1.tar.gz

---
+++ packages/openstack-dashboard/openstack-dashboard-0.0.1.tar.gz/openstack-dashboard-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="openstack-dashboard",
+    version="0.0.1",
+    packages=["openstack-dashboard"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
