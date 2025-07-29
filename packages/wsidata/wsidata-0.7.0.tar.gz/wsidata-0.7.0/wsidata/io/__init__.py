acd140ba009922b4e49eb9a37772636a988215d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:08:01 +0000

    Add package wsidata/0.7.0/wsidata-0.7.0.tar.gz

---
+++ packages/wsidata/wsidata-0.7.0.tar.gz/wsidata-0.7.0/wsidata/io/__init__.py
@@ -0,0 +1,11 @@
+from ._elems import (
+    add_agg_features,
+    add_features,
+    add_shapes,
+    add_table,
+    add_tiles,
+    add_tissues,
+    subset_tiles,
+    update_shapes_data,
+)
+from ._wsi import agg_wsi, open_wsi
