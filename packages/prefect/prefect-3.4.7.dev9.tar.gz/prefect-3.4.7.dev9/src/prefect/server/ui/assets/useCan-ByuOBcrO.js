8202d43c09aa3723fd3a666249f0151686010c5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:06:20 +0000

    Add package prefect/3.4.7.dev9/prefect-3.4.7.dev9.tar.gz

---
+++ packages/prefect/prefect-3.4.7.dev9.tar.gz/prefect-3.4.7.dev9/src/prefect/server/ui/assets/useCan-ByuOBcrO.js
@@ -0,0 +1,2 @@
+import{cH as n}from"./index-oAYIHVEV.js";const c=Symbol("canInjectionKey");function a(){return n(c)}export{c,a as u};
+//# sourceMappingURL=useCan-ByuOBcrO.js.map
---
+++ packages/prefect/prefect-3.4.7.dev9.tar.gz/prefect-3.4.7.dev9/src/prefect/server/ui/assets/usePrefectApi-5_rRaBxL.js
@@ -0,0 +1,2 @@
+import{cH as r}from"./index-oAYIHVEV.js";import{p as e}from"./api-BvRUJXkq.js";function o(){return r(e)}export{o as u};
+//# sourceMappingURL=usePrefectApi-5_rRaBxL.js.map
