1b94ee6bd848ab4b80e1d3c01915f65c2dbcb208
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:29 +0000

    Add package thetakde/0.3.0/thetakde-0.3.0.tar.gz

---
+++ packages/thetakde/thetakde-0.3.0.tar.gz/thetakde-0.3.0/setup.py
@@ -0,0 +1,27 @@
+# setup.py
+
+from setuptools import setup, find_packages
+# "name" needs to be lowercase
+
+setup(
+    name='thetakde',  # "name" must be lowercase
+    version='0.3.0',
+    author='DSML-book',
+    author_email='kroese@maths.uq.edu.au',
+    description='A package for the theta kernel density estimator',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/DSML-book/ThetaKDE',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'scipy',
+        'matplotlib',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
