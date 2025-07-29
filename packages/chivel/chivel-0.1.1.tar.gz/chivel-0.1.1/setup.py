2e0c5eaeba997ada07e02be24243e6a64bb8ee38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:28 +0000

    Add package chivel/0.1.1/chivel-0.1.1.tar.gz

---
+++ packages/chivel/chivel-0.1.1.tar.gz/chivel-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="chivel",
+    version="0.1.1",
+    description="Chivel C++/Python extension",
+    author="Mitchell Talyat",
+    packages=["chivel"],
+    package_data={"chivel": ["chivel.pyd", "chivel.pyi"]},
+    include_package_data=True,
+    zip_safe=False,
+)
