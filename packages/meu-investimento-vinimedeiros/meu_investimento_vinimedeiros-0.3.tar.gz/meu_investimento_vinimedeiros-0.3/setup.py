64480f0a0ca10b925dc2e4c6c6854fd57a320d32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:07:16 +0000

    Add package meu-investimento-vinimedeiros/0.3/meu_investimento_vinimedeiros-0.3.tar.gz

---
+++ packages/meu-investimento-vinimedeiros/meu_investimento_vinimedeiros-0.3.tar.gz/meu_investimento_vinimedeiros-0.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+    
+setup(
+   name='meu_investimento_vinimedeiros',
+   version='0.3',
+   packages=find_packages(),
+   install_requires=[],
+   long_description=long_description,
+   long_description_content_type="text/markdown",
+   author='Vinicius Medeiros',
+   author_email='vinicius.medeiros-2025@hotmail.com',
+   description='Uma biblioteca para cálculos de investimentos.',
+   url='https://github.com/viniciusmedeiros/meu_investimento',
+   classifiers=[
+       'Programming Language :: Python :: 3',
+       'License :: OSI Approved :: MIT License',
+       'Operating System :: OS Independent',
+   ],
+   python_requires='>=3.6',
+)
