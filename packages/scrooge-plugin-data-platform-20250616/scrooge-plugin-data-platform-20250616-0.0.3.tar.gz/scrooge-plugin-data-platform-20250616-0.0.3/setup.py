a69dbbf51f2a03b7cc89fdd3f847a61d3c6b4caf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:35 +0000

    Add package scrooge-plugin-data-platform-20250616/0.0.3/scrooge-plugin-data-platform-20250616-0.0.3.tar.gz

---
+++ packages/scrooge-plugin-data-platform-20250616/scrooge-plugin-data-platform-20250616-0.0.3.tar.gz/scrooge-plugin-data-platform-20250616-0.0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="scrooge-plugin-data-platform-20250616",
+    version="0.0.3",
+    packages=["scrooge-plugin-data-platform-20250616"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
