2b1c6b9cc666e9b1e677c906d2fab6f0970b83ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:06:24 +0000

    Add package pypo-renderers/1.0.2/pypo-renderers-1.0.2.tar.gz

---
+++ packages/pypo-renderers/pypo-renderers-1.0.2.tar.gz/pypo-renderers-1.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pypo-renderers",
+    version="1.0.2",
+    packages=["pypo-renderers"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
