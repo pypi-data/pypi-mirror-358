43e900fc88876da2c8502c292b5012c8eea1cb07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:41 +0000

    Add package iaas/0.1.0/iaas-0.1.0.tar.gz

---
+++ packages/iaas/iaas-0.1.0.tar.gz/iaas-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="iaas",
+    version="0.1.0",
+    packages=["iaas"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
