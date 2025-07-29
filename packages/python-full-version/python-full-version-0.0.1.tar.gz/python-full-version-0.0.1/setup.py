ce9729486a3d251383c158ff5f5f44344a09b803
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:06:17 +0000

    Add package python-full-version/0.0.1/python-full-version-0.0.1.tar.gz

---
+++ packages/python-full-version/python-full-version-0.0.1.tar.gz/python-full-version-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="python-full-version",
+    version="0.0.1",
+    packages=["python-full-version"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
