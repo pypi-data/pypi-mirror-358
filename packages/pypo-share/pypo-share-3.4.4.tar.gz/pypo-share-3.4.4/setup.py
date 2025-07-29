155f47c436b58f8bbe9741df753ca1285fd8f9d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:06:24 +0000

    Add package pypo-share/3.4.4/pypo-share-3.4.4.tar.gz

---
+++ packages/pypo-share/pypo-share-3.4.4.tar.gz/pypo-share-3.4.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pypo-share",
+    version="3.4.4",
+    packages=["pypo-share"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
