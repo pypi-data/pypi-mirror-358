2a148e8e872ca9b4f41091414355a962fbe31e50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:23 +0000

    Add package api-handler/0.3.0/api-handler-0.3.0.tar.gz

---
+++ packages/api-handler/api-handler-0.3.0.tar.gz/api-handler-0.3.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="api-handler",
+    version="0.3.0",
+    packages=["api-handler"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
