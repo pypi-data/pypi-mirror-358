11c6357b9323470374b6e35cc49ab9eda31d2173
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:46 +0000

    Add package public-utils/1.3.0/public-utils-1.3.0.tar.gz

---
+++ packages/public-utils/public-utils-1.3.0.tar.gz/public-utils-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="public-utils",
+    version="1.3.0",
+    packages=["public-utils"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
