ed3c00836d875ae5e66aaefacda40ab3a60bf514
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:53 +0000

    Add package async-auth-utils/11.0.0/async-auth-utils-11.0.0.tar.gz

---
+++ packages/async-auth-utils/async-auth-utils-11.0.0.tar.gz/async-auth-utils-11.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="async-auth-utils",
+    version="11.0.0",
+    packages=["async-auth-utils"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
