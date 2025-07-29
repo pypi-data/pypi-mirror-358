7541cbf70e24462e0bcdf49a1e67748c33e9e684
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:08:07 +0000

    Add package polynx/0.1.9/polynx-0.1.9-py3-none-any.whl

---
+++ packages/polynx/polynx-0.1.9-py3-none-any.whl/polynx/__init__.py
@@ -0,0 +1,22 @@
+__version__ = "0.1.9"
+from . import core as _core
+from .utils import plx_frame_patch, plx_expr_patch, DataFrame, LazyFrame, Series, Expr, plx_merge as merge
+from . import io as _io
+
+__all__ = ["DataFrame", "LazyFrame", "Series", "Expr"]
+
+for name in ['rolling_prod']:
+    plx_expr_patch(name, getattr(_core, f"plx_{name}"))
+
+
+for name in [
+    'eval','assign','dd', "dsort", "asort", "unstack", 
+    "pplot", "vcnt", "ucnt", "query", "eval", "wc", "gb", 
+    "describe", "round", "cum_max", "to_list", "max", "min",
+    "rename", "size"
+]:
+    plx_frame_patch(name, getattr(_core, f"plx_{name}"))
+
+
+for name in _io._wrapped_functions:
+    globals()[name] = getattr(_io, name)
