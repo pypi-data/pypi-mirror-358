0a44d514c277f861ffa947940403397cab30ae76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:42 +0000

    Add package async-retry/0.3.0/async-retry-0.3.0.tar.gz

---
+++ packages/async-retry/async-retry-0.3.0.tar.gz/async-retry-0.3.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="async-retry",
+    version="0.3.0",
+    packages=["async-retry"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
