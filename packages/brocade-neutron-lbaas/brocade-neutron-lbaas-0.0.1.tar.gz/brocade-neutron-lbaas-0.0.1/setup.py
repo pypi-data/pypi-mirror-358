2e29df3769834a083fc522f58077ef0b1ab75ab0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:00 +0000

    Add package brocade-neutron-lbaas/0.0.1/brocade-neutron-lbaas-0.0.1.tar.gz

---
+++ packages/brocade-neutron-lbaas/brocade-neutron-lbaas-0.0.1.tar.gz/brocade-neutron-lbaas-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="brocade-neutron-lbaas",
+    version="0.0.1",
+    packages=["brocade-neutron-lbaas"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
