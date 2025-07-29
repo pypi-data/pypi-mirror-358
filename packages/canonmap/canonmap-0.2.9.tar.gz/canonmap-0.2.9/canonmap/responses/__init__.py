da33161dc8c15aee99f5d0ff3e12cf4a26395bfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:07:08 +0000

    Add package canonmap/0.2.9/canonmap-0.2.9.tar.gz

---
+++ packages/canonmap/canonmap-0.2.9.tar.gz/canonmap-0.2.9/canonmap/responses/__init__.py
@@ -0,0 +1,21 @@
+from .entity_mapping_response import (
+    EntityMappingResponse,
+    SingleMapping,
+    MatchItem,
+)
+from .artifact_generation_response import (
+    ArtifactGenerationResponse,
+    GeneratedArtifact,
+    ProcessingStats,
+    ErrorInfo,
+)
+
+__all__ = [
+    "EntityMappingResponse",
+    "SingleMapping", 
+    "MatchItem",
+    "ArtifactGenerationResponse",
+    "GeneratedArtifact",
+    "ProcessingStats",
+    "ErrorInfo",
+]
