3649642741ac30359ef0b82586edb823ee4f37b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:12 +0000

    Add package ajax-select/0.0.1/ajax-select-0.0.1.tar.gz

---
+++ packages/ajax-select/ajax-select-0.0.1.tar.gz/ajax-select-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ajax-select",
+    version="0.0.1",
+    packages=["ajax-select"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
