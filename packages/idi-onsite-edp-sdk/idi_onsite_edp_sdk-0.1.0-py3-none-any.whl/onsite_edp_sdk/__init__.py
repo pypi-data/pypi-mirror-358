0c74eeea98c6f4791dd0ae78f8943a438a64383f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:57 +0000

    Add package idi-onsite-edp-sdk/0.1.0/idi_onsite_edp_sdk-0.1.0-py3-none-any.whl

---
+++ packages/idi-onsite-edp-sdk/idi_onsite_edp_sdk-0.1.0-py3-none-any.whl/onsite_edp_sdk/__init__.py
@@ -0,0 +1,23 @@
+"""ShopWorks OnSite EDP generation."""
+
+from .models import (
+    Contact,
+    Customer,
+    Design,
+    DesignLocation,
+    EDPDocument,
+    Order,
+    Payment,
+    Product,
+)
+
+__all__ = [
+    "Contact",
+    "Customer",
+    "Design",
+    "DesignLocation",
+    "EDPDocument",
+    "Order",
+    "Payment",
+    "Product",
+]
