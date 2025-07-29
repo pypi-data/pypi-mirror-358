b71ea91f9d3f4d7d901a7e279010a404ef646f80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:53 +0000

    Add package julep/2.14.3/julep-2.14.3-py3-none-any.whl

---
+++ packages/julep/julep-2.14.3-py3-none-any.whl/julep/types/vector_doc_search_param.py
@@ -0,0 +1,25 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing_extensions import Literal, TypedDict
+
+__all__ = ["VectorDocSearchParam"]
+
+
+class VectorDocSearchParam(TypedDict, total=False):
+    confidence: float
+
+    lang: str
+
+    limit: int
+
+    max_query_length: int
+
+    metadata_filter: object
+
+    mmr_strength: float
+
+    mode: Literal["vector"]
+
+    num_search_messages: int
