79cd8c4f6cd0acffb911f3b98d9a4428ae0f9894
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:15 +0000

    Add package ss-pylib/0.8.1/ss_pylib-0.8.1-py3-none-any.whl

---
+++ packages/ss-pylib/ss_pylib-0.8.1-py3-none-any.whl/sslib/util/__init__.py
@@ -0,0 +1,6 @@
+from .dict import DictUtil
+from .json import JsonUtil
+from .number import NumberUtil
+from .datetime_util import DatetimeUtil
+
+__all__ = ['DictUtil', 'JsonUtil', 'NumberUtil', 'DatetimeUtil']
---
+++ packages/ss-pylib/ss_pylib-0.8.1-py3-none-any.whl/sslib/util/string_util.py
@@ -0,0 +1,8 @@
+import re
+
+
+class StringUtil:
+    @staticmethod
+    def camel_to_snake(name: str) -> str:
+        s1 = re.sub(r'(.)([A-Z][a-z]+)', r'\1_\2', name)
+        return re.sub(r'([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
