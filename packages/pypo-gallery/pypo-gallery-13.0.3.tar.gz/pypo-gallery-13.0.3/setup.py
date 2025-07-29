cfdbb9ea508970022fb38229f1d03e7ec2d1cc4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:06:23 +0000

    Add package pypo-gallery/13.0.3/pypo-gallery-13.0.3.tar.gz

---
+++ packages/pypo-gallery/pypo-gallery-13.0.3.tar.gz/pypo-gallery-13.0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pypo-gallery",
+    version="13.0.3",
+    packages=["pypo-gallery"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
