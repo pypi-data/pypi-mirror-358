f9c46c1935f6014d948b073a970ccbadb963d927
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:06:04 +0000

    Add package testdesc/0.0.1/testdesc-0.0.1.tar.gz

---
+++ packages/testdesc/testdesc-0.0.1.tar.gz/testdesc-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="testdesc",
+    version="0.0.1",
+    packages=["testdesc"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
