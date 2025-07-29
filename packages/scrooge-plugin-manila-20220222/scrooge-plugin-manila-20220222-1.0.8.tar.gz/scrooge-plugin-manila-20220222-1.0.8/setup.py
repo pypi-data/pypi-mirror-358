b42b55b8d3e34e25c85c5b8c9bec6c9814e0e877
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:18:06 +0000

    Add package scrooge-plugin-manila-20220222/1.0.8/scrooge-plugin-manila-20220222-1.0.8.tar.gz

---
+++ packages/scrooge-plugin-manila-20220222/scrooge-plugin-manila-20220222-1.0.8.tar.gz/scrooge-plugin-manila-20220222-1.0.8/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="scrooge-plugin-manila-20220222",
+    version="1.0.8",
+    packages=["scrooge-plugin-manila-20220222"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
