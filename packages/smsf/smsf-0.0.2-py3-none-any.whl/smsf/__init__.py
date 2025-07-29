6d9928627101497648b55cc00632c6d8d78bc8a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:06:28 +0000

    Add package smsf/0.0.2/smsf-0.0.2-py3-none-any.whl

---
+++ packages/smsf/smsf-0.0.2-py3-none-any.whl/smsf/__init__.py
@@ -0,0 +1,8 @@
+"""SMSF - Situation Model Space Framework"""
+
+__version__ = "0.0.2"
+__author__ = "Adrian Brasoveanu"
+
+def hello():
+    """A simple hello function."""
+    return "Hello from SMSF!"
