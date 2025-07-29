f0f0598a72a9f1187b30f4cfb5fd0b74c53ee21f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:22 +0000

    Add package perm-admin/0.0.1/perm-admin-0.0.1.tar.gz

---
+++ packages/perm-admin/perm-admin-0.0.1.tar.gz/perm-admin-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="perm-admin",
+    version="0.0.1",
+    packages=["perm-admin"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
