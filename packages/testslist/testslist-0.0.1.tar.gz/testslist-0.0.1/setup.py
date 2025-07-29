b6ca1825822d749cbce8d50af4e03a0b00c59185
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:06:04 +0000

    Add package testslist/0.0.1/testslist-0.0.1.tar.gz

---
+++ packages/testslist/testslist-0.0.1.tar.gz/testslist-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="testslist",
+    version="0.0.1",
+    packages=["testslist"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
