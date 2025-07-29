f8f65ae1dbf7fa706a8bd73b444487bfcd9b5b1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:08:40 +0000

    Add package pycoro/0.1.0/pycoro-0.1.0-py3-none-any.whl

---
+++ packages/pycoro/pycoro-0.1.0-py3-none-any.whl/pycoro/typing.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, overload
+
+if TYPE_CHECKING:
+    from collections.abc import Generator
+
+    from pycoro.scheduler import Computation, Promise
+
+
+@overload
+def typesafe[O](v: Promise[O]) -> Generator[Promise[O], O, O]: ...  # type: ignore[OverlappingOverload]
+@overload
+def typesafe[I, O](v: Computation[I, O] | I) -> Generator[Computation[I, O] | I, Promise[O], Promise[O]]: ...
+def typesafe[I, O](v: Computation[I, O] | Promise[O] | I) -> Generator[Computation[I, O] | Promise[O] | I, Promise[O] | O, Promise[O] | O]:  # type: ignore[InconsistentOverload]
+    return (yield v)
