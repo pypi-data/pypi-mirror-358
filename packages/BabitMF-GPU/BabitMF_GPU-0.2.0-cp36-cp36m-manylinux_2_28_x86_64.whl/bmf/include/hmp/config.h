91c3e66a42e76b51790b583960b3c47ed9931f56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:04 +0000

    Add package BabitMF-GPU/0.2.0/BabitMF_GPU-0.2.0-cp36-cp36m-manylinux_2_28_x86_64.whl

---
+++ packages/BabitMF-GPU/BabitMF_GPU-0.2.0-cp36-cp36m-manylinux_2_28_x86_64.whl/bmf/include/hmp/config.h
@@ -0,0 +1,15 @@
+#pragma once
+
+//
+#define HMP_ENABLE_CUDA
+/* #undef HMP_ENABLE_FFMPEG */
+/* #undef HMP_ENABLE_OPENCV */
+/* #undef HMP_ENABLE_NPP */
+/* #undef HMP_ENABLE_OPENMP */
+/* #undef HMP_ENABLE_TORCH */
+/* #undef HMP_ENABLE_MOBILE */
+
+//HMP_VERSION
+#define HMP_VERSION_MAJOR 1
+#define HMP_VERSION_MINOR 2
+#define HMP_VERSION_PATCH 0
