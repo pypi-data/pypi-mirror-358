7241324fec34ed7115b3ab3365ae033d0b96863a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:47 +0000

    Add package odoo-addon-l10n-fi-banks/17.0.1.0.2.2/odoo_addon_l10n_fi_banks-17.0.1.0.2.2-py3-none-any.whl

---
+++ packages/odoo-addon-l10n-fi-banks/odoo_addon_l10n_fi_banks-17.0.1.0.2.2-py3-none-any.whl/odoo/addons/l10n_fi_banks/__manifest__.py
@@ -0,0 +1,33 @@
+##############################################################################
+#
+#    Author: Oy Tawasta OS Technologies Ltd and OBS
+#    Copyright 2015 Oy Tawasta OS Technologies Ltd. (http://www.tawasta.fi)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see http://www.gnu.org/licenses/agpl.html
+#
+##############################################################################
+
+{
+    "name": "Finnish Banks",
+    "summary": "Finnish and Nordic banks and their addresses",
+    "version": "17.0.1.0.2",
+    "category": "Localization",
+    "website": "https://github.com/OCA/l10n-finland",
+    "author": "Tawasta, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["base"],
+    "data": ["data/res_bank.xml"],
+}
