bb6ba5c5b680f5a26bbda5ee63bb2b6431d6aca4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:01 +0000

    Add package libdnnl.2.dylib/0.0.1/libdnnl.2.dylib-0.0.1.tar.gz

---
+++ packages/libdnnl.2.dylib/libdnnl.2.dylib-0.0.1.tar.gz/libdnnl.2.dylib-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="libdnnl.2.dylib",
+    version="0.0.1",
+    packages=["libdnnl.2.dylib"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
