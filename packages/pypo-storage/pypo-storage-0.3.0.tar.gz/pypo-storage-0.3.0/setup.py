3c8465e2992f9d0122c46bba27b10305e43ce0e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:06:25 +0000

    Add package pypo-storage/0.3.0/pypo-storage-0.3.0.tar.gz

---
+++ packages/pypo-storage/pypo-storage-0.3.0.tar.gz/pypo-storage-0.3.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pypo-storage",
+    version="0.3.0",
+    packages=["pypo-storage"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
