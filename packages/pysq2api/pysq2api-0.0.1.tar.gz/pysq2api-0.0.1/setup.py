8ccdd1a48376e77d8f72822c60aa3347e1d8ffd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:06:05 +0000

    Add package pysq2api/0.0.1/pysq2api-0.0.1.tar.gz

---
+++ packages/pysq2api/pysq2api-0.0.1.tar.gz/pysq2api-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pysq2api",
+    version="0.0.1",
+    packages=["pysq2api"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
