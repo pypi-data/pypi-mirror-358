18f9bf797640821483d612af642d9317d796424a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:01 +0000

    Add package gpt2-beamsearch-tester/0.0.1/gpt2-beamsearch-tester-0.0.1.tar.gz

---
+++ packages/gpt2-beamsearch-tester/gpt2-beamsearch-tester-0.0.1.tar.gz/gpt2-beamsearch-tester-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="gpt2-beamsearch-tester",
+    version="0.0.1",
+    packages=["gpt2-beamsearch-tester"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
