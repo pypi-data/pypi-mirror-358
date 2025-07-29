08e287b4257eeeb7b7b98036cefb32cdff9084f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:16 +0000

    Add package scrooge-plugin-mlplatform-spark-20240328/1.0.12/scrooge-plugin-mlplatform-spark-20240328-1.0.12.tar.gz

---
+++ packages/scrooge-plugin-mlplatform-spark-20240328/scrooge-plugin-mlplatform-spark-20240328-1.0.12.tar.gz/scrooge-plugin-mlplatform-spark-20240328-1.0.12/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="scrooge-plugin-mlplatform-spark-20240328",
+    version="1.0.12",
+    packages=["scrooge-plugin-mlplatform-spark-20240328"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
