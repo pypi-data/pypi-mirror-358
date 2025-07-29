2a0a18bf272d16593d4189186aee0e93de769d3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:18 +0000

    Add package tcliservice/0.0.1/tcliservice-0.0.1.tar.gz

---
+++ packages/tcliservice/tcliservice-0.0.1.tar.gz/tcliservice-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="tcliservice",
+    version="0.0.1",
+    packages=["tcliservice"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
