4f3dc978370898956dbabd61e5c27369ff73a1e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:01 +0000

    Add package httpetcd3/0.0.1/httpetcd3-0.0.1.tar.gz

---
+++ packages/httpetcd3/httpetcd3-0.0.1.tar.gz/httpetcd3-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="httpetcd3",
+    version="0.0.1",
+    packages=["httpetcd3"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
