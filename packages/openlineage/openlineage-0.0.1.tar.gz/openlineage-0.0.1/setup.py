32a1536244c3b092cfdfdb2aec91047c54066580
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:16 +0000

    Add package openlineage/0.0.1/openlineage-0.0.1.tar.gz

---
+++ packages/openlineage/openlineage-0.0.1.tar.gz/openlineage-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="openlineage",
+    version="0.0.1",
+    packages=["openlineage"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
