b4a5293d90b046ff267a985a4874b3d4962f2f4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:06:12 +0000

    Add package vault-kubeflow-cli/0.0.4/vault-kubeflow-cli-0.0.4.tar.gz

---
+++ packages/vault-kubeflow-cli/vault-kubeflow-cli-0.0.4.tar.gz/vault-kubeflow-cli-0.0.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="vault-kubeflow-cli",
+    version="0.0.4",
+    packages=["vault-kubeflow-cli"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
