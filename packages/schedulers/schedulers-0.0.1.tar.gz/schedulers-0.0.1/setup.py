c9cdf2ad035aecfe80726fc8e7093c075d6053c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:02 +0000

    Add package schedulers/0.0.1/schedulers-0.0.1.tar.gz

---
+++ packages/schedulers/schedulers-0.0.1.tar.gz/schedulers-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="schedulers",
+    version="0.0.1",
+    packages=["schedulers"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
