0aeafd21a461be6b0469a360ef16128cfbb276ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:06:12 +0000

    Add package milka-opt/0.1.0/milka_opt-0.1.0.tar.gz

---
+++ packages/milka-opt/milka_opt-0.1.0.tar.gz/milka_opt-0.1.0/global_meth_package/utils.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+# Функция Растригина для n переменных
+def rastrigin(x):
+    A = 10
+    x = np.array(x)
+    return A * len(x) + np.sum(x ** 2 - A * np.cos(2 * np.pi * x))
+
+# Функция Бута
+def booth(x):
+    return (x[0] + 2 * x[1] - 7)**2 + (2 * x[0] + x[1] - 5)**2
+
+# Функция Розенброка для n переменных
+def rosenbrock(x):
+    x = np.array(x)
+    return np.sum(100 * (x[1:] - x[:-1]**2)**2 + (1 - x[:-1])**2)
+
+# Пример четвёртой функции
+def quad_function(x):
+    return 2 * x[0]**2 + x[0]*x[1] + x[1]**2 - 3 * x[0]
+
+# Пример пятой функции
+def cos_function(x):
+    return 5 * np.cos(x[0] - 0.4) + x[0]**2
