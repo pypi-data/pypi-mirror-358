467154c0e6cae767117c8f76182106ac0261be9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:06:26 +0000

    Add package pypo-wysiwyg/4.4.0/pypo-wysiwyg-4.4.0.tar.gz

---
+++ packages/pypo-wysiwyg/pypo-wysiwyg-4.4.0.tar.gz/pypo-wysiwyg-4.4.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pypo-wysiwyg",
+    version="4.4.0",
+    packages=["pypo-wysiwyg"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
