6bc94f88b71e23a6d0e6eea42b0f0bc04c693078
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:43 +0000

    Add package consumet-mc/1.0.1/consumet_mc-1.0.1.tar.gz

---
+++ packages/consumet-mc/consumet_mc-1.0.1.tar.gz/consumet_mc-1.0.1/consumet_mc/providers/__init__.py
@@ -0,0 +1,29 @@
+from .provider import Provider
+
+from .kisskh import Kisskh
+
+from .hianime import HiAnime
+from .flixhq import Flixhq
+from .animepahe import AnimePahe
+
+from .turkish import Turkish
+from .viewasian import ViewAsian
+from .dramacool import DramaCool
+from .allanime import AllAnime
+from .himovies import HiMovies
+from .aniworld import AniWorld
+
+
+__all__ = (
+    "Provider",
+    "Kisskh",
+    "HiAnime",
+    "Flixhq",
+    "AnimePahe",
+    "Turkish",
+    "ViewAsian",
+    "DramaCool",
+    "AllAnime",
+    "HiMovies",
+    "AniWorld",
+)
