b76c32fe827e87a8d77c1952e5d92395213048ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:11 +0000

    Add package bootstrapdocs/0.0.1/bootstrapdocs-0.0.1.tar.gz

---
+++ packages/bootstrapdocs/bootstrapdocs-0.0.1.tar.gz/bootstrapdocs-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="bootstrapdocs",
+    version="0.0.1",
+    packages=["bootstrapdocs"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
