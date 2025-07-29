534db772f4ff6f91d34c9c2fdbcbcdc25e56b43d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:22 +0000

    Add package rpmtools/0.0.1/rpmtools-0.0.1.tar.gz

---
+++ packages/rpmtools/rpmtools-0.0.1.tar.gz/rpmtools-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="rpmtools",
+    version="0.0.1",
+    packages=["rpmtools"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
