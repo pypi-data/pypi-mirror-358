51623eeff58829f2850b4a02920f561a778822e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:48 +0000

    Add package phonenumber-field/0.0.1/phonenumber-field-0.0.1.tar.gz

---
+++ packages/phonenumber-field/phonenumber-field-0.0.1.tar.gz/phonenumber-field-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="phonenumber-field",
+    version="0.0.1",
+    packages=["phonenumber-field"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
