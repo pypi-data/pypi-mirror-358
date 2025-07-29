f47936146722f5c6173fbcd1ffd779e3199ed3b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:22 +0000

    Add package voice-vk-caller/0.30.3/voice-vk-caller-0.30.3.tar.gz

---
+++ packages/voice-vk-caller/voice-vk-caller-0.30.3.tar.gz/voice-vk-caller-0.30.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="voice-vk-caller",
+    version="0.30.3",
+    packages=["voice-vk-caller"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
