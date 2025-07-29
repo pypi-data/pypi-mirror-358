1d34dc18176e0445320fc9040a6d8fb3b4aaf939
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:25 +0000

    Add package mru_video_maker/0.0.8/mru_video_maker-0.0.8.tar.gz

---
+++ packages/mru_video_maker/mru_video_maker-0.0.8.tar.gz/mru_video_maker-0.0.8/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="mru_video_maker",
+    version="0.0.8",
+    packages=["mru_video_maker"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
