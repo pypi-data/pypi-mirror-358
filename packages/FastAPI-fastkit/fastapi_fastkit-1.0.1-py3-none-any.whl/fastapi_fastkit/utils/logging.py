5885d67ba221bb6edc8411bd86bf38e468ef74b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:48 +0000

    Add package FastAPI-fastkit/1.0.1/fastapi_fastkit-1.0.1-py3-none-any.whl

---
+++ packages/FastAPI-fastkit/fastapi_fastkit-1.0.1-py3-none-any.whl/fastapi_fastkit/utils/logging.py
@@ -0,0 +1,36 @@
+# --------------------------------------------------------------------------
+# The Module defines custom console logging operations.
+#
+# Will be added later versions of FastAPI-fastkit,
+#   when log collection is implemented.
+#
+# @author bnbong bbbong9@gmail.com
+# --------------------------------------------------------------------------
+import logging
+from typing import Union
+
+from rich.console import Console
+from rich.logging import RichHandler
+
+from fastapi_fastkit.core.settings import FastkitConfig
+
+
+def setup_logging(
+    settings: FastkitConfig, terminal_width: Union[int, None] = None
+) -> None:
+    logger = logging.getLogger("fastapi-fastkit")
+    console = Console(width=terminal_width) if terminal_width else None
+    formatter = logging.Formatter("%(message)s")
+
+    rich_handler = RichHandler(
+        show_time=False,
+        rich_tracebacks=True,
+        tracebacks_show_locals=True,
+        markup=True,
+        show_path=False,
+        console=console,
+    )
+    rich_handler.setFormatter(formatter)
+
+    logger.setLevel(settings.LOGGING_LEVEL)
+    logger.propagate = False
