4a82ec4d9e1ad30745891beee2165d29e0c87d37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:04 +0000

    Add package ktsaccessors/0.0.1/ktsaccessors-0.0.1.tar.gz

---
+++ packages/ktsaccessors/ktsaccessors-0.0.1.tar.gz/ktsaccessors-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ktsaccessors",
+    version="0.0.1",
+    packages=["ktsaccessors"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
