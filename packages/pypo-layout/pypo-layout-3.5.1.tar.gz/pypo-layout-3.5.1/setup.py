99f636e9e6148190c5bf2f58228403c44641feb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:06:23 +0000

    Add package pypo-layout/3.5.1/pypo-layout-3.5.1.tar.gz

---
+++ packages/pypo-layout/pypo-layout-3.5.1.tar.gz/pypo-layout-3.5.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pypo-layout",
+    version="3.5.1",
+    packages=["pypo-layout"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
