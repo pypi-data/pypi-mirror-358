2d49aaa131d7ad5c4926af33f4869c5b682c7a95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:21 +0000

    Add package voice-elasticsearch/0.2.5/voice-elasticsearch-0.2.5.tar.gz

---
+++ packages/voice-elasticsearch/voice-elasticsearch-0.2.5.tar.gz/voice-elasticsearch-0.2.5/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="voice-elasticsearch",
+    version="0.2.5",
+    packages=["voice-elasticsearch"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
