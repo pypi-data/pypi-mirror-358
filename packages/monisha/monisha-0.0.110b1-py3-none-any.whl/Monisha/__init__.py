7af40621a1a0ece8f4384324666324a7ece08c59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:50 +0000

    Add package monisha/0.0.110b1/monisha-0.0.110b1-py3-none-any.whl

---
+++ packages/monisha/monisha-0.0.110b1-py3-none-any.whl/Monisha/__init__.py
@@ -0,0 +1,20 @@
+appname = "monisha"
+version = "0.0.110b1"
+
+install = ["pytz",
+           "arrow",
+           "psutil",
+           "pillow",
+           "hachoir",
+           "requests",
+           "beautifulsoup4"]
+
+DATA01 = None
+DATA02 = ['Natural Language :: English',
+          'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 3.10',
+          'Programming Language :: Python :: 3.11',
+          'Programming Language :: Python :: 3.12',
+          'Programming Language :: Python :: 3.13',
+         ]
