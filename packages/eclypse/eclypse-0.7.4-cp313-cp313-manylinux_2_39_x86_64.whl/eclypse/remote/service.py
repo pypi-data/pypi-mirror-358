4bdf0b7f078c9956cb419387c7db7d216eccf78c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:09 +0000

    Add package eclypse/0.7.4/eclypse-0.7.4-cp313-cp313-manylinux_2_39_x86_64.whl

---
+++ packages/eclypse/eclypse-0.7.4-cp313-cp313-manylinux_2_39_x86_64.whl/eclypse/remote/service.py
@@ -0,0 +1,15 @@
+"""Package for managing application services implementations.
+
+For the complete documentation, refer to the :py:mod:`~eclypse_core.remote.service`
+core package.
+"""
+
+from eclypse_core.remote.service import (
+    RESTService,
+    Service,
+)
+
+__all__ = [
+    "Service",
+    "RESTService",
+]
