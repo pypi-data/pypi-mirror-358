f7fb6beab0e879abc0658186d07d4858c7fb45be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:04:52 +0000

    Add package HyTechMaster/0.1/hytechmaster-0.1.tar.gz

---
+++ packages/HyTechMaster/hytechmaster-0.1.tar.gz/hytechmaster-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='HyTechMaster',
+    version='0.1', 
+        authore='Arman Rathore',
+        author_email='armanrathore236@gmail.com',
+        description='This is speech to text package created by arman rathore'
+)
+packages = find_packages(),
+install_requirements = [
+    'selenium',
+    'webdriver_manager'
+]
+
