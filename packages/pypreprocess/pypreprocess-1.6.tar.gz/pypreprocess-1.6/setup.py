fbd823548bf82052142c0c24aa8bce1ddd039acf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:24 +0000

    Add package pypreprocess/1.6/pypreprocess-1.6.tar.gz

---
+++ packages/pypreprocess/pypreprocess-1.6.tar.gz/pypreprocess-1.6/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '1.6'
+DESCRIPTION = 'Preprocess SDK'
+
+# Setting up
+setup(
+    name="pypreprocess",
+    version=VERSION,
+    author="Preprocess",
+    author_email="<support@preprocess.co>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    license_files=('LICENSE',),
+    install_requires=['requests'],
+    keywords=['python', 'python3', 'preprocess', 'chunks', 'paragraphs', 'chunk', 'paragraph', 'llama', 'llamaondex', "langchain", "chunking", "llm", "rag"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "License :: OSI Approved :: MIT License"
+    ]
+)
