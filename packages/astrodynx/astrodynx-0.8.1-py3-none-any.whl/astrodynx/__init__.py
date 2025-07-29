b54507ad250035232b7edf549136066c8b1abcd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:50 +0000

    Add package astrodynx/0.8.1/astrodynx-0.8.1-py3-none-any.whl

---
+++ packages/astrodynx/astrodynx-0.8.1-py3-none-any.whl/astrodynx/__init__.py
@@ -0,0 +1,4 @@
+from astrodynx._version import version as version
+from astrodynx._version import version_tuple as version_tuple
+from astrodynx._version import __version__ as __version__
+from astrodynx._version import __version_tuple__ as __version_tuple__
