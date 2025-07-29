547defcf17972f33452474277124c816def6578f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:08:50 +0000

    Add package rocketcontent/1.1.1/rocketcontent-1.1.1.tar.gz

---
+++ packages/rocketcontent/rocketcontent-1.1.1.tar.gz/rocketcontent-1.1.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rocketcontent',
+    version='1.1.1',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'PyYAML',
+        'rich',
+        'markdown2'        
+    ],
+    author='Guillermo Avendano',
+    author_email='gavendano@rocketsoftware.com',
+    description='Content Services Api 12.4.x, 12.5.x',
+    long_description=open('README.md', encoding='utf-8').read(),  # Especifica la codificación
+    long_description_content_type='text/markdown',
+    url='https://docs.rocketsoftware.com/bundle/mobiusdev_123/page/rtb1690483435152.html',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.9',  # Especifica la versión mínima de Python
+)
