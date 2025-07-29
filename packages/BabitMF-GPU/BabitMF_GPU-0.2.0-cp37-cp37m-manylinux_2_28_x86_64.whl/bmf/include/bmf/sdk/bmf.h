7fb95664961022ae062f28bc6621407fcee8a90c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:07 +0000

    Add package BabitMF-GPU/0.2.0/BabitMF_GPU-0.2.0-cp37-cp37m-manylinux_2_28_x86_64.whl

---
+++ packages/BabitMF-GPU/BabitMF_GPU-0.2.0-cp37-cp37m-manylinux_2_28_x86_64.whl/bmf/include/bmf/sdk/bmf.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2023 Babit Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef BMF_H
+#define BMF_H
+
+// This is the main header file for users to import C++ SDK into their own proj$
+
+#include <bmf/sdk/common.h>
+#include <bmf/sdk/packet.h>
+#include <bmf/sdk/audio_frame.h>
+#include <bmf/sdk/video_frame.h>
+#include <bmf/sdk/task.h>
+#include <bmf/sdk/module.h>
+#include <bmf/sdk/module_registry.h>
+#include <bmf/sdk/bmf_av_packet.h>
+#include <bmf/sdk/cbytes.h>
+#include <bmf/sdk/error_define.h>
+#include <bmf/sdk/exception_factory.h>
+#include <bmf/sdk/rational.h>
+#include <bmf/sdk/json_param.h>
+#include <bmf/sdk/log_buffer.h>
+#include <bmf/sdk/log.h>
+#include <bmf/sdk/timestamp.h>
+#include <bmf/sdk/trace.h>
+
+#endif // BMF_H
