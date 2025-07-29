ca32bd2b9c175709e6652b50807220b45d9bcb1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:06:27 +0000

    Add package mras-dwlib/2.0.3/mras-dwlib-2.0.3.tar.gz

---
+++ packages/mras-dwlib/mras-dwlib-2.0.3.tar.gz/mras-dwlib-2.0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="mras-dwlib",
+    version="2.0.3",
+    packages=["mras-dwlib"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
