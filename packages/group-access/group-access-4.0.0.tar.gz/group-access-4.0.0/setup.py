22c63e6d418898d3ff26a4771b19bc45838e5b6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:48 +0000

    Add package group-access/4.0.0/group-access-4.0.0.tar.gz

---
+++ packages/group-access/group-access-4.0.0.tar.gz/group-access-4.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="group-access",
+    version="4.0.0",
+    packages=["group-access"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
