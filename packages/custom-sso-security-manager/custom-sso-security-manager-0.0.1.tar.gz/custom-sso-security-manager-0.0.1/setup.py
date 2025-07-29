e89020e03c81c97201eb8d47aadbb106bcfb5252
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:21 +0000

    Add package custom-sso-security-manager/0.0.1/custom-sso-security-manager-0.0.1.tar.gz

---
+++ packages/custom-sso-security-manager/custom-sso-security-manager-0.0.1.tar.gz/custom-sso-security-manager-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="custom-sso-security-manager",
+    version="0.0.1",
+    packages=["custom-sso-security-manager"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
