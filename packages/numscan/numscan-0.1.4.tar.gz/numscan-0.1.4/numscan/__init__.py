e80fcc724cc82e21b1f68c4bedc7f40b4daa4d23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:35 +0000

    Add package numscan/0.1.4/numscan-0.1.4.tar.gz

---
+++ packages/numscan/numscan-0.1.4.tar.gz/numscan-0.1.4/numscan/__init__.py
@@ -0,0 +1,6 @@
+from .fibonacci import FibonacciCounter
+from .prime import PrimeCounter
+from .squares import SquareCounter
+from .popculture import PopCultureCounter
+from .all import IntegerCounter
+from .base import NumberCounter
