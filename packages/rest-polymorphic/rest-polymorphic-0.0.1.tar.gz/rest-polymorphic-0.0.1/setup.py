85e5b516bb2dfacea3a26cc66d31fc3c020308e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:06:37 +0000

    Add package rest-polymorphic/0.0.1/rest-polymorphic-0.0.1.tar.gz

---
+++ packages/rest-polymorphic/rest-polymorphic-0.0.1.tar.gz/rest-polymorphic-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="rest-polymorphic",
+    version="0.0.1",
+    packages=["rest-polymorphic"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
