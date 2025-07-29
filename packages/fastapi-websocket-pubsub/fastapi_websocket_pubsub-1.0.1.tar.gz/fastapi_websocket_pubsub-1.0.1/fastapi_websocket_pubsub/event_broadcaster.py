04c9f61701cec30c187caf07ae57e549cae4bd31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:18:10 +0000

    Add package fastapi-websocket-pubsub/1.0.1/fastapi_websocket_pubsub-1.0.1.tar.gz

---
+++ packages/fastapi-websocket-pubsub/fastapi_websocket_pubsub-1.0.1.tar.gz/fastapi_websocket_pubsub-1.0.1/fastapi_websocket_pubsub/event_broadcaster.py
@@ -0,0 +1,309 @@
+import asyncio
+from typing import Any
+
+from broadcaster import Broadcast
+from fastapi_websocket_rpc.utils import gen_uid
+from pydantic.main import BaseModel
+
+from .event_notifier import ALL_TOPICS, EventNotifier, Subscription, TopicList
+from .logger import get_logger
+from .util import pydantic_serialize
+
+logger = get_logger("EventBroadcaster")
+
+
+# Cross service broadcast consts
+NotifierId = str
+
+
+class BroadcastNotification(BaseModel):
+    notifier_id: NotifierId
+    topics: TopicList
+    data: Any = None
+
+
+class EventBroadcasterException(Exception):
+    pass
+
+
+class BroadcasterAlreadyStarted(EventBroadcasterException):
+    pass
+
+
+class EventBroadcasterContextManager:
+    """
+    Manages the context for the EventBroadcaster
+    Friend-like class of EventBroadcaster (accessing "protected" members )
+    """
+
+    def __init__(
+        self,
+        event_broadcaster: "EventBroadcaster",
+        listen: bool = True,
+        share: bool = True,
+    ) -> None:
+        """
+        Provide a context manager for an EventBroadcaster, managing if it listens to events coming from the broadcaster
+        and if it subscribes to the internal notifier to share its events with the broadcaster
+
+        Args:
+            event_broadcaster (EventBroadcaster): the broadcaster we manage the context for.
+            share (bool, optional): Should we share events with the broadcaster. Defaults to True.
+            listen (bool, optional): Should we listen for incoming events from the broadcaster. Defaults to True.
+        """
+        self._event_broadcaster = event_broadcaster
+        self._share: bool = share
+        self._listen: bool = listen
+
+    async def __aenter__(self):
+        async with self._event_broadcaster._context_manager_lock:
+            if self._listen:
+                self._event_broadcaster._listen_count += 1
+                if self._event_broadcaster._listen_count == 1:
+                    # We have our first listener start the read-task for it (And all those who'd follow)
+                    logger.info(
+                        "Listening for incoming events from broadcast channel (first listener started)"
+                    )
+                    # Start task listening on incoming broadcasts
+                    await self._event_broadcaster.start_reader_task()
+
+            if self._share:
+                self._event_broadcaster._share_count += 1
+                if self._event_broadcaster._share_count == 1:
+                    # We have our first publisher
+                    # Init the broadcast used for sharing (reading has its own)
+                    logger.debug(
+                        "Subscribing to ALL_TOPICS, and sharing messages with broadcast channel"
+                    )
+                    # Subscribe to internal events form our own event notifier and broadcast them
+                    await self._event_broadcaster._subscribe_to_all_topics()
+                else:
+                    logger.debug(
+                        f"Did not subscribe to ALL_TOPICS: share count == {self._event_broadcaster._share_count}"
+                    )
+        return self
+
+    async def __aexit__(self, exc_type, exc, tb):
+        async with self._event_broadcaster._context_manager_lock:
+            try:
+                if self._listen:
+                    self._event_broadcaster._listen_count -= 1
+                    # if this was last listener - we can stop the reading task
+                    if self._event_broadcaster._listen_count == 0:
+                        # Cancel task reading broadcast subscriptions
+                        if self._event_broadcaster._subscription_task is not None:
+                            logger.info("Cancelling broadcast listen task")
+                            self._event_broadcaster._subscription_task.cancel()
+                            self._event_broadcaster._subscription_task = None
+
+                if self._share:
+                    self._event_broadcaster._share_count -= 1
+                    # if this was last sharer - we can stop subscribing to internal events - we aren't sharing anymore
+                    if self._event_broadcaster._share_count == 0:
+                        # Unsubscribe from internal events
+                        logger.debug("Unsubscribing from ALL TOPICS")
+                        await self._event_broadcaster._unsubscribe_from_topics()
+
+            except:
+                logger.exception("Failed to exit EventBroadcaster context")
+
+
+class EventBroadcaster:
+    """
+    Bridge EventNotifier to work across processes and machines by sharing their events through a broadcasting channel
+
+    Usage:
+    uri = "postgres://localhost:5432/db_name" #postgres example (also supports REDIS, Kafka, ...)
+    # start litsening for broadcast publications notifying the internal event-notifier, and subscribing to the internal notifier, broadcasting its notes
+    broadcaster = EventBroadcaster(uri, notifier):
+    async with broadcaster.get_context():
+        <Your Code>
+    """
+
+    def __init__(
+        self,
+        broadcast_url: str,
+        notifier: EventNotifier,
+        channel="EventNotifier",
+        broadcast_type=None,
+        is_publish_only=False,
+    ) -> None:
+        """
+
+        Args:
+            broadcast_url (str): the URL of the broadcasting service
+            notifier (EventNotifier): the event notifier managing our internal events - which will be bridge via the broadcaster
+            channel (str, optional): Channel name. Defaults to "EventNotifier".
+            broadcast_type (Broadcast, optional): Broadcast class to use. None - Defaults to Broadcast.
+            is_publish_only (bool, optional): [For default context] Should the broadcaster only transmit events and not listen to any. Defaults to False
+        """
+        # Broadcast init params
+        self._broadcast_url = broadcast_url
+        self._broadcast_type = broadcast_type or Broadcast
+        # Publish broadcast (initialized within async with statement)
+        self._sharing_broadcast_channel = None
+        # channel to operate on
+        self._channel = channel
+        # Async-io task for reading broadcasts (initialized within async with statement)
+        self._subscription_task = None
+        # Uniqueue instance id (used to avoid reading own notifications sent in broadcast)
+        self._id = gen_uid()
+        # The internal events notifier
+        self._notifier = notifier
+        self._is_publish_only = is_publish_only
+        self._publish_lock = None
+        # used to track creation / removal of resources needed per type (reader task->listen, and subscription to internal events->share)
+        self._listen_count: int = 0
+        self._share_count: int = 0
+        # If we opt to manage the context directly (i.e. call async with on the event broadcaster itself)
+        self._context_manager = None
+        self._context_manager_lock = asyncio.Lock()
+        self._tasks = set()
+        self.listening_broadcast_channel = None
+
+    async def __broadcast_notifications__(self, subscription: Subscription, data):
+        """
+        Share incoming internal notifications with the entire broadcast channel
+
+        Args:
+            subscription (Subscription): the subscription that got triggered
+            data: the event data
+        """
+        logger.info(
+            "Broadcasting incoming event: {}".format(
+                {"topic": subscription.topic, "notifier_id": self._id}
+            )
+        )
+        note = BroadcastNotification(
+            notifier_id=self._id, topics=[subscription.topic], data=data
+        )
+
+        # Publish event to broadcast
+        async with self._broadcast_type(
+            self._broadcast_url
+        ) as sharing_broadcast_channel:
+            await sharing_broadcast_channel.publish(
+                self._channel, pydantic_serialize(note)
+            )
+
+    async def _subscribe_to_all_topics(self):
+        return await self._notifier.subscribe(
+            self._id, ALL_TOPICS, self.__broadcast_notifications__
+        )
+
+    async def _unsubscribe_from_topics(self):
+        return await self._notifier.unsubscribe(self._id)
+
+    def get_context(self, listen=True, share=True):
+        """
+        Create a new context manager you can call 'async with' on, configuring the broadcaster for listening, sharing, or both.
+
+        Args:
+            listen (bool, optional): Should we listen for events incoming from the broadcast channel. Defaults to True.
+            share (bool, optional): Should we share events with the broadcast channel. Defaults to True.
+
+        Returns:
+            EventBroadcasterContextManager: the context
+        """
+        return EventBroadcasterContextManager(self, listen=listen, share=share)
+
+    def get_listening_context(self):
+        return EventBroadcasterContextManager(self, listen=True, share=False)
+
+    def get_sharing_context(self):
+        return EventBroadcasterContextManager(self, listen=False, share=True)
+
+    async def __aenter__(self):
+        """
+        Convince caller (also backward compaltability)
+        """
+        if self._context_manager is None:
+            self._context_manager = self.get_context(listen=not self._is_publish_only)
+        return await self._context_manager.__aenter__()
+
+    async def __aexit__(self, exc_type, exc, tb):
+        await self._context_manager.__aexit__(exc_type, exc, tb)
+
+    async def start_reader_task(self):
+        """Spawn a task reading incoming broadcasts and posting them to the intreal notifier
+        Raises:
+            BroadcasterAlreadyStarted: if called more than once per context
+        Returns:
+            the spawned task
+        """
+        # Make sure a task wasn't started already
+        if self._subscription_task is not None:
+            # we already started a task for this worker process
+            logger.debug(
+                "No need for listen task, already started broadcast listen task for this notifier"
+            )
+            return
+
+        # Init new broadcast channel for reading
+        try:
+            if self.listening_broadcast_channel is None:
+                self.listening_broadcast_channel = self._broadcast_type(
+                    self._broadcast_url
+                )
+                await self.listening_broadcast_channel.connect()
+        except Exception as e:
+            logger.error(
+                f"Failed to connect to broadcast channel for reading incoming events: {e}"
+            )
+            raise e
+
+        # Trigger the task
+        logger.debug("Spawning broadcast listen task")
+        self._subscription_task = asyncio.create_task(self.__read_notifications__())
+        return self._subscription_task
+
+    def get_reader_task(self):
+        return self._subscription_task
+
+    async def __read_notifications__(self):
+        """
+        read incoming broadcasts and posting them to the intreal notifier
+        """
+        logger.debug("Starting broadcaster listener")
+        try:
+            # Subscribe to our channel
+            async with self.listening_broadcast_channel.subscribe(
+                channel=self._channel
+            ) as subscriber:
+                async for event in subscriber:
+                    try:
+                        notification = BroadcastNotification.parse_raw(event.message)
+                        # Avoid re-publishing our own broadcasts
+                        if notification.notifier_id != self._id:
+                            logger.debug(
+                                "Handling incoming broadcast event: {}".format(
+                                    {
+                                        "topics": notification.topics,
+                                        "src": notification.notifier_id,
+                                    }
+                                )
+                            )
+                            # Notify subscribers of message received from broadcast
+                            task = asyncio.create_task(
+                                self._notifier.notify(
+                                    notification.topics,
+                                    notification.data,
+                                    notifier_id=self._id,
+                                )
+                            )
+
+                            self._tasks.add(task)
+
+                            def cleanup(task):
+                                self._tasks.remove(task)
+
+                            task.add_done_callback(cleanup)
+                    except:
+                        logger.exception("Failed handling incoming broadcast")
+                logger.info(
+                    "No more events to read from subscriber (underlying connection closed)"
+                )
+        finally:
+            if self.listening_broadcast_channel is not None:
+                await self.listening_broadcast_channel.disconnect()
+                self.listening_broadcast_channel = None
---
+++ packages/fastapi-websocket-pubsub/fastapi_websocket_pubsub-1.0.1.tar.gz/fastapi_websocket_pubsub-1.0.1/fastapi_websocket_pubsub/rpc_event_methods.py
@@ -0,0 +1,107 @@
+import asyncio
+
+from fastapi_websocket_rpc import RpcMethodsBase
+
+from .event_notifier import EventNotifier, Subscription, TopicList
+from .logger import get_logger
+from .util import pydantic_to_dict
+
+
+class RpcEventServerMethods(RpcMethodsBase):
+
+    def __init__(self, event_notifier: EventNotifier, rpc_channel_get_remote_id: bool = False):
+        super().__init__()
+        self.event_notifier = event_notifier
+        self._rpc_channel_get_remote_id = rpc_channel_get_remote_id
+        self.logger = get_logger('PubSubServer')
+
+    async def subscribe(self, topics: TopicList = []) -> bool:
+        """
+        provided by the server so that the client can subscribe to topics.
+        when new events are available on a topic, the server will call the
+        client's `notify` method.
+        """
+        try:
+            async def callback(subscription: Subscription, data):
+                # remove the actual function
+                sub = subscription.copy(exclude={"callback"})
+                self.logger.debug(
+                    f"Notifying other side: subscription={pydantic_to_dict(subscription, exclude={'callback'})}, data={data}, channel_id={self.channel.id}"
+                )
+                await self.channel.other.notify(subscription=sub, data=data)
+
+            if self._rpc_channel_get_remote_id:
+                # We'll use the remote channel id as our subscriber id
+                channel_other_channel_id = await self.channel.get_other_channel_id()
+                if channel_other_channel_id is None:
+                    self.logger.warning(
+                        "could not fetch remote channel id, using local channel id to subscribe")
+                    sub_id = self.channel.id
+                else:
+                    sub_id = channel_other_channel_id
+            else:
+                # We'll use our channel id as our subscriber id
+                sub_id = self.channel.id
+            await self.event_notifier.subscribe(sub_id, topics, callback, self.channel)
+            return True
+        except Exception as err:
+            self.logger.exception(
+                "Failed to subscribe to RPC events notifier", topics)
+            return False
+
+    async def unsubscribe(self, topics: TopicList = []) -> bool:
+        """
+        provided by the server so that the client can unsubscribe topics.
+        """
+        for topic in topics.copy():
+            if topic not in self.event_notifier._topics:
+                self.logger.warning(f"Cannot unsubscribe topic '{topic}' which is not subscribed.")
+                topics.remove(topic)
+        # We'll use the remote channel id as our subscriber id
+        sub_id = await self._get_channel_id_()
+        await self.event_notifier.unsubscribe(sub_id, topics)
+        return True
+
+    async def publish(self, topics: TopicList = [], data=None, sync=True, notifier_id=None) -> bool:
+        """
+        Publish an event through the server to subscribers
+
+        Args:
+            topics (TopicList): topics to publish
+            data (Any, optional): data to pass with the event to the subscribers. Defaults to None.
+            sync (bool, optional): Should the server finish publishing before returning to us
+            notifier_id(str,optional): A unique identifier of the source of the event
+                use a different id from the channel.id or the subscription id to receive own publications
+
+        Returns:
+            bool: was the publish successful
+        """
+        try:
+            # use the given id or use our channel id
+            notifier_id = notifier_id if notifier_id is not None else self.channel.id
+            promise = self.event_notifier.notify(
+                topics, data, notifier_id=notifier_id, channel=self.channel)
+            if sync:
+                await promise
+            else:
+                asyncio.create_task(promise)
+            return True
+        except Exception as err:
+            self.logger.error("Failed to publish to events notifier", topics)
+            return False
+
+    async def ping(self) -> str:
+        return "pong"
+
+
+class RpcEventClientMethods(RpcMethodsBase):
+
+    def __init__(self, client):
+        super().__init__()
+        self.client = client
+        self.logger = get_logger('PubSubClient')
+
+    async def notify(self, subscription=None, data=None):
+        self.logger.debug("Received notification of event",
+                          {'subscription': subscription, 'data': data})
+        await self.client.trigger_topic(topic=subscription["topic"], data=data)
