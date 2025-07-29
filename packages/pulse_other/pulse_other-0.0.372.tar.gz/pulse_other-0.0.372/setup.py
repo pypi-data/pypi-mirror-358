2b12681425972f37d86a364c81d580b03eb41563
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:49 +0000

    Add package pulse_other/0.0.372/pulse_other-0.0.372.tar.gz

---
+++ packages/pulse_other/pulse_other-0.0.372.tar.gz/pulse_other-0.0.372/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pulse_other",
+    version="0.0.372",
+    packages=["pulse_other"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
