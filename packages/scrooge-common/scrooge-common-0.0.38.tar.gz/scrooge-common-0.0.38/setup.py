9105d24b6209add3e3b695c4773fe060840dd814
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:43 +0000

    Add package scrooge-common/0.0.38/scrooge-common-0.0.38.tar.gz

---
+++ packages/scrooge-common/scrooge-common-0.0.38.tar.gz/scrooge-common-0.0.38/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="scrooge-common",
+    version="0.0.38",
+    packages=["scrooge-common"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
