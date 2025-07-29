0d389a51c0735521b06fe5962a85767b31e41d51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:32 +0000

    Add package xml-apis/1.4.1/xml-apis-1.4.01.tar.gz

---
+++ packages/xml-apis/xml-apis-1.4.01.tar.gz/xml-apis-1.4.01/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="xml-apis",
+    version="1.4.01",
+    packages=["xml-apis"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
