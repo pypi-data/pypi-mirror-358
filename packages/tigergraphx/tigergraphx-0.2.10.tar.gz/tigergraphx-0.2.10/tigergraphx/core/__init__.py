0bba62edf46abb0c4308247bbc2a6d4f4c2e859c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:12 +0000

    Add package tigergraphx/0.2.10/tigergraphx-0.2.10.tar.gz

---
+++ packages/tigergraphx/tigergraphx-0.2.10.tar.gz/tigergraphx-0.2.10/tigergraphx/core/__init__.py
@@ -0,0 +1,17 @@
+# Copyright 2025 TigerGraph Inc.
+# Licensed under the Apache License, Version 2.0.
+# See the LICENSE file or https://www.apache.org/licenses/LICENSE-2.0
+#
+# Permission is granted to use, copy, modify, and distribute this software
+# under the License. The software is provided "AS IS", without warranty.
+
+from .graph import Graph
+from .tigergraph_api import TigerGraphAPI
+from .tigergraph_database import TigerGraphDatabase
+
+
+__all__ = [
+    "Graph",
+    "TigerGraphAPI",
+    "TigerGraphDatabase",
+]
