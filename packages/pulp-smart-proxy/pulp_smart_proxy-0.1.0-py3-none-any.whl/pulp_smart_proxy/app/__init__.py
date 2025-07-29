e9218c52a26726f5a9d3603e1bf55f4d6faa6155
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:45 +0000

    Add package pulp-smart-proxy/0.1.0/pulp_smart_proxy-0.1.0-py3-none-any.whl

---
+++ packages/pulp-smart-proxy/pulp_smart_proxy-0.1.0-py3-none-any.whl/pulp_smart_proxy/app/__init__.py
@@ -0,0 +1,15 @@
+from pulpcore.plugin import PulpPluginAppConfig
+
+
+class PulpSmartProxyPluginAppConfig(PulpPluginAppConfig):
+    """Entry point for the smart_proxy plugin."""
+
+    name = "pulp_smart_proxy.app"
+    label = "smart_proxy"
+    version = "0.1.0"
+    python_package_name = "pulp_smart_proxy"
+    domain_compatible = True
+
+    def ready(self):
+        super().ready()
+        from . import checks
