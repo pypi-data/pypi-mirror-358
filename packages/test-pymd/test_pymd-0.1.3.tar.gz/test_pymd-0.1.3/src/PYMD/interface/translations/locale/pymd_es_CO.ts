d578fe691efdd4cda6181063288b4013e1806d87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:49 +0000

    Add package test-pymd/0.1.3/test_pymd-0.1.3.tar.gz

---
+++ packages/test-pymd/test_pymd-0.1.3.tar.gz/test_pymd-0.1.3/src/PYMD/interface/translations/locale/pymd_es_CO.ts
@@ -0,0 +1,278 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es_CO">
+<context>
+    <name>Lang_Dialog</name>
+    <message>
+        <location filename="../../dialog_language.ui" line="32"/>
+        <source>Select Language</source>
+        <translation>Seleccionar Lenguaje</translation>
+    </message>
+    <message>
+        <location filename="../../dialog_language.ui" line="138"/>
+        <source>English - GB</source>
+        <translation>Inglés - GB</translation>
+    </message>
+    <message>
+        <location filename="../../dialog_language.ui" line="156"/>
+        <source>Spanish - CO</source>
+        <translation>Español - CO</translation>
+    </message>
+</context>
+<context>
+    <name>LanguageManager</name>
+    <message>
+        <location filename="../../_lang.py" line="29"/>
+        <source>Save</source>
+        <translation>Guardar</translation>
+    </message>
+    <message>
+        <location filename="../../_lang.py" line="30"/>
+        <source>Cancel</source>
+        <translation>Cancelar</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../../_interface.py" line="66"/>
+        <source>Select a file</source>
+        <translation>Seleccione un archivo</translation>
+    </message>
+    <message>
+        <location filename="../../_interface.py" line="67"/>
+        <location filename="../../main_window.ui" line="366"/>
+        <location filename="../../main_window.ui" line="1473"/>
+        <source>Open File</source>
+        <translation>Abrir Archivo</translation>
+    </message>
+    <message>
+        <location filename="../../_interface.py" line="85"/>
+        <location filename="../../_interface.py" line="102"/>
+        <location filename="../../_interface.py" line="131"/>
+        <source>Warning</source>
+        <translation>Precaución</translation>
+    </message>
+    <message>
+        <location filename="../../_interface.py" line="85"/>
+        <source>The file does not contain plain text (make sure the file content is not just images)</source>
+        <translation>El archivo NO contiene text plano (Asegúrate de que el contenido del archivo no sean solo imágenes)</translation>
+    </message>
+    <message>
+        <location filename="../../_interface.py" line="94"/>
+        <source>Select a directory</source>
+        <translation>Seleccione un directorio</translation>
+    </message>
+    <message>
+        <location filename="../../_interface.py" line="102"/>
+        <source>The file is empty</source>
+        <translation>El archivo está vacío</translation>
+    </message>
+    <message>
+        <location filename="../../_interface.py" line="102"/>
+        <location filename="../../_interface.py" line="115"/>
+        <location filename="../../_interface.py" line="131"/>
+        <source>Accept</source>
+        <translation>Aceptar</translation>
+    </message>
+    <message>
+        <location filename="../../_interface.py" line="115"/>
+        <source>Close Program</source>
+        <translation>Cerrrar programa</translation>
+    </message>
+    <message>
+        <location filename="../../_interface.py" line="115"/>
+        <source>Do you want to close this program?</source>
+        <translation>¿Quieres cerrar el programa?</translation>
+    </message>
+    <message>
+        <location filename="../../_interface.py" line="131"/>
+        <source>Extracted content will be deleted once you change the language (unless you have already saved it)</source>
+        <translation>El contenido extraído será eliminado una vez cambie el idioma (a menos que ya lo hayas guardado)</translation>
+    </message>
+    <message>
+        <location filename="../../_interface.py" line="148"/>
+        <source>Ok</source>
+        <translation>Aceptar</translation>
+    </message>
+    <message>
+        <location filename="../../_interface.py" line="149"/>
+        <source>Cancel</source>
+        <translation>Cancelar</translation>
+    </message>
+    <message>
+        <location filename="../../main_window.ui" line="246"/>
+        <source>PYMD - SRM &amp; TRG</source>
+        <translation>PYMD - SRM Y TRG</translation>
+    </message>
+    <message>
+        <location filename="../../main_window.ui" line="335"/>
+        <source>Change language</source>
+        <translation>Cambiar idioma</translation>
+    </message>
+    <message>
+        <location filename="../../main_window.ui" line="344"/>
+        <source>Ctrl+A</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../main_window.ui" line="469"/>
+        <source>Markdown</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../main_window.ui" line="518"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;meta charset=&quot;utf-8&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+hr { height: 1px; border-width: 0; }
+li.unchecked::marker { content: &quot;\2610&quot;; }
+li.checked::marker { content: &quot;\2612&quot;; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Times New Roman&apos;; font-size:12pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Gabriola&apos;;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../main_window.ui" line="541"/>
+        <source>Preview</source>
+        <translation>Vista previa</translation>
+    </message>
+    <message>
+        <location filename="../../main_window.ui" line="600"/>
+        <source>Save file to Markdown (.md)</source>
+        <translation>Guardar archivo como Markdown</translation>
+    </message>
+    <message>
+        <location filename="../../main_window.ui" line="606"/>
+        <source>Convert</source>
+        <translation>Convertir</translation>
+    </message>
+    <message>
+        <location filename="../../main_window.ui" line="612"/>
+        <source>Ctrl+Enter</source>
+        <extracomment>Button to save/convert .md file</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../main_window.ui" line="681"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;©&#xa0;2025 Tutos Rive. Licensed under the &lt;a href=&quot;https://github.com/tutosrive/pymd/blob/main/LICENSE&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#27bf73;&quot;&gt;MIT License&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <extracomment>Copyright Notice - Footer</extracomment>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;© 2025 Tutos Rive. Bajo &lt;a href=&quot;https://github.com/tutosrive/pymd/blob/main/LICENSE&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#27bf73;&quot;&gt;Licencia MIT&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../../main_window.ui" line="1215"/>
+        <source>Menu</source>
+        <translation>Menú</translation>
+    </message>
+    <message>
+        <location filename="../../main_window.ui" line="1434"/>
+        <source>Preferences</source>
+        <translation>Preferencias</translation>
+    </message>
+    <message>
+        <location filename="../../main_window.ui" line="1453"/>
+        <source>Help</source>
+        <translation>Ayuda</translation>
+    </message>
+    <message>
+        <location filename="../../main_window.ui" line="1482"/>
+        <source>Ctrl+O</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../main_window.ui" line="1490"/>
+        <source>Exit</source>
+        <translation>Salir</translation>
+    </message>
+    <message>
+        <location filename="../../main_window.ui" line="1499"/>
+        <source>Ctrl+W</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../main_window.ui" line="1510"/>
+        <source>About</source>
+        <translation>Acerca de</translation>
+    </message>
+    <message>
+        <location filename="../../main_window.ui" line="1519"/>
+        <source>Ctrl+M</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../main_window.ui" line="1524"/>
+        <source>Language</source>
+        <translation>Idioma</translation>
+    </message>
+    <message>
+        <location filename="../../main_window.ui" line="1527"/>
+        <source>Ctrl+L</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../main_window.ui" line="1532"/>
+        <source>Theme</source>
+        <translation>Tema</translation>
+    </message>
+    <message>
+        <location filename="../../main_window.ui" line="1540"/>
+        <source>Ctrl+T</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../main_window.ui" line="1549"/>
+        <source>Open Source Licences</source>
+        <translation>Licencias de código abierto</translation>
+    </message>
+</context>
+<context>
+    <name>OSL</name>
+    <message>
+        <location filename="../../_osl.py" line="25"/>
+        <source>{__name_library.capitalize()} - original: {__links[__name_library.lower()]}</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>about_dialog</name>
+    <message>
+        <location filename="../../dialog_about.ui" line="21"/>
+        <source>About</source>
+        <translation>Acerca de</translation>
+    </message>
+    <message>
+        <location filename="../../dialog_about.ui" line="88"/>
+        <source>&lt;code&gt;pip install pymd&lt;/code&gt;</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>dialog_osl</name>
+    <message>
+        <location filename="../../osl.ui" line="32"/>
+        <source>Open Source Licenses - Notice</source>
+        <translation>Lincencias de código abierto - Aviso</translation>
+    </message>
+    <message>
+        <location filename="../../osl.ui" line="66"/>
+        <location filename="../../osl.ui" line="70"/>
+        <source>Select a library</source>
+        <translation>Seleccione una librería</translation>
+    </message>
+    <message>
+        <location filename="../../osl.ui" line="75"/>
+        <source>Markitdown</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../osl.ui" line="80"/>
+        <source>PySide6</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../osl.ui" line="190"/>
+        <source>CREDITS</source>
+        <translation>CRÉDITOS</translation>
+    </message>
+</context>
+</TS>
