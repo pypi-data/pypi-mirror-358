5972531899bbde0aba28189b12ff77af6b7ad47c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:18:18 +0000

    Add package eclypse-core/0.7.3/eclypse_core-0.7.3-cp313-cp313-manylinux_2_39_x86_64.whl

---
+++ packages/eclypse-core/eclypse_core-0.7.3-cp313-cp313-manylinux_2_39_x86_64.whl/eclypse_core-stubs/remote/communication/__init__.pyi
@@ -0,0 +1,5 @@
+from .interface import EclypseCommunicationInterface
+from .request import EclypseRequest
+from .route import Route
+
+__all__ = ["EclypseCommunicationInterface", "Route", "EclypseRequest"]
