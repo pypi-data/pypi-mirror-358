b4a678a3f331ef516ac76be9e871969b36671846
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:47 +0000

    Add package admin-auto-filters/0.0.1/admin-auto-filters-0.0.1.tar.gz

---
+++ packages/admin-auto-filters/admin-auto-filters-0.0.1.tar.gz/admin-auto-filters-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="admin-auto-filters",
+    version="0.0.1",
+    packages=["admin-auto-filters"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
