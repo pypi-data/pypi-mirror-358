4415d97093ecd2d4588d77be8d6e66bcbe09a85b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:41 +0000

    Add package bekzod-kutubxonasi/0.1/bekzod_kutubxonasi-0.1.tar.gz

---
+++ packages/bekzod-kutubxonasi/bekzod_kutubxonasi-0.1.tar.gz/bekzod_kutubxonasi-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bekzod_kutubxonasi",
+    version="0.1",
+    description="Loyihalar va metodlarni o'z ichiga olgan oddiy kutubxona",
+    author="Asqarov Bekzod",
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
