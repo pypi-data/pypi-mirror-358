bb1c0e7aa8a5a48828b6e8252301727d978a32a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:46 +0000

    Add package mrgcv/0.0.1/mrgcv-0.0.1.tar.gz

---
+++ packages/mrgcv/mrgcv-0.0.1.tar.gz/mrgcv-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="mrgcv",
+    version="0.0.1",
+    packages=["mrgcv"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
