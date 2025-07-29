f7a6de586bc61f9be7a9053170c9504cdd870598
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:04:58 +0000

    Add package amp-c/0.0.1/amp-c-0.0.1.tar.gz

---
+++ packages/amp-c/amp-c-0.0.1.tar.gz/amp-c-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="amp-c",
+    version="0.0.1",
+    packages=["amp-c"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
