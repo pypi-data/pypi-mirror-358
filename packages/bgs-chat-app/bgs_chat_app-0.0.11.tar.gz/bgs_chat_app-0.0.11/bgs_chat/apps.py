fa7379021aa390d22b19f45506c7837fef974ee9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:06:23 +0000

    Add package bgs-chat-app/0.0.11/bgs_chat_app-0.0.11.tar.gz

---
+++ packages/bgs-chat-app/bgs_chat_app-0.0.11.tar.gz/bgs_chat_app-0.0.11/bgs_chat/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class BgsChatConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'bgs_chat_app'
+    label = 'bgs_chat_app'
