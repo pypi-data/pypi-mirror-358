b28961c47ca83f7099984559fa7955dee8468447
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:45 +0000

    Add package scrooge-plugin-sdk/1.1.4/scrooge-plugin-sdk-1.1.4.tar.gz

---
+++ packages/scrooge-plugin-sdk/scrooge-plugin-sdk-1.1.4.tar.gz/scrooge-plugin-sdk-1.1.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="scrooge-plugin-sdk",
+    version="1.1.4",
+    packages=["scrooge-plugin-sdk"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
