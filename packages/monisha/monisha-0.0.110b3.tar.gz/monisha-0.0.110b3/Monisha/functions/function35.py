d2823de6d511e5c4306a99a7ab0f41da7e54f35f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:32 +0000

    Add package monisha/0.0.110b3/monisha-0.0.110b3.tar.gz

---
+++ packages/monisha/monisha-0.0.110b3.tar.gz/monisha-0.0.110b3/Monisha/functions/function35.py
@@ -0,0 +1,28 @@
+import arrow
+from .function05 import Doxo
+#============================================================
+
+class Peimed:
+
+    async def get01(moonus, location="Asia/Kolkata"):
+        moon01 = arrow.now(location)
+        moon02 = moon01.shift(days=moonus)
+        moones = moon02.format(Doxo.DATA05)
+        return moones
+
+    async def get02(moonus, location="Asia/Kolkata"):
+        moon01 = arrow.now(location)
+        moon02 = moon01.format(Doxo.DATA05)
+        moon03 = arrow.get(moon02, Doxo.DATA05)
+        moon04 = arrow.get(moonus, Doxo.DATA05)
+        moones = (moon04 - moon03).days
+        return moones
+
+    async def get03(location="Asia/Kolkata"):
+        nemons = arrow.now(location)
+        nomses = arrow.get(mesams, Doxo.DATA05)
+        cemons = nomses.replace(tzinfo=location)
+        moones = round((cemons - nemons).total_seconds())
+        return moones
+
+#============================================================
