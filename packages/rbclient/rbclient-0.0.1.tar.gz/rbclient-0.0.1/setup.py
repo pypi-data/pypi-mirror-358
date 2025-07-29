7e19faa1233f898878f7bc0298669edf97bc5d56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:06 +0000

    Add package rbclient/0.0.1/rbclient-0.0.1.tar.gz

---
+++ packages/rbclient/rbclient-0.0.1.tar.gz/rbclient-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="rbclient",
+    version="0.0.1",
+    packages=["rbclient"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
