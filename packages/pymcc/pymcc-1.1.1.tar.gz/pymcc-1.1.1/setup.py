2a703d9d42588b219c1a12ac5412828272cfa9b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:43 +0000

    Add package pymcc/1.1.1/pymcc-1.1.1.tar.gz

---
+++ packages/pymcc/pymcc-1.1.1.tar.gz/pymcc-1.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pymcc",
+    version="1.1.1",
+    packages=["pymcc"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
