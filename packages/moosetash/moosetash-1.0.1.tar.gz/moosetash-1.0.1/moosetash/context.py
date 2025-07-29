43c8e7deeaef844314b8f3bf0c8f44b0e8baf20c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:57:08 +0000

    Add package moosetash/1.0.1/moosetash-1.0.1.tar.gz

---
+++ packages/moosetash/moosetash-1.0.1.tar.gz/moosetash-1.0.1/moosetash/context.py
@@ -0,0 +1,73 @@
+"""Utilities for accessing rendering context"""
+
+from typing import Any, List, Optional
+
+from .exceptions import ContextAccessError, MissingVariable
+
+
+def get(obj: Any, variable: str) -> Any:
+    """Lookup key from obj"""
+    try:
+        return obj[variable]
+    except (TypeError, AttributeError, IndexError):
+        pass
+    except KeyError as ex:
+        raise MissingVariable(variable) from ex
+
+    try:
+        return getattr(obj, variable)
+    except (TypeError, AttributeError):
+        pass
+
+    try:
+        return obj[int(variable)]
+    except (IndexError, KeyError) as ex:
+        raise MissingVariable(variable) from ex
+    except ValueError:
+        pass
+
+    raise MissingVariable(variable)
+
+
+def deep_get(context: Any, variable: str, units: Optional[List[str]] = None) -> Any:
+    """Recursively fetch a variable from context"""
+    if variable == ".":
+        # Handle implicit interpolation
+        return context
+
+    value = context
+    has_match = False
+
+    if units is None:
+        units = variable.split(".")
+
+    for unit in units:
+        try:
+            value = get(value, unit)
+        except MissingVariable as ex:
+            if has_match:
+                return None
+            raise ex
+        except Exception as ex:
+            raise ContextAccessError(variable) from ex
+        has_match = True
+    return value
+
+
+def get_from_context(contexts: List[Any], variable: str) -> Any:
+    """Search list of context for a variable"""
+
+    value = None
+    units = variable.split(".")
+
+    for context in reversed(contexts):
+        try:
+            value = deep_get(context, variable, units)
+            break
+        except MissingVariable:
+            pass
+
+    if value is None:
+        raise MissingVariable(variable)
+
+    return value
