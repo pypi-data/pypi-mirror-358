a34d04e92eff7e6bd3c27b689cf096d9c3727d3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:06:00 +0000

    Add package pymrasd/0.0.1/pymrasd-0.0.1.tar.gz

---
+++ packages/pymrasd/pymrasd-0.0.1.tar.gz/pymrasd-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pymrasd",
+    version="0.0.1",
+    packages=["pymrasd"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
