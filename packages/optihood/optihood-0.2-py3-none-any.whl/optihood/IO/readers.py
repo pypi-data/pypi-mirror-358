894535e67831579e990879cf7772e27296887e75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:06:07 +0000

    Add package optihood/0.2/optihood-0.2-py3-none-any.whl

---
+++ packages/optihood/optihood-0.2-py3-none-any.whl/optihood/IO/readers.py
@@ -0,0 +1,191 @@
+import configparser as _cp
+import dataclasses as _dc
+import pathlib as _pl
+
+import pandas as _pd
+
+import optihood.entities as ent
+
+
+@_dc.dataclass
+class CsvReader:
+    """ use_function is currently implemented to deal with deprecation of a pandas feature
+    (_pd.to_numeric, errors="ignore").
+    This can be set to 'current' or 'future'.
+    """
+
+    dir_path: _pl.Path
+    use_function: str = 'future'  # or 'current'
+
+    def __post_init__(self):
+        if self.use_function == 'current':
+            self.make_nrs_numeric = self.make_nrs_numeric_current
+        elif self.use_function == 'future':
+            self.make_nrs_numeric = self.make_nrs_numeric_without_future_warning
+
+    def read(self, file_name: str) -> _pd.DataFrame:
+        # read_csv does not have as strong a parser as ExcelFile.parse.
+        # One issue the following addresses, is the "nr as string" outputs.
+
+        df = _pd.read_csv(self.dir_path / file_name)
+        [self.make_nrs_numeric(df, column) for column in df.columns]
+
+        return df
+
+    @staticmethod
+    def safe_to_numeric(value):
+        # TODO: cleanup differing versions.
+        try:
+            return _pd.to_numeric(value)
+        except (ValueError, TypeError):
+            return value
+          
+    @staticmethod
+    def make_nrs_numeric_current(df: _pd.DataFrame, column_name: str) -> None:
+        df[column_name] = df[column_name].apply(_pd.to_numeric, errors="ignore")
+        # df[column_name] = df[column_name].apply(CsvReader.safe_to_numeric)
+
+    @staticmethod
+    def make_nrs_numeric_without_future_warning(df: _pd.DataFrame, column_name: str) -> None:
+        # Fix using "coerce" and re-filling NaN values.
+        # Unfortunately, this applies to full text columns as well.
+        def parse_numbers(x):
+            # Suggested by pandas developers
+            # https://github.com/pandas-dev/pandas/issues/59221#issuecomment-2755021659
+            try:
+                return _pd.to_numeric(x)
+            except Exception:
+                return x
+
+        df[column_name] = df[column_name].apply(parse_numbers)
+
+
+@_dc.dataclass
+class CsvScenarioReader(CsvReader):
+    """Very simplified implementation.
+    Each CSV file inherently has its own data model.
+    This needs to be incorporated into a validation of these inputs.
+    """
+
+    relative_file_paths: dict[str, str] = _dc.field(init=False)
+
+    def __post_init__(self):
+        super().__post_init__()
+        paths = ent.CsvInputFilePathsRelative
+        self.relative_file_paths = {
+            ent.NodeKeys.buses: paths.buses,
+            ent.NodeKeys.grid_connection: paths.grid_connection,
+            ent.NodeKeys.commodity_sources: paths.commodity_sources,
+            ent.NodeKeys.solar: paths.solar,
+            ent.NodeKeys.transformers: paths.transformers,
+            ent.NodeKeys.demand: paths.demand,
+            ent.NodeKeys.storages: paths.storages,
+            ent.NodeKeys.stratified_storage: paths.stratified_storage,
+            ent.NodeKeys.profiles: paths.profiles,
+            ent.NodeKeys.links: paths.links,
+        }
+
+    def read_scenario(self) -> dict[str, _pd.DataFrame]:
+        # Fine-grained feedback could be provided using validators for the individual files.
+        # This would include information related to the models,
+        # e.g. T range should be between 7 and 12 deg C for chillers.
+
+        data = {}
+        errors = []
+        for key, rel_path in self.relative_file_paths.items():
+            try:
+                data[key] = self.read(rel_path)
+                # df_current = _pd.read_csv(path)
+            except FileNotFoundError as e:
+                if not key == ent.NodeKeys.links:
+                    errors.append(e)
+
+            # validation_error = self.validate(key, df_current)
+            # if not validation_error:
+            #     data[key] = df_current
+            # else:
+            #     errors.append(validation_error)
+
+        if errors:
+            # Should this be added to logging as well?
+            raise ExceptionGroup("Issues with CSV files.", errors)
+
+        return data
+
+
+def parse_config(configFilePath: str):
+    # Making this a class method would allow us to provide the parser.
+    config = _cp.ConfigParser()
+    config.read(configFilePath)
+    configData = {}
+    for section in config.sections():
+        configData[section] = config.items(section)
+    configData = {k.lower(): v for k, v in configData.items()}
+
+    return configData
+
+
+def add_unique_label_columns(nodal_data: dict[str, _pd.DataFrame]) -> dict[str, _pd.DataFrame]:
+    """Provides new columns with unique labels for "label", "to", "from", "connect"
+    This function also adds a unique label for the buildings in "building_model_parameters".
+
+    The DataFrames in the sheets are updated automatically as each df is a pointer to that sheet.
+    """
+    sheets = list(nodal_data.keys())
+    building_sheet_name = ent.NodeKeysOptional.building_model_parameters
+
+    if building_sheet_name in sheets:
+        sheets.remove(building_sheet_name)
+        df = nodal_data[building_sheet_name]
+        df[ent.BuildingModelParameters.building_unique] = get_unique_buildings(df)
+
+    sheets_with_no_need_for_unique_labels = [ent.NodeKeys.ice_storage, ent.NodeKeys.stratified_storage,
+                                             ent.NodeKeys.profiles, ent.NodeKeys.links]
+    # TODO: maybe remove time_step_data from sheets as well.
+
+    [sheets.remove(x) for x in sheets_with_no_need_for_unique_labels if x in sheets]
+    for sheet in sheets:
+        df = nodal_data[sheet]
+        df[ent.CommonLabels.label_unique] = get_unique_labels(
+            df[[ent.CommonLabels.label, ent.CommonLabels.building]])
+
+        if ent.CommonLabels.from_bus in df.columns:
+            df[ent.CommonLabels.from_unique] = get_unique_buses(
+                df[[ent.CommonLabels.from_bus, ent.CommonLabels.building]], ent.CommonLabels.from_bus)
+
+        if ent.CommonLabels.to in df.columns:
+            df[ent.CommonLabels.to_unique] = get_unique_buses(df[[ent.CommonLabels.to, ent.CommonLabels.building]],
+                                                              ent.CommonLabels.to)
+
+        if ent.CommonLabels.connect in df.columns:
+            df[ent.CommonLabels.connect_unique] = get_unique_buses(
+                df[[ent.CommonLabels.connect, ent.CommonLabels.building]], ent.CommonLabels.connect)
+
+    return nodal_data
+
+
+def get_unique_labels(df: _pd.DataFrame) -> list[str]:
+    return [f"{row[ent.CommonLabels.label.value]}__B{str(row[ent.CommonLabels.building.value]).zfill(3)}" for _, row
+            in df.iterrows()]
+
+
+def get_unique_buses(df: _pd.DataFrame, buses_column: str) -> list[list[str]]:
+    """Returns lists of strings for all cases, to simplify usage later."""
+    buses = []
+    for _, row in df.iterrows():
+        row_buses = []
+        for bus in row[buses_column].split(","):
+            row_buses.append(f"{bus}__B{str(row[ent.CommonLabels.building.value]).zfill(3)}")
+        buses.append(row_buses)
+    return buses
+
+
+def get_unique_buildings(df: _pd.DataFrame) -> list[str]:
+    """Returns strings for both cases, to simplify usage later."""
+    if ent.BuildingModelParameters.Circuit in df.columns:
+        return [
+            (f"Building_model__B{str(row[ent.BuildingModelParameters.Building_Number]).zfill(3)}"
+             f"_C{str(row[ent.BuildingModelParameters.Circuit]).zfill(3)}")
+            for _, row in df.iterrows()]
+
+    return [f"Building_model__B{str(row[ent.BuildingModelParameters.Building_Number]).zfill(3)}" for _, row in df.iterrows()]
---
+++ packages/optihood/optihood-0.2-py3-none-any.whl/tests/IO/test_readers.py
@@ -0,0 +1,214 @@
+import pathlib as _pl
+import unittest as _ut
+
+import pandas as _pd
+
+import optihood.IO.readers as ior
+import optihood.IO.writers as wr
+import optihood.energy_network as en
+import optihood.entities as ent
+import tests.xls_helpers as xlsh
+
+_CSV_DIR_PATH = xlsh.ROOT_DATA_DIR / "CSVs" / "basic_example_CSVs"
+_input_data_dir = xlsh.ROOT_DATA_DIR / "excels" / "basic_example"
+_input_data_path = _input_data_dir / "scenario.xls"
+
+
+def join_if_multiple(x):
+    """Needed to overcome changes when using subsequent IO for tests."""
+    if isinstance(x, str):
+        x = eval(x)
+
+    if len(x) > 1:
+        return ', '.join(x)
+
+    return x[0]
+
+
+def write_expected_file(nodal_data_with_unique_labels, expected_files_path: _pl.Path) -> None:
+    """Helper function to update the expected files."""
+    writer = wr.ScenarioFileWriterCSV(_pl.Path("irrelevant"), "individual")
+    writer.data = nodal_data_with_unique_labels
+    writer._write_scenario_to_file(expected_files_path)
+
+
+class TestCsvScenarioReader(_ut.TestCase):
+    def setUp(self):
+        self.maxDiff = None
+
+        network = en.EnergyNetworkClass(None)
+        data = _pd.ExcelFile(str(_input_data_path))
+        self.expected_nodal_data = network.get_nodal_data_from_Excel(data)
+        data.close()
+
+    def test_read(self):
+        """Test to ensure new reader produces 'identical' inputs to optihood."""
+        csvReader = ior.CsvScenarioReader(_CSV_DIR_PATH, "current")
+        nodal_data = csvReader.read_scenario()
+
+        errors = []
+
+        xlsh.check_assertion(self, errors, nodal_data.keys(), self.expected_nodal_data.keys())
+
+        for key, df_current in nodal_data.items():
+            # check_dtype=False to simplify comparison between Excel and CSV files.
+            xlsh.check_dataframe_assertion(errors, df_current, self.expected_nodal_data[key], check_dtype=False)
+
+        if errors:
+            raise ExceptionGroup(f"found {len(errors)} errors", errors)
+
+    def test_read_without_future_warning(self):
+        """Test to ensure new reader produces 'identical' inputs to optihood."""
+        csvReader = ior.CsvScenarioReader(_CSV_DIR_PATH)
+        nodal_data = csvReader.read_scenario()
+
+        errors = []
+
+        xlsh.check_assertion(self, errors, nodal_data.keys(), self.expected_nodal_data.keys())
+
+        for key, df_current in nodal_data.items():
+            # check_dtype=False to simplify comparison between Excel and CSV files.
+            xlsh.check_dataframe_assertion(errors, df_current, self.expected_nodal_data[key], check_dtype=False)
+
+        if errors:
+            raise ExceptionGroup(f"found {len(errors)} errors", errors)
+
+    def test_get_unique_labels(self):
+        """unit test"""
+        df = _pd.DataFrame({ent.CommonLabels.label: ["a", "a", "b", "c"],
+                            ent.CommonLabels.building: ["1", "2", "1", "1"],
+                            })
+        results = ior.get_unique_labels(df)
+        self.assertListEqual(results, ['a__B001', 'a__B002', 'b__B001', 'c__B001'])
+
+    def test_get_unique_buses(self):
+        """unit test"""
+        df = _pd.DataFrame({ent.CommonLabels.to: ["a", "a,b", "b,d,e", "c"],
+                            ent.CommonLabels.building: ["1", "2", "1", "1"],
+                            })
+        results = ior.get_unique_buses(df, ent.CommonLabels.to)
+        self.assertListEqual(results, [['a__B001'],
+                                       ['a__B002', 'b__B002'],
+                                       ['b__B001', 'd__B001', 'e__B001'],
+                                       ['c__B001']])
+
+    def test_add_unique_label_columns(self):
+        """end2end test
+        Unfortunately, the reader adds quotes to the entries.
+        This leads to very strange behavior in assert_frame_equal:
+        At positional index 0, first diff: ['electricityProdBus__B001'] != ['electricityProdBus__B001']
+        Thus, this test undoes the work of putting strings in lists, to put them back to a single string again.
+        """
+        csvReader = ior.CsvScenarioReader(_CSV_DIR_PATH)
+        nodal_data = csvReader.read_scenario()
+
+        nodal_data_with_unique_labels = ior.add_unique_label_columns(nodal_data)
+
+        expected_files_path = _pl.Path(__file__).parent / "expected_files" / "without_building_csvs"
+        # =================================
+        # TODO: Use this to update the expected files.  # pylint: disable=fixme
+        # write_expected_file(nodal_data_with_unique_labels, expected_files_path)
+        # =================================
+
+        csvReader = ior.CsvScenarioReader(expected_files_path)
+        expected_data = csvReader.read_scenario()
+
+        errors = []
+
+        xlsh.check_assertion(self, errors, nodal_data_with_unique_labels.keys(), expected_data.keys())
+
+        for key, df_current in nodal_data_with_unique_labels.items():
+            # check_dtype=False to simplify comparison between Excel and CSV files.
+            df_expected = expected_data[key]
+            from_unique = ent.CommonLabels.from_unique
+            if from_unique in df_current.columns:
+                df_expected[from_unique] = df_expected[from_unique].apply(join_if_multiple)
+                df_current[from_unique] = df_current[from_unique].apply(join_if_multiple)
+
+            to_unique = ent.CommonLabels.to_unique
+            if to_unique in df_current.columns:
+                df_expected[to_unique] = df_expected[to_unique].apply(join_if_multiple)
+                df_current[to_unique] = df_current[to_unique].apply(join_if_multiple)
+
+            connect_unique = ent.CommonLabels.connect_unique
+            if connect_unique in df_current.columns:
+                df_expected[connect_unique] = df_expected[connect_unique].apply(join_if_multiple)
+                df_current[connect_unique] = df_current[connect_unique].apply(join_if_multiple)
+
+            xlsh.check_dataframe_assertion(errors, df_current, df_expected, check_dtype=False)
+
+        if errors:
+            raise ExceptionGroup(f"found {len(errors)} errors", errors)
+
+    def test_get_unique_buildings_with_circuits(self):
+        """unit test"""
+        df = _pd.DataFrame({ent.BuildingModelParameters.Building_Number: [1, 1, 2, 3],
+                            ent.BuildingModelParameters.Circuit: [1, 2, 1, 1],
+                            })
+        results = ior.get_unique_buildings(df)
+        self.assertListEqual(results, ['Building_model__B001_C001', 'Building_model__B001_C002',
+                                       'Building_model__B002_C001', 'Building_model__B003_C001'])
+
+    def test_get_unique_buildings_without_circuits(self):
+        """unit test"""
+        df = _pd.DataFrame({ent.BuildingModelParameters.Building_Number: [1, 1, 2, 3],
+                            })
+        results = ior.get_unique_buildings(df)
+        self.assertListEqual(results, ['Building_model__B001', 'Building_model__B001', 'Building_model__B002',
+                                       'Building_model__B003'])
+
+    def test_add_unique_label_columns_including_buildings(self):
+        """End2end test"""
+        csvReader = ior.CsvScenarioReader(_CSV_DIR_PATH)
+        nodal_data = csvReader.read_scenario()
+        building_df = _pd.DataFrame({ent.BuildingModelParameters.Building_Number: [1, 1, 2, 3],
+                                     ent.BuildingModelParameters.Circuit: [1, 2, 1, 1],
+                                     })
+        nodal_data[ent.NodeKeysOptional.building_model_parameters] = building_df
+
+        nodal_data_with_unique_labels = ior.add_unique_label_columns(nodal_data)
+
+        expected_files_path = _pl.Path(__file__).parent / "expected_files" / "without_building_csvs"
+
+        # =================================
+        # TODO: Use this to update the expected files.  # pylint: disable=fixme
+        # write_expected_file(nodal_data_with_unique_labels, expected_files_path)
+        # =================================
+
+        csvReader = ior.CsvScenarioReader(expected_files_path)
+        expected_data = csvReader.read_scenario()
+
+        building_df_expected = building_df.copy()
+        building_df_expected[ent.BuildingModelParameters.building_unique] = ['Building_model__B001_C001',
+                                                                             'Building_model__B001_C002',
+                                                                             'Building_model__B002_C001',
+                                                                             'Building_model__B003_C001',
+                                                                             ]
+        expected_data[ent.NodeKeysOptional.building_model_parameters] = building_df_expected
+
+        errors = []
+
+        xlsh.check_assertion(self, errors, nodal_data_with_unique_labels.keys(), expected_data.keys())
+
+        for key, df_current in nodal_data_with_unique_labels.items():
+            # check_dtype=False to simplify comparison between Excel and CSV files.
+            df_expected = expected_data[key]
+            from_unique = ent.CommonLabels.from_unique
+            if from_unique in df_current.columns:
+                df_expected[from_unique] = df_expected[from_unique].apply(join_if_multiple)
+                df_current[from_unique] = df_current[from_unique].apply(join_if_multiple)
+
+            to_unique = ent.CommonLabels.to_unique
+            if to_unique in df_current.columns:
+                df_expected[to_unique] = df_expected[to_unique].apply(join_if_multiple)
+                df_current[to_unique] = df_current[to_unique].apply(join_if_multiple)
+
+            connect_unique = ent.CommonLabels.connect_unique
+            if connect_unique in df_current.columns:
+                df_expected[connect_unique] = df_expected[connect_unique].apply(join_if_multiple)
+                df_current[connect_unique] = df_current[connect_unique].apply(join_if_multiple)
+
+            xlsh.check_dataframe_assertion(errors, df_current, df_expected, check_dtype=False)
+
+        if errors:
+            raise ExceptionGroup(f"found {len(errors)} errors", errors)
