af2aa2559066319928dd77c935f1857f8e595675
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:06:32 +0000

    Add package scrooge-domain/0.0.34/scrooge-domain-0.0.34.tar.gz

---
+++ packages/scrooge-domain/scrooge-domain-0.0.34.tar.gz/scrooge-domain-0.0.34/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="scrooge-domain",
+    version="0.0.34",
+    packages=["scrooge-domain"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
