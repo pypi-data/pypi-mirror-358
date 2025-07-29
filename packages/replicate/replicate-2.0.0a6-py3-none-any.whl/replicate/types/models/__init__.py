773c03ed38f614745cb34a44b72f500e00bd057d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:06:51 +0000

    Add package replicate/2.0.0a6/replicate-2.0.0a6-py3-none-any.whl

---
+++ packages/replicate/replicate-2.0.0a6-py3-none-any.whl/replicate/types/models/__init__.py
@@ -0,0 +1,6 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from .readme_get_response import ReadmeGetResponse as ReadmeGetResponse
+from .prediction_create_params import PredictionCreateParams as PredictionCreateParams
