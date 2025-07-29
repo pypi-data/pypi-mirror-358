32ec997d542498bcc7fbb4304633177586219d68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:26 +0000

    Add package yt-meta/0.2.4/yt_meta-0.2.4-py3-none-any.whl

---
+++ packages/yt-meta/yt_meta-0.2.4-py3-none-any.whl/yt_meta/__init__.py
@@ -0,0 +1,14 @@
+# yt_meta/__init__.py
+
+from .client import YtMeta
+from .date_utils import parse_relative_date_string
+from .exceptions import MetadataParsingError, VideoUnavailableError
+
+__version__ = "0.2.4"
+
+__all__ = [
+    "YtMeta",
+    "MetadataParsingError",
+    "VideoUnavailableError",
+    "parse_relative_date_string",
+]
