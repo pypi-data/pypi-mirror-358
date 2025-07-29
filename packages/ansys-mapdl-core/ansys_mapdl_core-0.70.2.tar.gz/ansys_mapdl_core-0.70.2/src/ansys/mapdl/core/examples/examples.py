6071210e368fd6eff7cc534a79f1f5eca4b54af5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:04:55 +0000

    Add package ansys-mapdl-core/0.70.2/ansys_mapdl_core-0.70.2.tar.gz

---
+++ packages/ansys-mapdl-core/ansys_mapdl_core-0.70.2.tar.gz/ansys_mapdl_core-0.70.2/src/ansys/mapdl/core/examples/examples.py
@@ -0,0 +1,35 @@
+# Copyright (C) 2016 - 2025 ANSYS, Inc. and/or its affiliates.
+# SPDX-License-Identifier: MIT
+#
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+"""pymapdl examples"""
+import os
+
+# get location of this folder and the example files
+dir_path: str = os.path.dirname(os.path.realpath(__file__))
+
+# add any files you'd like to import here.  For example:
+wing_model: str = os.path.join(dir_path, "wing.dat")
+
+# be sure to add the input file directly in this directory
+# This way, files can be loaded with:
+# from ansys.mapdl.core import examples
+# examples.wing_model
