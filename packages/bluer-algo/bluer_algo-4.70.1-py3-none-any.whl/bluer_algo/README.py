04139d7ffba47dd1042417e9ca864a41a5590bf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:01 +0000

    Add package bluer-algo/4.70.1/bluer_algo-4.70.1-py3-none-any.whl

---
+++ packages/bluer-algo/bluer_algo-4.70.1-py3-none-any.whl/bluer_algo/README.py
@@ -0,0 +1,35 @@
+import os
+
+from bluer_objects import file, README
+
+from bluer_algo import NAME, VERSION, ICON, REPO_NAME
+
+
+items = README.Items(
+    [
+        {
+            "name": "image classifier",
+            "marquee": "https://github.com/kamangir/assets/raw/main/blue-plugin/marquee.png?raw=true",
+            "description": "an image classifier.",
+            "url": "./bluer_algo/docs/image-classifier.md",
+        }
+    ]
+)
+
+
+def build():
+    return all(
+        README.build(
+            items=readme.get("items", []),
+            path=os.path.join(file.path(__file__), readme["path"]),
+            ICON=ICON,
+            NAME=NAME,
+            VERSION=VERSION,
+            REPO_NAME=REPO_NAME,
+        )
+        for readme in [
+            {"items": items, "path": ".."},
+            {"path": "docs/image-classifier.md"},
+            {"path": "docs/image-classifier-ingest.md"},
+        ]
+    )
