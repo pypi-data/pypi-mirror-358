6856941a81858052b1103dcdc6bc2c2a87f40f24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:06:25 +0000

    Add package pypo-typewriter-importer/3.1.5/pypo-typewriter-importer-3.1.5.tar.gz

---
+++ packages/pypo-typewriter-importer/pypo-typewriter-importer-3.1.5.tar.gz/pypo-typewriter-importer-3.1.5/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pypo-typewriter-importer",
+    version="3.1.5",
+    packages=["pypo-typewriter-importer"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
