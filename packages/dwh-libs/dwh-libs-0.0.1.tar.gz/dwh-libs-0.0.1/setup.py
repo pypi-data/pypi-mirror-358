974852c45a9de2360c6ceb28012686e76aa1969f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:06 +0000

    Add package dwh-libs/0.0.1/dwh-libs-0.0.1.tar.gz

---
+++ packages/dwh-libs/dwh-libs-0.0.1.tar.gz/dwh-libs-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dwh-libs",
+    version="0.0.1",
+    packages=["dwh-libs"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
