1ac7a8614ca24681e768ca8f57965bb172f5a681
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:18 +0000

    Add package addressbook-pb2/0.0.1/addressbook-pb2-0.0.1.tar.gz

---
+++ packages/addressbook-pb2/addressbook-pb2-0.0.1.tar.gz/addressbook-pb2-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="addressbook-pb2",
+    version="0.0.1",
+    packages=["addressbook-pb2"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
