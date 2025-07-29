b03b2a966be25634ab2736b24766dbf32e901110
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:06:44 +0000

    Add package mras-datamining/2.2.6/mras-datamining-2.2.6.tar.gz

---
+++ packages/mras-datamining/mras-datamining-2.2.6.tar.gz/mras-datamining-2.2.6/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="mras-datamining",
+    version="2.2.6",
+    packages=["mras-datamining"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
