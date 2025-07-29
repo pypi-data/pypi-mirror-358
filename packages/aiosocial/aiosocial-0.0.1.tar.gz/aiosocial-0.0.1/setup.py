ba4b421a11c8eb674850c6f6f74d093c81616ca7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:04 +0000

    Add package aiosocial/0.0.1/aiosocial-0.0.1.tar.gz

---
+++ packages/aiosocial/aiosocial-0.0.1.tar.gz/aiosocial-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="aiosocial",
+    version="0.0.1",
+    packages=["aiosocial"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
