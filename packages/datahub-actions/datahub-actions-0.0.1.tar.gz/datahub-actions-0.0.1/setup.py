cc7dca4a4a32343afd180c00553e0d337afcbcf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:06:10 +0000

    Add package datahub-actions/0.0.1/datahub-actions-0.0.1.tar.gz

---
+++ packages/datahub-actions/datahub-actions-0.0.1.tar.gz/datahub-actions-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="datahub-actions",
+    version="0.0.1",
+    packages=["datahub-actions"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
