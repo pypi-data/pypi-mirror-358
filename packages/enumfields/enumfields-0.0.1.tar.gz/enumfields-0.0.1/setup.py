1a26d7562be338ccbbcfc6b73da451e9675f6f32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:12 +0000

    Add package enumfields/0.0.1/enumfields-0.0.1.tar.gz

---
+++ packages/enumfields/enumfields-0.0.1.tar.gz/enumfields-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="enumfields",
+    version="0.0.1",
+    packages=["enumfields"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
