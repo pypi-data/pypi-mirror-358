aa7cb81c355d6a8d6ab3334fde72926de28c96b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:54 +0000

    Add package tenants-access/3.0.0/tenants-access-3.0.0.tar.gz

---
+++ packages/tenants-access/tenants-access-3.0.0.tar.gz/tenants-access-3.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="tenants-access",
+    version="3.0.0",
+    packages=["tenants-access"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
