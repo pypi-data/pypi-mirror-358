b9a4b2d3071a6a4edd8933da825ae0f6fcb4aef4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:53 +0000

    Add package mras_gunicorn/20.0.0/mras_gunicorn-20.0.0.tar.gz

---
+++ packages/mras_gunicorn/mras_gunicorn-20.0.0.tar.gz/mras_gunicorn-20.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="mras_gunicorn",
+    version="20.0.0",
+    packages=["mras_gunicorn"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
