e96faf1bc9752b1817307781d3c7c9d75963ed31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:04:58 +0000

    Add package nanomd/0.4.0/nanomd-0.4.0.tar.gz

---
+++ packages/nanomd/nanomd-0.4.0.tar.gz/nanomd-0.4.0/nanomd/modules/polyA.py
@@ -0,0 +1,61 @@
+import time
+import typer, glob
+from pathlib import Path
+from typing_extensions import Annotated
+from rich.progress import Progress, SpinnerColumn, TextColumn
+from basebio import check_path_exists
+from ..utils.pod5_to_fast5 import convert_to_fast5_with_summary_file
+
+app = typer.Typer()
+
+@app.command()
+def polyA(
+    inputs: Annotated[str, typer.Option("--inputs", "-i", help="Regular matching pattern for pod5 files, such as 'path/to/*pod5'.")],
+    fastq: Annotated[str, typer.Option("--fastq", "-f", help="Fastq file name.")],
+    output: Annotated[Path, typer.Option("--output", "-o", help="Output file path.")]=".",
+    prefix: Annotated[str, typer.Option("--prefix", "-p", help="Prefix for output files.")]="prefix",
+    ):
+    """
+    Detect polyA with pod5 and fastq files.
+    """
+    
+    with Progress(
+        SpinnerColumn(),
+        TextColumn("[progress.description]{task.description}"),
+        transient=True,
+    ) as progress:
+        progress.add_task(description="Detect polyA start...", total=None)
+        start=time.time()
+        
+        summary_file=f"{prefix}_summary.csv"
+        if isinstance(inputs, str):
+            input_dirs = sorted([Path(p) for p in glob.glob(inputs)])
+        elif not inputs:
+            raise ValueError("inputs should not be empty")
+        progress.add_task(description="Pod5 to fast5...", total=None)
+        if not check_path_exists(summary_file):
+            convert_to_fast5_with_summary_file(input_dirs, output, summary_file, fastq)
+        progress.add_task(description=f"Pod5 to fast5 Done", total=None)
+
+        # output_fastq=f"{output}/{prefix}_nascentRNA.fastq"
+        # progress.add_task(description="Getting nascentRNA reads...", total=None)
+        # if not check_path_exists(output_fastq):
+        #     new_fq(output_file, model, input, output_fastq)
+        # progress.add_task(description="Getting nascentRNA reads Done", total=None)
+
+        # output_bam=f"{output}/{prefix}_nascentRNA.bam"
+        # progress.add_task(description="Mapping nascentRNA reads to reference...", total=None)
+        # if not check_path_exists(output_bam):
+        #     salmon_map(output_fastq, reference, output_bam)
+        # progress.add_task(description="Mapping nascentRNA reads to reference Done", total=None)
+
+        # output_quant=f"{output}/{prefix}_quant"
+        # progress.add_task(description="Quantifying nascentRNA reads ...", total=None)
+        # if not check_path_exists(output_quant):
+        #     salmon_quantify(output_bam, reference, output_quant)
+        # progress.add_task(description="Quantifying nascentRNA reads Done", total=None)
+
+        end=time.time()
+        time_cost=f"{(end - start) // 3600}h{((end - start) % 3600) // 60}m{(end - start) % 60:.2f}s"
+        print(f"Detect polyA Done, time cost: {time_cost}")
+        progress.add_task(description=f"Detect polyA Done, time cost: {time_cost}", total=None)
---
+++ packages/nanomd/nanomd-0.4.0.tar.gz/nanomd-0.4.0/nanomd/utils/pod5_to_fast5.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+__author__ = "legendzdy@dingtalk.com"
+"""
+Author: legendzdy@dingtalk.com
+Data: 20250612
+Description:
+function map.
+"""
+from concurrent.futures import Future, ProcessPoolExecutor, as_completed
+from pathlib import Path
+from typing import Dict, List
+from more_itertools import chunked
+
+import pod5 as p5
+from pod5.tools.utils import DEFAULT_THREADS, collect_inputs, limit_threads
+from pod5.tools.pod5_convert_to_fast5 import *
+
+def convert_to_fast5_with_summary_file(
+    inputs: List[Path],
+    output: Path,
+    summary_file: str,
+    fastq_name: str,
+    recursive: bool = False,
+    threads: int = DEFAULT_THREADS,
+    force_overwrite: bool = False,
+    file_read_count: int = 4000,
+):
+    if output.exists() and not output.is_dir():
+        raise FileExistsError("Cannot output to a file")
+
+    threads = limit_threads(threads)
+    columns = ["filename_fastq", "filename_fast5", "read_id", "run_id", "channel", "mux", "start_time", "duration", "num_events", "passes_filtering", "template_start", "num_events_template", "template_duration", "sequence_length_template", "mean_qscore_template", "strand_score_template", "median_template", "mad_template", "pore_type", "experiment_id", "sample_id", "end_reason", "alias", "type", "barcode_arrangement", "barcode_full_arrangement", "barcode_kit", "barcode_variant", "barcode_score", "barcode_front_id", "barcode_front_score", "barcode_front_refseq", "barcode_front_foundseq", "barcode_front_foundseq_length", "barcode_front_begin_index", "barcode_rear_id", "barcode_rear_score", "barcode_rear_refseq", "barcode_rear_foundseq", "barcode_rear_foundseq_length", "barcode_rear_end_index"]
+
+    # 数据行（单行列表）
+    data_row = ["test", "885", "1", "56.132750", "0.756500", "605", "TRUE", "56.154000", "588", "0.735250", "153", "8.314826", "0.000000", "94.303551", "8.406906", "not_set", "20220426-CDS0204-PAK00515", "no_sample", "signal_positive", "barcode11", "test_sample", "barcode11", "LWB11_var1", "LWB", "var1", "92.750000", "BC11_FWD", "89.250000", "CCGTGACGTT", "51", "34", "BC11_REV", "92.750000", "GCAATAA", "GCACATC", "52", "20"]
+
+    with ProcessPoolExecutor(max_workers=threads) as executor:
+        total_reads = 0
+        futures: Dict[Future, Path] = {}
+
+        # Enumerate over input pod5 files
+        for input_idx, source in enumerate(
+            collect_inputs(inputs, recursive, "*.pod5", threads=threads)
+        ):
+            # Open the inputs to read the read ids
+            with open(summary_file, "w") as fo:
+                fo.write("\t".join(columns) + "\n")
+                with p5.Reader(source) as reader:
+                    for chunk_idx, read_ids in enumerate(
+                        chunked(reader.read_ids, file_read_count)
+                    ):
+                        dest = (
+                            output / f"{source.stem}.{chunk_idx}_{input_idx}.fast5"
+                        ).resolve()
+
+                        if dest.exists() and not force_overwrite:
+                            raise FileExistsError(
+                                "Output path points to an existing file and --force-overwrite not set"
+                            )
+
+                        kwargs = {
+                            "source": source,
+                            "dest": dest,
+                            "read_ids": read_ids,
+                        }
+                        futures[executor.submit(convert_pod5_to_fast5, **kwargs)] = dest  # type: ignore
+
+                        for read_id in read_ids:
+                            raw = [fastq_name, f"{source.stem}.{chunk_idx}_{input_idx}.fast5", read_id] + data_row
+                            fo.write("\t".join(raw) + "\n")
+
+                    total_reads += len(reader.read_ids)
+
+        print(f"Converting pod5s into {len(futures)} fast5 files. Please wait...")
+
+        status = StatusMonitor(file_count=len(inputs))
+        status.increment(files_started=len(inputs), read_count=total_reads)
+
+        for idx, future in enumerate(as_completed(futures)):
+            (reads_converted, samples_converted) = future.result()
+
+            status.increment(
+                files_ended=1,
+                sample_count=samples_converted,
+                reads_processed=reads_converted,
+            )
+            status.print_status()
+
+        status.print_status(force=True)
+
+        print("Conversion complete")
