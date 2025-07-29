db3a6332f87fd23cee9cee7f6a221361724d2df8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:14 +0000

    Add package foxes-opt/0.5/foxes_opt-0.5.tar.gz

---
+++ packages/foxes-opt/foxes_opt-0.5.tar.gz/foxes_opt-0.5/foxes_opt/problems/layout/__init__.py
@@ -0,0 +1,9 @@
+"""
+Wind farm layout optimization problems.
+"""
+
+from .farm_layout import FarmLayoutOptProblem as FarmLayoutOptProblem
+from .regular_layout import RegularLayoutOptProblem as RegularLayoutOptProblem
+from .reggrids_layout import RegGridsLayoutOptProblem as RegGridsLayoutOptProblem
+
+from . import geom_layouts as geom_layouts
