8b0b5fbb83c2a3463c18aadb75cd090a748de83a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:06:04 +0000

    Add package audit-sdk/0.0.1/audit-sdk-0.0.1.tar.gz

---
+++ packages/audit-sdk/audit-sdk-0.0.1.tar.gz/audit-sdk-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="audit-sdk",
+    version="0.0.1",
+    packages=["audit-sdk"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
