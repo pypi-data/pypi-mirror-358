d38c25f3954ff62227d0120dd867ea12737d01c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:00 +0000

    Add package f5lbaasdriver/0.0.1/f5lbaasdriver-0.0.1.tar.gz

---
+++ packages/f5lbaasdriver/f5lbaasdriver-0.0.1.tar.gz/f5lbaasdriver-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="f5lbaasdriver",
+    version="0.0.1",
+    packages=["f5lbaasdriver"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
