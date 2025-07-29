3d5e525a4672d486b5c9758077b80037a99fa1f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:06:23 +0000

    Add package pypo-grpc-csrf/2.0.3/pypo-grpc-csrf-2.0.3.tar.gz

---
+++ packages/pypo-grpc-csrf/pypo-grpc-csrf-2.0.3.tar.gz/pypo-grpc-csrf-2.0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pypo-grpc-csrf",
+    version="2.0.3",
+    packages=["pypo-grpc-csrf"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
