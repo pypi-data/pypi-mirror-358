ed1772cf05da88dff4a8012a3609962ae06c5fd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:03 +0000

    Add package PreCliPy/0.1.4/preclipy-0.1.4.tar.gz

---
+++ packages/PreCliPy/preclipy-0.1.4.tar.gz/preclipy-0.1.4/PreCliPy/__init__.py
@@ -0,0 +1,13 @@
+from .disease import disease
+from .drug import drug
+from .pmc import pmc
+from .nct import nct
+from .link import link
+
+__all__ = [
+    "disease",
+    "drug",
+    "pmc",
+    "nct",
+    "link"
+]
