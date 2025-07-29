0b5fd112b15ac86b65bf037761ee35a5c61d600b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:06:29 +0000

    Add package pypo-dj_manager/5.0/pypo-dj_manager-5.0.tar.gz

---
+++ packages/pypo-dj_manager/pypo-dj_manager-5.0.tar.gz/pypo-dj_manager-5.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pypo-dj_manager",
+    version="5.0",
+    packages=["pypo-dj_manager"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
