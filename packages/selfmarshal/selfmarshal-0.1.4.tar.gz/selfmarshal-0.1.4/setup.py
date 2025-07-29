d2a7aff9f22a35df9cb5e59f38d80363621b9823
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:06:12 +0000

    Add package selfmarshal/0.1.4/selfmarshal-0.1.4.tar.gz

---
+++ packages/selfmarshal/selfmarshal-0.1.4.tar.gz/selfmarshal-0.1.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="selfmarshal",
+    version="0.1.4",
+    packages=["selfmarshal"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
