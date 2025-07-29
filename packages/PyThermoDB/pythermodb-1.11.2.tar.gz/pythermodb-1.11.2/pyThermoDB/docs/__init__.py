9a5014727929f11144968f3df3860d2167fc787a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:52 +0000

    Add package PyThermoDB/1.11.2/pythermodb-1.11.2.tar.gz

---
+++ packages/PyThermoDB/pythermodb-1.11.2.tar.gz/pythermodb-1.11.2/pyThermoDB/docs/__init__.py
@@ -0,0 +1,14 @@
+from .thermo import ThermoDB
+from .tableref import TableReference
+from .managedata import ManageData
+from .customref import CustomRef
+from .compbuilder import CompBuilder
+from .tabledata import TableData
+from .tableequation import TableEquation
+from .tablematrixdata import TableMatrixData
+from .tablematrixequation import TableMatrixEquation
+
+__all__ = ['ThermoDB', 'TableReference',
+           'ManageData', 'CompBuilder', 'CustomRef',
+           'TableData', 'TableEquation',
+           'TableMatrixData', 'TableMatrixEquation']
