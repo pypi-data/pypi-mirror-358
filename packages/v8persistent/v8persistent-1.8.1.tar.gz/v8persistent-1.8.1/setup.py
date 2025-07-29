dac841b937fc7a7434dd9c7e164886793541062e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:06:26 +0000

    Add package v8persistent/1.8.1/v8persistent-1.8.1.tar.gz

---
+++ packages/v8persistent/v8persistent-1.8.1.tar.gz/v8persistent-1.8.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="v8persistent",
+    version="1.8.1",
+    packages=["v8persistent"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
