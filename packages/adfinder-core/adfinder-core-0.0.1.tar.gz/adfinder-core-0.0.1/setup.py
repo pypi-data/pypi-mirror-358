5e5b8228a5537d53f9cb23a5572b3942d2d95e9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:14 +0000

    Add package adfinder-core/0.0.1/adfinder-core-0.0.1.tar.gz

---
+++ packages/adfinder-core/adfinder-core-0.0.1.tar.gz/adfinder-core-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="adfinder-core",
+    version="0.0.1",
+    packages=["adfinder-core"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
