0c5fff8041876a0ad55b0cf9a14a184a4057af70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:06:29 +0000

    Add package pypo-trt/7.1/pypo-trt-7.1.tar.gz

---
+++ packages/pypo-trt/pypo-trt-7.1.tar.gz/pypo-trt-7.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pypo-trt",
+    version="7.1",
+    packages=["pypo-trt"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
