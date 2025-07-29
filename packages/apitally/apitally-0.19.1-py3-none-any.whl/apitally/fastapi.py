3f87cdd742d812692f081d938e0fc295f74a9341
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:09 +0000

    Add package apitally/0.19.1/apitally-0.19.1-py3-none-any.whl

---
+++ packages/apitally/apitally-0.19.1-py3-none-any.whl/apitally/fastapi.py
@@ -0,0 +1,4 @@
+from apitally.starlette import ApitallyConsumer, ApitallyMiddleware, RequestLoggingConfig, set_consumer
+
+
+__all__ = ["ApitallyMiddleware", "ApitallyConsumer", "RequestLoggingConfig", "set_consumer"]
