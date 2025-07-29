718b35000ecda51abdef27bc7bf634a79652f558
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:18:06 +0000

    Add package scrooge-plugin-license-utils/1.0.11/scrooge-plugin-license-utils-1.0.11.tar.gz

---
+++ packages/scrooge-plugin-license-utils/scrooge-plugin-license-utils-1.0.11.tar.gz/scrooge-plugin-license-utils-1.0.11/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="scrooge-plugin-license-utils",
+    version="1.0.11",
+    packages=["scrooge-plugin-license-utils"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
