eb1b86bd2a9f656b85b8b6e6c76ff5f31df24d44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:08:28 +0000

    Add package demo-diffusion/1.4/demo_diffusion-1.4.tar.gz

---
+++ packages/demo-diffusion/demo_diffusion-1.4.tar.gz/demo_diffusion-1.4/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+      name='demo_diffusion',
+      version='1.4',
+      description='customize_diffusion',
+      url='https://github.com/NVIDIA/TensorRT/tree/release/10.12/demo/Diffusion',
+      author='fanss',
+      author_email='congcongxingzhe@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False
+)
