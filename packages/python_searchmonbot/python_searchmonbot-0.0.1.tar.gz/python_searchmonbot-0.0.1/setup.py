73cc32dc1a8e4c2e2c44545ba9a31f082c83baef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:01 +0000

    Add package python_searchmonbot/0.0.1/python_searchmonbot-0.0.1.tar.gz

---
+++ packages/python_searchmonbot/python_searchmonbot-0.0.1.tar.gz/python_searchmonbot-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="python_searchmonbot",
+    version="0.0.1",
+    packages=["python_searchmonbot"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
