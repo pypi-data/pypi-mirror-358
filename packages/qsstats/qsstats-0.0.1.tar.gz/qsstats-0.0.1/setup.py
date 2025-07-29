ce875e075d8f59b7ce40ae0d61d4aef39418d958
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:56 +0000

    Add package qsstats/0.0.1/qsstats-0.0.1.tar.gz

---
+++ packages/qsstats/qsstats-0.0.1.tar.gz/qsstats-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="qsstats",
+    version="0.0.1",
+    packages=["qsstats"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
