f0bf5b7efb1b2b6c7adf9db3ba346502321cbf12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:06:13 +0000

    Add package mras-pygetadmin/1.0.4/mras-pygetadmin-1.0.4.tar.gz

---
+++ packages/mras-pygetadmin/mras-pygetadmin-1.0.4.tar.gz/mras-pygetadmin-1.0.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="mras-pygetadmin",
+    version="1.0.4",
+    packages=["mras-pygetadmin"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
