81237026f7ec95110eb7b24dbefa67e3ce28f809
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:01 +0000

    Add package libiomp5md.dll/0.0.1/libiomp5md.dll-0.0.1.tar.gz

---
+++ packages/libiomp5md.dll/libiomp5md.dll-0.0.1.tar.gz/libiomp5md.dll-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="libiomp5md.dll",
+    version="0.0.1",
+    packages=["libiomp5md.dll"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
