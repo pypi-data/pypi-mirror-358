5e59728d0ea322fee4b6a552af25828d4be6c729
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:01 +0000

    Add package django-gradual-throttle/1.0.1/django_gradual_throttle-1.0.1-py3-none-any.whl

---
+++ packages/django-gradual-throttle/django_gradual_throttle-1.0.1-py3-none-any.whl/django_gradual_throttle/__init__.py
@@ -0,0 +1,10 @@
+"""
+Django Gradual Throttle - A Django middleware for gradual request throttling.
+"""
+
+__version__ = '1.0.0'
+__author__ = 'Dhruv Patel'
+__email__ = 'pateldhruvn2004@gmail.com'
+
+default_app_config = 'django_gradual_throttle.apps.DjangoGradualThrottleConfig'
+
