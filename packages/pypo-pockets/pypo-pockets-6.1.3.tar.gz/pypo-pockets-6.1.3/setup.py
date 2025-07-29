5162b7a931a7bd3960c9909ef73cc31227181c41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:06:23 +0000

    Add package pypo-pockets/6.1.3/pypo-pockets-6.1.3.tar.gz

---
+++ packages/pypo-pockets/pypo-pockets-6.1.3.tar.gz/pypo-pockets-6.1.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pypo-pockets",
+    version="6.1.3",
+    packages=["pypo-pockets"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
