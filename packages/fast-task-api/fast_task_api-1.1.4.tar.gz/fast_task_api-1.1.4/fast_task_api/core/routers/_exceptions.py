e5ffdf5385a81b3cc7185489420aa860746de101
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:06:10 +0000

    Add package fast-task-api/1.1.4/fast_task_api-1.1.4.tar.gz

---
+++ packages/fast-task-api/fast_task_api-1.1.4.tar.gz/fast_task_api-1.1.4/fast_task_api/core/routers/_exceptions.py
@@ -0,0 +1,21 @@
+
+class JobException(Exception):
+    """
+    If you raise an JobException the global exception handler will catch it and return a a failed job result.
+    Only use this exception for errors that are not related to the job function.
+    For example a file upload failed or other problem.
+    This prevents causing an internal server error.
+    """
+    pass
+
+
+class FileUploadException(JobException):
+    def __init__(self, file_name: str = "", message: str = "File upload failed"):
+        self.file_name = file_name
+
+        if file_name:
+            message = f"FileUploadException: {file_name} - {message}"
+        else:
+            message = f"FileUploadException: {message}"
+
+        super().__init__(message)
