d83269130c2814ba5d76d59262aef954d8f573b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:29 +0000

    Add package profile-sync/1.8.3/profile-sync-1.8.3.tar.gz

---
+++ packages/profile-sync/profile-sync-1.8.3.tar.gz/profile-sync-1.8.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="profile-sync",
+    version="1.8.3",
+    packages=["profile-sync"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
