a020d58f2e046c0974fe917a0b487bcc3cecb296
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:10 +0000

    Add package nested-admin/0.0.1/nested-admin-0.0.1.tar.gz

---
+++ packages/nested-admin/nested-admin-0.0.1.tar.gz/nested-admin-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="nested-admin",
+    version="0.0.1",
+    packages=["nested-admin"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
