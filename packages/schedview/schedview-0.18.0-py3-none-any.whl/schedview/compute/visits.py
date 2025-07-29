823fc456d82c0c6af7ea2a60093d76d2a071c5c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:06:15 +0000

    Add package schedview/0.18.0/schedview-0.18.0-py3-none-any.whl

---
+++ packages/schedview/schedview-0.18.0-py3-none-any.whl/schedview/compute/visits.py
@@ -0,0 +1,292 @@
+import datetime
+import warnings
+
+import numpy as np
+from astropy.time import Time
+from rubin_scheduler.site_models import SeeingModel
+
+import schedview.compute
+from schedview import band_column
+
+
+def add_day_obs(visits):
+    """Add day_obs columns to a visits DataFrame.
+
+    Parameter
+    ---------
+    `visits` : `pandas.DataFrame`
+        The DataFrame of visits to which to add day_obs columns
+
+    Returns
+    -------
+    `visits` : `pandas.DataFrame`
+        The modified DataFRame with additonal columns: day_obs_date,
+        day_obs_mjd, and day_obs_iso8601.
+    """
+    day_obs_mjd = np.floor(visits["observationStartMJD"] - 0.5).astype("int")
+    day_obs_datetime = Time(day_obs_mjd, format="mjd").datetime
+    day_obs_date = [datetime.date(t.year, t.month, t.day) for t in day_obs_datetime]
+    day_obs_iso8601 = tuple(str(d) for d in day_obs_date)
+    visits.insert(1, "day_obs_mjd", day_obs_mjd)
+    visits.insert(2, "day_obs_date", day_obs_date)
+    visits.insert(3, "day_obs_iso8601", day_obs_iso8601)
+    return visits
+
+
+def add_coords_tuple(visits):
+    """Add a coord tuple to a visits DataFrame.
+
+    Parameter
+    ---------
+    `visits` : `pandas.DataFrame`
+        The DataFrame of visits to which to add day_obs columns
+
+    Returns
+    -------
+    `visits` : `pandas.DataFrame`
+        The modified DataFrame with a 'coords' column that has an RA, dec tuple
+    """
+    coord_column = max(tuple(visits.columns).index("fieldRA"), tuple(visits.columns).index("fieldDec")) + 1
+    visits.insert(coord_column, "coords", list(zip(visits["fieldRA"], visits["fieldDec"])))
+    return visits
+
+
+def add_maf_metric(visits, metric, column_name, constraint=None, nighbor_column=None):
+    """Add a t_eff column to a visits DataFrame.
+
+    Parameter
+    ---------
+    `visits` : `pandas.DataFrame`
+        The DataFrame of visits to which to add day_obs columns
+    `metric` : `rubin_sim.maf.metrics.BaseMetric`
+        The metric to compute.
+    `column_name` : `str`
+        The name for the column with the metric value.
+    `constraint` : `str`
+        The conditions used to load the visits.
+
+    Returns
+    -------
+    `visits` : `pandas.DataFrame`
+        The modified DataFrame with a t_eff column.
+    """
+    if nighbor_column is None:
+        col_index = len(visits.columns)
+    else:
+        col_index = tuple(visits.columns).index(nighbor_column)
+
+    value = schedview.compute.compute_metric_by_visit(visits, metric, constraint)
+    visits.insert(col_index, column_name, value)
+    return visits
+
+
+def add_overhead(visits):
+    """Add columns with overhead between exposures to a visits DataFrame.
+
+    Parameter
+    ---------
+    `visits` : `pandas.DataFrame`
+        The DataFrame of visits to which to add day_obs columns
+
+    Returns
+    -------
+    `visits` : `pandas.DataFrame`
+        The modified DataFrame with additonal columns: overhead (in seconds)
+        and previous_filter.
+    """
+    overhead = (
+        visits["observationStartMJD"].diff() * 24 * 60 * 60
+        - visits["visitTime"].shift(1)
+        + visits["visitTime"]
+        - visits["visitExposureTime"]
+    )
+    slew_time_col_index = tuple(visits.columns).index("slewTime")
+    visits.insert(slew_time_col_index + 1, "overhead", overhead)
+
+    filter_col_index = tuple(visits.columns).index(band_column(visits))
+    previous_filter = visits[band_column(visits)].shift(1)
+    visits.insert(filter_col_index + 1, "previous_filter", previous_filter)
+    return visits
+
+
+def compute_overhead_summary(visits, sun_n12_setting, sun_n12_rising):
+    """Create a dictionary of overhead summary stats.
+
+    Parameters
+    ----------
+    `visits` : `pandas.DataFrame`
+        The table of visits, with overhead data (see add_overhead)
+    `sun_n12_setting`: `float`
+        The MJD of evening twilight.
+    `sun_n12_rising`: `float`
+        The MJD of morning twilight.
+
+    Returns
+    -------
+    `summary` : `dict`
+        A dictionary of summary statistics
+    """
+    visit_start = visits["observationStartMJD"]
+    visit_end = visit_start + visits["visitTime"] / (24 * 60 * 60)
+
+    relative_start_time = (visit_start.min() - sun_n12_setting) * 60 * 24
+    relative_end_time = (visit_end.max() - sun_n12_rising) * 60 * 24
+    total_time = (visit_end.max() - visit_start.min()) * 24
+    num_exposures = len(visits)
+    total_exptime = visits.visitExposureTime.sum() / (60 * 60)
+    mean_gap_time = 60 * 60 * (total_time - total_exptime) / (num_exposures - 1)
+    median_gap_time = visits.overhead.median()
+
+    summary = {
+        "relative_start_time": relative_start_time,
+        "relative_end_time": relative_end_time,
+        "total_time": total_time,
+        "num_exposures": num_exposures,
+        "total_exptime": total_exptime,
+        "mean_gap_time": mean_gap_time,
+        "median_gap_time": median_gap_time,
+    }
+
+    return summary
+
+
+def add_instrumental_fwhm(visits):
+    """Add columns with overhead between exposures to a visits DataFrame.
+
+    Parameter
+    ---------
+    `visits` : `pandas.DataFrame`
+        The DataFrame of visits to which to add day_obs columns
+
+    Returns
+    -------
+    `visits` : `pandas.DataFrame`
+        The modified DataFRame with additonal columns: overhead (in seconds)
+        and previous_filter.
+    """
+    # Get a seeing model that applies atmospheric and wavelength corrections,
+    # but not instrumental contributions.
+    seeing_model = SeeingModel(telescope_seeing=0.0, optical_design_seeing=0.0, camera_seeing=0.0)
+    seeing_indx_dict = {b: i for i, b in enumerate(seeing_model.filter_list)}
+
+    noninst_seeing = np.array(
+        tuple(
+            seeing_model(v.seeingFwhm500, v.airmass)["fwhmEff"][
+                seeing_indx_dict[v[band_column(visits)]]
+            ].item()
+            for i, v in visits.iterrows()
+        )
+    )
+
+    inst_fwhm = np.sqrt(visits["seeingFwhmEff"] ** 2 - noninst_seeing**2)
+    seeing_col_index = tuple(visits.columns).index("seeingFwhmEff")
+    visits.insert(seeing_col_index, "inst_fwhm", inst_fwhm)
+
+    return visits
+
+
+def accum_stats_by_target_band_night(visits):
+    """Create a DataFrame of accumulated statistics by target/band/night.
+
+    Parameters
+    ----------
+    visits : `pandas.DataFrame`
+        A DataFrame of visits with (at least) the following columns:
+
+        ``"day_obs_iso8601"``
+            The day_obs (as defined in SITCOMTN-32) in YYYY-MM-DD format
+            (`str`),
+        ``"teff"``
+            The effective exposure time (`float`).
+        ``band_column(visits)``
+            The filter (`str`).
+        ``"target_name"``
+            The target name (`str`).
+
+    Returns
+    -------
+    accum_stats : `pandas.DataFrame`
+        A `pandas.DataFrame` with the `target` as its index, `day_obs_iso8601`
+        as its first column, and a multi-level index of other stats by band.
+    """
+    day_obs_col = "day_obs_iso8601"
+    teff_col = "t_eff"
+
+    if day_obs_col not in visits:
+        raise ValueError(
+            f"{day_obs_col} column not found for visits; use the rubin_sim.maf.stackers.DayObsISOStacker."
+        )
+
+    if teff_col not in visits:
+        warnings.warn(f"{teff_col} column not found for visits; use the rubin_sim.maf.stackers.TeffStacker.")
+
+    agg_func = {
+        teff_col: "sum",
+        "start_timestamp": "min",
+        "count": "sum",
+        "visitExposureTime": "sum",
+        "fiveSigmaDepth": "median",
+        "skyBrightness": "median",
+        "sunAlt": "median",
+        "moonAlt": "median",
+        "moonPhase": "median",
+        "moonDistance": "median",
+        "airmass": "median",
+        "seeingFwhmEff": "median",
+        "cloud": "median",
+    }
+    # Get rid of columns we do not have in the visits
+    for column in list(agg_func):
+        if column not in ["count"] + list(visits.columns):
+            del agg_func[column]
+
+    accum_stats = (
+        visits.assign(count=1)
+        .groupby(["target_name", day_obs_col, band_column(visits)])
+        .agg(agg_func)
+        .reset_index()
+    )
+
+    accum_stats = (
+        accum_stats.pivot(
+            index=["target_name", day_obs_col], columns=band_column(visits), values=agg_func.keys()
+        )
+        .reset_index()
+        .set_index(["target_name", day_obs_col])
+    )
+
+    # If there are no visits for a given night, the t_eff and count are 0
+    for band in visits[band_column(visits)].unique():
+        if teff_col in visits:
+            accum_stats.loc[:, (teff_col, band)] = accum_stats.loc[:, (teff_col, band)].fillna(0)
+        accum_stats.loc[:, ("count", band)] = accum_stats.loc[:, ("count", band)].fillna(0)
+
+    return accum_stats
+
+
+def accum_teff_by_night(visits):
+    """Create a DataFrame of accumulated t_eff by target/band/night.
+
+    Parameters
+    ----------
+    visits : `pandas.DataFrame`
+        A DataFrame of visits with (at least) the following columns:
+
+        ``"day_obs_iso8601"``
+            The day_obs (as defined in SITCOMTN-32) in YYYY-MM-DD format
+            (`str`),
+        ``"teff"``
+            The effective exposure time (`float`).
+        ``band_column(visits)``
+            The filter (`str`).
+        ``"target_name"``
+            The target name (`str`).
+
+    Returns
+    -------
+    accum_teff : `pandas.DataFrame`
+        A `pandas.DataFrame` with the `target` as its index, `day_obs_iso8601`
+        as its first column, columns with the t_eff for each band.
+    """
+    stats_by_night = accum_stats_by_target_band_night(visits)
+    return stats_by_night.loc[:, "t_eff"]
---
+++ packages/schedview/schedview-0.18.0-py3-none-any.whl/schedview/plot/visits.py
@@ -0,0 +1,452 @@
+from typing import Any, Optional
+
+import bokeh
+import bokeh.io
+import bokeh.layouts
+import bokeh.models
+import bokeh.plotting
+import bokeh.transform
+import hvplot
+import numpy as np
+import pandas as pd
+from astropy.time import Time
+
+# Imported to help sphinx make the link
+from rubin_scheduler.scheduler.model_observatory import ModelObservatory  # noqa F401
+
+import schedview.compute.astro
+from schedview import band_column
+from schedview.collect import read_opsim
+
+from .colors import make_band_cmap
+
+
+def visits_tooltips(weather: bool = False) -> list:
+    deg = "\u00b0"
+    tooltips = [
+        (
+            "Start time",
+            "@start_timestamp{%F %T} UTC (mjd=@observationStartMJD{00000.00}, LST=@observationStartLST"
+            + deg
+            + ")",
+        ),
+        ("flush by mjd", "@flush_by_mjd{00000.00}"),
+        ("Scheduler note", "@scheduler_note"),
+        ("Band", "@band"),
+        (
+            "Field coordinates",
+            "RA=@fieldRA" + deg + ", Decl=@fieldDec" + deg + ", Az=@azimuth" + deg + ", Alt=@altitude" + deg,
+        ),
+        ("Parallactic angle", "@paraAngle" + deg),
+        ("Rotator angle", "@rotTelPos" + deg),
+        ("Rotator angle (backup)", "@rotTelPos_backup" + deg),
+        ("Cumulative telescope azimuth", "@cummTelAz" + deg),
+        ("Airmass", "@airmass"),
+        ("Moon distance", "@moonDistance" + deg),
+        (
+            "Moon",
+            "RA=@sunRA"
+            + deg
+            + ", Decl=@sunDec"
+            + deg
+            + ", Az=@sunAz"
+            + deg
+            + ", Alt=@sunAlt"
+            + deg
+            + ", phase=@moonPhase"
+            + deg,
+        ),
+        (
+            "Sun",
+            "RA=@moonRA"
+            + deg
+            + ", Decl=@moonDec"
+            + deg
+            + ", Az=@moonAz"
+            + deg
+            + ", Alt=@moonAlt"
+            + deg
+            + ", elong=@solarElong"
+            + deg,
+        ),
+        ("Sky brightness", "@skyBrightness mag arcsec^-2"),
+        ("Exposure time", "@visitExposureTime seconds (@numExposures exposures)"),
+        ("Visit time", "@visitTime seconds"),
+        ("Slew distance", "@slewDistance" + deg),
+        ("Slew time", "@slewTime seconds"),
+        ("Field ID", "@fieldId"),
+        ("Proposal ID", "@proposalId"),
+        ("Block ID", "@block_id"),
+        ("Scripted ID", "@scripted_id"),
+    ]
+
+    if weather:
+        tooltips += [
+            (
+                "Seeing",
+                '@seeingFwhm500" (500nm), @seeingFwhmEff" (Eff), @seeingFwhmGeom" (Geom)',
+            ),
+            ("Cloud", "@cloud"),
+            ("5-sigma depth", "@fiveSigmaDepth"),
+        ]
+
+    return tooltips
+
+
+def plot_visits(visits):
+    """Instantiate an explorer to interactively examine a set of visits.
+
+    Parameters
+    ----------
+    visits : `pandas.DataFrame`
+        One row per visit, as created by `schedview.collect.read_opsim`
+
+    Returns
+    -------
+    figure : `hvplot.ui.hvDataFrameExplorer`
+        The figure itself.
+    """
+    visit_explorer = hvplot.explorer(
+        visits, kind="scatter", x="start_timestamp", y="airmass", by=["scheduler_note"]
+    )
+    return visit_explorer
+
+
+def create_visit_explorer(visits, night_date, observatory=None, timezone="Chile/Continental"):
+    """Create an explorer to interactively examine a set of visits.
+
+    Parameters
+    ----------
+    visits : `str` or `pandas.DataFrame`
+        One row per visit, as created by `schedview.collect.read_opsim`,
+        or the name of a file from which such visits should be loaded.
+    night_date : `datetime.date`
+        The calendar date in the evening local time.
+    observatory : `ModelObservatory`, optional
+        Provides the location of the observatory, used to compute
+        night start and end times.
+        By default None.
+    timezone : `str`, optional
+        _description_, by default "Chile/Continental"
+
+    Returns
+    -------
+    figure : `hvplot.ui.hvDataFrameExplorer`
+        The figure itself.
+    data : `dict`
+        The arguments used to produce the figure using
+        `plot_visits`.
+    """
+    site = None if observatory is None else observatory.location
+    night_events = schedview.compute.astro.night_events(night_date=night_date, site=site, timezone=timezone)
+    start_time = Time(night_events.loc["sunset", "UTC"])
+    end_time = Time(night_events.loc["sunrise", "UTC"])
+
+    # Collect
+    if isinstance(visits, str):
+        visits = read_opsim(visits, Time(start_time).iso, Time(end_time).iso)
+
+    # Plot
+    data = {"visits": visits}
+    visit_explorer = plot_visits(visits)
+
+    return visit_explorer, data
+
+
+def plot_visit_param_vs_time(
+    visits: pd.DataFrame,
+    column_name: str,
+    plot: bokeh.plotting.figure | None = None,
+    show_column_selector: bool = False,
+    show_sim_selector: bool = False,
+    hovertool: bool = True,
+    color_transform: bokeh.models.mappers.CategoricalColorMapper | None = None,
+    marker_transform: bokeh.models.mappers.CategoricalMarkerMapper | None = None,
+    **kwargs,
+) -> bokeh.models.ui.ui_element.UIElement:
+    """Plot a column in the visit table vs. time.
+
+    Parameters
+    ----------
+    visits: `pandas.DataFrame`
+        One row per visit, as created by `schedview.collect.opsim.read_opsim`.
+    column_name: `str`
+        The name of the column to plot against time.
+    plot: `bokeh.plotting.figure` or None
+        The figure on which to plot the visits. None creates a new
+        figure. Defaults to None.
+    show_column_selector: `bool`
+        Include a drop-down to select which column to plot?
+        Defaults to False.
+    color_transform: `CategoricalColorMapper` or None
+        The color mapper from band to color.
+    marker_transform: `CategoricalMarkerMapper` or None
+        The marker mapper from label to marker.
+    **kwargs
+        Additional keyword arguments to be passed to
+        `bokeh.plotting.figure.scatter`.
+
+    Returns
+    -------
+    `plot` : `bokeh.models.plots.Plot`
+        The figure with the plot.
+    """
+    if plot is None:
+        plot = bokeh.plotting.figure(y_axis_label=column_name, x_axis_label="Time (UTC)")
+
+    if len(visits) == 0:
+        plot.text(x=0.5, y=0.5, text="No visits.")
+        return plot
+
+    # Make mypy happy
+    assert isinstance(plot, bokeh.plotting.figure)
+
+    if isinstance(visits, bokeh.models.ColumnarDataSource):
+        source = visits
+    else:
+        source = bokeh.models.ColumnDataSource(visits)
+
+    if "label" not in source.data:
+        source.data["label"] = np.full_like(source.data["start_timestamp"], "")
+
+    if "sim_alpha" not in source.data:
+        source.data["sim_alpha"] = np.full_like(source.data["start_timestamp"], 1.0, dtype=np.float64)
+
+    scatter_kwargs = {"fill_alpha": 0.0, "line_alpha": "sim_alpha"}
+
+    if color_transform is None:
+        scatter_kwargs["color"] = make_band_cmap(band_column(visits))
+    else:
+        scatter_kwargs["color"] = bokeh.transform.factor_cmap(
+            band_column(visits), color_transform.palette, color_transform.factors
+        )
+
+    if marker_transform is not None:
+        scatter_kwargs["marker"] = {"field": "label", "transform": marker_transform}
+
+    scatter_kwargs.update(kwargs)
+
+    timeline = plot.scatter(
+        x="start_timestamp",
+        y=column_name,
+        source=source,
+        **scatter_kwargs,
+    )
+
+    plot.xaxis[0].formatter = bokeh.models.DatetimeTickFormatter(hours="%H:%M")
+
+    # Bokeh's fully automatic legend creation can't make legends for
+    # color and marker independently. So, follow
+    # https://discourse.bokeh.org/t/representing-data-with-two-categories-by-both-color-and-marker-shape/4122/4
+    # Plot some fake points with colors, make them invisible,
+    # and make a legend for them.
+    # Then, do something analogous for makers (if needed).
+
+    # Generate placement for sample points.
+    # This needs to be in the plot to avoid messing up the axis limits,
+    # but otherwise does not matter, because the points will be invisible.
+    # Do not use data to get the sample y value, because the user can
+    # change which data is getting plotted. Use a nan instead, which
+    # will be ignored by the limit calculation.
+    sample_x, sample_y = visits["start_timestamp"].iloc[0], np.nan
+
+    # Create an invisible renderer to drive color legend:
+    sample_color_renderer = plot.rect(
+        x=sample_x,
+        y=sample_y,
+        height=1,
+        width=1,
+        fill_alpha=1,
+        line_alpha=1,
+        color=scatter_kwargs["color"].transform.palette,
+    )
+    sample_color_renderer.visible = False
+
+    legend = bokeh.models.Legend(
+        items=[
+            bokeh.models.LegendItem(
+                label=scatter_kwargs["color"].transform.factors[i], renderers=[sample_color_renderer], index=i
+            )
+            for i in range(len(scatter_kwargs["color"].transform.factors))
+        ],
+        location="bottom_center",
+        orientation="horizontal",
+    )
+    plot.add_layout(legend, "below")
+
+    if isinstance(marker_transform, bokeh.models.CategoricalMarkerMapper):
+        # create an invisible renderer to drive shape legend
+        sample_marker_renderer = plot.scatter(
+            x=sample_x,
+            y=sample_y,
+            fill_alpha=0.0,
+            line_alpha=1.0,
+            color="black",
+            marker=marker_transform.markers,
+        )
+        sample_marker_renderer.visible = False
+
+        legend = bokeh.models.Legend(
+            items=[
+                bokeh.models.LegendItem(
+                    label=marker_transform.factors[i], renderers=[sample_marker_renderer], index=i
+                )
+                for i in range(len(marker_transform.factors))
+            ],
+            location="bottom_left",
+            orientation="vertical",
+        )
+        plot.add_layout(legend, "below")
+
+    if hovertool:
+        hover_tool = bokeh.models.HoverTool(
+            renderers=[timeline], tooltips=visits_tooltips(), formatters={"@start_timestamp": "datetime"}
+        )
+        plot.add_tools(hover_tool)
+
+    if show_column_selector:
+        ignored_columns = {"level_0", "index", "sim_alpha", "sim_index"}
+        # Only offer numeric fields as options
+        options = []
+        # Use a loop instead of a list comprehension to make it easier
+        # to appease mypy
+        for k in sorted(set(source.column_names) - ignored_columns):
+            column_data = source.data[k]
+            assert isinstance(column_data, np.ndarray)
+            if np.issubdtype(column_data.dtype, np.number):
+                options.append(k)
+
+        column_selector = bokeh.models.Select(value=column_name, options=options, name="visitcolselect")
+
+        column_selector_callback = bokeh.models.CustomJS(
+            args={"timeline": timeline, "source": source, "yaxis": plot.yaxis[0]},
+            code="""
+                timeline.glyph.y.field = this.value
+                yaxis.axis_label = this.value
+                source.change.emit()
+            """,
+        )
+        column_selector.js_on_change("value", column_selector_callback)
+
+    if show_sim_selector:
+        if "label" not in source.column_names:
+            raise ValueError("A sim selector needs the label column")
+        options = ["All"] + list(o for o in np.unique(source.data["label"]) if o != "Completed")
+        default_sim = "All"
+        sim_selector = bokeh.models.Select(value=default_sim, options=options, name="simselect")
+
+        sim_selector_callback = bokeh.models.CustomJS(
+            args={"source": source},
+            code="""
+                for (let i = 0; i < source.data['label'].length; i++) {
+                    if (source.data['label'][i] === 'Completed') {
+                        source.data['sim_alpha'][i] = 1.0;
+                    } else if (['All', source.data['label'][i]].includes(this.value)) {
+                        source.data['sim_alpha'][i] = 0.8;
+                    } else {
+                        source.data['sim_alpha'][i] = 0.0;
+                    }
+                }
+                source.change.emit()
+            """,
+        )
+        sim_selector.js_on_change("value", sim_selector_callback)
+
+    if show_column_selector or show_sim_selector:
+        selector_row_contents = []
+        if show_column_selector:
+            selector_row_contents.append(column_selector)
+        if show_sim_selector:
+            selector_row_contents.append(sim_selector)
+        ui_element = bokeh.layouts.column([bokeh.layouts.row(selector_row_contents), plot])
+    else:
+        ui_element = plot
+
+    return ui_element
+
+
+def create_visit_table(
+    visits: pd.DataFrame | bokeh.models.ColumnarDataSource,
+    visible_column_names: list[str] = [
+        "observationId",
+        "observationStartMJD",
+        "fieldRA",
+        "fieldDec",
+        "band",
+    ],
+    show: bool = True,
+    **data_table_kwargs: Optional[Any],
+) -> bokeh.models.ui.ui_element.UIElement:
+    """Create an interactive table of visits.
+
+    Parameters
+    ----------
+    visits : `pd.DataFrame` or `bokeh.models.ColumnarDataSource`
+        The visits to include in the table
+    visible_column_names : `list[str]`
+        The columns to display, by default
+        ['observationId', 'observationStartMJD',
+        'fieldRA', 'fieldDec', 'band']
+    show : `bool`
+        Show the plot?, by default True
+
+    Returns
+    -------
+    element : `bokeh.models.ui.ui_element.UIElement`
+        The bokeh UI element with the table.
+    """
+
+    data = (
+        visits
+        if isinstance(visits, bokeh.models.ColumnarDataSource)
+        else bokeh.models.ColumnDataSource(visits)
+    )
+
+    date_columns = []
+    date_colname = (
+        "start_timestamp" if "start_timestamp" in data.column_names else "observationStartDatetime64"
+    )
+    if date_colname in data.column_names:
+        date_columns = [
+            bokeh.models.TableColumn(
+                field="start_timestamp",
+                title="UTC Time",
+                formatter=bokeh.models.DateFormatter(format="%H:%M:%S"),
+            )
+        ]
+        visible_column_names.insert(0, date_colname)
+
+    data_columns = [
+        bokeh.models.TableColumn(field=cn, title=cn, name=f"tablecol{cn}", visible=cn in visible_column_names)
+        for cn in data.column_names
+        if cn != date_colname
+    ]
+    columns = date_columns + data_columns
+    visit_table = bokeh.models.DataTable(
+        source=data, columns=columns, name="visit_table", **data_table_kwargs
+    )
+    multi_choice = bokeh.models.MultiChoice(value=visible_column_names, options=data.column_names, height=128)
+
+    table_update_callback = bokeh.models.CustomJS(
+        args={"multi_choice": multi_choice, "visit_table": visit_table},
+        code="""
+            for (var col_idx=0; col_idx<visit_table.columns.length; col_idx++) {
+                visit_table.columns[col_idx].visible = false
+                for (var choice_idx=0; choice_idx<multi_choice.value.length; choice_idx++) {
+                    if (visit_table.columns[col_idx].field == multi_choice.value[choice_idx]) {
+                        visit_table.columns[col_idx].visible = true
+                    }
+                }
+            }
+            visit_table.change.emit()
+        """,
+    )
+
+    multi_choice.js_on_change("value", table_update_callback)
+
+    ui_element = bokeh.layouts.column([multi_choice, visit_table])
+
+    if show:
+        bokeh.io.show(ui_element)
+
+    return ui_element
