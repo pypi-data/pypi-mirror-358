1b4c0169b63643bb6c905515732910510085fdd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:40 +0000

    Add package fastddm/0.3.14/fastddm-0.3.14-cp311-cp311-macosx_11_0_arm64.whl

---
+++ packages/fastddm/fastddm-0.3.14-cp311-cp311-macosx_11_0_arm64.whl/fastddm/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+import sys
+
+if sys.version_info < (3, 9):
+    sys.exit("Error, Python < 3.9 is not supported.")
+
+setup(
+    name="fastddm",
+    use_scm_version={
+        "root": "/Users/runner/work/fastddm/fastddm",
+        "fallback_version": "0.3.14",
+    },
+    setup_requires=["setuptools_scm"],
+    packages=["fastddm"],
+    package_dir={"fastddm": "/Users/runner/work/fastddm/fastddm/build/temp.macosx-11.0-arm64-cpython-311/src/python/fastddm"},
+    package_data={"fastddm": ["_core.*", "_core_cuda.*"]},
+)
