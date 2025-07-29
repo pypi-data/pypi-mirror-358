90532226dd6a475d4122bb0637dc4f77c9a5412e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:06:17 +0000

    Add package pypo-auth/7.0.2/pypo-auth-7.0.2.tar.gz

---
+++ packages/pypo-auth/pypo-auth-7.0.2.tar.gz/pypo-auth-7.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pypo-auth",
+    version="7.0.2",
+    packages=["pypo-auth"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
