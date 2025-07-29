01e08a6accd6bf99ed773873e1a07cb02fc500da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:56 +0000

    Add package mras-pymrasd/0.1.5/mras-pymrasd-0.1.5.tar.gz

---
+++ packages/mras-pymrasd/mras-pymrasd-0.1.5.tar.gz/mras-pymrasd-0.1.5/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="mras-pymrasd",
+    version="0.1.5",
+    packages=["mras-pymrasd"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
