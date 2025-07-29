2d73db1f2dd0c7585684958a95e1dd72e2433c2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:18:19 +0000

    Add package ophyd-async/0.11/ophyd_async-0.11-py3-none-any.whl

---
+++ packages/ophyd-async/ophyd_async-0.11-py3-none-any.whl/ophyd_async/plan_stubs/__init__.py
@@ -0,0 +1,32 @@
+"""Plan stubs for connecting, setting up and flying devices."""
+
+from ._ensure_connected import ensure_connected
+from ._fly import (
+    fly_and_collect,
+    prepare_static_seq_table_flyer_and_detectors_with_same_trigger,
+    time_resolved_fly_and_collect_with_static_seq_table,
+)
+from ._nd_attributes import setup_ndattributes, setup_ndstats_sum
+from ._panda import apply_panda_settings
+from ._settings import (
+    apply_settings,
+    apply_settings_if_different,
+    get_current_settings,
+    retrieve_settings,
+    store_settings,
+)
+
+__all__ = [
+    "fly_and_collect",
+    "prepare_static_seq_table_flyer_and_detectors_with_same_trigger",
+    "time_resolved_fly_and_collect_with_static_seq_table",
+    "ensure_connected",
+    "setup_ndattributes",
+    "setup_ndstats_sum",
+    "apply_panda_settings",
+    "apply_settings",
+    "apply_settings_if_different",
+    "get_current_settings",
+    "retrieve_settings",
+    "store_settings",
+]
