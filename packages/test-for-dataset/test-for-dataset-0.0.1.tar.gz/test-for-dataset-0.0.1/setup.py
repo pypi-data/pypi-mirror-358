6720b496b76f567415abb92435ab435aa7b30cff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:22 +0000

    Add package test-for-dataset/0.0.1/test-for-dataset-0.0.1.tar.gz

---
+++ packages/test-for-dataset/test-for-dataset-0.0.1.tar.gz/test-for-dataset-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="test-for-dataset",
+    version="0.0.1",
+    packages=["test-for-dataset"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
