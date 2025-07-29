acae2c61203c5bf17a699252e8d30baa9d912fc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:42 +0000

    Add package jdal/0.39.1/jdal-0.39.1.tar.gz

---
+++ packages/jdal/jdal-0.39.1.tar.gz/jdal-0.39.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="jdal",
+    version="0.39.1",
+    packages=["jdal"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
