8392d34b0be040b7782aea92b05226651fa30ba8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:04:27 +0000

    Add package tum_esm_utils/2.7.8/tum_esm_utils-2.7.8.tar.gz

---
+++ packages/tum_esm_utils/tum_esm_utils-2.7.8.tar.gz/tum_esm_utils-2.7.8/tum_esm_utils/__init__.py
@@ -0,0 +1,47 @@
+"""Python utilities by the Professorship of Environmental
+Sensing and Modeling at the Technical University of Munich.
+
+GitHub Repository https://github.com/tum-esm/utils
+Documentation: https://tum-esm-utils.netlify.app
+PyPI: https://pypi.org/project/tum-esm-utils"""
+
+from . import (
+    code,
+    datastructures,
+    decorators,
+    files,
+    mathematics,
+    processes,
+    shell,
+    system,
+    text,
+    timing,
+    validators,
+)
+
+# ignore import errors from the following submodules
+# because they requires extras to be installed
+
+# requires extra "em27"
+try:
+    from . import em27
+except ImportError:
+    pass
+
+# requires extra "plotting"
+try:
+    from . import plotting
+except ImportError:
+    pass
+
+# requires extra "opus"
+try:
+    from . import opus
+except ImportError:
+    pass
+
+# requires extra "modeling"
+try:
+    from . import column
+except ImportError:
+    pass
