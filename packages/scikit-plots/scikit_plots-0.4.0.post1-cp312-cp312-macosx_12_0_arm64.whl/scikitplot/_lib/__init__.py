4bf29dd3f20594c5fee69183db4377258524f43b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:30 +0000

    Add package scikit-plots/0.4.0.post1/scikit_plots-0.4.0.post1-cp312-cp312-macosx_12_0_arm64.whl

---
+++ packages/scikit-plots/scikit_plots-0.4.0.post1-cp312-cp312-macosx_12_0_arm64.whl/scikitplot/_lib/__init__.py
@@ -0,0 +1,60 @@
+"""
+Module containing private utility functions
+===========================================
+
+This module serves as the core API for the `xp` array namespace,
+providing essential functionalities for array operations.
+::
+
+    from scikitplot import _lib
+    _lib.test()
+
+See Also
+--------
+array_api_compat :
+    Python array API standard https://data-apis.org/array-api/latest/index.html
+
+Core Features
+--------------
+- Create and manipulate arrays.
+- Perform mathematical and statistical operations.
+- Provide utility functions for array handling and processing.
+
+Behind `xp` functionality
+----------------------------
+
+The `_lib` module offers a simple and intuitive interface for users to interact
+with array data. Below are some key functionalities:
+
+1. **Array Creation**
+2. **Mathematical Operations**
+3. **Statistical Functions**
+4. **Array Manipulation**
+5. **Utilities**
+
+Notes
+-----
+- Ensure that the `xp` namespace is correctly imported before using the
+  functionalities within this module.
+- For more advanced usage, refer to the documentation of the individual
+  functions available in this module.
+"""
+
+# scikitplot/_lib/__init__.py
+
+# https://data-apis.org/array-api/latest/index.html
+# https://github.com/data-apis/array-api-compat/blob/main/array_api_compat/numpy/__init__.py
+try:
+    from . import _ccallback_c as _ccallback_c
+    from .array_api_compat import __version__ as __array_api_compat_version__  # xpc
+    from .array_api_compat.numpy import __array_api_version__
+    from .array_api_extra import __version__ as __array_api_extra_version__  # xpx
+except:
+    __array_api_version__ = "2023.12"
+    __array_api_compat_version__ = "1.10.1.dev0"
+    __array_api_extra_version__ = "0.5.1.dev0"
+
+from ._testutils import PytestTester
+
+test = PytestTester(__name__)
+del PytestTester
