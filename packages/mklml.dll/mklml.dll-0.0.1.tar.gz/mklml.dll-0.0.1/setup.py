44f0109154c368410202bf4447c88e30f30c42b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:02 +0000

    Add package mklml.dll/0.0.1/mklml.dll-0.0.1.tar.gz

---
+++ packages/mklml.dll/mklml.dll-0.0.1.tar.gz/mklml.dll-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="mklml.dll",
+    version="0.0.1",
+    packages=["mklml.dll"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
