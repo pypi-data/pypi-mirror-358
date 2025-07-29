21766b78c52b7647e5e8b2c50269e925fc021353
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:05:57 +0000

    Add package test-pymd/0.1.5/test_pymd-0.1.5.tar.gz

---
+++ packages/test-pymd/test_pymd-0.1.5.tar.gz/test_pymd-0.1.5/src/PYMD/_pymd.py
@@ -0,0 +1,15 @@
+from .database import Manager
+from chromologger import Logger as Log
+
+# Initial paths to files
+log:Log = Log('logs/log_pymd.log')
+
+class Pymd:
+    @staticmethod
+    def run_app():
+        try:
+            Manager.database_config()
+            from .interface import run
+            run()
+        except Exception as e:
+            log.log_e(e)
