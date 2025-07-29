d31de53d0caced6295bc737e1654ec7443702508
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:47 +0000

    Add package farmclient/0.1.0/farmclient-0.1.0.tar.gz

---
+++ packages/farmclient/farmclient-0.1.0.tar.gz/farmclient-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="farmclient",
+    version="0.1.0",
+    packages=["farmclient"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
