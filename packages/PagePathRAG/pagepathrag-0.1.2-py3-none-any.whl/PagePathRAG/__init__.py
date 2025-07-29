e413ee920d643eee282ba43507049deb3f98e00d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:06:25 +0000

    Add package PagePathRAG/0.1.2/pagepathrag-0.1.2-py3-none-any.whl

---
+++ packages/PagePathRAG/pagepathrag-0.1.2-py3-none-any.whl/PagePathRAG/__init__.py
@@ -0,0 +1,8 @@
+from .page_path_rag import PagePathRAG, PagePathRAGPrompt
+
+__version__ = "0.1.2"
+__author__ = "PagePathRAG Team"
+__email__ = "cnatom@icloud.com"
+__description__ = "基于PathRAG的页面路径检索增强生成系统 - 专门用于将UI交互路径转化为知识图谱并进行智能查询的Python库"
+
+__all__ = ["PagePathRAG", "PagePathRAGPrompt"]
