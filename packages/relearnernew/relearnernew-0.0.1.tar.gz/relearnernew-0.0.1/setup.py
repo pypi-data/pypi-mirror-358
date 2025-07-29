db565880a6eb2f7d7a643c735ce590e35da3519a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:06:03 +0000

    Add package relearnernew/0.0.1/relearnernew-0.0.1.tar.gz

---
+++ packages/relearnernew/relearnernew-0.0.1.tar.gz/relearnernew-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="relearnernew",
+    version="0.0.1",
+    packages=["relearnernew"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
