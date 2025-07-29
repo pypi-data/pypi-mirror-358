a03619f4e77dae1934203f526e701102dd7495ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:43 +0000

    Add package fstek/0.0.1/fstek-0.0.1.tar.gz

---
+++ packages/fstek/fstek-0.0.1.tar.gz/fstek-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="fstek",
+    version="0.0.1",
+    packages=["fstek"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
