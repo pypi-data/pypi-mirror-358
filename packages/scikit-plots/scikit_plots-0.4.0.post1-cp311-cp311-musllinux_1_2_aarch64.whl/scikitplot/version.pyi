1e248b323c1e1d9c32f5fe7e35be0f37d73107e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:29 +0000

    Add package scikit-plots/0.4.0.post1/scikit_plots-0.4.0.post1-cp311-cp311-musllinux_1_2_aarch64.whl

---
+++ packages/scikit-plots/scikit_plots-0.4.0.post1-cp311-cp311-musllinux_1_2_aarch64.whl/scikitplot/version.pyi
@@ -0,0 +1,34 @@
+# pylint: skip-file
+# ruff: noqa: PGH004
+# ruff: noqa
+# flake8: noqa
+# type: ignore
+# mypy: ignore-errors
+
+from typing import Final, LiteralString
+
+# from typing_extensions import LiteralString
+
+__all__ = (
+    "__git_hash__",
+    "__version__",
+    "_version",
+    "full_version",
+    "git_revision",
+    "release",
+    "short_git_revision",
+    "short_version",
+    "version",
+)
+__version__: Final[LiteralString]
+version: Final[LiteralString]
+full_version: Final[LiteralString]
+
+_version: Final[LiteralString]
+short_version: Final[LiteralString]
+
+__git_hash__: Final[LiteralString]
+git_revision: Final[LiteralString]
+short_git_revision: Final[LiteralString]
+
+release: Final[bool]
