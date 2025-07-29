f5a695d895f971147fe86d6057787d0dfa8318e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:06:04 +0000

    Add package wingpy/1.1.0/wingpy-1.1.0-py3-none-any.whl

---
+++ packages/wingpy/wingpy-1.1.0-py3-none-any.whl/wingpy/__init__.py
@@ -0,0 +1,31 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# Copyright (C) 2025 Wingmen Solutions ApS
+# This file is part of wingpy, distributed under the terms of the GNU GPLv3.
+# See the LICENSE, NOTICE, and AUTHORS files for more information.
+
+from importlib import metadata
+
+from wingpy.base import RestApiBaseClass
+from wingpy.cisco import (
+    CiscoAPIC,
+    CiscoCatalystCenter,
+    CiscoFMC,
+    CiscoHyperfabric,
+    CiscoISE,
+    CiscoMerakiDashboard,
+)
+from wingpy.generic import GenericRESTAPI
+
+__version__ = metadata.version("wingpy")
+
+__all__ = [
+    "CiscoAPIC",
+    "CiscoCatalystCenter",
+    "CiscoFMC",
+    "CiscoISE",
+    "GenericRESTAPI",
+    "CiscoHyperfabric",
+    "CiscoMerakiDashboard",
+    "RestApiBaseClass",
+]
