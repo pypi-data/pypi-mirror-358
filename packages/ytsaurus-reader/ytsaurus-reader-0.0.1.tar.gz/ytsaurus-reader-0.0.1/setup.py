6d156f1184330f69939145da83d947c8f6fdd0bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:52 +0000

    Add package ytsaurus-reader/0.0.1/ytsaurus-reader-0.0.1.tar.gz

---
+++ packages/ytsaurus-reader/ytsaurus-reader-0.0.1.tar.gz/ytsaurus-reader-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ytsaurus-reader",
+    version="0.0.1",
+    packages=["ytsaurus-reader"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
