e560a544ef2232bbfb46a1f28e1da6953b2552e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:22 +0000

    Add package awx-display-callback/0.0.1/awx-display-callback-0.0.1.tar.gz

---
+++ packages/awx-display-callback/awx-display-callback-0.0.1.tar.gz/awx-display-callback-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="awx-display-callback",
+    version="0.0.1",
+    packages=["awx-display-callback"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
