9f7aed1f19d3afc85955e701e3a40f0b8edbe292
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:57 +0000

    Add package syz-base-mac/0.0.5/syz_base_mac-0.0.5-py3-none-any.whl

---
+++ packages/syz-base-mac/syz_base_mac-0.0.5-py3-none-any.whl/syz_base_mac/pyarmor_runtime_000000/__init__.py
@@ -0,0 +1,11 @@
+"""PyArmor Runtime"""
+import os
+import sys
+
+# 获取当前目录
+current_dir = os.path.dirname(os.path.abspath(__file__))
+# 将 pyarmor_runtime.so 所在目录添加到 sys.path
+if current_dir not in sys.path:
+    sys.path.insert(0, current_dir)
+
+from pyarmor_runtime import __pyarmor__
