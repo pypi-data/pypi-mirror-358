4f5428d407cdf8f9977dd465eb393f515eb142df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:06:24 +0000

    Add package pypo-restframework/2.3.0/pypo-restframework-2.3.0.tar.gz

---
+++ packages/pypo-restframework/pypo-restframework-2.3.0.tar.gz/pypo-restframework-2.3.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pypo-restframework",
+    version="2.3.0",
+    packages=["pypo-restframework"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
