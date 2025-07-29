f160344a43d8c23b5ad191e07fae5933d489bea1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:47 +0000

    Add package dirtyfields/0.0.1/dirtyfields-0.0.1.tar.gz

---
+++ packages/dirtyfields/dirtyfields-0.0.1.tar.gz/dirtyfields-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dirtyfields",
+    version="0.0.1",
+    packages=["dirtyfields"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
