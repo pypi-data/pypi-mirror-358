a9efd413fa4a509d1df4a4d4323c5ed4d232239d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:29 +0000

    Add package reactions-access/5.2.2/reactions-access-5.2.2.tar.gz

---
+++ packages/reactions-access/reactions-access-5.2.2.tar.gz/reactions-access-5.2.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="reactions-access",
+    version="5.2.2",
+    packages=["reactions-access"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
