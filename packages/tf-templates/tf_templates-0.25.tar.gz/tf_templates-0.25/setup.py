910db546d7c8d0f3c32a1fa1d16851fb04a04e7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:05:46 +0000

    Add package tf-templates/0.25/tf_templates-0.25.tar.gz

---
+++ packages/tf-templates/tf_templates-0.25.tar.gz/tf_templates-0.25/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tf_templates",
+    version="0.25",
+    packages=find_packages(),
+    install_requires=["requests"],
+    author="YourName",
+    description="Notebook runtime diagnostics logger",
+    long_description="Some templates for tensorflow.",
+    long_description_content_type="text/markdown",
+    url="https://github.com/aitestpackage/tf-templates/new/main",
+    include_package_data=True,  # <- REQUIRED
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Framework :: Jupyter",
+        "Intended Audience :: Developers",
+    ],
+)
