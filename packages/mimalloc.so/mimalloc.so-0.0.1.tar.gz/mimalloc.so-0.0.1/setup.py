a9f3f216054591e1f9bdce109d84bf938d0506f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:02 +0000

    Add package mimalloc.so/0.0.1/mimalloc.so-0.0.1.tar.gz

---
+++ packages/mimalloc.so/mimalloc.so-0.0.1.tar.gz/mimalloc.so-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="mimalloc.so",
+    version="0.0.1",
+    packages=["mimalloc.so"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
