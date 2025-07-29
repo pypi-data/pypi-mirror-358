647d3a6bdd26707297acebb4994a273efed4ee33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:06:15 +0000

    Add package radosgw/0.0.1/radosgw-0.0.1.tar.gz

---
+++ packages/radosgw/radosgw-0.0.1.tar.gz/radosgw-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="radosgw",
+    version="0.0.1",
+    packages=["radosgw"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
