8e8e1d86caa52202153985244044f6abc462b9dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:06:15 +0000

    Add package jcip-annotations/1.0.post1/jcip-annotations-1.0-1.tar.gz

---
+++ packages/jcip-annotations/jcip-annotations-1.0-1.tar.gz/jcip-annotations-1.0-1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="jcip-annotations",
+    version="1.0-1",
+    packages=["jcip-annotations"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
