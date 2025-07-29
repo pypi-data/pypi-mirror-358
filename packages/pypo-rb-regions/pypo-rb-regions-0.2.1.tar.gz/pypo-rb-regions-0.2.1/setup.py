5d247d25b1bd571ecacd12e26b83dea7d9c841b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:06:23 +0000

    Add package pypo-rb-regions/0.2.1/pypo-rb-regions-0.2.1.tar.gz

---
+++ packages/pypo-rb-regions/pypo-rb-regions-0.2.1.tar.gz/pypo-rb-regions-0.2.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pypo-rb-regions",
+    version="0.2.1",
+    packages=["pypo-rb-regions"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
