879bb84e08edf7d94b269be9be799540aa0856c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:48 +0000

    Add package polars-bio/0.10.3/polars_bio-0.10.3-cp38-abi3-win_amd64.whl

---
+++ packages/polars-bio/polars_bio-0.10.3-cp38-abi3-win_amd64.whl/polars_bio/range_utils.py
@@ -0,0 +1,35 @@
+from typing import Union
+
+import bioframe as bf
+import pandas as pd
+import polars as pl
+from matplotlib import pyplot as plt
+
+
+class Utils:
+    @staticmethod
+    def visualize_intervals(
+        df: Union[pd.DataFrame, pl.DataFrame], label: str = "overlapping pair"
+    ) -> None:
+        """
+        Visualize the overlapping intervals.
+
+        Parameters:
+            df: Pandas DataFrame or Polars DataFrame. The DataFrame containing the overlapping intervals
+            label: TBD
+
+        """
+        assert isinstance(
+            df, (pd.DataFrame, pl.DataFrame)
+        ), "df must be a Pandas or Polars DataFrame"
+        df = df if isinstance(df, pd.DataFrame) else df.to_pandas()
+        for i, reg_pair in df.iterrows():
+            bf.vis.plot_intervals_arr(
+                starts=[reg_pair.start_1, reg_pair.start_2],
+                ends=[reg_pair.end_1, reg_pair.end_2],
+                colors=["skyblue", "lightpink"],
+                levels=[2, 1],
+                xlim=(0, 16),
+                show_coords=True,
+            )
+            plt.title(f"{label} #{i}")
