0014ed1ac0c57a4b610f7028839ea020fe3309be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:06:12 +0000

    Add package kubehadoop/0.0.1/kubehadoop-0.0.1.tar.gz

---
+++ packages/kubehadoop/kubehadoop-0.0.1.tar.gz/kubehadoop-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="kubehadoop",
+    version="0.0.1",
+    packages=["kubehadoop"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
