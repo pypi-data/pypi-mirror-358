d31026852c39a158780dcca23d5a54c668ab2282
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:05:32 +0000

    Add package chap-core/1.0.17/chap_core-1.0.17-py3-none-any.whl

---
+++ packages/chap-core/chap_core-1.0.17-py3-none-any.whl/chap_core/__init__.py
@@ -0,0 +1,11 @@
+"""Top-level package for chap-core."""
+
+__author__ = """Sandvelab"""
+__email__ = "knutdrand@gmail.com"
+__version__ = "1.0.17"
+__minimum_modelling_app_version__ = "1.1.0"
+from . import fetch
+from . import data
+from .models.model_template_interface import ModelTemplateInterface
+
+__all__ = ["fetch", "data", "ModelTemplateInterface"]
