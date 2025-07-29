6b74423d6b73306a3f93de5db337fe96e30b179c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:04:57 +0000

    Add package dbruntime/0.0.1/dbruntime-0.0.1.tar.gz

---
+++ packages/dbruntime/dbruntime-0.0.1.tar.gz/dbruntime-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dbruntime",
+    version="0.0.1",
+    packages=["dbruntime"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
