c11dc0748bb4d6615981dfe9f4b0f3f55a8dcaff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:06:35 +0000

    Add package biometry-inference/0.0.1/biometry-inference-0.0.1.tar.gz

---
+++ packages/biometry-inference/biometry-inference-0.0.1.tar.gz/biometry-inference-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="biometry-inference",
+    version="0.0.1",
+    packages=["biometry-inference"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
