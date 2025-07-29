3a23d33691662936dc5752f2189ee52b334aa657
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:14 +0000

    Add package voice-models/1.2.6/voice-models-1.2.6.tar.gz

---
+++ packages/voice-models/voice-models-1.2.6.tar.gz/voice-models-1.2.6/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="voice-models",
+    version="1.2.6",
+    packages=["voice-models"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
