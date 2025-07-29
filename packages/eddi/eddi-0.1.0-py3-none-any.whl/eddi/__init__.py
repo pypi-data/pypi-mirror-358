83f62173267311e49ffcf43582a53f8373c41b9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:57 +0000

    Add package eddi/0.1.0/eddi-0.1.0-py3-none-any.whl

---
+++ packages/eddi/eddi-0.1.0-py3-none-any.whl/eddi/__init__.py
@@ -0,0 +1,7 @@
+"""Eddi - Interactive lighting system with pose-driven control."""
+
+__version__ = "0.1.0"
+
+def hello():
+    """Minimal function to establish package works."""
+    return "Hello from Eddi!"
