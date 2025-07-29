121d1542f42e448628cbd237613dd2b5ec7e4d59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:06:31 +0000

    Add package quda-ml/0.1.8/quda_ml-0.1.8-py3-none-any.whl

---
+++ packages/quda-ml/quda_ml-0.1.8-py3-none-any.whl/quda/cli/app.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+---------------------------------------------
+Copyright (c) 2025 ZhangYundi
+Licensed under the MIT License. 
+Created on 2025/6/26 15:52
+Email: yundi.xxii@outlook.com
+Description: 应用层
+---------------------------------------------
+"""
+
+import typer
+
+def init_config():
+    from quda.ml import init_config
+
+    init_config()
+
