b895e56e64d5fb9d5d5c6a10a8935a00f939b160
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:07:19 +0000

    Add package fastddm/0.3.13/fastddm-0.3.13-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/fastddm/fastddm-0.3.13-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/fastddm/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+import sys
+
+if sys.version_info < (3, 9):
+    sys.exit("Error, Python < 3.9 is not supported.")
+
+setup(
+    name="fastddm",
+    use_scm_version={
+        "root": "/project",
+        "fallback_version": "0.3.13",
+    },
+    setup_requires=["setuptools_scm"],
+    packages=["fastddm"],
+    package_dir={"fastddm": "/project/build/temp.linux-x86_64-cpython-39/src/python/fastddm"},
+    package_data={"fastddm": ["_core.*", "_core_cuda.*"]},
+)
