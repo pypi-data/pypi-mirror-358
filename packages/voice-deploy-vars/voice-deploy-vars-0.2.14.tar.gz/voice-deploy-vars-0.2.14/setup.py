0005f99479090d4e14d20c8e4b3cc79d75f7be5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:21 +0000

    Add package voice-deploy-vars/0.2.14/voice-deploy-vars-0.2.14.tar.gz

---
+++ packages/voice-deploy-vars/voice-deploy-vars-0.2.14.tar.gz/voice-deploy-vars-0.2.14/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="voice-deploy-vars",
+    version="0.2.14",
+    packages=["voice-deploy-vars"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
