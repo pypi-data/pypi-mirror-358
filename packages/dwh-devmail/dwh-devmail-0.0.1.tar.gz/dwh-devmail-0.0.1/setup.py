3e81d6a5e22c385a794b85a4239fd97a98280795
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:06 +0000

    Add package dwh-devmail/0.0.1/dwh-devmail-0.0.1.tar.gz

---
+++ packages/dwh-devmail/dwh-devmail-0.0.1.tar.gz/dwh-devmail-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dwh-devmail",
+    version="0.0.1",
+    packages=["dwh-devmail"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
