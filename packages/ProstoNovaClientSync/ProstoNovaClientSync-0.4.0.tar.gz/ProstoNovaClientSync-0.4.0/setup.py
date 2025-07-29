1dedfa0e5b7b9749496dce6bacfb47c0d4b97b16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:18:06 +0000

    Add package ProstoNovaClientSync/0.4.0/ProstoNovaClientSync-0.4.0.tar.gz

---
+++ packages/ProstoNovaClientSync/ProstoNovaClientSync-0.4.0.tar.gz/ProstoNovaClientSync-0.4.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ProstoNovaClientSync",
+    version="0.4.0",
+    packages=["ProstoNovaClientSync"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
