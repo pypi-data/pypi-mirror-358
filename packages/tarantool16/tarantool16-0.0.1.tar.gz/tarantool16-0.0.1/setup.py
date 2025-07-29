834df2324b1e6fb025db73879ecf4a77c9977473
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:10 +0000

    Add package tarantool16/0.0.1/tarantool16-0.0.1.tar.gz

---
+++ packages/tarantool16/tarantool16-0.0.1.tar.gz/tarantool16-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="tarantool16",
+    version="0.0.1",
+    packages=["tarantool16"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
