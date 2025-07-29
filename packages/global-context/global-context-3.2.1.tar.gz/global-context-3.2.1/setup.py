20dbab354d646d313e062c862efb21b6746b4115
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:53 +0000

    Add package global-context/3.2.1/global-context-3.2.1.tar.gz

---
+++ packages/global-context/global-context-3.2.1.tar.gz/global-context-3.2.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="global-context",
+    version="3.2.1",
+    packages=["global-context"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
