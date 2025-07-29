d4141a66e09dc27d6977bc29fe055d1b84ecb2e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:06:22 +0000

    Add package autofaker/1.0.20/autofaker-1.0.20-py3-none-any.whl

---
+++ packages/autofaker/autofaker-1.0.20-py3-none-any.whl/autofaker/dataframe.py
@@ -0,0 +1,30 @@
+import dataclasses
+
+import pandas as pd
+
+from autofaker.attributes import Attributes
+from autofaker.generator import TypeDataGenerator
+
+
+class PandasDataFrameGenerator:
+    def __init__(self, t, rows: int = 3, use_fake_data: bool = False):
+        self.data = []
+        for _ in range(rows):
+            row = TypeDataGenerator.create(t, use_fake_data=use_fake_data).generate()
+            self.data.append(row)
+
+    def generate(self):
+        members = [
+            attr
+            for attr in dir(self.data[0])
+            if dataclasses.is_dataclass(getattr(self.data[0], attr))
+            or not callable(getattr(self.data[0], attr))
+            and not attr.startswith("__")
+        ]
+        rows = []
+        for d in self.data:
+            row = []
+            for member in members:
+                row.append(Attributes(d).get_attribute(member))
+            rows.append(row)
+        return pd.DataFrame(rows, columns=members)
---
+++ packages/autofaker/autofaker-1.0.20-py3-none-any.whl/autofaker/fakes.py
@@ -0,0 +1,30 @@
+from faker import Faker
+
+from autofaker.attributes import Attributes
+from autofaker.base import TypeDataGeneratorBase
+from autofaker.builtins import StringGenerator
+
+faker = Faker()
+
+
+def generate(field_name: str):
+    try:
+        attributes = Attributes(faker)
+        func = attributes.get_attribute(field_name)
+        return func()
+    except AttributeError:
+        return None
+
+
+class FakeStringGenerator(TypeDataGeneratorBase):
+    def __init__(self, field_name: str):
+        self.field_name = field_name
+
+    def generate(self):
+        fake = generate(self.field_name)
+        return fake if fake is not None else StringGenerator().generate()
+
+
+class FakeIntegerGenerator(TypeDataGeneratorBase):
+    def generate(self):
+        return faker.random_int()
