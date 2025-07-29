ec24b3c92cbfd4afb01652de531fe0b6ff5c63e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:52 +0000

    Add package conans/0.0.1/conans-0.0.1.tar.gz

---
+++ packages/conans/conans-0.0.1.tar.gz/conans-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="conans",
+    version="0.0.1",
+    packages=["conans"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
