8ddbea4cc38c353ba5a0b2ad4d9d70976f5df221
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:09:00 +0000

    Add package pygments-style-marcosven/0.1.0/pygments_style_marcosven-0.1.0.tar.gz

---
+++ packages/pygments-style-marcosven/pygments_style_marcosven-0.1.0.tar.gz/pygments_style_marcosven-0.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pygments-style-marcosven",
+    version="0.1.0",
+    packages=find_packages(),
+    entry_points={
+        'pygments.styles': [
+            'marcoSven = marco_sven:MarcoSvenStyle',
+        ],
+    },
+    author="marcoSven",
+    author_email="me@marcoSven.com",
+    description="Custom Pygments style for Aider",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/YOUR_GITHUB/pygments-marcosven",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    install_requires=["pygments>=2.0"],
+    python_requires=">=3.7",
+)
