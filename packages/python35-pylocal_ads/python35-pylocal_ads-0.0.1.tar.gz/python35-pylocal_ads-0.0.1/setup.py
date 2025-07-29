8b224c01124554e691276b448f80c0865c98f5ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:10 +0000

    Add package python35-pylocal_ads/0.0.1/python35-pylocal_ads-0.0.1.tar.gz

---
+++ packages/python35-pylocal_ads/python35-pylocal_ads-0.0.1.tar.gz/python35-pylocal_ads-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="python35-pylocal_ads",
+    version="0.0.1",
+    packages=["python35-pylocal_ads"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
