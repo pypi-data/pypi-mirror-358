b7be9f1466f64df4fcacf0310ddbb851e4774b9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:06:04 +0000

    Add package testsrunner/0.0.1/testsrunner-0.0.1.tar.gz

---
+++ packages/testsrunner/testsrunner-0.0.1.tar.gz/testsrunner-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="testsrunner",
+    version="0.0.1",
+    packages=["testsrunner"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
