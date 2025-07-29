9796c7a1cf5e525f59866f598e3825bfac7472e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:09 +0000

    Add package allure-commons/0.0.1/allure-commons-0.0.1.tar.gz

---
+++ packages/allure-commons/allure-commons-0.0.1.tar.gz/allure-commons-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="allure-commons",
+    version="0.0.1",
+    packages=["allure-commons"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
