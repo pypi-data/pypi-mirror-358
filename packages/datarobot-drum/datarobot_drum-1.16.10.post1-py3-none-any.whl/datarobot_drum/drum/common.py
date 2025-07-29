5c7c9904177daa59902e3a330cbb1955a64b195c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:04:29 +0000

    Add package datarobot-drum/1.16.10.post1/datarobot_drum-1.16.10.post1-py3-none-any.whl

---
+++ packages/datarobot-drum/datarobot_drum-1.16.10.post1-py3-none-any.whl/datarobot_drum/drum/common.py
@@ -0,0 +1,96 @@
+"""
+Copyright 2021 DataRobot, Inc. and its affiliates.
+All rights reserved.
+This is proprietary source code of DataRobot, Inc. and its affiliates.
+Released under the terms of DataRobot Tool and Utility Agreement.
+"""
+import logging
+import os
+import sys
+from distutils.util import strtobool
+
+from contextlib import contextmanager
+from pathlib import Path
+
+from datarobot_drum.drum.enum import (
+    MODEL_CONFIG_FILENAME,
+    PredictionServerMimetypes,
+    PayloadFormat,
+)
+from datarobot_drum.drum.exceptions import DrumCommonException
+
+
+@contextmanager
+def reroute_stdout_to_stderr():
+    keep = sys.stdout
+    sys.stdout = sys.stderr
+    try:
+        yield
+    finally:
+        sys.stdout = keep
+
+
+@contextmanager
+def verbose_stdout(verbose):
+    new_target = sys.stdout
+    old_target = sys.stdout
+    if not verbose:
+        new_target = open(os.devnull, "w")
+        sys.stdout = new_target
+    try:
+        yield new_target
+    finally:
+        sys.stdout = old_target
+
+
+def config_logging():
+    logging.basicConfig(format="%(asctime)-15s %(levelname)s %(name)s:  %(message)s")
+
+
+def get_metadata(options):
+    code_dir = Path(options.code_dir)
+    if options.model_config is None:
+        raise DrumCommonException(
+            "You must have a file with the name {} in the directory {}. \n"
+            "You don't. \nWhat you do have is these files: \n{} ".format(
+                MODEL_CONFIG_FILENAME, code_dir, os.listdir(code_dir)
+            )
+        )
+    return options.model_config
+
+
+class SupportedPayloadFormats:
+    def __init__(self):
+        self._formats = {}
+        self._mimetype_to_payload_format = {
+            None: PayloadFormat.CSV,
+            PredictionServerMimetypes.EMPTY: PayloadFormat.CSV,
+            PredictionServerMimetypes.TEXT_CSV: PayloadFormat.CSV,
+            PredictionServerMimetypes.TEXT_PLAIN: PayloadFormat.CSV,
+            PredictionServerMimetypes.TEXT_MTX: PayloadFormat.MTX,
+        }
+
+    def add(self, payload_format, format_version=None):
+        self._formats[payload_format] = format_version
+
+    def is_mimetype_supported(self, mimetype):
+        payload_format = self._mimetype_to_payload_format.get(mimetype)
+        if payload_format is None:
+            return False
+
+        return payload_format in self._formats
+
+    def __iter__(self):
+        for payload_format, format_version in self._formats.items():
+            yield payload_format, format_version
+
+
+def to_bool(value):
+    if value is None:
+        return False
+    if isinstance(value, bool):
+        return value
+    return strtobool(value)
+
+
+FIT_METADATA_FILENAME = "fit_runtime_data.json"
---
+++ packages/datarobot-drum/datarobot_drum-1.16.10.post1-py3-none-any.whl/datarobot_drum/drum/server.py
@@ -0,0 +1,47 @@
+"""
+Copyright 2021 DataRobot, Inc. and its affiliates.
+All rights reserved.
+This is proprietary source code of DataRobot, Inc. and its affiliates.
+Released under the terms of DataRobot Tool and Utility Agreement.
+"""
+from flask import Flask, Blueprint
+import os
+
+from datarobot_drum.drum.enum import URL_PREFIX_ENV_VAR_NAME
+
+HTTP_200_OK = 200
+HTTP_400_BAD_REQUEST = 400
+HTTP_404_NOT_FOUND = 404
+HTTP_422_UNPROCESSABLE_ENTITY = 422
+HTTP_500_INTERNAL_SERVER_ERROR = 500
+HTTP_513_DRUM_PIPELINE_ERROR = 513
+
+
+def get_flask_app(api_blueprint):
+    app = _create_flask_app()
+    url_prefix = os.environ.get(URL_PREFIX_ENV_VAR_NAME, "")
+    app.register_blueprint(api_blueprint, url_prefix=url_prefix)
+    return app
+
+
+def base_api_blueprint(termination_hook=None, predictor=None):
+    model_api = Blueprint("model_api", __name__)
+
+    @model_api.route("/", methods=["GET"])
+    @model_api.route("/ping/", methods=["GET"], strict_slashes=False)
+    def ping():
+        """This route is used to ensure that server has started"""
+        if hasattr(predictor, "liveness_probe"):
+            return predictor.liveness_probe()
+
+        return {"message": "OK"}, HTTP_200_OK
+
+    return model_api
+
+
+def empty_api_blueprint(termination_hook=None):
+    return Blueprint("model_api", __name__)
+
+
+def _create_flask_app():
+    return Flask(__name__)
