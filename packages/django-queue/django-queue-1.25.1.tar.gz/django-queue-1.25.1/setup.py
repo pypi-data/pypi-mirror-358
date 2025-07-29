8fbed546a8567912397c0afd94d90ef80e359e08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:58 +0000

    Add package django-queue/1.25.1/django-queue-1.25.1.tar.gz

---
+++ packages/django-queue/django-queue-1.25.1.tar.gz/django-queue-1.25.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="django-queue",
+    version="1.25.1",
+    packages=["django-queue"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
