a4a568b374c1672337449d061bf4e24a691f07f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:01 +0000

    Add package libdnnl.so.2/0.0.1/libdnnl.so.2-0.0.1.tar.gz

---
+++ packages/libdnnl.so.2/libdnnl.so.2-0.0.1.tar.gz/libdnnl.so.2-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="libdnnl.so.2",
+    version="0.0.1",
+    packages=["libdnnl.so.2"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
