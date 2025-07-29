2148013de14b3b900c2845f2f39821904f865466
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:53 +0000

    Add package ph-logging/0.8.1/ph-logging-0.8.1.tar.gz

---
+++ packages/ph-logging/ph-logging-0.8.1.tar.gz/ph-logging-0.8.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ph-logging",
+    version="0.8.1",
+    packages=["ph-logging"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
