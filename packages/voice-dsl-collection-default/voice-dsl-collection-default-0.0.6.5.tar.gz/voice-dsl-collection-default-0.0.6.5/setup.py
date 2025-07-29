8de3e95447ce2c60d2f4052fb26441fff05e3e52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:06:03 +0000

    Add package voice-dsl-collection-default/0.0.6.5/voice-dsl-collection-default-0.0.6.5.tar.gz

---
+++ packages/voice-dsl-collection-default/voice-dsl-collection-default-0.0.6.5.tar.gz/voice-dsl-collection-default-0.0.6.5/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="voice-dsl-collection-default",
+    version="0.0.6.5",
+    packages=["voice-dsl-collection-default"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
