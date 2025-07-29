6c1efd62ca2a5454e80e5f2115892dd2ca25226e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:56 +0000

    Add package templated-email/0.0.1/templated-email-0.0.1.tar.gz

---
+++ packages/templated-email/templated-email-0.0.1.tar.gz/templated-email-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="templated-email",
+    version="0.0.1",
+    packages=["templated-email"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
