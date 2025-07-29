c14a624102f47581aace59f56355094c2683c375
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:30 +0000

    Add package tags-access/1.3.3/tags-access-1.3.3.tar.gz

---
+++ packages/tags-access/tags-access-1.3.3.tar.gz/tags-access-1.3.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="tags-access",
+    version="1.3.3",
+    packages=["tags-access"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
