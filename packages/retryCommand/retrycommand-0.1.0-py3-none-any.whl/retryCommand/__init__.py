6ece6f0a21f9464df15b29d0f6a82c79ab8c8dfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:43 +0000

    Add package retryCommand/0.1.0/retrycommand-0.1.0-py3-none-any.whl

---
+++ packages/retryCommand/retrycommand-0.1.0-py3-none-any.whl/retryCommand/__init__.py
@@ -0,0 +1,9 @@
+"""
+Retry Command
+~~~~~~~~~~~~~~
+Retry a command automatically until it succeeds or reaches the maximum number of retries.
+
+:copyright: (c) 2025 by kuankuan.
+:license: Mulan PSL v2, see LICENSE for more details.
+"""
+from .cli import *
---
+++ packages/retryCommand/retrycommand-0.1.0-py3-none-any.whl/retryCommand/__main__.py
@@ -0,0 +1,15 @@
+"""
+retryCommand.__main__
+"""
+# pylint: disable=invalid-name
+
+from .cli import retryCommand
+
+def main():
+    """
+    The main function to retry a command.
+    """
+    retryCommand()
+
+if __name__ == "__main__":
+    main()
