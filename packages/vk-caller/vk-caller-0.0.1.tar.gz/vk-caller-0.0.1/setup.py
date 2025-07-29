3d1615eeb8351563ce2766573a19cad1a9e6007b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:56 +0000

    Add package vk-caller/0.0.1/vk-caller-0.0.1.tar.gz

---
+++ packages/vk-caller/vk-caller-0.0.1.tar.gz/vk-caller-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="vk-caller",
+    version="0.0.1",
+    packages=["vk-caller"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
