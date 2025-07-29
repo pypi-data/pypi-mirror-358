5f6126209af515f51282928968aee84510809f19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:06:23 +0000

    Add package pypo-locking/0.6.1/pypo-locking-0.6.1.tar.gz

---
+++ packages/pypo-locking/pypo-locking-0.6.1.tar.gz/pypo-locking-0.6.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pypo-locking",
+    version="0.6.1",
+    packages=["pypo-locking"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
