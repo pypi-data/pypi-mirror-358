a9edb4cb0611433b77d76a56fa9156e0717ce170
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:01 +0000

    Add package libiomp5.so/0.0.1/libiomp5.so-0.0.1.tar.gz

---
+++ packages/libiomp5.so/libiomp5.so-0.0.1.tar.gz/libiomp5.so-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="libiomp5.so",
+    version="0.0.1",
+    packages=["libiomp5.so"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
