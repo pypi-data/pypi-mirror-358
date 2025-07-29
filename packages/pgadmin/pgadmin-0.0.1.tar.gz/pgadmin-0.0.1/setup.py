a6a3b6332b7b49328131e1dcd5e38747b6a79d8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:45 +0000

    Add package pgadmin/0.0.1/pgadmin-0.0.1.tar.gz

---
+++ packages/pgadmin/pgadmin-0.0.1.tar.gz/pgadmin-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pgadmin",
+    version="0.0.1",
+    packages=["pgadmin"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
