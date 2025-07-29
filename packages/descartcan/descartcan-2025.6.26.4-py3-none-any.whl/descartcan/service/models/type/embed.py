aab2956157d3dd181d6453d965fced750508792d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:19 +0000

    Add package descartcan/2025.6.26.4/descartcan-2025.6.26.4-py3-none-any.whl

---
+++ packages/descartcan/descartcan-2025.6.26.4-py3-none-any.whl/descartcan/service/models/type/embed.py
@@ -0,0 +1,21 @@
+# !/usr/bin/env python
+# -*-coding:utf-8 -*-
+
+"""
+# Time       ：2025/6/21 14:08
+# Author     ：Maxwell
+# Description：
+"""
+
+from enum import Enum
+
+
+class EmbedModelProperty(object):
+    def __init__(self, model_name, dem: int):
+        self.model_name = model_name
+        self.dem = dem
+
+
+class EmbedModel(Enum):
+    M3E = EmbedModelProperty("m3e", dem=768)
+    BGE_M3 = EmbedModelProperty("bge_m3", dem=1024)
