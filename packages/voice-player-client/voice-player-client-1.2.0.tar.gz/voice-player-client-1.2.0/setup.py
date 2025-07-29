ee0784175fae616fe1226a9a3043e6f4b6ff60f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:22 +0000

    Add package voice-player-client/1.2.0/voice-player-client-1.2.0.tar.gz

---
+++ packages/voice-player-client/voice-player-client-1.2.0.tar.gz/voice-player-client-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="voice-player-client",
+    version="1.2.0",
+    packages=["voice-player-client"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
