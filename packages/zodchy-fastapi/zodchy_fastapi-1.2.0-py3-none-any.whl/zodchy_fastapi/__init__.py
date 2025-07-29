5ec0c249008e46eb95e6dc108fd63e489d44207b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:22 +0000

    Add package zodchy-fastapi/1.2.0/zodchy_fastapi-1.2.0-py3-none-any.whl

---
+++ packages/zodchy-fastapi/zodchy_fastapi-1.2.0-py3-none-any.whl/zodchy_fastapi/__init__.py
@@ -0,0 +1,12 @@
+from . import endpoints, handlers, schema, wrappers, routers, middleware, adapters, parsers
+
+__all__ = [
+    "endpoints",
+    "handlers",
+    "schema",
+    "wrappers",
+    "routers",
+    "middleware",
+    "adapters",
+    "parsers",
+]
