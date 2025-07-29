75f6ed9b373e438141c75cf69f1a9e1b1d38f8be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:06:12 +0000

    Add package mras-pycube/1.2.5/mras-pycube-1.2.5.tar.gz

---
+++ packages/mras-pycube/mras-pycube-1.2.5.tar.gz/mras-pycube-1.2.5/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="mras-pycube",
+    version="1.2.5",
+    packages=["mras-pycube"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
