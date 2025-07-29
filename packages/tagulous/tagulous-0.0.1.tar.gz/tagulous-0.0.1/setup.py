2f1f1210f9087862b8cf358179ec828ba3c2f32a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:06:24 +0000

    Add package tagulous/0.0.1/tagulous-0.0.1.tar.gz

---
+++ packages/tagulous/tagulous-0.0.1.tar.gz/tagulous-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="tagulous",
+    version="0.0.1",
+    packages=["tagulous"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
