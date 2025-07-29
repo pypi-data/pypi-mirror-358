bb87c1b585ed8704f077ec3e9a9eddba52d0061b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:01 +0000

    Add package libmklml-gnu.so/0.0.1/libmklml-gnu.so-0.0.1.tar.gz

---
+++ packages/libmklml-gnu.so/libmklml-gnu.so-0.0.1.tar.gz/libmklml-gnu.so-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="libmklml-gnu.so",
+    version="0.0.1",
+    packages=["libmklml-gnu.so"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
