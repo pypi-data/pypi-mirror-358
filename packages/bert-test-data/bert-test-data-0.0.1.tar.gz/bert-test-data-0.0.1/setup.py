35c4ea9f964962d33f19ae3d53998a4b535b18dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:00 +0000

    Add package bert-test-data/0.0.1/bert-test-data-0.0.1.tar.gz

---
+++ packages/bert-test-data/bert-test-data-0.0.1.tar.gz/bert-test-data-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="bert-test-data",
+    version="0.0.1",
+    packages=["bert-test-data"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
