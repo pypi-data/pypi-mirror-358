492dd40654de94c3c678b9c9529315eaeb9c1467
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:06:17 +0000

    Add package pypo-article/31.0.1/pypo-article-31.0.1.tar.gz

---
+++ packages/pypo-article/pypo-article-31.0.1.tar.gz/pypo-article-31.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pypo-article",
+    version="31.0.1",
+    packages=["pypo-article"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
