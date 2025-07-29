4c2ff0c69c44d42fb38b936cc81ec5e700012150
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:28 +0000

    Add package scaluq/0.1.0/scaluq-0.1.0-cp312-abi3-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/scaluq/scaluq-0.1.0-cp312-abi3-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl/scaluq/host/bf16/__init__.py
@@ -0,0 +1,6 @@
+import scaluq
+if scaluq.get_default_execution_space() == 'cuda':
+    from ...scaluq_core.host.bf16 import *
+else:
+    from ...scaluq_core.default.bf16 import *
+from . import gate
