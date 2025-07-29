a31741c0f01e4e222296be84b07ea7cb0ad3f8c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:04:50 +0000

    Add package test-orchestrator/0.0.1/test-orchestrator-0.0.1.tar.gz

---
+++ packages/test-orchestrator/test-orchestrator-0.0.1.tar.gz/test-orchestrator-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="test-orchestrator",
+    version="0.0.1",
+    packages=["test-orchestrator"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
