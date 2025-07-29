5dad0746b3aeff35cff613d372fd05f0e4d776e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:19 +0000

    Add package engine-for-testing/0.0.1/engine-for-testing-0.0.1.tar.gz

---
+++ packages/engine-for-testing/engine-for-testing-0.0.1.tar.gz/engine-for-testing-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="engine-for-testing",
+    version="0.0.1",
+    packages=["engine-for-testing"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
