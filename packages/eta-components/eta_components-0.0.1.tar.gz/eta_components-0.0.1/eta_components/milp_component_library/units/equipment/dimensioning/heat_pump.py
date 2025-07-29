3251f8bfcab2d993ec2c28d275cc4a1313c6f405
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:25 +0000

    Add package eta-components/0.0.1/eta_components-0.0.1.tar.gz

---
+++ packages/eta-components/eta_components-0.0.1.tar.gz/eta_components-0.0.1/eta_components/milp_component_library/units/equipment/dimensioning/heat_pump.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+from eta_components.milp_component_library.units.equipment.base_equipment import BaseWaterWaterHeatPump
+from eta_components.milp_component_library.units.equipment.dimensioning.base_dimensioning import BaseDimensioning
+
+
+class WaterWaterHeatPump(BaseWaterWaterHeatPump, BaseDimensioning):
+    """Converter model for a water-water heat pump."""
