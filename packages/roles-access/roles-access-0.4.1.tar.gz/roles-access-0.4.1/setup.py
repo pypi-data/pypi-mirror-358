eaabdb89f3cce2d6d23996a8cc332aad8925aadf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:29 +0000

    Add package roles-access/0.4.1/roles-access-0.4.1.tar.gz

---
+++ packages/roles-access/roles-access-0.4.1.tar.gz/roles-access-0.4.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="roles-access",
+    version="0.4.1",
+    packages=["roles-access"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
