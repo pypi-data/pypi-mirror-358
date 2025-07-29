c5a3ea2e7a24464d5c26d668eeb4dad80c1de2fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:34 +0000

    Add package unitorch/0.0.0.33/unitorch-0.0.0.33-py3-none-any.whl

---
+++ packages/unitorch/unitorch-0.0.0.33-py3-none-any.whl/unitorch/cli/fastapis/stable_3/__init__.py
@@ -0,0 +1,12 @@
+# Copyright (c) FULIUCANSHENG.
+# Licensed under the MIT License.
+
+from unitorch.cli.fastapis.stable_3.text2image import (
+    Stable3ForText2ImageFastAPIPipeline,
+)
+from unitorch.cli.fastapis.stable_3.image2image import (
+    Stable3ForImage2ImageFastAPIPipeline,
+)
+from unitorch.cli.fastapis.stable_3.inpainting import (
+    Stable3ForImageInpaintingFastAPIPipeline,
+)
