f2a53560811defe458c798715fc893f8b3a29997
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:22 +0000

    Add package triton-pre-mlir/0.0.1/triton-pre-mlir-0.0.1.tar.gz

---
+++ packages/triton-pre-mlir/triton-pre-mlir-0.0.1.tar.gz/triton-pre-mlir-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="triton-pre-mlir",
+    version="0.0.1",
+    packages=["triton-pre-mlir"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
