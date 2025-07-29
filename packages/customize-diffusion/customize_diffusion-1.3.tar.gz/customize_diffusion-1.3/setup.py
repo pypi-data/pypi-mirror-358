da6d766ac661688affc605b14b63b0e640157209
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:43 +0000

    Add package customize-diffusion/1.3/customize_diffusion-1.3.tar.gz

---
+++ packages/customize-diffusion/customize_diffusion-1.3.tar.gz/customize_diffusion-1.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+setup(
+      name='customize_diffusion',
+      version='1.3',
+      description='customize_diffusion',
+      url='https://github.com/NVIDIA/TensorRT/tree/release/10.12/demo/Diffusion',
+      author='fanss',
+      author_email='congcongxingzhe@gmail.com',
+      license='MIT',
+      packages=['demo_diffusion'],
+      zip_safe=False
+)
