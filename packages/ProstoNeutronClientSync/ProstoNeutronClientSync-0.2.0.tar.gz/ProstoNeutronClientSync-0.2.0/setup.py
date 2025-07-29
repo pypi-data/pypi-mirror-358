e6da7f119295f63d79d6f6fbb2ae3111ba98420b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:18:06 +0000

    Add package ProstoNeutronClientSync/0.2.0/ProstoNeutronClientSync-0.2.0.tar.gz

---
+++ packages/ProstoNeutronClientSync/ProstoNeutronClientSync-0.2.0.tar.gz/ProstoNeutronClientSync-0.2.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ProstoNeutronClientSync",
+    version="0.2.0",
+    packages=["ProstoNeutronClientSync"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
