6541a80e383f5337536c258f1486088c8104e8bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:13 +0000

    Add package webpack-loader/0.0.1/webpack-loader-0.0.1.tar.gz

---
+++ packages/webpack-loader/webpack-loader-0.0.1.tar.gz/webpack-loader-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="webpack-loader",
+    version="0.0.1",
+    packages=["webpack-loader"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
