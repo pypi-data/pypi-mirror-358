f93392d54c3dea40879c62b3c2cd2b8543408738
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:29 +0000

    Add package gocommon/1.4.10/gocommon-1.4.10.tar.gz

---
+++ packages/gocommon/gocommon-1.4.10.tar.gz/gocommon-1.4.10/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="gocommon",
+    version="1.4.10",
+    packages=["gocommon"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
