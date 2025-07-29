399c083e1870c58ce39525c331ec440b1ff23b2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:56 +0000

    Add package pygrt-kit/0.9.0/pygrt_kit-0.9.0-py3-none-win_amd64.whl

---
+++ packages/pygrt-kit/pygrt_kit-0.9.0-py3-none-win_amd64.whl/pygrt/C_extension/include/common/colorstr.h
@@ -0,0 +1,19 @@
+/**
+ * @file   colorstr.h
+ * @author Zhu Dengda (zhudengda@mail.iggcas.ac.cn)
+ * @date   2024-12
+ * 
+ * 定义一些字符串宏，以打印有颜色的字符串
+ *                   
+ */
+
+
+#pragma once 
+
+#define DEFAULT_RESTORE  "\033[0m" 
+#define REGULAR_RED      "\033[0;31m"
+#define BOLD_RED         "\033[1;31m"
+#define REGULAR_GREEN    "\033[0;32m"
+#define BOLD_GREEN       "\033[1;32m"
+#define REGULAR_BLUE     "\033[0;35m"
+#define BOLD_BLUE        "\033[1;35m"
