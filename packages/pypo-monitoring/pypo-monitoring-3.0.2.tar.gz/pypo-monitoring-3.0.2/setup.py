d01b27df24be9039fadaf6ebce4bdc7c39a10be0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:06:23 +0000

    Add package pypo-monitoring/3.0.2/pypo-monitoring-3.0.2.tar.gz

---
+++ packages/pypo-monitoring/pypo-monitoring-3.0.2.tar.gz/pypo-monitoring-3.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pypo-monitoring",
+    version="3.0.2",
+    packages=["pypo-monitoring"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
