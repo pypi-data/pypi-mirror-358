7016d7f989b1d4d4b23eca629956334b6ffddf63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:05:48 +0000

    Add package git-smart-commit-ollama/0.1.0/git_smart_commit_ollama-0.1.0.tar.gz

---
+++ packages/git-smart-commit-ollama/git_smart_commit_ollama-0.1.0.tar.gz/git_smart_commit_ollama-0.1.0/README.md
@@ -0,0 +1,10 @@
+# git-smart-commit-ollama
+
+Generate concise Git commit messages using local LLMs via Ollama.
+
+## Usage
+
+```bash
+git add .
+git-smart-commit
+```
---
+++ packages/git-smart-commit-ollama/git_smart_commit_ollama-0.1.0.tar.gz/git_smart_commit_ollama-0.1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name="git-smart-commit-ollama",
+    version="0.1.0",
+    py_modules=["app"],
+    entry_points={
+        "console_scripts": [
+            "git-smart-commit=app:main"
+        ]
+    },
+    description="Generate concise Git commit messages using local LLMs via Ollama",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Saranya K",
+    author_email="your@email.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+    ],
+    python_requires=">=3.7",
+)
