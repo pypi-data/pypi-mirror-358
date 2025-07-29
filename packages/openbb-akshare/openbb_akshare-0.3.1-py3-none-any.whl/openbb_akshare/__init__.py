483b43a8c1e02584c25a57ce60075a7ee3d233d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:06:00 +0000

    Add package openbb-akshare/0.3.1/openbb_akshare-0.3.1-py3-none-any.whl

---
+++ packages/openbb-akshare/openbb_akshare-0.3.1-py3-none-any.whl/openbb_akshare/__init__.py
@@ -0,0 +1,26 @@
+"""openbb_akshare OpenBB Platform Provider."""
+
+from openbb_core.provider.abstract.provider import Provider
+from openbb_akshare.models.company_news import AKShareCompanyNewsFetcher
+from openbb_akshare.models.equity_quote import AKShareEquityQuoteFetcher
+from openbb_akshare.models.equity_historical import AKShareEquityHistoricalFetcher
+from openbb_akshare.models.historical_dividends import AKShareHistoricalDividendsFetcher
+
+# mypy: disable-error-code="list-item"
+
+provider = Provider(
+    name="akshare",
+    description="Data provider for openbb-akshare.",
+    # Only add 'credentials' if they are needed.
+    # For multiple login details, list them all here.
+    # credentials=["api_key"],
+    website="https://akshare.akfamily.xyz/",
+    # Here, we list out the fetchers showing what our provider can get.
+    # The dictionary key is the fetcher's name, used in the `router.py`.
+    fetcher_dict={
+        "CompanyNews": AKShareCompanyNewsFetcher,
+        "EquityQuote": AKShareEquityQuoteFetcher,
+        "EquityHistorical": AKShareEquityHistoricalFetcher,
+        "HistoricalDividends": AKShareHistoricalDividendsFetcher,
+    }
+)
