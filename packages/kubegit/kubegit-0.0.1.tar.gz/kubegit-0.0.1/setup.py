6162fa86502247fa2c491540effe8a069a825608
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:06:11 +0000

    Add package kubegit/0.0.1/kubegit-0.0.1.tar.gz

---
+++ packages/kubegit/kubegit-0.0.1.tar.gz/kubegit-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="kubegit",
+    version="0.0.1",
+    packages=["kubegit"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
