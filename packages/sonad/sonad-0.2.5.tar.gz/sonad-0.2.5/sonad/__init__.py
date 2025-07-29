91c494a1ed1557e1eea9ee6c9341caf48a29466b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:08:51 +0000

    Add package sonad/0.2.5/sonad-0.2.5.tar.gz

---
+++ packages/sonad/sonad-0.2.5.tar.gz/sonad-0.2.5/sonad/__init__.py
@@ -0,0 +1,8 @@
+"""
+SOftware NAme Disambiguation tool
+SOftware NAme Disambiguation: A tool for disambiguating software names and their URLs in scientific literature.
+"""
+
+__version__ = "0.1.0"
+__author__ = "Jelena Duric"
+__email__ = "djuricjelena611@gmail.com"
