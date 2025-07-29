a3313f215b1b23d98e36a5edeebde94322fd2355
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:15 +0000

    Add package multiselectfield/0.0.1/multiselectfield-0.0.1.tar.gz

---
+++ packages/multiselectfield/multiselectfield-0.0.1.tar.gz/multiselectfield-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="multiselectfield",
+    version="0.0.1",
+    packages=["multiselectfield"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
