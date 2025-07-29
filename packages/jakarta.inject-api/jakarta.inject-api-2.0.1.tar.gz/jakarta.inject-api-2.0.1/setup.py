6964935cdc0af993994596ddee43b2b5db45ef53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:04:51 +0000

    Add package jakarta.inject-api/2.0.1/jakarta.inject-api-2.0.1.tar.gz

---
+++ packages/jakarta.inject-api/jakarta.inject-api-2.0.1.tar.gz/jakarta.inject-api-2.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="jakarta.inject-api",
+    version="2.0.1",
+    packages=["jakarta.inject-api"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
