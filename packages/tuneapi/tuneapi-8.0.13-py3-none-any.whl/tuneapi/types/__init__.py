51b6018639711334f9d969fe0bfb7b8dd5999068
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:09:04 +0000

    Add package tuneapi/8.0.13/tuneapi-8.0.13-py3-none-any.whl

---
+++ packages/tuneapi/tuneapi-8.0.13-py3-none-any.whl/tuneapi/types/__init__.py
@@ -0,0 +1,44 @@
+# Copyright © 2024-2025 Frello Technology Private Limited
+# Copyright © 2025-2025 Yash Bonde github.com/yashbonde
+# MIT License
+
+import functools
+
+from tuneapi.types.chats import (
+    Message,
+    Thread,
+    ThreadsList,
+    ModelInterface,
+    Usage,
+    ThreadsTree,
+    Prop,
+    Tool,
+    EmbeddingGen,
+    WebVTTCue,
+    Transcript,
+    get_transcript,
+    ImageGen,
+    system,
+    human,
+    assistant,
+    function_call,
+    function_resp,
+)
+
+from tuneapi.types.evals import (
+    Evals,
+)
+
+from typing import Dict as D, List as L, Optional as O, Tuple as T
+
+from pydantic import (
+    BaseModel as BM,
+    Field as _Field,
+)
+
+
+def Field(description: str, /):
+    return _Field(..., description=description)
+
+
+F = Field
