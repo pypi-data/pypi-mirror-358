d9b7b4350b15054bee9a0c9e9c6c415db6e60227
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:06:17 +0000

    Add package pypo-crosslink/3.0.1/pypo-crosslink-3.0.1.tar.gz

---
+++ packages/pypo-crosslink/pypo-crosslink-3.0.1.tar.gz/pypo-crosslink-3.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pypo-crosslink",
+    version="3.0.1",
+    packages=["pypo-crosslink"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
