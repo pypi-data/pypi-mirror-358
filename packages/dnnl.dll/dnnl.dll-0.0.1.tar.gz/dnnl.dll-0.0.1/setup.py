24fa346f6cb4c5f0cbac651b6c40d79dca204b4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:01 +0000

    Add package dnnl.dll/0.0.1/dnnl.dll-0.0.1.tar.gz

---
+++ packages/dnnl.dll/dnnl.dll-0.0.1.tar.gz/dnnl.dll-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dnnl.dll",
+    version="0.0.1",
+    packages=["dnnl.dll"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
