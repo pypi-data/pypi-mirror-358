7a83f8f542a9912c0bae8798b53c388c1cdb3f5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:06:15 +0000

    Add package ceph-iscsi-config/0.0.1/ceph-iscsi-config-0.0.1.tar.gz

---
+++ packages/ceph-iscsi-config/ceph-iscsi-config-0.0.1.tar.gz/ceph-iscsi-config-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ceph-iscsi-config",
+    version="0.0.1",
+    packages=["ceph-iscsi-config"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
