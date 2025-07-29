e6145ebf6714447da98ea005f150f5ed85c1595f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:45 +0000

    Add package windockd/1.1.2/windockd-1.1.2.tar.gz

---
+++ packages/windockd/windockd-1.1.2.tar.gz/windockd-1.1.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="windockd",
+    version="1.1.2",
+    packages=find_packages(),
+    package_data={
+        "windockd": ["resources/*.zip"],
+    },
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "windockd=windockd.cli:cli"
+        ]
+    },
+    install_requires=[
+        "click>=8.0.0",
+    ],
+    author="Your Name",
+    description="Lightweight Windows Container Runtime CLI",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/RohanRusta21/windockd",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+    ],
+)
