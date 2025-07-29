89ec100725d62d67c3d00b0faa9b043ed8bc4782
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:18:06 +0000

    Add package smartwatts/2.4.1/smartwatts-2.4.1-py3-none-any.whl

---
+++ packages/smartwatts/smartwatts-2.4.1-py3-none-any.whl/smartwatts/actor/factory.py
@@ -0,0 +1,56 @@
+# BSD 3-Clause License
+#
+# Copyright (c) 2023, Inria
+# Copyright (c) 2023, University of Lille
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+from powerapi.pusher import PusherActor
+
+from .actor import SmartWattsFormulaActor
+from .config import SmartWattsFormulaConfig
+
+
+class SmartWattsFormulaActorFactory:
+    """
+    Factory to create SmartWatts formula actors.
+    """
+
+    def __init__(self, actor_config: SmartWattsFormulaConfig):
+        """
+        Initialize a new SmartWatts formula factory.
+        """
+        self.actor_config = actor_config
+
+    def __call__(self, name: str, pushers: dict[str, PusherActor]) -> SmartWattsFormulaActor:
+        """
+        Create a new SmartWatts formula actor.
+        :param name: Name of the actor
+        :param pushers: Dictionary of available pushers
+        :return: A new SmartWatts formula actor
+        """
+        return SmartWattsFormulaActor(name, pushers, self.actor_config)
