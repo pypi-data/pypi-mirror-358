82f68ac1354cc5ec225f5bf1be62924f6aec3aa7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:45 +0000

    Add package velosaurus-calc/1.0.10/velosaurus_calc-1.0.10-py3-none-any.whl

---
+++ packages/velosaurus-calc/velosaurus_calc-1.0.10-py3-none-any.whl/velosaurus_calc/calculator.py
@@ -0,0 +1,11 @@
+def add_floats(a: float, b: float) -> float:
+    res = a + b
+    return res
+
+
+def subtract_floats(a: float, b: float) -> float:
+    return a - b
+
+
+if __name__ == "__main__":
+    print(add_floats(2, 4))
