8caab5a89a127f5d759af1c806ee73ab81172cfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:01 +0000

    Add package libmklml-intel.so/0.0.1/libmklml-intel.so-0.0.1.tar.gz

---
+++ packages/libmklml-intel.so/libmklml-intel.so-0.0.1.tar.gz/libmklml-intel.so-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="libmklml-intel.so",
+    version="0.0.1",
+    packages=["libmklml-intel.so"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
