a2c682c986673b4f99f3245f0c99cae6f0eae806
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:27 +0000

    Add package boto3-refresh-session/1.2.4/boto3_refresh_session-1.2.4.tar.gz

---
+++ packages/boto3-refresh-session/boto3_refresh_session-1.2.4.tar.gz/boto3_refresh_session-1.2.4/boto3_refresh_session/__init__.py
@@ -0,0 +1,7 @@
+from .ecs import ECSRefreshableSession
+from .session import RefreshableSession
+from .sts import STSRefreshableSession
+
+__all__ = ["RefreshableSession"]
+__version__ = "1.2.4"
+__author__ = "Mike Letts"
