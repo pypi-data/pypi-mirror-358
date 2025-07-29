31cfc60f9810c8cb421a41102cd2d770996477a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:18:06 +0000

    Add package scrooge-ra-orm-models/1.0.45/scrooge-ra-orm-models-1.0.45.tar.gz

---
+++ packages/scrooge-ra-orm-models/scrooge-ra-orm-models-1.0.45.tar.gz/scrooge-ra-orm-models-1.0.45/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="scrooge-ra-orm-models",
+    version="1.0.45",
+    packages=["scrooge-ra-orm-models"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
