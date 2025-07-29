c7b846d62f425ad93d39ad61a565adb47b0e54b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:06:05 +0000

    Add package infra-analytics/0.0.6/infra_analytics-0.0.6.tar.gz

---
+++ packages/infra-analytics/infra_analytics-0.0.6.tar.gz/infra_analytics-0.0.6/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+# Read README.md with UTF-8 encoding
+try:
+    with open("README.md", "r", encoding="utf-8") as fh:
+        long_description = fh.read()
+except UnicodeDecodeError:
+    # Fallback to system default encoding if UTF-8 fails
+    with open("README.md", "r") as fh:
+        long_description = fh.read()
+
+# get required lib
+with open("script/requirements.txt", "r") as file:
+    requirements = file.read().strip().split("\n")
+
+setup(
+    name="infra_analytics",
+    version="0.0.6",
+    author="hautx2",
+    author_email="hautx2@fpt.com",
+    description="Sort description",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=find_packages(),
+    # requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
