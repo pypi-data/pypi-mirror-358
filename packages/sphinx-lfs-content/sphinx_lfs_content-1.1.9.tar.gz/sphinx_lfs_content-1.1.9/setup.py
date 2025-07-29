7a57dda8e34d5cc80db0a93f03f0867a12972699
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:33 +0000

    Add package sphinx-lfs-content/1.1.9/sphinx_lfs_content-1.1.9.tar.gz

---
+++ packages/sphinx-lfs-content/sphinx_lfs_content-1.1.9.tar.gz/sphinx_lfs_content-1.1.9/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+import os
+
+
+# Read the contents of README.md
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+
+setup(
+    name="sphinx_lfs_content",
+    version="1.1.9",
+    author="Dominic Kempf",
+    author_email="dominic.kempf@iwr.uni-heidelberg.de",
+    description="Ensure existence of LFS content in your LFS builds",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+    ],
+    classifiers=[
+        "Framework :: Sphinx :: Extension",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
---
+++ packages/sphinx-lfs-content/sphinx_lfs_content-1.1.9.tar.gz/sphinx_lfs_content-1.1.9/sphinx_lfs_content/__init__.py
@@ -0,0 +1,54 @@
+import hashlib
+import os
+import requests
+import shutil
+import sphinx
+import subprocess
+import tarfile
+import tempfile
+
+
+GIT_LFS_FILE = "https://github.com/git-lfs/git-lfs/releases/download/v3.6.1/git-lfs-linux-amd64-v3.6.1.tar.gz"
+GIT_LFS_CHECKSUM = "2138d2e405a12f1a088272e06790b76699b79cb90d0317b77aafaf35de908d76"
+
+
+def lfs_setup(_, config):
+    # If we already have git-lfs, we do not need to set it up
+    if shutil.which("git-lfs") is None:
+        # Download the latest git-lfs tarball and check its checksum
+        git_lfs_content = requests.get(GIT_LFS_FILE).content
+        checksum = hashlib.sha256(git_lfs_content).hexdigest()
+        if checksum != GIT_LFS_CHECKSUM:
+            raise ValueError("CheckSum of git-lfs tarball was incorrect!")
+
+        # Create a temporary directory to install git-lfs into
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            # Write it to file (can this be short cut and merged with unpacking?)
+            with open(os.path.join(tmp_dir, "git-lfs.tar.gz"), "wb") as tar:
+                tar.write(git_lfs_content)
+
+            # Unpack the tarball
+            with tarfile.open(os.path.join(tmp_dir, "git-lfs.tar.gz"), "r:gz") as tar:
+                tar.extractall(path=tmp_dir)
+
+            # Setup a modified environment that has the temporary directory in PATH
+            # This works around a bug in git-lfs where git-lfs is called recursively,
+            # but the inner calls rely on git-lfs being in PATH.
+            env = os.environ
+            env["PATH"] = os.environ["PATH"] + os.path.pathsep + os.path.join(tmp_dir, "git-lfs-3.6.1")
+
+            # Fetch the LFS content of the repository
+            subprocess.check_call("git-lfs install".split(), env=env)
+            subprocess.check_call("git-lfs fetch".split(), env=env)
+            subprocess.check_call("git-lfs checkout".split(), env=env)
+
+    # Execute all of the given post commands
+    for cmd in config.lfs_content_post_commands:
+        subprocess.check_call(cmd, shell=True)
+
+
+def setup(app):
+    app.add_config_value("lfs_content_post_commands", [], rebuild="")
+    app.connect("config-inited", lfs_setup)
+
+    return {"version": "1.1.9", "parallel_read_safe": True}
