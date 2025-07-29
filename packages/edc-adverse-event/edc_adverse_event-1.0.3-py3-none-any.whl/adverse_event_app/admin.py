c3456c78b2c5a24d0c685a6a79dd65e3e6943d58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:22 +0000

    Add package edc-adverse-event/1.0.3/edc_adverse_event-1.0.3-py3-none-any.whl

---
+++ packages/edc-adverse-event/edc_adverse_event-1.0.3-py3-none-any.whl/adverse_event_app/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from edc_model_admin.history import SimpleHistoryAdmin
+
+from edc_adverse_event.modeladmin_mixins import (
+    AeFollowupModelAdminMixin,
+    AeInitialModelAdminMixin,
+    HospitalizationModelAdminMixin,
+)
+
+from .models import AeFollowup, AeInitial, Hospitalization
+
+
+@admin.register(AeInitial)
+class AeInitialAdmin(AeInitialModelAdminMixin, SimpleHistoryAdmin):
+    pass
+
+
+@admin.register(AeFollowup)
+class AeFollowupAdmin(AeFollowupModelAdminMixin, SimpleHistoryAdmin):
+    pass
+
+
+@admin.register(Hospitalization)
+class HospitalizationAdmin(HospitalizationModelAdminMixin, SimpleHistoryAdmin):
+    pass
