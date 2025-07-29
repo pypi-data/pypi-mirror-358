8ba8cafddbb228b68c406867463e5a6341a82540
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:05:05 +0000

    Add package consumet-mc/1.0.0/consumet_mc-1.0.0-py3-none-any.whl

---
+++ packages/consumet-mc/consumet_mc-1.0.0-py3-none-any.whl/consumet_mc/__init__.py
@@ -0,0 +1,38 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from mov_cli.plugins import PluginHookData
+
+from .providers import (
+    Kisskh,
+    HiAnime,
+    Flixhq,
+    AnimePahe,
+    Turkish,
+    ViewAsian,
+    DramaCool,
+    AllAnime,
+    AniWorld,
+    HiMovies,
+)
+
+plugin: PluginHookData = {
+    "version": 1,
+    "package_name": "consumet-mc",  # Required for the plugin update checker.
+    "scrapers": {
+        "hianime": HiAnime,
+        "animepahe": AnimePahe,
+        "allanime": AllAnime,
+        "aniworld": AniWorld,
+        "dramacool": DramaCool,
+        "kisskh": Kisskh,
+        "viewasian": ViewAsian,
+        "flixhq": Flixhq,
+        "himovies": HiMovies,
+        "turkish": Turkish,
+        "DEFAULT": HiAnime,
+    },
+}
+
+__version__ = "1.0.0"
