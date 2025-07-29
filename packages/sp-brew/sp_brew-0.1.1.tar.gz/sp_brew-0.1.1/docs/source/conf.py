fcdbd4720334cf1143c61acd7e81ce7dc235e70b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:22 +0000

    Add package sp-brew/0.1.1/sp_brew-0.1.1.tar.gz

---
+++ packages/sp-brew/sp_brew-0.1.1.tar.gz/sp_brew-0.1.1/docs/source/conf.py
@@ -0,0 +1,20 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("../src"))
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx_autodoc_typehints",
+]
+
+html_theme = "furo"
+project = "sp_brew"
+copyright = "2025, Damiano Massella"
+author = "Damiano Massella"
+release = "0.1.0"
+
+templates_path = ["_templates"]
+exclude_patterns = []
+html_static_path = ["_static"]
---
+++ packages/sp-brew/sp_brew-0.1.1.tar.gz/sp_brew-0.1.1/src/sp_brew/__init__.py
@@ -0,0 +1,5 @@
+from . import utils_files, utils_math
+
+
+def main() -> None:
+    print("Hello from sp-brew!")
