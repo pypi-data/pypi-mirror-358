c1dc13059765ea045d1510593fe60220eb772907
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:04 +0000

    Add package ktstools/0.0.1/ktstools-0.0.1.tar.gz

---
+++ packages/ktstools/ktstools-0.0.1.tar.gz/ktstools-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ktstools",
+    version="0.0.1",
+    packages=["ktstools"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
