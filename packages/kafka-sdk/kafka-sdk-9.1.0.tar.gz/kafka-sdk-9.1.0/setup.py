40ca85d13a4165f897f11bdd87272207ce9317ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:53 +0000

    Add package kafka-sdk/9.1.0/kafka-sdk-9.1.0.tar.gz

---
+++ packages/kafka-sdk/kafka-sdk-9.1.0.tar.gz/kafka-sdk-9.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="kafka-sdk",
+    version="9.1.0",
+    packages=["kafka-sdk"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
