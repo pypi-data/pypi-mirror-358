f9a3e3bc0e574134b213684cfcfa393905e71f4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:06:12 +0000

    Add package pymrasab/0.0.1/pymrasab-0.0.1.tar.gz

---
+++ packages/pymrasab/pymrasab-0.0.1.tar.gz/pymrasab-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pymrasab",
+    version="0.0.1",
+    packages=["pymrasab"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
