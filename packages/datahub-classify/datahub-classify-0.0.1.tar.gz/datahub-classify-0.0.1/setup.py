625383ce95d1e8c85cef7aae949801dc746d8b54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:06:11 +0000

    Add package datahub-classify/0.0.1/datahub-classify-0.0.1.tar.gz

---
+++ packages/datahub-classify/datahub-classify-0.0.1.tar.gz/datahub-classify-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="datahub-classify",
+    version="0.0.1",
+    packages=["datahub-classify"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
