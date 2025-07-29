5dd12b64b2193c3564e8f31e53b02165793f97ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:02 +0000

    Add package ufal/0.0.1/ufal-0.0.1.tar.gz

---
+++ packages/ufal/ufal-0.0.1.tar.gz/ufal-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ufal",
+    version="0.0.1",
+    packages=["ufal"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
