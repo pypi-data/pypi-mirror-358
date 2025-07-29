16a9dee76f0a9837f565be0ad4d915a753f0b14e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:06:40 +0000

    Add package psbody/0.0.1/psbody-0.0.1.tar.gz

---
+++ packages/psbody/psbody-0.0.1.tar.gz/psbody-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="psbody",
+    version="0.0.1",
+    packages=["psbody"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
