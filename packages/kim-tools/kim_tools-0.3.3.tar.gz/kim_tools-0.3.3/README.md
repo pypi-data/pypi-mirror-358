8bd37035079fa35f1990e4a0934252b4c26c76b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:59 +0000

    Add package kim-tools/0.3.3/kim_tools-0.3.3.tar.gz

---
+++ packages/kim-tools/kim_tools-0.3.3.tar.gz/kim_tools-0.3.3/README.md
@@ -0,0 +1,8 @@
+# kim-tools
+
+![testing](https://github.com/openkim/kim-tools/actions/workflows/testing.yml/badge.svg)
+[![docs](https://app.readthedocs.org/projects/kim-tools/badge/?version=latest)](https://kim-tools.readthedocs.io/en/latest/)
+[![PyPI](https://img.shields.io/pypi/v/kim-tools.svg)](https://pypi.org/project/kim-tools/)
+
+KIMTestDriver and SingleCrystalTestDriver classes for creating OpenKIM Test Drivers, and helper routines for writing
+KIM Tests and Verification Checks. Documentation at https://kim-tools.readthedocs.io.
---
+++ packages/kim-tools/kim_tools-0.3.3.tar.gz/kim_tools-0.3.3/tests/test_ase.py
@@ -0,0 +1,22 @@
+import numpy as np
+from ase.calculators.lj import LennardJones
+from lj_fail_no_neighbors import LennardJonesFailNoNeighbors
+
+from kim_tools import get_isolated_energy_per_atom
+
+
+def test_get_isolated_energy_per_atom():
+    assert np.isclose(
+        get_isolated_energy_per_atom(
+            "LJ_ElliottAkerson_2015_Universal__MO_959249795837_003", "H"
+        ),
+        0,
+    )
+    assert np.isclose(
+        get_isolated_energy_per_atom(LennardJones(), "H"),
+        0,
+    )
+    assert np.isclose(
+        get_isolated_energy_per_atom(LennardJonesFailNoNeighbors(), "H"),
+        0,
+    )
