0d19d3ea25d169bb77314e6d3f1eac8101566fc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:27 +0000

    Add package booking-api/0.0.1/booking-api-0.0.1.tar.gz

---
+++ packages/booking-api/booking-api-0.0.1.tar.gz/booking-api-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="booking-api",
+    version="0.0.1",
+    packages=["booking-api"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
