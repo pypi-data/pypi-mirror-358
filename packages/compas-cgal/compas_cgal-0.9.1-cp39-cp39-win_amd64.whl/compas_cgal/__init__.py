e33847e120b875a3537912c29b2f9f5d61883588
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:47 +0000

    Add package compas-cgal/0.9.1/compas_cgal-0.9.1-cp39-cp39-win_amd64.whl

---
+++ packages/compas-cgal/compas_cgal-0.9.1-cp39-cp39-win_amd64.whl/compas_cgal/__init__.py
@@ -0,0 +1,29 @@
+import os
+
+
+__author__ = ["tom van mele", "petras vestartas"]
+__copyright__ = "Block Research Group - ETH Zurich"
+__license__ = "MIT License"
+__email__ = ["van.mele@arch.ethz.ch", "vestartas@arch.ethz.ch"]
+__version__ = "0.9.1"
+
+HERE = os.path.dirname(__file__)
+
+HOME = os.path.abspath(os.path.join(HERE, "../../"))
+DATA = os.path.abspath(os.path.join(HOME, "data"))
+DOCS = os.path.abspath(os.path.join(HOME, "docs"))
+TEMP = os.path.abspath(os.path.join(HOME, "temp"))
+
+
+__all_plugins__ = [
+    "compas_cgal.booleans",
+    "compas_cgal.intersections",
+    "compas_cgal.meshing",
+    "compas_cgal.measure",
+    "compas_cgal.reconstruction",
+    "compas_cgal.triangulation",
+    "compas_cgal.slicer",
+    "compas_cgal.subdivision",
+]
+
+__all__ = ["HOME", "DATA", "DOCS", "TEMP"]
