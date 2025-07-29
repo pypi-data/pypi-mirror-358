f3ea61489add3fef16a71ef5b161f01394ab60f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:01 +0000

    Add package di-attachments/0.0.1/di-attachments-0.0.1.tar.gz

---
+++ packages/di-attachments/di-attachments-0.0.1.tar.gz/di-attachments-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="di-attachments",
+    version="0.0.1",
+    packages=["di-attachments"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
