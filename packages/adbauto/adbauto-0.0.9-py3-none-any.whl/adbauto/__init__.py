c97f451ec1caba6eb220dfe467e9f6cac0cb627d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:52 +0000

    Add package adbauto/0.0.9/adbauto-0.0.9-py3-none-any.whl

---
+++ packages/adbauto/adbauto-0.0.9-py3-none-any.whl/adbauto/__init__.py
@@ -0,0 +1,17 @@
+from .adb import get_emulator_device, shell, list_devices, start_scrcpy, stop_scrcpy
+from .screen import find_image, tap_image, tap_img_when_visible
+from .input import tap, scroll
+
+__all__ = [
+    "get_emulator_device",
+    "shell",
+    "list_devices",
+    "start_scrcpy",
+    "stop_scrcpy",
+    "find_image",
+    "find_all_images",
+    "tap_image",
+    "tap_img_when_visible",
+    "tap",
+    "scroll",
+]
