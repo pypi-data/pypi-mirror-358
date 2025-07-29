ef902ab23cf0009ef5d8f316519e82d0a9c583d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:13 +0000

    Add package tag-parser/0.0.1/tag-parser-0.0.1.tar.gz

---
+++ packages/tag-parser/tag-parser-0.0.1.tar.gz/tag-parser-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="tag-parser",
+    version="0.0.1",
+    packages=["tag-parser"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
