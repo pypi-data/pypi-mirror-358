b4e04622b41bde982fe8d97bf144dabb338474d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:06:26 +0000

    Add package my-simple-package-1-6-3-2-2-2/0.1.0/my-simple-package-1-6-3-2-2-2-0.1.0.tar.gz

---
+++ packages/my-simple-package-1-6-3-2-2-2/my-simple-package-1-6-3-2-2-2-0.1.0.tar.gz/my-simple-package-1-6-3-2-2-2-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="my-simple-package-1-6-3-2-2-2",
+    version="0.1.0",
+    packages=["my-simple-package-1-6-3-2-2-2"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
