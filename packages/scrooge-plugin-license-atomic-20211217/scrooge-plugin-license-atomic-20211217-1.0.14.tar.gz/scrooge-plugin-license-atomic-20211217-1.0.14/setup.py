598ed3e483ef1d07d06db469fc9054af6c91f3ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:16 +0000

    Add package scrooge-plugin-license-atomic-20211217/1.0.14/scrooge-plugin-license-atomic-20211217-1.0.14.tar.gz

---
+++ packages/scrooge-plugin-license-atomic-20211217/scrooge-plugin-license-atomic-20211217-1.0.14.tar.gz/scrooge-plugin-license-atomic-20211217-1.0.14/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="scrooge-plugin-license-atomic-20211217",
+    version="1.0.14",
+    packages=["scrooge-plugin-license-atomic-20211217"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
