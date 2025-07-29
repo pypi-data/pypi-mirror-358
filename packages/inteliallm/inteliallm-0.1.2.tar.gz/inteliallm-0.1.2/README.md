5e5a6da44067ac7af65fc48af2917f12bdc0f420
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:44 +0000

    Add package inteliallm/0.1.2/inteliallm-0.1.2.tar.gz

---
+++ packages/inteliallm/inteliallm-0.1.2.tar.gz/inteliallm-0.1.2/README.md
@@ -0,0 +1,1 @@
+Classe para facilitar o acesso ao litellm
---
+++ packages/inteliallm/inteliallm-0.1.2.tar.gz/inteliallm-0.1.2/src/inteliallm/__init__.py
@@ -0,0 +1,6 @@
+# Importa a classe do módulo 'client.py' para o nível do pacote 'inteliallm'.
+from ./client import InteliaLLMClient
+
+# Também é uma boa prática expor a versão do pacote aqui.
+# Poetry pode gerenciar isso para você com plugins, ou você pode definir manualmente.
+__version__ = "0.1.1"
