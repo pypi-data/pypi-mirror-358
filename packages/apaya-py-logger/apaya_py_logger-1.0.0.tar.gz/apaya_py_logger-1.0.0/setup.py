e2f9548589b7d6b74365147999e28a9311854357
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:48 +0000

    Add package apaya-py-logger/1.0.0/apaya_py_logger-1.0.0.tar.gz

---
+++ packages/apaya-py-logger/apaya_py_logger-1.0.0.tar.gz/apaya_py_logger-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="apaya-py-logger",
+    version="1.0.0",
+    packages=find_packages(),
+    author="Your Name",
+    description="Advanced Python logging with compression and rotation",
+    python_requires=">=3.7",
+)
