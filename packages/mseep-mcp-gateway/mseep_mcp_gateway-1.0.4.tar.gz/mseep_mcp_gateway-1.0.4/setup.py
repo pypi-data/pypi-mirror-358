2af7bca76ed2d2f1ef46c371db6d0373946a3873
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:05:01 +0000

    Add package mseep-mcp-gateway/1.0.4/mseep_mcp_gateway-1.0.4.tar.gz

---
+++ packages/mseep-mcp-gateway/mseep_mcp_gateway-1.0.4.tar.gz/mseep_mcp_gateway-1.0.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mseep-mcp-gateway',
+    version='1.0.4',
+    description='A gateway for MCP servers',
+    long_description="Package managed by MseeP.ai",
+    long_description_content_type="text/plain",
+    author='mseep',
+    author_email='support@skydeck.ai',
+    maintainer='mseep',
+    maintainer_email='support@skydeck.ai',
+    url='https://github.com/mseep',
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=['mcp[cli]>=1.6.0'],
+    keywords=['mseep'],
+)
