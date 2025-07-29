5bab91599179b11bf56b50da0e8c2c4f755416f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:06:14 +0000

    Add package vertagus/0.2.0.dev0/vertagus-0.2.0.dev0-py3-none-any.whl

---
+++ packages/vertagus/vertagus-0.2.0.dev0-py3-none-any.whl/vertagus/core/stage.py
@@ -0,0 +1,45 @@
+import typing as T
+from vertagus.core.manifest_base import ManifestBase
+from vertagus.core.rule_bases import SingleVersionRule, VersionComparisonRule
+from vertagus.rules.comparison.library import ManifestsComparisonRule
+from vertagus.core.tag_base import AliasBase
+from .package_base import Package
+
+
+class Stage(Package):
+    
+    def __init__(self,
+                 name: str,
+                 manifests: list[ManifestBase],
+                 current_version_rules: list[T.Type[SingleVersionRule]],
+                 version_increment_rules: list[VersionComparisonRule],
+                 manifest_versions_comparison_rules: list[ManifestsComparisonRule],
+                 aliases: list[type[AliasBase]] = None
+                 ):
+        super().__init__(
+            manifests=manifests,
+            current_version_rules=current_version_rules,
+            version_increment_rules=version_increment_rules,
+            manifest_versions_comparison_rules=manifest_versions_comparison_rules
+        )
+        self.name = name
+        self.aliases = aliases or []
+
+    @property
+    def current_version_rules(self):
+        return self._current_version_rules
+    
+    @property
+    def version_increment_rules(self):
+        return self._version_increment_rules
+    
+    @property
+    def manifest_versions_comparison_rules(self):
+        return self._manifest_versions_comparison_rules
+    
+    @property
+    def manifests(self):
+        return self._manifests
+
+    def get_version_aliases(self, version: str) -> list[AliasBase]:
+        return [alias(version) for alias in self.aliases]
---
+++ packages/vertagus/vertagus-0.2.0.dev0-py3-none-any.whl/vertagus/core/tag_base.py
@@ -0,0 +1,14 @@
+class Tag:
+
+    def __init__(self, tag_text: str):
+        self.tag_text = tag_text
+
+    def as_string(self, prefix: str | None = None) -> str:
+        if prefix:
+            return prefix + self.tag_text
+        return self.tag_text
+
+
+class AliasBase(Tag):
+    name: str = ""
+    description: str = ""
