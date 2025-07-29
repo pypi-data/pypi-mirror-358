e6a3b0d9b9c2653eef18348ed5fa98bbd741ea23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:46 +0000

    Add package arithmath/1.0.0/arithmath-1.0.0-py3-none-any.whl

---
+++ packages/arithmath/arithmath-1.0.0-py3-none-any.whl/arithmath/__init__.py
@@ -0,0 +1,7 @@
+from .basic import add, subtraction, multiply, division
+
+__version__ = '1.0.0'
+__author__ = 'Harsh Jain'
+__email__ = 'harshjacksn@gmail.com'
+
+__all__ = ['add', 'subtraction', 'multiply', 'division']
