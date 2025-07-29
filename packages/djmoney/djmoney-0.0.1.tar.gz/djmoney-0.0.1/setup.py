83182a2ece050f819f4ca7ee7f8ded754ac14f04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:15 +0000

    Add package djmoney/0.0.1/djmoney-0.0.1.tar.gz

---
+++ packages/djmoney/djmoney-0.0.1.tar.gz/djmoney-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="djmoney",
+    version="0.0.1",
+    packages=["djmoney"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
