ea201976c865855c37ef8296b00f95fe3f82d498
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:57 +0000

    Add package datapipelab/0.3.2/datapipelab-0.3.2.tar.gz

---
+++ packages/datapipelab/datapipelab-0.3.2.tar.gz/datapipelab-0.3.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='datapipelab',
+    version='0.3.2',
+    description='A data pipeline library with connectors, sources, processors, and sinks.',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'json5',
+        'loguru',
+        # 'azure-storage-blob',
+        # 'google-cloud-storage',
+        # 'pandas'
+    ],
+)
