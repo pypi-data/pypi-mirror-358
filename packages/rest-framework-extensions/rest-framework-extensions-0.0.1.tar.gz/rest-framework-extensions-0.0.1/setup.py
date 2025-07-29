6b691809cec1a9d9a49f6d848a655d02145e6ce5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:15 +0000

    Add package rest-framework-extensions/0.0.1/rest-framework-extensions-0.0.1.tar.gz

---
+++ packages/rest-framework-extensions/rest-framework-extensions-0.0.1.tar.gz/rest-framework-extensions-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="rest-framework-extensions",
+    version="0.0.1",
+    packages=["rest-framework-extensions"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
