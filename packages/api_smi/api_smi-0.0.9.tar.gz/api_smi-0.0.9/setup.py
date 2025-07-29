1bfd1efca87e584a74c98e8223233b92495e08ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:49 +0000

    Add package api_smi/0.0.9/api_smi-0.0.9.tar.gz

---
+++ packages/api_smi/api_smi-0.0.9.tar.gz/api_smi-0.0.9/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="api_smi",
+    version="0.0.9",
+    packages=["api_smi"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
