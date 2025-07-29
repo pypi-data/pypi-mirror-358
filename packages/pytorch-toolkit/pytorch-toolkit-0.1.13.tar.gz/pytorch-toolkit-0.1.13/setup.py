5709366ae925d240ce734cb46261c59ec2a9c2b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:54 +0000

    Add package pytorch-toolkit/0.1.13/pytorch-toolkit-0.1.13.tar.gz

---
+++ packages/pytorch-toolkit/pytorch-toolkit-0.1.13.tar.gz/pytorch-toolkit-0.1.13/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pytorch-toolkit",
+    version="0.1.13",
+    packages=["pytorch-toolkit"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
