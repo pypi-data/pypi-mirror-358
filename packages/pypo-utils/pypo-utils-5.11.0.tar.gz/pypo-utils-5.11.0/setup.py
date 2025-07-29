e48622718ea612468f92f25965d832e2ca430244
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:06:25 +0000

    Add package pypo-utils/5.11.0/pypo-utils-5.11.0.tar.gz

---
+++ packages/pypo-utils/pypo-utils-5.11.0.tar.gz/pypo-utils-5.11.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pypo-utils",
+    version="5.11.0",
+    packages=["pypo-utils"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
