0868bc284bf958aa19489440cedb6c672d841897
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:04:58 +0000

    Add package bert-perf-test/0.0.1/bert-perf-test-0.0.1.tar.gz

---
+++ packages/bert-perf-test/bert-perf-test-0.0.1.tar.gz/bert-perf-test-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="bert-perf-test",
+    version="0.0.1",
+    packages=["bert-perf-test"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
