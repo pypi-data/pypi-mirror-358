fef860a6de290d38dff7f20d6abf28633b554bd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:08:40 +0000

    Add package pycoro/0.1.0/pycoro-0.1.0.tar.gz

---
+++ packages/pycoro/pycoro-0.1.0.tar.gz/pycoro-0.1.0/src/pycoro/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import annotations
+
+from pycoro.scheduler import Computation, Promise, Scheduler
+from pycoro.typing import typesafe
+
+__all__ = ["Computation", "Promise", "Scheduler", "typesafe"]
---
+++ packages/pycoro/pycoro-0.1.0.tar.gz/pycoro-0.1.0/tests/test_scheduler.py
@@ -0,0 +1,133 @@
+from __future__ import annotations
+
+from collections.abc import Callable
+from typing import TYPE_CHECKING, Any, assert_type
+
+import pytest
+
+from pycoro import Promise, Scheduler, typesafe
+from pycoro.io.function import FunctionIO
+
+if TYPE_CHECKING:
+    from pycoro import Computation
+
+
+def test_coroutine_invocation() -> None:
+    def coroutine(n: int) -> Computation[Callable[[], str], str]:
+        if n == 0:
+            return "I finished"
+
+        foo_promise = yield from typesafe(lambda: f"foo.{n}")
+        assert_type(foo_promise, Promise)
+        bar_promise = yield from typesafe(lambda: f"bar.{n}")
+        assert_type(bar_promise, Promise)
+        baz_promise = yield from typesafe(coroutine(n - 1))
+        assert_type(baz_promise, Promise[str])
+
+        foo = yield from typesafe(foo_promise)
+        assert_type(foo, Any)
+        bar = yield from typesafe(bar_promise)
+        assert_type(bar, Any)
+        baz = yield from typesafe(baz_promise)
+        assert_type(baz, str)
+
+        return f"{foo}.{bar}.{baz}"
+
+    io = FunctionIO[Callable[[], str], str](100)
+    io.worker()
+
+    s = Scheduler(io, 100)
+    n = 10
+    h = s.add(coroutine(n))
+    while s.size() > 0:
+        cqes = io.dequeue(100)
+        for cqe in cqes:
+            cqe.callback(cqe.value)
+
+        s.run_until_blocked(0)
+
+    assert h.result().startswith(f"foo.{n}")
+    assert h.result().endswith("finished")
+
+    s.shutdown()
+    io.shutdown()
+
+
+def test_coroutine_with_failure() -> None:
+    def fail() -> None:
+        raise NotImplementedError
+
+    def coroutine(*, exit: bool) -> Computation[Callable[[], None], None]:
+        if exit:
+            return
+        foo_promise = yield from typesafe(fail)
+        assert_type(foo_promise, Promise)
+        bar_promise = yield from typesafe(coroutine(exit=True))
+        assert_type(bar_promise, Promise[None])
+
+        foo = yield from typesafe(foo_promise)
+        assert_type(foo, Any)
+        bar = yield from (typesafe(bar_promise))
+        assert_type(bar, None)
+
+    io = FunctionIO[Callable[[], None], None](100)
+    io.worker()
+
+    s = Scheduler(io, 100)
+    h = s.add(coroutine(exit=False))
+    while s.size() > 0:
+        cqes = io.dequeue(100)
+        for cqe in cqes:
+            cqe.callback(cqe.value)
+
+        s.run_until_blocked(0)
+
+    with pytest.raises(NotImplementedError):
+        h.result()
+
+    s.shutdown()
+    io.shutdown()
+
+
+def test_function() -> None:
+    io = FunctionIO[Callable[[], str], str](100)
+    io.worker()
+
+    s = Scheduler(io, 100)
+    h = s.add(lambda: "hi")
+    while s.size() > 0:
+        cqes = io.dequeue(100)
+        for cqe in cqes:
+            cqe.callback(cqe.value)
+
+        s.run_until_blocked(0)
+
+    assert h.result() == "hi"
+
+    s.shutdown()
+    io.shutdown()
+
+
+def test_structure_concurrency() -> None:
+    def coro() -> Computation[Callable[[], str], str]:
+        _ = yield from typesafe(lambda: "hi")
+        _ = yield from typesafe(lambda: "hi")
+        _ = yield from typesafe(lambda: "hi")
+        return "I'm done"
+
+    io = FunctionIO[Callable[[], str], str](100)
+    io.worker()
+
+    s = Scheduler(io, 100)
+    h = s.add(coro())
+    while s.size() > 0:
+        cqes = io.dequeue(100)
+        for cqe in cqes:
+            cqe.callback(cqe.value)
+
+        s.run_until_blocked(0)
+
+    assert h.result() == "I'm done"
+
+    s.shutdown()
+    io.shutdown()
