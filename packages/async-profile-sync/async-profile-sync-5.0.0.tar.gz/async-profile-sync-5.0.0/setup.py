87e64c5fc18fb3a8bf5031356f86abb510535958
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:53 +0000

    Add package async-profile-sync/5.0.0/async-profile-sync-5.0.0.tar.gz

---
+++ packages/async-profile-sync/async-profile-sync-5.0.0.tar.gz/async-profile-sync-5.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="async-profile-sync",
+    version="5.0.0",
+    packages=["async-profile-sync"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
