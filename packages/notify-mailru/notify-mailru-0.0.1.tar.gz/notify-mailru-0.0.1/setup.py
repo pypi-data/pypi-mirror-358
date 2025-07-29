d0fc71a0d0c13be41bf02bda274879549497675b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:22 +0000

    Add package notify-mailru/0.0.1/notify-mailru-0.0.1.tar.gz

---
+++ packages/notify-mailru/notify-mailru-0.0.1.tar.gz/notify-mailru-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="notify-mailru",
+    version="0.0.1",
+    packages=["notify-mailru"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
