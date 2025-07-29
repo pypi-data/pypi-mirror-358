a909733926cfd1bf81134a2d2f0f5e1fef70dd50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:42 +0000

    Add package investing-algorithm-framework/6.9.3/investing_algorithm_framework-6.9.3.tar.gz

---
+++ packages/investing-algorithm-framework/investing_algorithm_framework-6.9.3.tar.gz/investing_algorithm_framework-6.9.3/investing_algorithm_framework/infrastructure/database/sql_alchemy.py
@@ -0,0 +1,46 @@
+import logging
+
+from sqlalchemy import create_engine, StaticPool
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+
+from investing_algorithm_framework.domain import SQLALCHEMY_DATABASE_URI, \
+    OperationalException
+
+Session = sessionmaker()
+logger = logging.getLogger("investing_algorithm_framework")
+
+
+class SQLAlchemyAdapter:
+
+    def __init__(self, app):
+        self._app = app
+        if SQLALCHEMY_DATABASE_URI not in app.config \
+                or app.config[SQLALCHEMY_DATABASE_URI] is None:
+            raise OperationalException("SQLALCHEMY_DATABASE_URI not set")
+
+        global Session
+        engine = create_engine(
+            app.config[SQLALCHEMY_DATABASE_URI],
+            connect_args={'check_same_thread': False},
+            poolclass=StaticPool
+        )
+        Session.configure(bind=engine)
+
+
+def setup_sqlalchemy(app, throw_exception_if_not_set=True):
+
+    try:
+        SQLAlchemyAdapter(app)
+    except OperationalException as e:
+        if throw_exception_if_not_set:
+            raise e
+
+    return app
+
+
+class SQLBaseModel(DeclarativeBase):
+    pass
+
+
+def create_all_tables():
+    SQLBaseModel.metadata.create_all(bind=Session().bind)
