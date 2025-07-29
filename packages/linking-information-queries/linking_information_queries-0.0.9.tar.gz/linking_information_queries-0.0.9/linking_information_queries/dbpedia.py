f68d0c1ab9792952d3a3d05980a36b750c556b71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:06:04 +0000

    Add package linking-information-queries/0.0.9/linking_information_queries-0.0.9.tar.gz

---
+++ packages/linking-information-queries/linking_information_queries-0.0.9.tar.gz/linking_information_queries-0.0.9/linking_information_queries/dbpedia.py
@@ -0,0 +1,14 @@
+from .helpers import get_results
+
+def retrieve_dbpedia_url(url):
+    id = url.split("=")[-1].strip()
+    query_dbpedia_url = f"""SELECT ?url
+        WHERE {{
+            ?url dbo:wikiPageID {int(id)} .
+            }}
+            """
+    results = get_results(query_dbpedia_url, "https://dbpedia.org/sparql/")
+    for result in results["results"]["bindings"]:
+        for key, value in result.items():
+            dbpedia_url = value.get("value", "No DBpedia URL found")
+    return dbpedia_url
