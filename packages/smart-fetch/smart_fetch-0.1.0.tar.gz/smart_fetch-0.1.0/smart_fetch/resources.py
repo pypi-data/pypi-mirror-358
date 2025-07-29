0d7dbeb5ba39bef342f25ca548e242b06681b08d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:10 +0000

    Add package smart-fetch/0.1.0/smart_fetch-0.1.0.tar.gz

---
+++ packages/smart-fetch/smart_fetch-0.1.0.tar.gz/smart_fetch-0.1.0/smart_fetch/resources.py
@@ -0,0 +1,39 @@
+ALLERGY_INTOLERANCE = "AllergyIntolerance"
+BINARY = "Binary"
+BUNDLE = "Bundle"
+CONDITION = "Condition"
+DEVICE = "Device"
+DIAGNOSTIC_REPORT = "DiagnosticReport"
+DOCUMENT_REFERENCE = "DocumentReference"
+ENCOUNTER = "Encounter"
+IMMUNIZATION = "Immunization"
+MEDICATION = "Medication"
+MEDICATION_REQUEST = "MedicationRequest"
+OBSERVATION = "Observation"
+OPERATION_OUTCOME = "OperationOutcome"
+PATIENT = "Patient"
+PROCEDURE = "Procedure"
+SERVICE_REQUEST = "ServiceRequest"
+
+# All resources that are linked to patients, in the order we usually like to process them.
+# Patient first, Encounter, then the rest.
+PATIENT_TYPES = [
+    PATIENT,
+    ENCOUNTER,
+    ALLERGY_INTOLERANCE,
+    CONDITION,
+    DEVICE,
+    DIAGNOSTIC_REPORT,
+    DOCUMENT_REFERENCE,
+    IMMUNIZATION,
+    MEDICATION_REQUEST,
+    OBSERVATION,
+    PROCEDURE,
+    SERVICE_REQUEST,
+]
+
+SCOPE_TYPES = {
+    *PATIENT_TYPES,
+    BINARY,
+    MEDICATION,
+}
