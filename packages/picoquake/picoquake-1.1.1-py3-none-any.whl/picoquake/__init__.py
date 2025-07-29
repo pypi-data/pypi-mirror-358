699c52acd93a0cbfef41629b0c53a787bf34d015
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:27 +0000

    Add package picoquake/1.1.1/picoquake-1.1.1-py3-none-any.whl

---
+++ packages/picoquake/picoquake-1.1.1-py3-none-any.whl/picoquake/__init__.py
@@ -0,0 +1,6 @@
+from .interface import PicoQuake
+from .configuration import SampleRate, Filter, AccRange, GyroRange
+from .data import AcquisitionData, IMUSample
+from .plot import *
+
+__version__ = "1.1.1"
