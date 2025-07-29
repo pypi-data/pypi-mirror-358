72fd0da78bdbbfe551034ae0659e93099fbffff9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:41 +0000

    Add package edc-transfer/1.0.1/edc_transfer-1.0.1.tar.gz

---
+++ packages/edc-transfer/edc_transfer-1.0.1.tar.gz/edc_transfer-1.0.1/README.rst
@@ -0,0 +1,19 @@
+|pypi| |actions| |codecov| |downloads|
+
+edc-transfer
+-------------
+
+Patient transfer admin, forms, model mixins
+
+
+.. |pypi| image:: https://img.shields.io/pypi/v/edc-transfer.svg
+    :target: https://pypi.python.org/pypi/edc-transfer
+
+.. |actions| image:: https://github.com/clinicedc/edc-transfer/actions/workflows/build.yml/badge.svg
+  :target: https://github.com/clinicedc/edc-transfer/actions/workflows/build.yml
+
+.. |codecov| image:: https://codecov.io/gh/clinicedc/edc-transfer/branch/develop/graph/badge.svg
+  :target: https://codecov.io/gh/clinicedc/edc-transfer
+
+.. |downloads| image:: https://pepy.tech/badge/edc-transfer
+   :target: https://pepy.tech/project/edc-transfer
---
+++ packages/edc-transfer/edc_transfer-1.0.1.tar.gz/edc_transfer-1.0.1/edc_transfer/tests/test_settings.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+import sys
+from pathlib import Path
+
+from edc_test_settings.default_test_settings import DefaultTestSettings
+
+app_name = "edc_transfer"
+base_dir = Path(__file__).absolute().parent.parent.parent
+
+project_settings = DefaultTestSettings(
+    calling_file=__file__,
+    BASE_DIR=base_dir,
+    APP_NAME=app_name,
+    ETC_DIR=base_dir / app_name / "tests" / "etc",
+    SUBJECT_SCREENING_MODEL="edc_metadata.subjectscreening",
+    SUBJECT_CONSENT_MODEL="edc_metadata.subjectconsent",
+    SUBJECT_VISIT_MODEL="edc_visit_tracking.subjectvisit",
+    SUBJECT_VISIT_MISSED_MODEL="edc_metadata.subjectvisitmissed",
+    LIST_MODEL_APP_LABEL="edc_transfer",
+    EDC_SITES_REGISTER_DEFAULT=True,
+    INSTALLED_APPS=[
+        "django.contrib.admin",
+        "django.contrib.auth",
+        "django.contrib.contenttypes",
+        "django.contrib.sessions",
+        "django.contrib.messages",
+        "django.contrib.staticfiles",
+        "django.contrib.sites",
+        "django_crypto_fields.apps.AppConfig",
+        "django_revision.apps.AppConfig",
+        "multisite",
+        "edc_appointment.apps.AppConfig",
+        "edc_action_item.apps.AppConfig",
+        "edc_crf.apps.AppConfig",
+        "edc_device.apps.AppConfig",
+        "edc_dashboard.apps.AppConfig",
+        "edc_facility.apps.AppConfig",
+        "edc_lab.apps.AppConfig",
+        "edc_list_data.apps.AppConfig",
+        "edc_metadata.apps.AppConfig",
+        "edc_offstudy.apps.AppConfig",
+        "edc_registration.apps.AppConfig",
+        "edc_identifier.apps.AppConfig",
+        "edc_notification.apps.AppConfig",
+        "edc_sites.apps.AppConfig",
+        "edc_timepoint.apps.AppConfig",
+        "edc_visit_schedule.apps.AppConfig",
+        "edc_visit_tracking.apps.AppConfig",
+        "edc_transfer.apps.AppConfig",
+    ],
+    RANDOMIZATION_LIST_PATH=base_dir / app_name / "tests" / "test_randomization_list.csv",
+    add_dashboard_middleware=True,
+    use_test_urls=True,
+).settings
+
+for k, v in project_settings.items():
+    setattr(sys.modules[__name__], k, v)
