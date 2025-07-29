482d92b29b04b5909562d910567e750cee7f487d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:06:01 +0000

    Add package greater-tables/5.2.0/greater_tables-5.2.0-py3-none-any.whl

---
+++ packages/greater-tables/greater_tables-5.2.0-py3-none-any.whl/greater_tables/__init__.py
@@ -0,0 +1,8 @@
+__project__ = 'greater_tables'
+__author__ = 'Stephen J Mildenhall'
+__version__ = '5.2.0'
+
+from . _version import  __version__
+from . core import GT
+from . fabrications import Fabricator
+from . etcher import Etcher
