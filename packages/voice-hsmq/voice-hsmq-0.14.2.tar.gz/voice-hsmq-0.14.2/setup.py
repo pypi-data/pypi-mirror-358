7cf104d03e0b894227fcb6f436e7751c2caffb62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:22 +0000

    Add package voice-hsmq/0.14.2/voice-hsmq-0.14.2.tar.gz

---
+++ packages/voice-hsmq/voice-hsmq-0.14.2.tar.gz/voice-hsmq-0.14.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="voice-hsmq",
+    version="0.14.2",
+    packages=["voice-hsmq"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
