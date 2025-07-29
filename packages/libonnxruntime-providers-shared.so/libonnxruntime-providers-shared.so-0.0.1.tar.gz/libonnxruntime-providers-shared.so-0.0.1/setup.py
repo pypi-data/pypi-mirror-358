dc1d4f3b1dfe40c27b903b49159608aaff30e172
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:02 +0000

    Add package libonnxruntime-providers-shared.so/0.0.1/libonnxruntime-providers-shared.so-0.0.1.tar.gz

---
+++ packages/libonnxruntime-providers-shared.so/libonnxruntime-providers-shared.so-0.0.1.tar.gz/libonnxruntime-providers-shared.so-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="libonnxruntime-providers-shared.so",
+    version="0.0.1",
+    packages=["libonnxruntime-providers-shared.so"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
