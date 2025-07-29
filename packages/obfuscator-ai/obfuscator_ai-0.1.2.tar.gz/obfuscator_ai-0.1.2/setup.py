4c3ec5d444d45321eb19374439888bced5f0f336
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:59 +0000

    Add package obfuscator-ai/0.1.2/obfuscator_ai-0.1.2.tar.gz

---
+++ packages/obfuscator-ai/obfuscator_ai-0.1.2.tar.gz/obfuscator_ai-0.1.2/setup.py
@@ -0,0 +1,36 @@
+﻿from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="obfuscator-ai",
+    version="0.1.2",
+    author="911TheStalker",
+    author_email="webinadvance@gmail.com",
+    description="AI-powered code obfuscation utility",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        # Add your dependencies here
+    ],
+    entry_points={
+        "console_scripts": [
+            "obfuscator-ai=obfuscator_ai.main:main",
+        ],
+    },
+)
