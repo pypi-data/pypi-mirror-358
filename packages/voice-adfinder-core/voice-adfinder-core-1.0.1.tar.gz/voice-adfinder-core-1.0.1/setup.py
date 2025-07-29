00c9195dc73b59558556e6ea0d39450665fea2a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:14 +0000

    Add package voice-adfinder-core/1.0.1/voice-adfinder-core-1.0.1.tar.gz

---
+++ packages/voice-adfinder-core/voice-adfinder-core-1.0.1.tar.gz/voice-adfinder-core-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="voice-adfinder-core",
+    version="1.0.1",
+    packages=["voice-adfinder-core"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
