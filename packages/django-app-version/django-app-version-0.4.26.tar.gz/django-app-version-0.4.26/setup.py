f419d68d4badf0001082f2e65078f68a6f1997fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:06:15 +0000

    Add package django-app-version/0.4.26/django-app-version-0.4.26.tar.gz

---
+++ packages/django-app-version/django-app-version-0.4.26.tar.gz/django-app-version-0.4.26/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="django-app-version",
+    version="0.4.26",
+    packages=["django-app-version"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
