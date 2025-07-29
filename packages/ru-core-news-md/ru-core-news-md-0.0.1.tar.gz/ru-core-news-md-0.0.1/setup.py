127ab145f5f4615282663e7cd355d451bc6c4e4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:20 +0000

    Add package ru-core-news-md/0.0.1/ru-core-news-md-0.0.1.tar.gz

---
+++ packages/ru-core-news-md/ru-core-news-md-0.0.1.tar.gz/ru-core-news-md-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ru-core-news-md",
+    version="0.0.1",
+    packages=["ru-core-news-md"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
