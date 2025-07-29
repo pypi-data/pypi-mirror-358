b79cd641b3e568116a8cbed50c5eb5eab443e761
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:46 +0000

    Add package external-auth-utils/1.1.0/external-auth-utils-1.1.0.tar.gz

---
+++ packages/external-auth-utils/external-auth-utils-1.1.0.tar.gz/external-auth-utils-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="external-auth-utils",
+    version="1.1.0",
+    packages=["external-auth-utils"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
