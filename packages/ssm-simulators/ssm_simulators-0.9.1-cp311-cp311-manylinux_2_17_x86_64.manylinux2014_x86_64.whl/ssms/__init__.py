0f2713c6a02d4e6706da8a478b4eb974b822722f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:06:39 +0000

    Add package ssm-simulators/0.9.1/ssm_simulators-0.9.1-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/ssm-simulators/ssm_simulators-0.9.1-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/ssms/__init__.py
@@ -0,0 +1,5 @@
+from . import basic_simulators, config, dataset_generators, support_utils  # noqa: D104
+
+__version__ = "0.9.1"
+
+__all__ = ["basic_simulators", "dataset_generators", "config", "support_utils"]
