619ae6b0677ec143518c13bf14a4d270e4151df0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:40 +0000

    Add package observability/0.0.6/observability-0.0.6.tar.gz

---
+++ packages/observability/observability-0.0.6.tar.gz/observability-0.0.6/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="observability",
+    version="0.0.6",
+    packages=["observability"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
