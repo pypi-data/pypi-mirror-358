ecd6db4c6b35d6ca1dfd9a18a07bc16d67449b5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:19 +0000

    Add package chyt-connect/0.0.1/chyt-connect-0.0.1.tar.gz

---
+++ packages/chyt-connect/chyt-connect-0.0.1.tar.gz/chyt-connect-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="chyt-connect",
+    version="0.0.1",
+    packages=["chyt-connect"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
