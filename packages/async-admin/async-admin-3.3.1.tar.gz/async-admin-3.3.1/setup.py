7d249f0df76708406db14bf6ab89cc2975eea447
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:53 +0000

    Add package async-admin/3.3.1/async-admin-3.3.1.tar.gz

---
+++ packages/async-admin/async-admin-3.3.1.tar.gz/async-admin-3.3.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="async-admin",
+    version="3.3.1",
+    packages=["async-admin"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
