b81e74440085797c36ded9b82f391610549e5d72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:06:07 +0000

    Add package triton-python-backend-utils/0.0.1/triton-python-backend-utils-0.0.1.tar.gz

---
+++ packages/triton-python-backend-utils/triton-python-backend-utils-0.0.1.tar.gz/triton-python-backend-utils-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="triton-python-backend-utils",
+    version="0.0.1",
+    packages=["triton-python-backend-utils"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
