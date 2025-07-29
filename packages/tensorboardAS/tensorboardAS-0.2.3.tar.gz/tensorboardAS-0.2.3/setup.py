18732e920bf248203259c288a2d774be111d7f9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:06:42 +0000

    Add package tensorboardAS/0.2.3/tensorboardAS-0.2.3.tar.gz

---
+++ packages/tensorboardAS/tensorboardAS-0.2.3.tar.gz/tensorboardAS-0.2.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="tensorboardAS",
+    version="0.2.3",
+    packages=["tensorboardAS"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
