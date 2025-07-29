b70fe1c90a7ba1b51b831ca7e324b6a12e7c7d47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:50 +0000

    Add package thetakde/0.3.4/thetakde-0.3.4-py3-none-any.whl

---
+++ packages/thetakde/thetakde-0.3.4-py3-none-any.whl/thetakde/__init__.py
@@ -0,0 +1,10 @@
+# ThetaKDE/__init__.py
+
+from .DCT import dct, idct
+from .thetakde import kde
+
+__version__ = '0.3.4'
+__author__ = 'Dirk Kroese'
+
+def initialize():
+    print("ThetaKDE package initialized")
