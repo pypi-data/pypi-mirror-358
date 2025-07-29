d0bdd8cac828877969abc9b6a6d1d16d8bd99afa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:06:17 +0000

    Add package pypo-entrypoint/6.0.2/pypo-entrypoint-6.0.2.tar.gz

---
+++ packages/pypo-entrypoint/pypo-entrypoint-6.0.2.tar.gz/pypo-entrypoint-6.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pypo-entrypoint",
+    version="6.0.2",
+    packages=["pypo-entrypoint"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
