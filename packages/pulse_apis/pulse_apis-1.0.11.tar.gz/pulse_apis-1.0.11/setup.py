b6a33688a9d632a6414a3c510eb4ca0bbf464e7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:49 +0000

    Add package pulse_apis/1.0.11/pulse_apis-1.0.11.tar.gz

---
+++ packages/pulse_apis/pulse_apis-1.0.11.tar.gz/pulse_apis-1.0.11/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pulse_apis",
+    version="1.0.11",
+    packages=["pulse_apis"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
