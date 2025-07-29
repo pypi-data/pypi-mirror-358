deb35f1333e80f283112e5790fad284b5a96c965
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:22 +0000

    Add package nlp-data-natasha/1.0.5/nlp-data-natasha-1.0.5.tar.gz

---
+++ packages/nlp-data-natasha/nlp-data-natasha-1.0.5.tar.gz/nlp-data-natasha-1.0.5/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="nlp-data-natasha",
+    version="1.0.5",
+    packages=["nlp-data-natasha"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
