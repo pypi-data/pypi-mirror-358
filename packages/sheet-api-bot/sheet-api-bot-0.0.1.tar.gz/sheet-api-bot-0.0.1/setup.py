ce91bd7f8e4e7dceba2bf712a82b3a23366ede37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:02 +0000

    Add package sheet-api-bot/0.0.1/sheet-api-bot-0.0.1.tar.gz

---
+++ packages/sheet-api-bot/sheet-api-bot-0.0.1.tar.gz/sheet-api-bot-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="sheet-api-bot",
+    version="0.0.1",
+    packages=["sheet-api-bot"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
