5debf10d7f05f640ee5de3a87862ab16a8a082f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:06:15 +0000

    Add package voice-mrs-sdk/0.4.0/voice-mrs-sdk-0.4.0.tar.gz

---
+++ packages/voice-mrs-sdk/voice-mrs-sdk-0.4.0.tar.gz/voice-mrs-sdk-0.4.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="voice-mrs-sdk",
+    version="0.4.0",
+    packages=["voice-mrs-sdk"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
