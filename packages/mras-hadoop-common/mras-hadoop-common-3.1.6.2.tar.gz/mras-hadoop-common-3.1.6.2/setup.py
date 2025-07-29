4cccebd29abcd56165efc93b02572d29df71eed0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:59 +0000

    Add package mras-hadoop-common/3.1.6.2/mras-hadoop-common-3.1.6.2.tar.gz

---
+++ packages/mras-hadoop-common/mras-hadoop-common-3.1.6.2.tar.gz/mras-hadoop-common-3.1.6.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="mras-hadoop-common",
+    version="3.1.6.2",
+    packages=["mras-hadoop-common"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
