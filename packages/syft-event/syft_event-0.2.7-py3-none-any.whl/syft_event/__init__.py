831213338529f1f23443db6cd622911cd3382971
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:06:03 +0000

    Add package syft-event/0.2.7/syft_event-0.2.7-py3-none-any.whl

---
+++ packages/syft-event/syft_event-0.2.7-py3-none-any.whl/syft_event/__init__.py
@@ -0,0 +1,6 @@
+from .router import EventRouter
+from .server2 import SyftEvents
+from .types import Request, Response
+
+__version__ = "0.2.7"
+__all__ = ["SyftEvents", "Request", "Response", "EventRouter"]
