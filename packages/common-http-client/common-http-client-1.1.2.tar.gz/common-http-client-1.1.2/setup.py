fb6c4b7d8bccc1ed6afa8d82e6a9c276a18ad182
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:06:10 +0000

    Add package common-http-client/1.1.2/common-http-client-1.1.2.tar.gz

---
+++ packages/common-http-client/common-http-client-1.1.2.tar.gz/common-http-client-1.1.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="common-http-client",
+    version="1.1.2",
+    packages=["common-http-client"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
