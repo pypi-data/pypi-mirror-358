3b67100240effee357b213440413a78f46619fb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:22 +0000

    Add package voice-fsm/0.0.37/voice-fsm-0.0.37.tar.gz

---
+++ packages/voice-fsm/voice-fsm-0.0.37.tar.gz/voice-fsm-0.0.37/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="voice-fsm",
+    version="0.0.37",
+    packages=["voice-fsm"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
