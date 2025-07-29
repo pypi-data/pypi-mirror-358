20c20713885460a7ec07e9c73d8e10bdf40c176f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:06:23 +0000

    Add package pypo-httpradius/1.1.2/pypo-httpradius-1.1.2.tar.gz

---
+++ packages/pypo-httpradius/pypo-httpradius-1.1.2.tar.gz/pypo-httpradius-1.1.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pypo-httpradius",
+    version="1.1.2",
+    packages=["pypo-httpradius"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
