7020f4b94549eaa0d3aa176678df9eed85d98582
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:04:56 +0000

    Add package commons-collections/3.2.2/commons-collections-3.2.2.tar.gz

---
+++ packages/commons-collections/commons-collections-3.2.2.tar.gz/commons-collections-3.2.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="commons-collections",
+    version="3.2.2",
+    packages=["commons-collections"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
