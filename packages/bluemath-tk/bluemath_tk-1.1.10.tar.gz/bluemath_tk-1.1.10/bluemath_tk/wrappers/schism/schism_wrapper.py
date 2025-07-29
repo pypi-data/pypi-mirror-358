09cb1d41cf825999d5acf8141c550f3ab839320d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:11 +0000

    Add package bluemath-tk/1.1.10/bluemath_tk-1.1.10.tar.gz

---
+++ packages/bluemath-tk/bluemath_tk-1.1.10.tar.gz/bluemath_tk-1.1.10/bluemath_tk/wrappers/schism/schism_wrapper.py
@@ -0,0 +1,63 @@
+import os
+
+from .._base_wrappers import BaseModelWrapper
+
+
+class SchismModelWrapper(BaseModelWrapper):
+    """
+    Wrapper for the Schism model.
+
+    Attributes
+    ----------
+    default_parameters : dict
+        The default parameters type for the wrapper.
+    available_launchers : dict
+        The available launchers for the wrapper.
+    """
+
+    default_parameters = {}
+
+    available_launchers = {"geoocean-cluster": "launchSchism.sh"}
+
+    def __init__(
+        self,
+        templates_dir: str,
+        metamodel_parameters: dict,
+        fixed_parameters: dict,
+        output_dir: str,
+        templates_name: dict = "all",
+        debug: bool = True,
+    ) -> None:
+        """
+        Initialize the Schism model wrapper.
+        """
+
+        super().__init__(
+            templates_dir=templates_dir,
+            metamodel_parameters=metamodel_parameters,
+            fixed_parameters=fixed_parameters,
+            output_dir=output_dir,
+            templates_name=templates_name,
+            default_parameters=self.default_parameters,
+        )
+        self.set_logger_name(
+            name=self.__class__.__name__, level="DEBUG" if debug else "INFO"
+        )
+
+    def build_case(
+        self,
+        case_context: dict,
+        case_dir: str,
+    ) -> None:
+        """
+        Build the input files for a case.
+
+        Parameters
+        ----------
+        case_context : dict
+            The case context.
+        case_dir : str
+            The case directory.
+        """
+
+        os.mkdir(os.path.join(case_dir, "outputs"), exist_ok=True)
