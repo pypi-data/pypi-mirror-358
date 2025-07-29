f4407e34c033cfa9af949c2210991307c7bb2013
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:06:17 +0000

    Add package voice-common/1.172.0/voice-common-1.172.0.tar.gz

---
+++ packages/voice-common/voice-common-1.172.0.tar.gz/voice-common-1.172.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="voice-common",
+    version="1.172.0",
+    packages=["voice-common"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
