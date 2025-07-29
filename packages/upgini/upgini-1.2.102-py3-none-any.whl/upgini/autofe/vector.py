d739b10d53c43709e8a7bdf4c5ec2e150854a975
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:14 +0000

    Add package upgini/1.2.102/upgini-1.2.102-py3-none-any.whl

---
+++ packages/upgini/upgini-1.2.102-py3-none-any.whl/upgini/autofe/vector.py
@@ -0,0 +1,45 @@
+from typing import List, Optional
+
+import pandas as pd
+
+from upgini.autofe.operator import OperatorRegistry, PandasOperator, VectorizableMixin
+
+
+class Mean(PandasOperator, VectorizableMixin):
+    name: str = "mean"
+    output_type: Optional[str] = "float"
+    is_vector: bool = True
+    group_index: int = 0
+
+    def calculate_vector(self, data: List[pd.Series]) -> pd.Series:
+        return pd.DataFrame(data).T.fillna(0).mean(axis=1)
+
+
+class Sum(PandasOperator, VectorizableMixin):
+    name: str = "sum"
+    is_vector: bool = True
+    group_index: int = 0
+
+    def calculate_vector(self, data: List[pd.Series]) -> pd.Series:
+        return pd.DataFrame(data).T.fillna(0).sum(axis=1)
+
+
+class Vectorize(PandasOperator, VectorizableMixin):
+    name: str = "vectorize"
+    is_vector: bool = True
+    group_index: int = 0
+
+    def calculate_vector(self, data: List[pd.Series]) -> pd.Series:
+        return pd.DataFrame(data).T.apply(lambda x: x.to_list(), axis=1)
+
+
+class OnnxModel(PandasOperator, metaclass=OperatorRegistry):
+    name: str = "onnx"
+    is_vector: bool = True
+    output_type: Optional[str] = "float"
+
+
+class CatboostModel(PandasOperator, metaclass=OperatorRegistry):
+    name: str = "catboost"
+    is_vector: bool = True
+    output_type: Optional[str] = "float"
