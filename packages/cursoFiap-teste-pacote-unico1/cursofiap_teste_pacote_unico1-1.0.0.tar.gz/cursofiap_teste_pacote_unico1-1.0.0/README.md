e20695736f5a69501209116e5af6cb28248fc3ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:05:22 +0000

    Add package cursoFiap-teste-pacote-unico1/1.0.0/cursofiap_teste_pacote_unico1-1.0.0.tar.gz

---
+++ packages/cursoFiap-teste-pacote-unico1/cursofiap_teste_pacote_unico1-1.0.0.tar.gz/cursofiap_teste_pacote_unico1-1.0.0/README.md
@@ -0,0 +1,15 @@
+# CursoFiap
+A simple example library.
+## Installation
+```sh
+pip install cursoFiap
+```
+## Usage
+```python
+from cursoFiap import hello_world
+print(hello_world())
+```
+
+## Licença
+
+Distribuído sob a licença MIT.
---
+++ packages/cursoFiap-teste-pacote-unico1/cursofiap_teste_pacote_unico1-1.0.0.tar.gz/cursofiap_teste_pacote_unico1-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+setup(
+    name='cursoFiap-teste-pacote-unico1',
+    version='1.0.0',
+    packages=find_packages(),
+    description='Descricao da sua lib cursoFiap',
+    author='Rafael Lua',
+    author_email='seu.email@example.com',
+    url='',  
+    license='MIT',  
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
