8da4d4d490083e5075222f196a9a8fa9b77c22a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:52 +0000

    Add package pyterminal-emulator/1.0.2/pyterminal_emulator-1.0.2.tar.gz

---
+++ packages/pyterminal-emulator/pyterminal_emulator-1.0.2.tar.gz/pyterminal_emulator-1.0.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pyterminal-emulator",  # Replace with a unique name for your package
+    version="1.0.2",  # Start with 0.0.1 and increment for new releases
+    author="Aarav Shah",  # Replace with your name
+    author_email="aaravprogrammers@gmail.com",  # Replace with your email
+    description="A Linux Terminal Emulator development Package for various os",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ap1311/pyterminal-emulator",  # Optional: Link to your GitHub repo
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",  # Or whatever license you choose
+        "Operating System :: OS Independent",
+        "Topic :: Utilities",
+        "Topic :: System :: Shells",
+    ],
+    install_requires=[
+        'requests',
+        'pycurl',
+    ],
+    entry_points={
+        'console_scripts': [
+            'pyterminalx=pyterminalx.commands:main',  # Example
+        ],
+    },
+)
