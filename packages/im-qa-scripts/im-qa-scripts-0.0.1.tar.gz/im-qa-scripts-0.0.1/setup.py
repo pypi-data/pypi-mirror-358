dc24062219a8e8c2b7a80009b01fc1473ab297e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:20 +0000

    Add package im-qa-scripts/0.0.1/im-qa-scripts-0.0.1.tar.gz

---
+++ packages/im-qa-scripts/im-qa-scripts-0.0.1.tar.gz/im-qa-scripts-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="im-qa-scripts",
+    version="0.0.1",
+    packages=["im-qa-scripts"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
