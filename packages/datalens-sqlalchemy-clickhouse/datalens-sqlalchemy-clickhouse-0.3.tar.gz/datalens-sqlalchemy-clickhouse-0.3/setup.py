66e5a19c45996214bc172b331a6d4f92ec804a55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:05:19 +0000

    Add package datalens-sqlalchemy-clickhouse/0.3/datalens-sqlalchemy-clickhouse-0.3.tar.gz

---
+++ packages/datalens-sqlalchemy-clickhouse/datalens-sqlalchemy-clickhouse-0.3.tar.gz/datalens-sqlalchemy-clickhouse-0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="datalens-sqlalchemy-clickhouse",
+    version="0.3",
+    packages=["datalens-sqlalchemy-clickhouse"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
