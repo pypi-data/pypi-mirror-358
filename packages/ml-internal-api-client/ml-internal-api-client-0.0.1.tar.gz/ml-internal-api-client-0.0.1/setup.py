9e769c037b0fbb1848d2180905b0751091ddde0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:27 +0000

    Add package ml-internal-api-client/0.0.1/ml-internal-api-client-0.0.1.tar.gz

---
+++ packages/ml-internal-api-client/ml-internal-api-client-0.0.1.tar.gz/ml-internal-api-client-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ml-internal-api-client",
+    version="0.0.1",
+    packages=["ml-internal-api-client"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
