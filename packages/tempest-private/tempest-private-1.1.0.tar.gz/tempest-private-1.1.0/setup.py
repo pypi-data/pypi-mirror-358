efc5ce1b870204b59fab7ec3ae4b09857f6d4c08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:39 +0000

    Add package tempest-private/1.1.0/tempest-private-1.1.0.tar.gz

---
+++ packages/tempest-private/tempest-private-1.1.0.tar.gz/tempest-private-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="tempest-private",
+    version="1.1.0",
+    packages=["tempest-private"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
