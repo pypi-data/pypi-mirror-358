a95898fd3dfacf2a57d5116da9d173763db93191
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:06:12 +0000

    Add package RegressionLibrary/0.1/RegressionLibrary-0.1.tar.gz

---
+++ packages/RegressionLibrary/RegressionLibrary-0.1.tar.gz/RegressionLibrary-0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup 
+
+
+setup(
+    name='RegressionLibrary',
+    version='0.1',
+    description='Educational regression models built from scratch in Python',
+    long_description_content_type='text/markdown',
+    author='Ali Khalid Ali Khalid',
+    author_email='ali88883737@gmail.com',
+    url = 'https://github.com/Alouakhalid/RegressionLibrary',
+    py_modules=['Regression'],
+    install_requires=[
+        'numpy',
+        'tqdm'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
