c748f83874097c6a35319b0594ee325f6fbef289
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:20 +0000

    Add package pyxfr/0.6.3/pyxfr-0.6.3-cp313-none-macosx_13_0_arm64.whl

---
+++ packages/pyxfr/pyxfr-0.6.3-cp313-none-macosx_13_0_arm64.whl/pyxfr/__init__.py
@@ -0,0 +1,12 @@
+"""
+pyxfr: the transferase Python API. For detailed help, see individual modules
+within this package:
+
+  >>> from pyxfr import pyxfr, pyxfr_utils
+  >>> help(pyxfr)
+  >>> help(pyxfr_utils)
+"""
+
+from .pyxfr import *
+
+from .pyxfr_utils import generate_bins, load_methbase_metadata
