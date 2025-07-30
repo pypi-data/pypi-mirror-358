25530027f99e8573ebffdd22936d55f265e2f56a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:39 +0000

    Add package simulation-job-batch/0.0.1/simulation-job-batch-0.0.1.tar.gz

---
+++ packages/simulation-job-batch/simulation-job-batch-0.0.1.tar.gz/simulation-job-batch-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="simulation-job-batch",
+    version="0.0.1",
+    packages=["simulation-job-batch"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
