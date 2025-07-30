a18209b433470cc3957f94949b084e28bd051451
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:18 +0000

    Add package log-brewery-service/0.0.1/log-brewery-service-0.0.1.tar.gz

---
+++ packages/log-brewery-service/log-brewery-service-0.0.1.tar.gz/log-brewery-service-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="log-brewery-service",
+    version="0.0.1",
+    packages=["log-brewery-service"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
