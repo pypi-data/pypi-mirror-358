a700f2c670bb5d3aa1dd98b3c345c4954c5f1305
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:28 +0000

    Add package ublib/0.0.1/ublib-0.0.1.tar.gz

---
+++ packages/ublib/ublib-0.0.1.tar.gz/ublib-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ublib",
+    version="0.0.1",
+    packages=["ublib"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
