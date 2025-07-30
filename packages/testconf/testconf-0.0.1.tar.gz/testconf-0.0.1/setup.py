6a68de38e111d341640a02acee27c11d02db32c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:30 +0000

    Add package testconf/0.0.1/testconf-0.0.1.tar.gz

---
+++ packages/testconf/testconf-0.0.1.tar.gz/testconf-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="testconf",
+    version="0.0.1",
+    packages=["testconf"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
