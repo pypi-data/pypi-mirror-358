3c6fa90e55ae8983bd27a8703a1dd8c3594c5923
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:35 +0000

    Add package datalens-connector-chyt/0.0.1/datalens-connector-chyt-0.0.1.tar.gz

---
+++ packages/datalens-connector-chyt/datalens-connector-chyt-0.0.1.tar.gz/datalens-connector-chyt-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="datalens-connector-chyt",
+    version="0.0.1",
+    packages=["datalens-connector-chyt"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
