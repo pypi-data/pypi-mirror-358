ce2b7ab31cb30be1fdde1d1ba28f00064148fcfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:39 +0000

    Add package pyvkteamsclient/2024.5.6/pyvkteamsclient-2024.5.6.tar.gz

---
+++ packages/pyvkteamsclient/pyvkteamsclient-2024.5.6.tar.gz/pyvkteamsclient-2024.5.6/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pyvkteamsclient",
+    version="2024.5.6",
+    packages=["pyvkteamsclient"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
