8f3a3721b45ce2fbd6444b860cb9273ca2173e59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:55 +0000

    Add package cabaret/0.2.0/cabaret-0.2.0-py3-none-any.whl

---
+++ packages/cabaret/cabaret-0.2.0-py3-none-any.whl/cabaret/__init__.py
@@ -0,0 +1,5 @@
+from cabaret.image import generate_image
+from cabaret.observatory import Observatory
+from cabaret.camera import Camera
+from cabaret.site import Site
+from cabaret.telescope import Telescope
