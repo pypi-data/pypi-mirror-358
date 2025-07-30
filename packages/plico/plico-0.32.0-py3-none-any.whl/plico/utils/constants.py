6e191d504d218a238218cead7b8a9712dc4b2911
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:09:22 +0000

    Add package plico/0.32.0/plico-0.32.0-py3-none-any.whl

---
+++ packages/plico/plico-0.32.0-py3-none-any.whl/plico/utils/constants.py
@@ -0,0 +1,12 @@
+
+import pickle
+
+class Constants:
+    PICKLE_PROTOCOL=  pickle.HIGHEST_PROTOCOL
+    APP_NAME= "inaf.arcetri.ao.plico"
+    APP_AUTHOR= "INAF Arcetri Adaptive Optics"
+
+    PORT_REPLY_OFFSET= 0
+    PORT_STATUS_OFFSET= 1
+    PORT_PUBLISHER_OFFSET= 2
+    PORT_DISPLAY_OFFSET= 3
