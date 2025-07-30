2c762ad652e57fce008e960e968b47a3f9f4e969
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:28 +0000

    Add package auto-diffusers/2.0.27/auto_diffusers-2.0.27.tar.gz

---
+++ packages/auto-diffusers/auto_diffusers-2.0.27.tar.gz/auto_diffusers-2.0.27/setup.py
@@ -0,0 +1,48 @@
+import os
+import re
+import sys
+
+from setuptools import find_packages, setup
+
+_deps = [
+    "diffusers>=0.32.0",
+    "transformers>=4.41.2",
+]
+
+deps = {b: a for a, b in (re.findall(r"^(([^!=<>~]+)(?:[!=<>~].*)?$)", x)[0] for x in _deps)}
+
+
+def deps_list(*pkgs):
+    return [deps[pkg] for pkg in pkgs]
+
+
+version_range_max = max(sys.version_info[1], 10) + 1
+
+setup(
+    name="auto_diffusers",
+    version="2.0.27",
+    description="diffusers with search engine",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    keywords="huggingface civitai diffusers model search deep learning diffusion pytorch stable diffusion",
+    license="Apache 2.0 License",
+    author="suzukimain",
+    author_email="subarucosmosmain@gmail.com",
+    url="https://github.com/suzukimain/auto_diffusers",
+    package_dir={"": "src"},
+    packages=find_packages("src"),
+    package_data={"": ["py.typed"]},
+    python_requires=">=3.8.0",
+    include_package_data=True,
+    install_requires=list(_deps),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: Apache Software License",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Programming Language :: Python :: 3",
+    ]
+    + [f"Programming Language :: Python :: 3.{i}" for i in range(8, version_range_max)]  
+)
