e44d20beae8b789ff654b81dd806b821b2129886
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:23 +0000

    Add package execstats/0.0.1/execstats-0.0.1.tar.gz

---
+++ packages/execstats/execstats-0.0.1.tar.gz/execstats-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="execstats",
+    version="0.0.1",
+    packages=["execstats"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
