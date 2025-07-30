fcf98ed96d28db883ddae1000610bd5936e2546a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:07:00 +0000

    Add package vkteam-api-client/1.0.1/vkteam-api-client-1.0.1.tar.gz

---
+++ packages/vkteam-api-client/vkteam-api-client-1.0.1.tar.gz/vkteam-api-client-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="vkteam-api-client",
+    version="1.0.1",
+    packages=["vkteam-api-client"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
