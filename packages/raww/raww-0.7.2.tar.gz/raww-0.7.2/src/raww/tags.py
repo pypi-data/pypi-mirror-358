977da4f6b9a13df28526c2baf91c499d783aefe6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:11:27 +0000

    Add package raww/0.7.2/raww-0.7.2.tar.gz

---
+++ packages/raww/raww-0.7.2.tar.gz/raww-0.7.2/src/raww/tags.py
@@ -0,0 +1,36 @@
+import click
+
+from .data import rewrite_data, get_tags, get_sessions, get_active_session
+
+
+@click.command('tags')
+@click.option('--new')
+def tag(
+    new: str
+):
+
+    mytags = get_tags()
+
+    if new:
+        if new in mytags:
+            click.echo(f'🦇 tag {new} already exists')
+            exit(1)
+        else:
+            mysessions = get_sessions()
+            active_session = get_active_session()
+
+            new_data = {
+                'tags': [*mytags, new],
+                'active_session': active_session,
+                'sessions': [*mysessions]
+            }
+
+            rewrite_data(new_data)
+            click.echo(f'🦇 new tag - {new}')
+            exit(0)
+    else:
+        if mytags == []:
+            click.echo('🦇 your tag list is empty right now')
+            exit(1)
+        for tag in mytags:
+            click.echo(f'* {tag}')
