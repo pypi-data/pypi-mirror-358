ed26dd511d54467d3691099f413a8bdb76730806
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:31 +0000

    Add package qpidtoollibs/0.0.1/qpidtoollibs-0.0.1.tar.gz

---
+++ packages/qpidtoollibs/qpidtoollibs-0.0.1.tar.gz/qpidtoollibs-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="qpidtoollibs",
+    version="0.0.1",
+    packages=["qpidtoollibs"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
