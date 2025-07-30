5deb56879acab1b208bf6a2ad10453e1d2e6f27c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:33:29 +0000

    Add package pytsv/0.1.71/pytsv-0.1.71.tar.gz

---
+++ packages/pytsv/pytsv-0.1.71.tar.gz/pytsv-0.1.71/config/project.py
@@ -0,0 +1,9 @@
+""" project definitions """
+name = "pytsv"
+description_short = "Pytsv is a the Swiss army knife for TSV files"
+keywords = [
+    "python",
+    "tsv",
+    "format",
+    "csv",
+]
