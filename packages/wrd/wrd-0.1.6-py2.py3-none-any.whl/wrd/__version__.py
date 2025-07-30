ffbfd0b2271b62e3c930ee5796af60d0f8d2ebfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:19:22 +0000

    Add package wrd/0.1.6/wrd-0.1.6-py2.py3-none-any.whl

---
+++ packages/wrd/wrd-0.1.6-py2.py3-none-any.whl/wrd/__version__.py
@@ -0,0 +1,7 @@
+"""Version information for WRD package."""
+
+# This version should match the version in pyproject.toml
+__version__ = "0.1.2"
+__author__ = "Tom Sapletta"
+__license__ = "Apache-2.0"
+__email__ = "info@softreck.dev"
