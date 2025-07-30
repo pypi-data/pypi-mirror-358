4fecbef986874ec500393b4a0b39eceb707f6ec1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:08 +0000

    Add package offline-asrld-epd/0.1.5/offline-asrld-epd-0.1.5.tar.gz

---
+++ packages/offline-asrld-epd/offline-asrld-epd-0.1.5.tar.gz/offline-asrld-epd-0.1.5/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="offline-asrld-epd",
+    version="0.1.5",
+    packages=["offline-asrld-epd"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
