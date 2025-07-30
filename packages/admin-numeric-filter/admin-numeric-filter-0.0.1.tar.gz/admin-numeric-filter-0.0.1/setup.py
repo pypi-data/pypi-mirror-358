e2badc219812d5aff64199a72609eb69b1c49753
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:21 +0000

    Add package admin-numeric-filter/0.0.1/admin-numeric-filter-0.0.1.tar.gz

---
+++ packages/admin-numeric-filter/admin-numeric-filter-0.0.1.tar.gz/admin-numeric-filter-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="admin-numeric-filter",
+    version="0.0.1",
+    packages=["admin-numeric-filter"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
