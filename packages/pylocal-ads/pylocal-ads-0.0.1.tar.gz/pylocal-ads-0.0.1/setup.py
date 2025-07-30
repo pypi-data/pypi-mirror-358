c07486b698c2046aa671101b92256b635592e967
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:11 +0000

    Add package pylocal-ads/0.0.1/pylocal-ads-0.0.1.tar.gz

---
+++ packages/pylocal-ads/pylocal-ads-0.0.1.tar.gz/pylocal-ads-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pylocal-ads",
+    version="0.0.1",
+    packages=["pylocal-ads"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
