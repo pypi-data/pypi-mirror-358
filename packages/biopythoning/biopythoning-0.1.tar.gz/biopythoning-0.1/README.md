0028b17aa1c453a1eac08d7e5ba2f62c20443a22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:07:33 +0000

    Add package biopythoning/0.1/biopythoning-0.1.tar.gz

---
+++ packages/biopythoning/biopythoning-0.1.tar.gz/biopythoning-0.1/README.md
@@ -0,0 +1,5 @@
+# Biopythoning
+
+This is a custom bioinformatics package created by Sujan.
+
+It includes modules like DNA tools, PDB parsing, Entrez, and more.
---
+++ packages/biopythoning/biopythoning-0.1.tar.gz/biopythoning-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='biopythoning',                  # This is the name others will install
+    version='0.1',
+    packages=find_packages(),             # Includes 'sujan' and subfolders with __init__.py
+    install_requires=[],
+    author='meow',
+    description='All of meow\'s custom bioinformatics tools',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
