d5730294ba9d4e1a84115a72624144c83da7d7d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:36 +0000

    Add package scrooge-plugin-cheap-vm-20240830/1.0.1/scrooge-plugin-cheap-vm-20240830-1.0.1.tar.gz

---
+++ packages/scrooge-plugin-cheap-vm-20240830/scrooge-plugin-cheap-vm-20240830-1.0.1.tar.gz/scrooge-plugin-cheap-vm-20240830-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="scrooge-plugin-cheap-vm-20240830",
+    version="1.0.1",
+    packages=["scrooge-plugin-cheap-vm-20240830"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
