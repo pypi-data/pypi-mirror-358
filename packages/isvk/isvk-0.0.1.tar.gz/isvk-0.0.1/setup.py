37a7ccf5e8da038015749e5f4dd73cd1db303cea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:03 +0000

    Add package isvk/0.0.1/isvk-0.0.1.tar.gz

---
+++ packages/isvk/isvk-0.0.1.tar.gz/isvk-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="isvk",
+    version="0.0.1",
+    packages=["isvk"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
