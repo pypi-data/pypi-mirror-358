db816288e0575d9dd859a31242622f4d438e7242
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:15 +0000

    Add package pyspeexdsp/0.0.1/pyspeexdsp-0.0.1.tar.gz

---
+++ packages/pyspeexdsp/pyspeexdsp-0.0.1.tar.gz/pyspeexdsp-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pyspeexdsp",
+    version="0.0.1",
+    packages=["pyspeexdsp"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
