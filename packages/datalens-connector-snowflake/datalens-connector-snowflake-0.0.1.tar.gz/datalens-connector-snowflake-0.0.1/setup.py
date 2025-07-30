6a84bf227b11c8b7dee9daccc36b747b7ffa787f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:37 +0000

    Add package datalens-connector-snowflake/0.0.1/datalens-connector-snowflake-0.0.1.tar.gz

---
+++ packages/datalens-connector-snowflake/datalens-connector-snowflake-0.0.1.tar.gz/datalens-connector-snowflake-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="datalens-connector-snowflake",
+    version="0.0.1",
+    packages=["datalens-connector-snowflake"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
