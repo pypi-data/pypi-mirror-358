2c6c22ff99f5d11b31d334192b6151f65e2e1cf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:08 +0000

    Add package HikariCP/5.1.0/HikariCP-5.1.0.tar.gz

---
+++ packages/HikariCP/HikariCP-5.1.0.tar.gz/HikariCP-5.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="HikariCP",
+    version="5.1.0",
+    packages=["HikariCP"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
