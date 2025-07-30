4ea7b49688594c7fa7d96d6e8e21b09e774a6692
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:30 +0000

    Add package clickhouse-backend/0.0.1/clickhouse-backend-0.0.1.tar.gz

---
+++ packages/clickhouse-backend/clickhouse-backend-0.0.1.tar.gz/clickhouse-backend-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="clickhouse-backend",
+    version="0.0.1",
+    packages=["clickhouse-backend"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
