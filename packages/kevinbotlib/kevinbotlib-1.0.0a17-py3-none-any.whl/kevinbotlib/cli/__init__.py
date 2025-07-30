4970a7f347752cc63117bd5a4aaf529954a421f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:27 +0000

    Add package kevinbotlib/1.0.0a17/kevinbotlib-1.0.0a17-py3-none-any.whl

---
+++ packages/kevinbotlib/kevinbotlib-1.0.0a17-py3-none-any.whl/kevinbotlib/cli/__init__.py
@@ -0,0 +1,37 @@
+"""
+KevinbotLib Command-line Interface
+"""
+
+import click
+
+from kevinbotlib.__about__ import __version__
+from kevinbotlib.cli.apps import apps
+from kevinbotlib.cli.fileserver import fileserver
+from kevinbotlib.cli.hardware import hardware
+from kevinbotlib.cli.logs import logs
+
+
+@click.group(context_settings={"help_option_names": ["-h", "--help"], "max_content_width": 120})
+@click.version_option(version=__version__, prog_name="KevinbotLib")
+def cli():
+    """
+    \b
+    ██╗  ██╗███████╗██╗   ██╗██╗███╗   ██╗██████╗  ██████╗ ████████╗██╗     ██╗██████╗
+    ██║ ██╔╝██╔════╝██║   ██║██║████╗  ██║██╔══██╗██╔═══██╗╚══██╔══╝██║     ██║██╔══██╗
+    █████╔╝ █████╗  ██║   ██║██║██╔██╗ ██║██████╔╝██║   ██║   ██║   ██║     ██║██████╔╝
+    ██╔═██╗ ██╔══╝  ╚██╗ ██╔╝██║██║╚██╗██║██╔══██╗██║   ██║   ██║   ██║     ██║██╔══██╗
+    ██║  ██╗███████╗ ╚████╔╝ ██║██║ ╚████║██████╔╝╚██████╔╝   ██║   ███████╗██║██████╔╝
+    ╚═╝  ╚═╝╚══════╝  ╚═══╝  ╚═╝╚═╝  ╚═══╝╚═════╝  ╚═════╝    ╚═╝   ╚══════╝╚═╝╚═════╝
+    """
+
+
+def main():  # no cov
+    cli.add_command(apps)
+    cli.add_command(fileserver)
+    cli.add_command(hardware)
+    cli.add_command(logs)
+    cli(prog_name="kevinbotlib")
+
+
+if __name__ == "__main__":
+    main()
