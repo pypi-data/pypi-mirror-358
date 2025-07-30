3c02f4f5b489111979b9aee20165e7c88b1963a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:11 +0000

    Add package PrintTolTest/0.2.1/printtoltest-0.2.1-py3-none-any.whl

---
+++ packages/PrintTolTest/printtoltest-0.2.1-py3-none-any.whl/PrintTolTest/tolerance.py
@@ -0,0 +1,7 @@
+def calculate_tolerance(expected, measured):
+    tolerances = {}
+    for axis, e, m in zip(['X', 'Y', 'Z'], expected, measured):
+        signed = ((m - e) / e) * 100
+        absolute = abs(signed)
+        tolerances[axis] = {'signed': signed, 'absolute': absolute}
+    return tolerances
