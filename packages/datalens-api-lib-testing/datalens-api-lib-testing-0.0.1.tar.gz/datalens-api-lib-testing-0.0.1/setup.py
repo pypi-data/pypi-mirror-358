032d900e7bbbd307e48f17bb421b8ff30c38ef48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:37 +0000

    Add package datalens-api-lib-testing/0.0.1/datalens-api-lib-testing-0.0.1.tar.gz

---
+++ packages/datalens-api-lib-testing/datalens-api-lib-testing-0.0.1.tar.gz/datalens-api-lib-testing-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="datalens-api-lib-testing",
+    version="0.0.1",
+    packages=["datalens-api-lib-testing"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
