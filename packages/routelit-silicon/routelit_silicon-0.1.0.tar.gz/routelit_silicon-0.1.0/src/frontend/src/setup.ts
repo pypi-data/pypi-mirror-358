e95e301f1602e8429e8d408a1faafe597ccfee07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:33:45 +0000

    Add package routelit-silicon/0.1.0/routelit_silicon-0.1.0.tar.gz

---
+++ packages/routelit-silicon/routelit_silicon-0.1.0.tar.gz/routelit_silicon-0.1.0/src/frontend/src/setup.ts
@@ -0,0 +1,15 @@
+import { componentStore } from "routelit-client";
+import './silicon.min.css'
+import './lib.css'
+import {
+  Root,
+  Sidebar,
+  Main,
+} from "./components";
+
+componentStore.register("root", Root);
+componentStore.register("sidebar", Sidebar);
+componentStore.register("main", Main);
+componentStore.forceUpdate();
+
+console.log(" setup done routelit elements ", componentStore);
