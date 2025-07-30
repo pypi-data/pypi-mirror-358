fc4d00bc8fcc04251e7f61803f42da8a2606e6e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:22 +0000

    Add package server-config/0.0.1/server-config-0.0.1.tar.gz

---
+++ packages/server-config/server-config-0.0.1.tar.gz/server-config-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="server-config",
+    version="0.0.1",
+    packages=["server-config"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
