a7a4705c285c8d7a5a6b932dc6c0b0a6c7c74095
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:20 +0000

    Add package vk-observeit/0.0.6/vk-observeit-0.0.6.tar.gz

---
+++ packages/vk-observeit/vk-observeit-0.0.6.tar.gz/vk-observeit-0.0.6/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="vk-observeit",
+    version="0.0.6",
+    packages=["vk-observeit"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
