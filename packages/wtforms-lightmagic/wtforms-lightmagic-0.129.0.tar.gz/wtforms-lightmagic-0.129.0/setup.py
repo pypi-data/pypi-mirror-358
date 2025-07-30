6990623391c732e231c052ee8f38b632b9a66fca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:30 +0000

    Add package wtforms-lightmagic/0.129.0/wtforms-lightmagic-0.129.0.tar.gz

---
+++ packages/wtforms-lightmagic/wtforms-lightmagic-0.129.0.tar.gz/wtforms-lightmagic-0.129.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="wtforms-lightmagic",
+    version="0.129.0",
+    packages=["wtforms-lightmagic"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
