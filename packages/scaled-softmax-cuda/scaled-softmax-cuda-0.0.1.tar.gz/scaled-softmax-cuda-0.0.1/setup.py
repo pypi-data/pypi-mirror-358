d9c007da5d07fd2a9de301e926f25e37dd1b6d1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:00 +0000

    Add package scaled-softmax-cuda/0.0.1/scaled-softmax-cuda-0.0.1.tar.gz

---
+++ packages/scaled-softmax-cuda/scaled-softmax-cuda-0.0.1.tar.gz/scaled-softmax-cuda-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="scaled-softmax-cuda",
+    version="0.0.1",
+    packages=["scaled-softmax-cuda"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
