4d2857fa7268aebd0150bf410b2d3e690825c1d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:28 +0000

    Add package ok-pyvault/0.0.1/ok-pyvault-0.0.1.tar.gz

---
+++ packages/ok-pyvault/ok-pyvault-0.0.1.tar.gz/ok-pyvault-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ok-pyvault",
+    version="0.0.1",
+    packages=["ok-pyvault"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
