623a3fc0d1fd32e53a6ae7d151b9036733f15d70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:07:00 +0000

    Add package vk-bot-client/0.0.1/vk-bot-client-0.0.1.tar.gz

---
+++ packages/vk-bot-client/vk-bot-client-0.0.1.tar.gz/vk-bot-client-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="vk-bot-client",
+    version="0.0.1",
+    packages=["vk-bot-client"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
