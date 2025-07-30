2eabc77c0f6e64a3be86db6234cd840db9690062
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:19 +0000

    Add package conftest/0.0.1/conftest-0.0.1.tar.gz

---
+++ packages/conftest/conftest-0.0.1.tar.gz/conftest-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="conftest",
+    version="0.0.1",
+    packages=["conftest"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
