a25a72c86641d4c26b89c6858f958dade3ea3af4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:57 +0000

    Add package pypo-aggregator/0.0.1/pypo-aggregator-0.0.1.tar.gz

---
+++ packages/pypo-aggregator/pypo-aggregator-0.0.1.tar.gz/pypo-aggregator-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pypo-aggregator",
+    version="0.0.1",
+    packages=["pypo-aggregator"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
