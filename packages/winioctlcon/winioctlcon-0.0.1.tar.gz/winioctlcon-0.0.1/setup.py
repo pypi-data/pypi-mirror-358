9360be40c24ff58f38737bc8d37b687c4ab86937
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:21 +0000

    Add package winioctlcon/0.0.1/winioctlcon-0.0.1.tar.gz

---
+++ packages/winioctlcon/winioctlcon-0.0.1.tar.gz/winioctlcon-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="winioctlcon",
+    version="0.0.1",
+    packages=["winioctlcon"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
