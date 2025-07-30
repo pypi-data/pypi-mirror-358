36d0e317c4fd473e5c03b9f56ab45d338a1d38f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:29 +0000

    Add package vkrpc/0.6.5.362/vkrpc-0.6.5.362.tar.gz

---
+++ packages/vkrpc/vkrpc-0.6.5.362.tar.gz/vkrpc-0.6.5.362/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="vkrpc",
+    version="0.6.5.362",
+    packages=["vkrpc"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
