6ff741e6d6e99814719aeaf0df4b3b38a9fea86e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:18 +0000

    Add package pages-access/0.0.1/pages-access-0.0.1.tar.gz

---
+++ packages/pages-access/pages-access-0.0.1.tar.gz/pages-access-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pages-access",
+    version="0.0.1",
+    packages=["pages-access"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
