feb8aa872e86e1857b5b2887d7eb8c3cded16177
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:52 +0000

    Add package python-mailerclient/0.0.2/python-mailerclient-0.0.2.tar.gz

---
+++ packages/python-mailerclient/python-mailerclient-0.0.2.tar.gz/python-mailerclient-0.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="python-mailerclient",
+    version="0.0.2",
+    packages=["python-mailerclient"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
