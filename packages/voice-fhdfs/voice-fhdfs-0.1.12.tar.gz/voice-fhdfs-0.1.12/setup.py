024c83cf31c7122182ac5d45ec59c9b10e1505e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:20 +0000

    Add package voice-fhdfs/0.1.12/voice-fhdfs-0.1.12.tar.gz

---
+++ packages/voice-fhdfs/voice-fhdfs-0.1.12.tar.gz/voice-fhdfs-0.1.12/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="voice-fhdfs",
+    version="0.1.12",
+    packages=["voice-fhdfs"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
