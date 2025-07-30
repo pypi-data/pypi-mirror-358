9e4cc547c2e76ef6940098f9aa22bc3ba1ea4eb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:41 +0000

    Add package fbank/0.0.1/fbank-0.0.1.tar.gz

---
+++ packages/fbank/fbank-0.0.1.tar.gz/fbank-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="fbank",
+    version="0.0.1",
+    packages=["fbank"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
