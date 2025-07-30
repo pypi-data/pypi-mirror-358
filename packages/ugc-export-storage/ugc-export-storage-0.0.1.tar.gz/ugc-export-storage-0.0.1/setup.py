f1b7e4f024a9e3e40d52d628f729a4b204fd077a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:21 +0000

    Add package ugc-export-storage/0.0.1/ugc-export-storage-0.0.1.tar.gz

---
+++ packages/ugc-export-storage/ugc-export-storage-0.0.1.tar.gz/ugc-export-storage-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ugc-export-storage",
+    version="0.0.1",
+    packages=["ugc-export-storage"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
