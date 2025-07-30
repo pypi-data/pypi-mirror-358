1873e77cf8f3a9c00caace050edeecda671f098f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:52 +0000

    Add package cache-access/1.0.0/cache-access-1.0.0.tar.gz

---
+++ packages/cache-access/cache-access-1.0.0.tar.gz/cache-access-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="cache-access",
+    version="1.0.0",
+    packages=["cache-access"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
