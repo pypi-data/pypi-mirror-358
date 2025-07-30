6715df9da9fabe079454b8a37f5c20589b57d6ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:44 +0000

    Add package netZooPy/0.10.9/netzoopy-0.10.9.tar.gz

---
+++ packages/netZooPy/netzoopy-0.10.9.tar.gz/netzoopy-0.10.9/netZooPy/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+
+from netZooPy import cobra
+from netZooPy import panda
+from netZooPy import puma
+from netZooPy import lioness
+from netZooPy import condor
+from netZooPy import sambar
+from netZooPy import bonobo
+
+__version__ = "0.10.9"
