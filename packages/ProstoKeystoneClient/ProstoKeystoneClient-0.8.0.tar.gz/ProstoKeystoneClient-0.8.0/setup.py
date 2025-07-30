5f8a12a4207d71408c808151bd8b14c5af280758
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:37 +0000

    Add package ProstoKeystoneClient/0.8.0/ProstoKeystoneClient-0.8.0.tar.gz

---
+++ packages/ProstoKeystoneClient/ProstoKeystoneClient-0.8.0.tar.gz/ProstoKeystoneClient-0.8.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ProstoKeystoneClient",
+    version="0.8.0",
+    packages=["ProstoKeystoneClient"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
