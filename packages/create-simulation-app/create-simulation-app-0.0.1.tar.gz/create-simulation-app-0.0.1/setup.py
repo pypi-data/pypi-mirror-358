cdb5a5991d1c4fcebc09bd52878f14cb339baf23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:38 +0000

    Add package create-simulation-app/0.0.1/create-simulation-app-0.0.1.tar.gz

---
+++ packages/create-simulation-app/create-simulation-app-0.0.1.tar.gz/create-simulation-app-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="create-simulation-app",
+    version="0.0.1",
+    packages=["create-simulation-app"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
