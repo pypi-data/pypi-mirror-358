4150f92a35235faa513d073bb82e73b1b436f46f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:54 +0000

    Add package fast-encdec-multihead-attn/0.0.1/fast-encdec-multihead-attn-0.0.1.tar.gz

---
+++ packages/fast-encdec-multihead-attn/fast-encdec-multihead-attn-0.0.1.tar.gz/fast-encdec-multihead-attn-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="fast-encdec-multihead-attn",
+    version="0.0.1",
+    packages=["fast-encdec-multihead-attn"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
