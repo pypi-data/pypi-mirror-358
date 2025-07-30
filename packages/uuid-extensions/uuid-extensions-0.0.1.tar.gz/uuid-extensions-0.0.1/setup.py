7d041728e0eb148b61753f2892f93173784da596
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:07:01 +0000

    Add package uuid-extensions/0.0.1/uuid-extensions-0.0.1.tar.gz

---
+++ packages/uuid-extensions/uuid-extensions-0.0.1.tar.gz/uuid-extensions-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="uuid-extensions",
+    version="0.0.1",
+    packages=["uuid-extensions"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
