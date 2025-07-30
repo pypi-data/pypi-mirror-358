4362cf403a8818c08b710093aa4d6c79377e920e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:11 +0000

    Add package monitored-jobs/0.0.1/monitored-jobs-0.0.1.tar.gz

---
+++ packages/monitored-jobs/monitored-jobs-0.0.1.tar.gz/monitored-jobs-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="monitored-jobs",
+    version="0.0.1",
+    packages=["monitored-jobs"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
