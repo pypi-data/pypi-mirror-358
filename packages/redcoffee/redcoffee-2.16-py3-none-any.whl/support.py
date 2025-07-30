c72bba41dd27292abcf7560e61cc19ccb0f9a718
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:30 +0000

    Add package redcoffee/2.16/redcoffee-2.16-py3-none-any.whl

---
+++ packages/redcoffee/redcoffee-2.16-py3-none-any.whl/support.py
@@ -0,0 +1,12 @@
+import random
+def pick_random_support_message():
+
+    support_messages=[
+        "💖 Hey friend! If you like this tool, why not give it a ⭐ on GitHub? It makes me super happy! (≧◡≦). Link: https://github.com/Anubhav9/redcoffee",
+        "🎉 You’re awesome! If this CLI made your life easier, drop a ⭐ on GitHub to show some love! 🌸. Link: https://github.com/Anubhav9/redcoffee",
+        "💕 This project runs on coffee & GitHub stars! ☕✨ Send some love by starring the repo! ⭐. Link: https://github.com/Anubhav9/redcoffee"
+    ]
+    return random.choice(support_messages)
+
+def warning_for_path_change(resolved_path):
+    return f"Attention User‼️ Your SonarQube Analysis PDF report will now be available at the following path {resolved_path}"
