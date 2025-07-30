405b0e256da194379f9f21e48f524cf023c80c65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:41 +0000

    Add package dbname/0.0.1/dbname-0.0.1.tar.gz

---
+++ packages/dbname/dbname-0.0.1.tar.gz/dbname-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dbname",
+    version="0.0.1",
+    packages=["dbname"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
