b110c792541891693ac0f80b6bc7be9d54282181
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:45:14 +0000

    Add package uxarray/2025.6.0/uxarray-2025.6.0-py3-none-any.whl

---
+++ packages/uxarray/uxarray-2025.6.0-py3-none-any.whl/uxarray/io/_scrip.py
@@ -0,0 +1,260 @@
+import numpy as np
+import polars as pl
+import xarray as xr
+
+from uxarray.constants import INT_DTYPE, INT_FILL_VALUE
+from uxarray.conventions import ugrid
+from uxarray.grid.connectivity import _replace_fill_values
+
+
+def _to_ugrid(in_ds, out_ds):
+    """If input dataset (``in_ds``) file is an unstructured SCRIP file,
+    function will reassign SCRIP variables to UGRID conventions in output file
+    (``out_ds``).
+    """
+
+    source_dims_dict = {}
+
+    # Check if any of imask, area or rank are present
+    if any(key in in_ds for key in ["grid_imask", "grid_rank", "grid_area"]):
+        # Create node_lon & node_lat variables from grid_corner_lat/lon
+        # Turn latitude and longitude scrip arrays into 1D
+        corner_lat = in_ds["grid_corner_lat"].values.ravel()
+        corner_lon = in_ds["grid_corner_lon"].values.ravel()
+
+        # Use Polars to find unique coordinate pairs
+        df = pl.DataFrame({"lon": corner_lon, "lat": corner_lat}).with_row_count(
+            "original_index"
+        )
+
+        # Get unique rows (first occurrence). This preserves the order in which they appear.
+        unique_df = df.unique(subset=["lon", "lat"], keep="first")
+
+        # unq_ind: The indices of the unique rows in the original array
+        unq_ind = unique_df["original_index"].to_numpy().astype(INT_DTYPE)
+
+        # To get the inverse index (unq_inv): map each original row back to its unique row index.
+        # Add a unique_id to the unique_df which will serve as the "inverse" mapping.
+        unique_df = unique_df.with_row_count("unique_id")
+
+        # Join original df with unique_df to find out which unique_id corresponds to each row
+        df_joined = df.join(
+            unique_df.drop("original_index"), on=["lon", "lat"], how="left"
+        )
+        unq_inv = df_joined["unique_id"].to_numpy().astype(INT_DTYPE)
+
+        # Extract unique lon and lat values using unq_ind
+        unq_lon = corner_lon[unq_ind]
+        unq_lat = corner_lat[unq_ind]
+
+        # Reshape face nodes array into original shape for use in 'face_node_connectivity'
+        unq_inv = np.reshape(unq_inv, (len(in_ds.grid_size), len(in_ds.grid_corners)))
+
+        # Create node_lon & node_lat
+        out_ds[ugrid.NODE_COORDINATES[0]] = xr.DataArray(
+            unq_lon, dims=[ugrid.NODE_DIM], attrs=ugrid.NODE_LON_ATTRS
+        )
+
+        out_ds[ugrid.NODE_COORDINATES[1]] = xr.DataArray(
+            unq_lat, dims=[ugrid.NODE_DIM], attrs=ugrid.NODE_LAT_ATTRS
+        )
+
+        # Create face_lon & face_lat from grid_center_lat/lon
+        out_ds[ugrid.FACE_COORDINATES[0]] = xr.DataArray(
+            in_ds["grid_center_lon"].values,
+            dims=[ugrid.FACE_DIM],
+            attrs=ugrid.FACE_LON_ATTRS,
+        )
+
+        out_ds[ugrid.FACE_COORDINATES[1]] = xr.DataArray(
+            in_ds["grid_center_lat"].values,
+            dims=[ugrid.FACE_DIM],
+            attrs=ugrid.FACE_LAT_ATTRS,
+        )
+
+        # standardize fill values and data type face nodes
+        face_nodes = _replace_fill_values(
+            xr.DataArray(data=unq_inv),
+            original_fill=-1,
+            new_fill=INT_FILL_VALUE,
+            new_dtype=INT_DTYPE,
+        )
+
+        # set the face nodes data compiled in "connect" section
+        out_ds["face_node_connectivity"] = xr.DataArray(
+            data=face_nodes.data,
+            dims=ugrid.FACE_NODE_CONNECTIVITY_DIMS,
+            attrs=ugrid.FACE_NODE_CONNECTIVITY_ATTRS,
+        )
+
+    else:
+        raise Exception("Structured scrip files are not yet supported")
+
+    # populate source dims
+    source_dims_dict[in_ds["grid_center_lon"].dims[0]] = "n_face"
+
+    return source_dims_dict
+
+
+def _read_scrip(ext_ds):
+    """Function to reassign lat/lon variables to node variables.
+
+    Currently, supports unstructured SCRIP grid files following traditional SCRIP
+    naming practices (grid_corner_lat, grid_center_lat, etc) and SCRIP files with
+    UGRID conventions.
+
+    Unstructured grid SCRIP files will have ``grid_rank=1`` and include variables
+    ``grid_imask`` and ``grid_area`` in the dataset.
+
+    More information on structured vs unstructured SCRIP files can be found here on the `Earth System Modeling Framework <https://earthsystemmodeling.org/docs/release/ESMF_6_2_0/ESMF_refdoc/node3.html>`_ website.
+
+    Parameters
+    ----------
+    ext_ds : xarray.Dataset, required
+        SCRIP datafile of interest
+
+    Returns
+    -------
+    ds : xarray.Dataset
+        ugrid aware :class:`xarray.Dataset`
+    """
+    ds = xr.Dataset()
+
+    source_dims_dict = _to_ugrid(ext_ds, ds)
+
+    return ds, source_dims_dict
+
+
+def _encode_scrip(face_node_connectivity, node_lon, node_lat, face_areas):
+    """Function to reassign UGRID formatted variables to SCRIP formatted
+    variables.
+
+    Currently, supports creating unstructured SCRIP grid files following traditional
+    SCRIP naming practices (grid_corner_lat, grid_center_lat, etc).
+
+    Unstructured grid SCRIP files typically have ``grid_rank=1`` and include variables
+    ``grid_imask`` and ``grid_area`` in the dataset.
+
+    More information on structured vs unstructured SCRIP files can be found here on the `Earth System Modeling Framework <https://earthsystemmodeling.org/docs/release/ESMF_6_2_0/ESMF_refdoc/node3.html>`_ website.
+
+    Parameters
+    ----------
+    outfile : str
+        Name of file to be created. Saved to working directory, or to
+        specified location if full path to new file is provided.
+
+    face_node_connectivity : xarray.DataArray
+        Face-node connectivity. This variable should come from the ``Grid``
+        object that calls this function
+
+    node_lon : xarray.DataArray
+        Nodes' x values. This variable should come from the ``Grid`` object
+        that calls this function
+
+    node_lat : xarray.DataArray
+        Nodes' y values. This variable should come from the ``Grid`` object
+        that calls this function
+
+    face_areas : numpy.ndarray
+        Face areas. This variable should come from the ``Grid`` object
+        that calls this function
+
+    Returns
+    -------
+    ds : xarray.Dataset
+        Dataset to be returned by ``_encode_scrip``. The function returns
+        the output dataset in SCRIP format for immediate use.
+    """
+    # Create empty dataset to put new scrip format data into
+    ds = xr.Dataset()
+
+    # Make grid corner lat/lon
+    f_nodes = face_node_connectivity.values.astype(INT_DTYPE).ravel()
+
+    # Create arrays to hold lat/lon data
+    lat_nodes = node_lat[f_nodes].values
+    lon_nodes = node_lon[f_nodes].values
+
+    # Reshape arrays to be 2D instead of 1D
+    reshp_lat = np.reshape(
+        lat_nodes, [face_node_connectivity.shape[0], face_node_connectivity.shape[1]]
+    )
+    reshp_lon = np.reshape(
+        lon_nodes, [face_node_connectivity.shape[0], face_node_connectivity.shape[1]]
+    )
+
+    # Add data to new scrip output file
+    ds["grid_corner_lat"] = xr.DataArray(
+        data=reshp_lat, dims=["grid_size", "grid_corners"]
+    )
+
+    ds["grid_corner_lon"] = xr.DataArray(
+        data=reshp_lon, dims=["grid_size", "grid_corners"]
+    )
+
+    # Create Grid rank, always 1 for unstructured grids
+    ds["grid_rank"] = xr.DataArray(data=[1], dims=["grid_rank"])
+
+    # Create grid_dims value of len grid_size
+    ds["grid_dims"] = xr.DataArray(data=[len(lon_nodes)], dims=["grid_rank"])
+
+    # Create grid_imask representing fill values
+    ds["grid_imask"] = xr.DataArray(
+        data=np.ones(len(reshp_lon), dtype=int), dims=["grid_size"]
+    )
+
+    ds["grid_area"] = xr.DataArray(data=face_areas, dims=["grid_size"])
+
+    # Calculate and create grid center lat/lon using helper function
+    center_lat, center_lon = grid_center_lat_lon(ds)
+
+    ds["grid_center_lon"] = xr.DataArray(data=center_lon, dims=["grid_size"])
+
+    ds["grid_center_lat"] = xr.DataArray(data=center_lat, dims=["grid_size"])
+
+    return ds
+
+
+def grid_center_lat_lon(ds):
+    """Using scrip file variables ``grid_corner_lat`` and ``grid_corner_lon``,
+    calculates the ``grid_center_lat`` and ``grid_center_lon``.
+
+    Parameters
+    ----------
+    ds : xarray.Dataset
+        Dataset that contains ``grid_corner_lat`` and ``grid_corner_lon``
+        data variables
+
+    Returns
+    -------
+    center_lon : :class:`numpy.ndarray`
+        The calculated center longitudes of the grid box based on the corner
+        points
+    center_lat : :class:`numpy.ndarray`
+        The calculated center latitudes of the grid box based on the corner
+        points
+    """
+
+    # Calculate and create grid center lat/lon
+    scrip_corner_lon = ds["grid_corner_lon"]
+    scrip_corner_lat = ds["grid_corner_lat"]
+
+    # convert to radians
+    rad_corner_lon = np.deg2rad(scrip_corner_lon)
+    rad_corner_lat = np.deg2rad(scrip_corner_lat)
+
+    # get nodes per face
+    nodes_per_face = rad_corner_lat.shape[1]
+
+    # geographic center of each cell
+    x = np.sum(np.cos(rad_corner_lat) * np.cos(rad_corner_lon), axis=1) / nodes_per_face
+    y = np.sum(np.cos(rad_corner_lat) * np.sin(rad_corner_lon), axis=1) / nodes_per_face
+    z = np.sum(np.sin(rad_corner_lat), axis=1) / nodes_per_face
+
+    center_lon = np.rad2deg(np.arctan2(y, x))
+    center_lat = np.rad2deg(np.arctan2(z, np.sqrt(x**2 + y**2)))
+
+    # Make negative lons positive
+    center_lon[center_lon < 0] += 360
+
+    return center_lat, center_lon
---
+++ packages/uxarray/uxarray-2025.6.0-py3-none-any.whl/uxarray/subset/grid_accessor.py
@@ -0,0 +1,165 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, List, Optional, Set, Tuple, Union
+
+import numpy as np
+
+if TYPE_CHECKING:
+    from uxarray.grid import Grid
+
+
+class GridSubsetAccessor:
+    """Accessor for performing unstructured grid subsetting, accessed through
+    ``Grid.subset``"""
+
+    def __init__(self, uxgrid: Grid) -> None:
+        self.uxgrid = uxgrid
+
+    def __repr__(self):
+        prefix = "<uxarray.Grid.subset>\n"
+        methods_heading = "Supported Methods:\n"
+
+        methods_heading += "  * nearest_neighbor(center_coord, k, element, inverse_indices, **kwargs)\n"
+        methods_heading += (
+            "  * bounding_circle(center_coord, r, element, inverse_indices, **kwargs)\n"
+        )
+        methods_heading += "  * bounding_box(lon_bounds, lat_bounds, inverse_indices)\n"
+
+        return prefix + methods_heading
+
+    def bounding_box(
+        self,
+        lon_bounds: Tuple[float, float],
+        lat_bounds: Tuple[float, float],
+        inverse_indices: Union[List[str], Set[str], bool] = False,
+    ):
+        """Subsets an unstructured grid between two latitude and longitude
+        points which form a bounding box.
+
+        A bounding box may span the antimeridian, when the pair of longitude points is given in descending order (
+        i.e. the first longitude point is greater than the second).
+
+        Parameters
+        ----------
+        lon_bounds: tuple, list, np.ndarray
+            (lon_left, lon_right) where lon_left < lon_right when the bounding box does not span
+            the antimeridian, otherwise lon_left > lon_right, both between [-180, 180]
+        lat_bounds: tuple, list, np.ndarray
+            (lat_bottom, lat_top) where lat_top > lat_bottom and between [-90, 90]
+        method: str
+            Bounding Box Method, currently supports 'coords', which ensures the coordinates of the corner nodes,
+            face centers, or edge centers lie within the bounds.
+        element: str
+            Element for use with `coords` comparison, one of `nodes`, `face centers`, or `edge centers`
+        inverse_indices : Union[List[str], Set[str], bool], optional
+            Controls storage of original grid indices. Options:
+            - True: Stores original face indices
+            - List/Set of strings: Stores specified index types (valid values: "face", "edge", "node")
+            - False: No index storage (default)
+        """
+
+        faces_between_lons = self.uxgrid.get_faces_between_longitudes(lon_bounds)
+        face_between_lats = self.uxgrid.get_faces_between_latitudes(lat_bounds)
+
+        faces = np.intersect1d(faces_between_lons, face_between_lats)
+
+        return self.uxgrid.isel(n_face=faces, inverse_indices=inverse_indices)
+
+    def bounding_circle(
+        self,
+        center_coord: Union[Tuple, List, np.ndarray],
+        r: Union[float, int],
+        element: Optional[str] = "face centers",
+        inverse_indices: Union[List[str], Set[str], bool] = False,
+        **kwargs,
+    ):
+        """Subsets an unstructured grid by returning all elements within some
+        radius (in degrees) from a center coord.
+
+        Parameters
+        ----------
+        center_coord : tuple, list, np.ndarray
+            Longitude and latitude of the center of the bounding circle
+        r: scalar, int, float
+            Radius of bounding circle (in degrees)
+        element: str
+            Element for use with `coords` comparison, one of `nodes`, `face centers`, or `edge centers`
+        inverse_indices : Union[List[str], Set[str], bool], optional
+            Controls storage of original grid indices. Options:
+            - True: Stores original face indices
+            - List/Set of strings: Stores specified index types (valid values: "face", "edge", "node")
+            - False: No index storage (default)
+        """
+
+        coords = np.asarray(center_coord)
+
+        tree = self._get_tree(coords, element)
+
+        ind = tree.query_radius(coords, r)
+
+        if len(ind) == 0:
+            raise ValueError(
+                f"No elements founding within the bounding circle with radius {r} when querying {element}"
+            )
+
+        return self._index_grid(ind, element, inverse_indices)
+
+    def nearest_neighbor(
+        self,
+        center_coord: Union[Tuple, List, np.ndarray],
+        k: int,
+        element: Optional[str] = "face centers",
+        inverse_indices: Union[List[str], Set[str], bool] = False,
+        **kwargs,
+    ):
+        """Subsets an unstructured grid by returning the ``k`` closest
+        neighbors from a center coordinate.
+
+        Parameters
+        ----------
+        center_coord : tuple, list, np.ndarray
+            Longitude and latitude of the center of the bounding circle
+        k: int
+            Number of neighbors to query
+        element: str
+            Element for use with `coords` comparison, one of `nodes`, `face centers`, or `edge centers`
+        inverse_indices : Union[List[str], Set[str], bool], optional
+            Controls storage of original grid indices. Options:
+            - True: Stores original face indices
+            - List/Set of strings: Stores specified index types (valid values: "face", "edge", "node")
+            - False: No index storage (default)
+        """
+
+        coords = np.asarray(center_coord)
+
+        tree = self._get_tree(coords, element)
+
+        _, ind = tree.query(coords, k)
+
+        return self._index_grid(ind, element, inverse_indices=inverse_indices)
+
+    def _get_tree(self, coords, tree_type):
+        """Internal helper for obtaining the desired KDTree or BallTree."""
+        if coords.ndim > 1:
+            raise ValueError("Coordinates must be one-dimensional")
+
+        if len(coords) == 2:
+            # Spherical coordinates
+            tree = self.uxgrid.get_ball_tree(tree_type)
+        elif len(coords) == 3:
+            # Cartesian coordinates
+            tree = self.uxgrid.get_kd_tree(tree_type)
+        else:
+            raise ValueError("Unsupported coordinates provided.")
+
+        return tree
+
+    def _index_grid(self, ind, tree_type, inverse_indices=False):
+        """Internal helper for indexing a grid with indices based off the
+        provided tree type."""
+        if tree_type == "nodes":
+            return self.uxgrid.isel(inverse_indices, n_node=ind)
+        elif tree_type == "edge centers":
+            return self.uxgrid.isel(inverse_indices, n_edge=ind)
+        else:
+            return self.uxgrid.isel(inverse_indices, n_face=ind)
