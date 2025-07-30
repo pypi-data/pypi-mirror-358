300d24bfcbba22e0c9513ba4db68a5c0b6575ea3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:42 +0000

    Add package scrooge-plugin-ephemeral-20220228/1.0.4/scrooge-plugin-ephemeral-20220228-1.0.4.tar.gz

---
+++ packages/scrooge-plugin-ephemeral-20220228/scrooge-plugin-ephemeral-20220228-1.0.4.tar.gz/scrooge-plugin-ephemeral-20220228-1.0.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="scrooge-plugin-ephemeral-20220228",
+    version="1.0.4",
+    packages=["scrooge-plugin-ephemeral-20220228"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
