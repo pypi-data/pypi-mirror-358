f7d1f947d1c821c5bd43764d6139adef9277c0e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:05:16 +0000

    Add package datalens-pivot-pandas/0.0.1/datalens-pivot-pandas-0.0.1.tar.gz

---
+++ packages/datalens-pivot-pandas/datalens-pivot-pandas-0.0.1.tar.gz/datalens-pivot-pandas-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="datalens-pivot-pandas",
+    version="0.0.1",
+    packages=["datalens-pivot-pandas"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
