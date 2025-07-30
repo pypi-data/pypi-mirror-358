ed8ab4c79574c3a304e9edfb0cf17f1f1f17de00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:44:26 +0000

    Add package pdblc/0.1.2/pdblc-0.1.2.tar.gz

---
+++ packages/pdblc/pdblc-0.1.2.tar.gz/pdblc-0.1.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pdblc",
+    version="0.1.2",
+    packages=find_packages(),
+    install_requires=[],
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="A package containing 10 custom programs",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/pdblc",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+) 
