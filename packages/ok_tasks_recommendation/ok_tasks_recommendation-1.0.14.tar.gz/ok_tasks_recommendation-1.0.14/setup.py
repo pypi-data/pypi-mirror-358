8ae837709a5f2c364bee5702a9283bc9e5f9e51f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:14 +0000

    Add package ok_tasks_recommendation/1.0.14/ok_tasks_recommendation-1.0.14.tar.gz

---
+++ packages/ok_tasks_recommendation/ok_tasks_recommendation-1.0.14.tar.gz/ok_tasks_recommendation-1.0.14/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ok_tasks_recommendation",
+    version="1.0.14",
+    packages=["ok_tasks_recommendation"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
