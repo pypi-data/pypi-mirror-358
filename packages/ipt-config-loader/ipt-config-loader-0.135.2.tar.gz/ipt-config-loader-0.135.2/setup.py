b847b42dd453596188213a3eb9417e64f9c897bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:28 +0000

    Add package ipt-config-loader/0.135.2/ipt-config-loader-0.135.2.tar.gz

---
+++ packages/ipt-config-loader/ipt-config-loader-0.135.2.tar.gz/ipt-config-loader-0.135.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ipt-config-loader",
+    version="0.135.2",
+    packages=["ipt-config-loader"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
