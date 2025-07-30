8af7557ddff61c6ab63d6b9ed22b0189d3c748f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:06 +0000

    Add package kladun-sync/0.5.0/kladun-sync-0.5.0.tar.gz

---
+++ packages/kladun-sync/kladun-sync-0.5.0.tar.gz/kladun-sync-0.5.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="kladun-sync",
+    version="0.5.0",
+    packages=["kladun-sync"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
