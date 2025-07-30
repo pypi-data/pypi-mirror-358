d89ac327e496394a4d48d672f81091127118c9dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:09 +0000

    Add package asr-sparrow/0.0.1/asr-sparrow-0.0.1.tar.gz

---
+++ packages/asr-sparrow/asr-sparrow-0.0.1.tar.gz/asr-sparrow-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="asr-sparrow",
+    version="0.0.1",
+    packages=["asr-sparrow"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
