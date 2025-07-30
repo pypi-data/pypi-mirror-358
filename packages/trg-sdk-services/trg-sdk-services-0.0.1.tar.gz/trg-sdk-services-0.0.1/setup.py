a94032ba7dc92154cb4d24179ac516ddbf45d7c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:09 +0000

    Add package trg-sdk-services/0.0.1/trg-sdk-services-0.0.1.tar.gz

---
+++ packages/trg-sdk-services/trg-sdk-services-0.0.1.tar.gz/trg-sdk-services-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="trg-sdk-services",
+    version="0.0.1",
+    packages=["trg-sdk-services"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
