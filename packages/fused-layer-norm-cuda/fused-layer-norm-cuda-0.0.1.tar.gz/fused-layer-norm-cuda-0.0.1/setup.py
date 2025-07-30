a10783004796bb6c76575b2c596713d06a5bf37f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:36 +0000

    Add package fused-layer-norm-cuda/0.0.1/fused-layer-norm-cuda-0.0.1.tar.gz

---
+++ packages/fused-layer-norm-cuda/fused-layer-norm-cuda-0.0.1.tar.gz/fused-layer-norm-cuda-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="fused-layer-norm-cuda",
+    version="0.0.1",
+    packages=["fused-layer-norm-cuda"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
