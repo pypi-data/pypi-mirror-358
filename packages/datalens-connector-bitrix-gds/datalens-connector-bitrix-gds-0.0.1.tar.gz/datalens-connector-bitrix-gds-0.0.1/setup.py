cf738a5e003ba4235abb0c15e5aae48f92410337
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:35 +0000

    Add package datalens-connector-bitrix-gds/0.0.1/datalens-connector-bitrix-gds-0.0.1.tar.gz

---
+++ packages/datalens-connector-bitrix-gds/datalens-connector-bitrix-gds-0.0.1.tar.gz/datalens-connector-bitrix-gds-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="datalens-connector-bitrix-gds",
+    version="0.0.1",
+    packages=["datalens-connector-bitrix-gds"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
