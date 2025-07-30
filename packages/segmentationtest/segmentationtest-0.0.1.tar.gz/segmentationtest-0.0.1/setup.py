fafa3108ebf92c4133f298a997af12f62b9db6ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:19 +0000

    Add package segmentationtest/0.0.1/segmentationtest-0.0.1.tar.gz

---
+++ packages/segmentationtest/segmentationtest-0.0.1.tar.gz/segmentationtest-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="segmentationtest",
+    version="0.0.1",
+    packages=["segmentationtest"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
