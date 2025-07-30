b57a1fbf49ba3cd9398e3f92f6efadbb49271835
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:40 +0000

    Add package create-tests-job/0.0.1/create-tests-job-0.0.1.tar.gz

---
+++ packages/create-tests-job/create-tests-job-0.0.1.tar.gz/create-tests-job-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="create-tests-job",
+    version="0.0.1",
+    packages=["create-tests-job"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
