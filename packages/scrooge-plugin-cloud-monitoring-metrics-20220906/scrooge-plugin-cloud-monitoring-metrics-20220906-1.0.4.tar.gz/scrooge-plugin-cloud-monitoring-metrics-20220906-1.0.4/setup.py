8d22deb4cc8b881544fa5a2a6dec9199931db4dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:37 +0000

    Add package scrooge-plugin-cloud-monitoring-metrics-20220906/1.0.4/scrooge-plugin-cloud-monitoring-metrics-20220906-1.0.4.tar.gz

---
+++ packages/scrooge-plugin-cloud-monitoring-metrics-20220906/scrooge-plugin-cloud-monitoring-metrics-20220906-1.0.4.tar.gz/scrooge-plugin-cloud-monitoring-metrics-20220906-1.0.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="scrooge-plugin-cloud-monitoring-metrics-20220906",
+    version="1.0.4",
+    packages=["scrooge-plugin-cloud-monitoring-metrics-20220906"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
