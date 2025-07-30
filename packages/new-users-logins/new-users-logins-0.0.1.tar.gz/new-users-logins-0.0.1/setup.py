b85a95008ef4d573308dcd1e6e81c79ea825714b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:20 +0000

    Add package new-users-logins/0.0.1/new-users-logins-0.0.1.tar.gz

---
+++ packages/new-users-logins/new-users-logins-0.0.1.tar.gz/new-users-logins-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="new-users-logins",
+    version="0.0.1",
+    packages=["new-users-logins"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
