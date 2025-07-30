f5ac0f386bb66f2a73f745af4f6562a4af691e5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:12 +0000

    Add package httpgeobase/0.0.1/httpgeobase-0.0.1.tar.gz

---
+++ packages/httpgeobase/httpgeobase-0.0.1.tar.gz/httpgeobase-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="httpgeobase",
+    version="0.0.1",
+    packages=["httpgeobase"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
