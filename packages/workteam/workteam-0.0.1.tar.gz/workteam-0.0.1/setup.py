4695754e2946d626b68acc45811081e94b59e987
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:39 +0000

    Add package workteam/0.0.1/workteam-0.0.1.tar.gz

---
+++ packages/workteam/workteam-0.0.1.tar.gz/workteam-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="workteam",
+    version="0.0.1",
+    packages=["workteam"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
