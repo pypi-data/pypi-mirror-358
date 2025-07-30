d84db1339e4577867daa28944255d20f4be2be3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:58 +0000

    Add package test-name-lesson-data/0.0.1/test-name-lesson-data-0.0.1.tar.gz

---
+++ packages/test-name-lesson-data/test-name-lesson-data-0.0.1.tar.gz/test-name-lesson-data-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="test-name-lesson-data",
+    version="0.0.1",
+    packages=["test-name-lesson-data"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
