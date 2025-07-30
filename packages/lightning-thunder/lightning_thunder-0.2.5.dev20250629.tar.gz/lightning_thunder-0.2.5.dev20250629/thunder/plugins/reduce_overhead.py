9502926874073b60b3e8927f4401941075530c18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:24 +0000

    Add package lightning-thunder/0.2.5.dev20250629/lightning_thunder-0.2.5.dev20250629.tar.gz

---
+++ packages/lightning-thunder/lightning_thunder-0.2.5.dev20250629.tar.gz/lightning_thunder-0.2.5.dev20250629/thunder/plugins/reduce_overhead.py
@@ -0,0 +1,20 @@
+from thunder import Plugin
+from thunder.core.recipe import Plugin, PluginPolicy
+from thunder.transforms.cudagraph import CUDAGraphTransform
+
+
+class ReduceOverhead(Plugin):
+    """
+    Plugin to enable CUDA Graphs and reduce CPU overhead.
+    """
+
+    policy = PluginPolicy.POST
+
+    def setup_transforms(self):
+        """
+        Fetches the CUDAGraph transform.
+
+        Returns:
+            list[Transform]: A list containing the CUDAGraph transform.
+        """
+        return [CUDAGraphTransform()]
