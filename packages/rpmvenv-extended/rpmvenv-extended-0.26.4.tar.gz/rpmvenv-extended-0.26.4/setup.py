f17f0d40ddef962258cffa688e40750e8907840b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:06 +0000

    Add package rpmvenv-extended/0.26.4/rpmvenv-extended-0.26.4.tar.gz

---
+++ packages/rpmvenv-extended/rpmvenv-extended-0.26.4.tar.gz/rpmvenv-extended-0.26.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="rpmvenv-extended",
+    version="0.26.4",
+    packages=["rpmvenv-extended"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
