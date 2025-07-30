419220e6d798e7b963bd12e9f08f89e228f5aa8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:01 +0000

    Add package sphinx-uml/0.2.0/sphinx_uml-0.2.0-py3-none-any.whl

---
+++ packages/sphinx-uml/sphinx_uml-0.2.0-py3-none-any.whl/sphinx_uml/uml_generate_directive.py
@@ -0,0 +1,206 @@
+import argparse
+from pathlib import Path
+from docutils.parsers.rst import directives
+from pylint.pyreverse.main import writer
+from pylint.pyreverse.diagrams import (
+    ClassDiagram,
+    PackageDiagram,
+)
+from sphinx.ext.graphviz import (
+    figure_wrapper,
+    graphviz,
+)
+from sphinx.util.docutils import SphinxDirective
+from sphinx.util.typing import OptionSpec
+from typing import ClassVar
+
+
+class UmlNode(graphviz):
+    """
+    Defines a UML ``docutils`` node.
+    We populate its attribute so that we can rely on the
+    default ``graphviz`` node.
+    """
+    @classmethod
+    def from_dot(cls, dotcode: str) -> "UmlNode":
+        """
+        Builds a :py:class:`UmlNode` instance from a Graphviz
+        dot string.
+
+        Args:
+            dotcode (str): A Graphviz dot string.
+                *Example:* ``digraph G {0 -> 1}``
+
+        Returns:
+            The resulting :py:class:`UmlNode` instance.
+        """
+        node = cls()
+        node["code"] = dotcode
+        node["options"] = {"graphviz_dot": "dot"}
+
+        # We rely on sphinx.ext.inheritance_diagram CSS classes
+        # to be responsive to dark/light themes.
+        # TODO: improve this!
+        node["classes"] = ["inheritance"]
+        return node
+
+    @classmethod
+    def from_pyreverse(
+        cls,
+        diagram: ClassDiagram | PackageDiagram,
+        config: argparse.Namespace
+    ) -> "UmlNode":
+        """
+        Builds a :py:class:`UmlNode` from diagram definitions
+        obtained from pyreverse.
+
+        Args:
+            diagram (ClassDiagram | PackageDiagram): The diagram
+                that must be exported.
+            config (argparse.Namespace): The configuration
+                obtained from the Sphinx configuration file.
+
+        Returns:
+            The resulting :py:class:`UmlNode` instance.
+        """
+        from .pyreverse import DotPrinter, SphinxHtmlProxy
+        dwriter = writer.DiagramWriter(config)
+        dwriter.printer_class = DotPrinter
+        # TODO Build xrefs as in
+        # /usr/lib/python3/dist-packages/sphinx/ext/inheritance_diagram.py?
+        dwriter.api_doc = SphinxHtmlProxy()
+        dwriter.api_doc.sphinx_html_dir = config.sphinx_html_dir
+        dwriter.write([diagram])
+        dotcode = "\n".join(dwriter.printer.lines)
+        return cls.from_dot(dotcode)
+
+
+class UMLGenerateDirective(SphinxDirective):
+    """
+    UML directive to generate a pyreverse diagram
+    """
+
+    # Sphinx stuff to control argument passing
+    has_content = False
+    required_arguments = 1
+    optional_arguments = 0
+    final_argument_whitespace = True
+    option_spec: ClassVar[OptionSpec] = {
+        "caption": directives.unchanged,
+        "classes": directives.flag,
+        "packages": directives.flag,
+    }
+
+    def _build_args(self) -> list[str]:
+        args = list()
+        config = self.config
+        if config.uml_filter_mode:
+            assert config.uml_filter_mode
+            args.extend(("--filter-mode", config.uml_filter_mode))
+        if config.uml_class:
+            args.extend(("--class", config.uml_class))
+        if config.uml_show_ancestors:
+            args.extend(("--show-ancestors", config.uml_show_ancestors))
+        if config.uml_all_ancestors:
+            args.append("--all-ancestors")
+        if config.uml_show_associated:
+            args.extend(("--show-associated", config.uml_show_associated))
+        if config.uml_all_associated:
+            args.append("--all-associated")
+        if config.uml_show_builtin:
+            args.append("--show-builtin")
+        if config.uml_module_names:
+            args.extend(("--module-names", config.uml_module_names))
+        if config.uml_only_classnames:
+            args.append("--only-classnames")
+        if config.uml_ignore:
+            args.extend(("--ignore", config.uml_ignore))
+        if config.uml_colorized:
+            args.append("--colorized")
+        return args
+
+    def html_root_dir(self) -> str:
+        """
+        Returns:
+            The HTML prefix to move from the current HTML document
+            to the HTML root directory.
+
+        Example:
+            Assume that:
+
+            - the documentation is built in:
+              ``"~/git/sphinx-uml/docs"``;
+            - the current document is;
+              ``"~/git/sphinx-uml/docs/users/examples.rst"``;
+
+            Then, the returned value is ``"../"``.
+            As the HTML hierarchy follows the RST hierarchy, we use
+            this prefix to setup our :py:class:`SphinxHtmlProxy`.
+        """
+        doc = self.state.document
+        env = doc.settings.env
+        base_dir = Path(env.srcdir).absolute()
+        cur_path = Path(doc.current_source)
+        rel_path = str(cur_path.relative_to(base_dir).parent)
+        if rel_path == ".":
+            return rel_path
+        n = len(rel_path.split("/"))
+        ret = "/".join([".."] * n)
+        return ret
+
+    def run(self):
+        """
+        To test this extension, as a developer:
+
+        .. shell
+
+            make install-sphinx-custom clean-doc docs
+
+        To test this `pyreverse2`, called by this extension:
+
+        .. shell
+
+            pyreverse2 \\
+               --output svg \\
+               --project example.a \\
+               --sphinx-html-dir docs/_html \\
+               --output-directory docs/ \\
+               -m y \\
+               example.a
+        """
+        # ..uml: module_name
+        module_name = self.arguments[0]
+
+        # :classes:, :packages:, :caption:
+        with_classes = "classes" in self.options
+        with_packages = "packages" in self.options
+        caption = self.options.get("caption")
+
+        pyprocess_args = self._build_args() + [
+            "--sphinx-html-dir", self.html_root_dir(),
+            module_name
+        ]
+
+        # make install-sphinx-custom clean-doc docs
+        from .pyreverse import Run, ParsePyreverseArgs
+
+        parser = ParsePyreverseArgs(pyprocess_args)
+        runner = Run(parser.config)
+        diadefs = runner.diadefs(parser.remaining_args)
+
+        # Craft the list of nodes to be appended to the doctree's AST.
+        ret = list()
+        for diagram in diadefs:
+            if isinstance(diagram, PackageDiagram):
+                if not with_packages:
+                    continue
+            elif isinstance(diagram, ClassDiagram):
+                if not with_classes:
+                    continue
+            else:
+                raise ValueError(f"Invalid type {type(diagram)}")
+            node = UmlNode.from_pyreverse(diagram, runner.config)
+            if caption:
+                node = figure_wrapper(self, node, caption)
+            ret.append(node)
+        return ret
