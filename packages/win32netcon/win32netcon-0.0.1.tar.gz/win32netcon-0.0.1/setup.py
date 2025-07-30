3bb601ab81845019c72c50ea8fe10ddc7063612c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:20 +0000

    Add package win32netcon/0.0.1/win32netcon-0.0.1.tar.gz

---
+++ packages/win32netcon/win32netcon-0.0.1.tar.gz/win32netcon-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="win32netcon",
+    version="0.0.1",
+    packages=["win32netcon"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
