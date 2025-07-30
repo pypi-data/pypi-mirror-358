9b72d0ed65e4a9e0fe821addabacd67e1e4dfb76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:22 +0000

    Add package fasttext-pybind/0.0.1/fasttext-pybind-0.0.1.tar.gz

---
+++ packages/fasttext-pybind/fasttext-pybind-0.0.1.tar.gz/fasttext-pybind-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="fasttext-pybind",
+    version="0.0.1",
+    packages=["fasttext-pybind"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
