56b67955f4b19bfde15288113ac47dbbdedd22ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:06 +0000

    Add package silky-voice/0.1.32/silky-voice-0.1.32.tar.gz

---
+++ packages/silky-voice/silky-voice-0.1.32.tar.gz/silky-voice-0.1.32/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="silky-voice",
+    version="0.1.32",
+    packages=["silky-voice"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
