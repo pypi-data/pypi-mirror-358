af26c2225a6cee1e7083e7792b780f986eb81908
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:34:05 +0000

    Add package asgl/2.1.4/asgl-2.1.4.tar.gz

---
+++ packages/asgl/asgl-2.1.4.tar.gz/asgl-2.1.4/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+from os import path
+
+# read the contents of your README file
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name='asgl',
+    version='2.1.4',
+    author='Alvaro Mendez Civieta',
+    author_email='alvaromc317@gmail.com',
+    license='GNU General Public License',
+    zip_safe=False,
+    url='https://github.com/alvaromc317/asgl',
+    dowload_url='https://github.com/alvaromc317/asgl/archive/refs/tags/2.1.4.tar.gz',
+    description='A regression solver for high dimensional penalized linear, quantile and logistic regression models',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords=['variable-selection', 'regression', 'classification', 'penalization', 'lasso',
+              'adaptive-lasso', 'group-lasso', 'sparse-group-lasso', 'high-dimension', 'quantile-regression'],
+    python_requires='>=3.9',
+    install_requires=["cvxpy >= 1.5.0",
+                      "numpy >= 1.20.0",
+                      "scikit-learn >= 1.6",
+                      "scipy >= 1.1",
+                      "pytest >= 7.1.2"],
+    packages=find_packages()
+)
