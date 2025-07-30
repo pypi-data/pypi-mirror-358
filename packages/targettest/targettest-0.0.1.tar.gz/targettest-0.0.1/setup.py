b6cca595571c92a9872cdafe6df15ee87b543092
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:10 +0000

    Add package targettest/0.0.1/targettest-0.0.1.tar.gz

---
+++ packages/targettest/targettest-0.0.1.tar.gz/targettest-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="targettest",
+    version="0.0.1",
+    packages=["targettest"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
