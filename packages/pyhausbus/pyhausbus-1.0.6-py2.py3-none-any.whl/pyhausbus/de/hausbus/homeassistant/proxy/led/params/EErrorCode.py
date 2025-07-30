efc96d31f38ba4bf3b295705052522ecd9982936
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:07:14 +0000

    Add package pyhausbus/1.0.6/pyhausbus-1.0.6-py2.py3-none-any.whl

---
+++ packages/pyhausbus/pyhausbus-1.0.6-py2.py3-none-any.whl/pyhausbus/de/hausbus/homeassistant/proxy/led/params/EErrorCode.py
@@ -0,0 +1,26 @@
+import pyhausbus.HausBusUtils as HausBusUtils
+from enum import Enum
+
+class EErrorCode(Enum):
+  ANY_ERROR=255
+  SER_UNKNOWN=-1
+
+  @staticmethod
+  def _fromBytes(data:bytearray, offset):
+    checkValue = HausBusUtils.bytesToInt(data, offset)
+    for act in EErrorCode.__members__.values():
+      if (act.value == checkValue):
+        return act
+
+    return EErrorCode.SER_UNKNOWN
+
+  @staticmethod
+  def value_of(name: str) -> 'EFirmwareId':
+    try:
+      return EFirmwareId[name]
+    except KeyError:
+      return EFirmwareId.SER_UNKNOWN 
+
+
+
+
