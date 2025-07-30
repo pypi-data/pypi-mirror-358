e86e0da7d6b79a9ce9575aacf9c717d043a44ad0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:08:45 +0000

    Add package scnetwork/1.1.3/scnetwork-1.1.3.tar.gz

---
+++ packages/scnetwork/scnetwork-1.1.3.tar.gz/scnetwork-1.1.3/scnetwork/__init__.py
@@ -0,0 +1,31 @@
+"""
+This code was architected, developed, and programmed by Ben-Hur Varriano for Sapiens Technology®️ (as well as all other AI algorithms of the company),
+and any unauthorized alteration, adaptation, and/or distribution, as well as public comments and/or postings regarding the operation and/or mathematics involved in the algorithm, are strictly prohibited.
+Failure to comply with these rules may result in legal action against the author by our team of attorneys.
+
+The module in question comprises a sophisticated mathematical algorithm operated by a set of advanced applied mathematics functions, equations,
+and calculations that collaboratively develop large language models (LLMs) with a novel approach and a revolutionary concept.
+It differentiates itself from other architectures due to its ease of construction and efficiency in model training and inference.
+The parameterization, training, and inference processes are significantly faster and require computational resources several orders of magnitude lower than those used by conventional model architectures.
+Furthermore, the architecture can be used to train instruction-based models, chat-based models, or a combination of both.
+Its tensor processing via streaming enables an infinite context window, and its adaptive response calculations allow the model to generalize to inputs it was not explicitly trained on.
+
+We named the network SCNet, an abbreviation of "Semantic Comparison Network", referring to the underlying algorithm (also authored by Ben-Hur Varriano) that originated the current code.
+"""
+# --------------------------> A SAPIENS TECHNOLOGY®️ PRODUCTION) <--------------------------
+from .scnetwork import *
+"""
+This code was architected, developed, and programmed by Ben-Hur Varriano for Sapiens Technology®️ (as well as all other AI algorithms of the company),
+and any unauthorized alteration, adaptation, and/or distribution, as well as public comments and/or postings regarding the operation and/or mathematics involved in the algorithm, are strictly prohibited.
+Failure to comply with these rules may result in legal action against the author by our team of attorneys.
+
+The module in question comprises a sophisticated mathematical algorithm operated by a set of advanced applied mathematics functions, equations,
+and calculations that collaboratively develop large language models (LLMs) with a novel approach and a revolutionary concept.
+It differentiates itself from other architectures due to its ease of construction and efficiency in model training and inference.
+The parameterization, training, and inference processes are significantly faster and require computational resources several orders of magnitude lower than those used by conventional model architectures.
+Furthermore, the architecture can be used to train instruction-based models, chat-based models, or a combination of both.
+Its tensor processing via streaming enables an infinite context window, and its adaptive response calculations allow the model to generalize to inputs it was not explicitly trained on.
+
+We named the network SCNet, an abbreviation of "Semantic Comparison Network", referring to the underlying algorithm (also authored by Ben-Hur Varriano) that originated the current code.
+"""
+# --------------------------> A SAPIENS TECHNOLOGY®️ PRODUCTION) <--------------------------
