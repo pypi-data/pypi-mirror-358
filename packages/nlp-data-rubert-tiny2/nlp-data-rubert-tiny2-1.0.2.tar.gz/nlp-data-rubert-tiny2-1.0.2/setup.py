437bfede8d69b6fa24acb27ab440f67b5adf7880
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:57 +0000

    Add package nlp-data-rubert-tiny2/1.0.2/nlp-data-rubert-tiny2-1.0.2.tar.gz

---
+++ packages/nlp-data-rubert-tiny2/nlp-data-rubert-tiny2-1.0.2.tar.gz/nlp-data-rubert-tiny2-1.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="nlp-data-rubert-tiny2",
+    version="1.0.2",
+    packages=["nlp-data-rubert-tiny2"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
