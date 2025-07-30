cfbddd3430196852b984f406c6e98dce36765123
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:06 +0000

    Add package pagedown/0.0.1/pagedown-0.0.1.tar.gz

---
+++ packages/pagedown/pagedown-0.0.1.tar.gz/pagedown-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pagedown",
+    version="0.0.1",
+    packages=["pagedown"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
