c0f54ae6fd1908fd537430ed37dda2989201617a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:05:08 +0000

    Add package scrooge-settings/2.0.7/scrooge-settings-2.0.7.tar.gz

---
+++ packages/scrooge-settings/scrooge-settings-2.0.7.tar.gz/scrooge-settings-2.0.7/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="scrooge-settings",
+    version="2.0.7",
+    packages=["scrooge-settings"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
