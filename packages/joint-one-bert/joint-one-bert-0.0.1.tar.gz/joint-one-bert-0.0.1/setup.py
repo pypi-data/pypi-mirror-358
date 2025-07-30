d6e3c94cb3358e5d6770b6e0d22f0862e8835528
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:41 +0000

    Add package joint-one-bert/0.0.1/joint-one-bert-0.0.1.tar.gz

---
+++ packages/joint-one-bert/joint-one-bert-0.0.1.tar.gz/joint-one-bert-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="joint-one-bert",
+    version="0.0.1",
+    packages=["joint-one-bert"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
