0d057a2665024c429a1f701045b3843218d64756
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:21 +0000

    Add package generated-pb/0.0.1/generated-pb-0.0.1.tar.gz

---
+++ packages/generated-pb/generated-pb-0.0.1.tar.gz/generated-pb-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="generated-pb",
+    version="0.0.1",
+    packages=["generated-pb"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
