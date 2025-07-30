f3c46fbfb5b69db5dedb7397e5e21acc76e9e869
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:05 +0000

    Add package pyhamcrest-matchers/0.0.1/pyhamcrest-matchers-0.0.1.tar.gz

---
+++ packages/pyhamcrest-matchers/pyhamcrest-matchers-0.0.1.tar.gz/pyhamcrest-matchers-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pyhamcrest-matchers",
+    version="0.0.1",
+    packages=["pyhamcrest-matchers"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
