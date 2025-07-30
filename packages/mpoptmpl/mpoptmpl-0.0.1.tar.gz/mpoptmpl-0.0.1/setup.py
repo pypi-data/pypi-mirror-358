4322687459587e3503eccaa29a47d66d8abc6905
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:21 +0000

    Add package mpoptmpl/0.0.1/mpoptmpl-0.0.1.tar.gz

---
+++ packages/mpoptmpl/mpoptmpl-0.0.1.tar.gz/mpoptmpl-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="mpoptmpl",
+    version="0.0.1",
+    packages=["mpoptmpl"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
