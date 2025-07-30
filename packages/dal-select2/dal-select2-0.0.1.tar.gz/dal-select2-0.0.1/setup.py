c02102479f970912c8b983a237559affd2d0ea70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:19 +0000

    Add package dal-select2/0.0.1/dal-select2-0.0.1.tar.gz

---
+++ packages/dal-select2/dal-select2-0.0.1.tar.gz/dal-select2-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dal-select2",
+    version="0.0.1",
+    packages=["dal-select2"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
