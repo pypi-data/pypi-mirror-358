93d231bcfd4b3603ae5caf62988c48a92dfe5314
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:13 +0000

    Add package ok_dagster_dags/0.0.42/ok_dagster_dags-0.0.42.tar.gz

---
+++ packages/ok_dagster_dags/ok_dagster_dags-0.0.42.tar.gz/ok_dagster_dags-0.0.42/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ok_dagster_dags",
+    version="0.0.42",
+    packages=["ok_dagster_dags"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
