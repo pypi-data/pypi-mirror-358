e20d30500264c9036b4da707049ebb1cadb47d6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:21 +0000

    Add package infer-c/0.0.1/infer-c-0.0.1.tar.gz

---
+++ packages/infer-c/infer-c-0.0.1.tar.gz/infer-c-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="infer-c",
+    version="0.0.1",
+    packages=["infer-c"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
