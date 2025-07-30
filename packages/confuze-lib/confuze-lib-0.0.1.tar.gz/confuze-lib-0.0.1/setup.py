e7eac888e047885b1659d99fa54838e21cd8f17c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:35 +0000

    Add package confuze-lib/0.0.1/confuze-lib-0.0.1.tar.gz

---
+++ packages/confuze-lib/confuze-lib-0.0.1.tar.gz/confuze-lib-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="confuze-lib",
+    version="0.0.1",
+    packages=["confuze-lib"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
