77fbeddf6ae867cb07105936223d792ab2f20f0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:07:01 +0000

    Add package dwh-luigi-extensions/0.0.1/dwh-luigi-extensions-0.0.1.tar.gz

---
+++ packages/dwh-luigi-extensions/dwh-luigi-extensions-0.0.1.tar.gz/dwh-luigi-extensions-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dwh-luigi-extensions",
+    version="0.0.1",
+    packages=["dwh-luigi-extensions"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
