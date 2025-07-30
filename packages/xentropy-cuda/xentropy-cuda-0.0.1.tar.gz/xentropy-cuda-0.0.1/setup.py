bd258812a03ae65b33c75bfae7c65aff9b2d0b34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:37 +0000

    Add package xentropy-cuda/0.0.1/xentropy-cuda-0.0.1.tar.gz

---
+++ packages/xentropy-cuda/xentropy-cuda-0.0.1.tar.gz/xentropy-cuda-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="xentropy-cuda",
+    version="0.0.1",
+    packages=["xentropy-cuda"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
