27971db655911cb5433a8e3ed07597421655553e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:35 +0000

    Add package datalens-connector-mysql/0.0.1/datalens-connector-mysql-0.0.1.tar.gz

---
+++ packages/datalens-connector-mysql/datalens-connector-mysql-0.0.1.tar.gz/datalens-connector-mysql-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="datalens-connector-mysql",
+    version="0.0.1",
+    packages=["datalens-connector-mysql"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
