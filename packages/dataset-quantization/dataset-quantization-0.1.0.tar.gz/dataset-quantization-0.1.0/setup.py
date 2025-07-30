49ec22a29fc0d1a88b89b0ed7cfc0eceb48b9c8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:07:02 +0000

    Add package dataset-quantization/0.1.0/dataset-quantization-0.1.0.tar.gz

---
+++ packages/dataset-quantization/dataset-quantization-0.1.0.tar.gz/dataset-quantization-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dataset-quantization",
+    version="0.1.0",
+    packages=["dataset-quantization"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
