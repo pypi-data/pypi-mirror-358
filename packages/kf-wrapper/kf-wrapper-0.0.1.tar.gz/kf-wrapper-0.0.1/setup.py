bb0d94d6ff92a474e3eef4e3c57054833b5a9001
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:15 +0000

    Add package kf-wrapper/0.0.1/kf-wrapper-0.0.1.tar.gz

---
+++ packages/kf-wrapper/kf-wrapper-0.0.1.tar.gz/kf-wrapper-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="kf-wrapper",
+    version="0.0.1",
+    packages=["kf-wrapper"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
