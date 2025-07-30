ca3a99959068b5ce115aa0e8eb12cef14da1df45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:14 +0000

    Add package prog5-11/0.0.1/prog5-11-0.0.1.tar.gz

---
+++ packages/prog5-11/prog5-11-0.0.1.tar.gz/prog5-11-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="prog5-11",
+    version="0.0.1",
+    packages=["prog5-11"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
