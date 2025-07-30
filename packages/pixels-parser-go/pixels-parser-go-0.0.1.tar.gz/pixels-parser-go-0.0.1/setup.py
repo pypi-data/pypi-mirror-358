97f57930bfd95efeafe6831b9e552c556e992fca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:18 +0000

    Add package pixels-parser-go/0.0.1/pixels-parser-go-0.0.1.tar.gz

---
+++ packages/pixels-parser-go/pixels-parser-go-0.0.1.tar.gz/pixels-parser-go-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pixels-parser-go",
+    version="0.0.1",
+    packages=["pixels-parser-go"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
