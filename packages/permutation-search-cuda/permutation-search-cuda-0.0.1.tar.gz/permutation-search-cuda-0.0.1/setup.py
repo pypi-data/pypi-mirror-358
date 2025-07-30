14cb34c8d45483a41921d429f3de5940cdc095c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:04 +0000

    Add package permutation-search-cuda/0.0.1/permutation-search-cuda-0.0.1.tar.gz

---
+++ packages/permutation-search-cuda/permutation-search-cuda-0.0.1.tar.gz/permutation-search-cuda-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="permutation-search-cuda",
+    version="0.0.1",
+    packages=["permutation-search-cuda"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
