66efc70e68ac14c4953da80dc85fd450e06c48a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:20 +0000

    Add package dvc-datasets/0.0.1/dvc-datasets-0.0.1.tar.gz

---
+++ packages/dvc-datasets/dvc-datasets-0.0.1.tar.gz/dvc-datasets-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dvc-datasets",
+    version="0.0.1",
+    packages=["dvc-datasets"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
