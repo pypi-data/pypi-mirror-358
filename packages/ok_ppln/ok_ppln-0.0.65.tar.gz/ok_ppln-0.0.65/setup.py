fc8eb00d8a5e2941814666054614db9500a9c296
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:13 +0000

    Add package ok_ppln/0.0.65/ok_ppln-0.0.65.tar.gz

---
+++ packages/ok_ppln/ok_ppln-0.0.65.tar.gz/ok_ppln-0.0.65/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ok_ppln",
+    version="0.0.65",
+    packages=["ok_ppln"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
