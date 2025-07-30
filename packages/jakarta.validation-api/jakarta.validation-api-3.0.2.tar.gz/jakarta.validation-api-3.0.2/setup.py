c335a6b8e284d15317452e30d3eeb061fc4bb4f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:21 +0000

    Add package jakarta.validation-api/3.0.2/jakarta.validation-api-3.0.2.tar.gz

---
+++ packages/jakarta.validation-api/jakarta.validation-api-3.0.2.tar.gz/jakarta.validation-api-3.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="jakarta.validation-api",
+    version="3.0.2",
+    packages=["jakarta.validation-api"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
