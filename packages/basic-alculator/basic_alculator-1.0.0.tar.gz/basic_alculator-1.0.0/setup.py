dbde270b7205c620507c466c2be0d3fb183f2387
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:12 +0000

    Add package basic-alculator/1.0.0/basic_alculator-1.0.0.tar.gz

---
+++ packages/basic-alculator/basic_alculator-1.0.0.tar.gz/basic_alculator-1.0.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="basic-alculator",
+    version="1.0.0",
+    author="avik-sarkar",
+    author_email="aviksarkar44545@gmail.com",
+    description="This is a basic calculator package that can do additions, subtractions, etc.",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",  # ✅ This line is essential
+    url="https://github.com/AvikSarkar0/Basic_Calculator.git",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "Calculator=Calculator.simple_calculator:main",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.12',
+)
