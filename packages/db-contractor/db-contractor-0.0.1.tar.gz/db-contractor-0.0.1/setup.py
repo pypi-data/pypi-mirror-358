f7380508077c49cf9cfccf7d015d422bc368473f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:09 +0000

    Add package db-contractor/0.0.1/db-contractor-0.0.1.tar.gz

---
+++ packages/db-contractor/db-contractor-0.0.1.tar.gz/db-contractor-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="db-contractor",
+    version="0.0.1",
+    packages=["db-contractor"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
