5de3a5930b4b241c4d68f4d5b9d24fd9ef287241
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:53 +0000

    Add package bnp/0.0.1/bnp-0.0.1.tar.gz

---
+++ packages/bnp/bnp-0.0.1.tar.gz/bnp-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="bnp",
+    version="0.0.1",
+    packages=["bnp"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
