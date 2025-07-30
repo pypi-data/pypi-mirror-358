078fdb5ffc8cbcbd9c12d31d9889db5bd7be18df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:55 +0000

    Add package pypo-rubric/0.0.1/pypo-rubric-0.0.1.tar.gz

---
+++ packages/pypo-rubric/pypo-rubric-0.0.1.tar.gz/pypo-rubric-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pypo-rubric",
+    version="0.0.1",
+    packages=["pypo-rubric"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
