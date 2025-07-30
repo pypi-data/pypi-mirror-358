680b1b15ff45221bcd9e37c0a450292208a29da1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:38 +0000

    Add package pycbsclient/0.0.1/pycbsclient-0.0.1.tar.gz

---
+++ packages/pycbsclient/pycbsclient-0.0.1.tar.gz/pycbsclient-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pycbsclient",
+    version="0.0.1",
+    packages=["pycbsclient"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
