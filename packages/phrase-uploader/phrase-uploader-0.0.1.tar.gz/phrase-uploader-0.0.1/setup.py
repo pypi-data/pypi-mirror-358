8e29b6447920171f188c6d1c0372b7912296f442
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:18 +0000

    Add package phrase-uploader/0.0.1/phrase-uploader-0.0.1.tar.gz

---
+++ packages/phrase-uploader/phrase-uploader-0.0.1.tar.gz/phrase-uploader-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="phrase-uploader",
+    version="0.0.1",
+    packages=["phrase-uploader"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
