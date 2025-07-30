064f5a3abcbb0d58f61cb000b998866d34c19999
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:05:13 +0000

    Add package datalens-data-api/0.0.0/datalens-data-api-0.0.0.tar.gz

---
+++ packages/datalens-data-api/datalens-data-api-0.0.0.tar.gz/datalens-data-api-0.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="datalens-data-api",
+    version="0.0.0",
+    packages=["datalens-data-api"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
