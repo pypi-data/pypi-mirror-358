a5012169f02cbbb04862f2e593d2e412740e7a3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:41 +0000

    Add package calc-params/0.0.1/calc-params-0.0.1.tar.gz

---
+++ packages/calc-params/calc-params-0.0.1.tar.gz/calc-params-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="calc-params",
+    version="0.0.1",
+    packages=["calc-params"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
