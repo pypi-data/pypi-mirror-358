1a90ad73a3ba7304c2ff1e61645de89415aa16dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:10:31 +0000

    Add package macdefaultbrowsy/2.0.5/macdefaultbrowsy-2.0.5-py3-none-any.whl

---
+++ packages/macdefaultbrowsy/macdefaultbrowsy-2.0.5-py3-none-any.whl/macdefaultbrowsy/__init__.py
@@ -0,0 +1,15 @@
+# ruff: noqa: F401
+# macdefaultbrowsy/__init__.py
+
+"""A Python port of macdefaultbrowser to manage the default web browser on macOS."""
+
+from .macdefaultbrowsy import (
+    get_available_browsers,
+    read_default_browser,
+    set_default_browser,
+    print_browsers_list,
+)
+
+from .__version__ import __version__
+
+__all__ = ["get_available_browsers", "read_default_browser", "set_default_browser", "print_browsers_list"]
