961145643c3226c3790a074ed3d02260258b5a16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:07 +0000

    Add package flash-attn-cuda/0.0.1/flash-attn-cuda-0.0.1.tar.gz

---
+++ packages/flash-attn-cuda/flash-attn-cuda-0.0.1.tar.gz/flash-attn-cuda-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="flash-attn-cuda",
+    version="0.0.1",
+    packages=["flash-attn-cuda"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
