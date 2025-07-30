5363451d7f96508bd917477569c60026d624ea39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:46:14 +0000

    Add package disks/0.0.1/disks-0.0.1.tar.gz

---
+++ packages/disks/disks-0.0.1.tar.gz/disks-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="disks",
+    version="0.0.1",
+    description="Placeholder package to reserve the name 'disks'",
+    author="Gafa",  # أو أي اسم عايزه
+    packages=find_packages(),
+    python_requires=">=3.6",
+)
