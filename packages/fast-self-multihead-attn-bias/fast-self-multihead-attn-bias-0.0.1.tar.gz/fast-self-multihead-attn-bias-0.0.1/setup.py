e735fe43db2112d15eeeb1700f5f82f65a7ead81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:35 +0000

    Add package fast-self-multihead-attn-bias/0.0.1/fast-self-multihead-attn-bias-0.0.1.tar.gz

---
+++ packages/fast-self-multihead-attn-bias/fast-self-multihead-attn-bias-0.0.1.tar.gz/fast-self-multihead-attn-bias-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="fast-self-multihead-attn-bias",
+    version="0.0.1",
+    packages=["fast-self-multihead-attn-bias"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
