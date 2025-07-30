925ac1c6ac4937edc041431894b4546a3a881609
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:28 +0000

    Add package lexitheras/0.1.0/lexitheras-0.1.0-py3-none-any.whl

---
+++ packages/lexitheras/lexitheras-0.1.0-py3-none-any.whl/lexitheras/__init__.py
@@ -0,0 +1,12 @@
+"""
+Lexitheras - Perseus Greek vocabulary to Anki deck converter
+"""
+
+__version__ = "0.1.0"
+__author__ = "Conor Reid"
+
+from .scraper import PerseusVocabScraper
+from .deck import AnkiDeckCreator
+from .search import PerseusTextSearcher
+
+__all__ = ["PerseusVocabScraper", "AnkiDeckCreator", "PerseusTextSearcher"]
