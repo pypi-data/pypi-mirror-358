d6a4e052742be0b4a2d1b69d4600a79db6adf3f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:27 +0000

    Add package stat-modifier/0.0.1/stat-modifier-0.0.1.tar.gz

---
+++ packages/stat-modifier/stat-modifier-0.0.1.tar.gz/stat-modifier-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="stat-modifier",
+    version="0.0.1",
+    packages=["stat-modifier"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
