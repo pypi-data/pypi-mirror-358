45a083765cc8678188f262370d1706cd49a0c2b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:19 +0000

    Add package imageops/0.0.1/imageops-0.0.1.tar.gz

---
+++ packages/imageops/imageops-0.0.1.tar.gz/imageops-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="imageops",
+    version="0.0.1",
+    packages=["imageops"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
