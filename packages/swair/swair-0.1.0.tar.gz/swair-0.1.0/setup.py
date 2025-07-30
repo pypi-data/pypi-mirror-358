7a32dbb5341db0293d8660d8beb9ef3a5e13082d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:37 +0000

    Add package swair/0.1.0/swair-0.1.0.tar.gz

---
+++ packages/swair/swair-0.1.0.tar.gz/swair-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='swair',
+    version='0.1.0',
+    packages=find_packages(),
+    author='fswair',
+    description='swair is a Python package.',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
