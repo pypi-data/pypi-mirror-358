896367ffd20f8a515f8ed917c9afdeff0648eaea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:19 +0000

    Add package ordb/0.5.3/ordb-0.5.3-py3-none-any.whl

---
+++ packages/ordb/ordb-0.5.3-py3-none-any.whl/ordb/__init__.py
@@ -0,0 +1,20 @@
+"""ordb - Norwegian dictionary search tool."""
+
+__version__ = "0.5.2"
+__author__ = "Konrad M. Lawson"
+
+# Main public API
+from .core import (
+    search_exact, search_fuzzy, search_prefix, search_anywhere_term, 
+    search_fulltext, search_anywhere, search_expressions_only,
+    search_all_examples, setup_database
+)
+from .config import SearchConfig
+from .display import format_result, display_statistics
+
+__all__ = [
+    "search_exact", "search_fuzzy", "search_prefix", "search_anywhere_term",
+    "search_fulltext", "search_anywhere", "search_expressions_only", 
+    "search_all_examples", "setup_database", "SearchConfig", 
+    "format_result", "display_statistics"
+]
