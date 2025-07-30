9e3bf06ef5e9775714a1d5245fc4a5f51694d13c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:20 +0000

    Add package appcenter-scripts/0.0.1/appcenter-scripts-0.0.1.tar.gz

---
+++ packages/appcenter-scripts/appcenter-scripts-0.0.1.tar.gz/appcenter-scripts-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="appcenter-scripts",
+    version="0.0.1",
+    packages=["appcenter-scripts"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
