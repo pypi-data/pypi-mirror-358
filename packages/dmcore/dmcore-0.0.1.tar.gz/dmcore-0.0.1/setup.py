bf12dc23027a2a43d1be9d3f10d97fe209f554e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:07:01 +0000

    Add package dmcore/0.0.1/dmcore-0.0.1.tar.gz

---
+++ packages/dmcore/dmcore-0.0.1.tar.gz/dmcore-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dmcore",
+    version="0.0.1",
+    packages=["dmcore"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
