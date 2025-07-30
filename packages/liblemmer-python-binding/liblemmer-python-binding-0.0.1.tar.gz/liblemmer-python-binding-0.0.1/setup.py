2947e7ca3342fe163c474cd85c07fd8fb28292b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:22 +0000

    Add package liblemmer-python-binding/0.0.1/liblemmer-python-binding-0.0.1.tar.gz

---
+++ packages/liblemmer-python-binding/liblemmer-python-binding-0.0.1.tar.gz/liblemmer-python-binding-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="liblemmer-python-binding",
+    version="0.0.1",
+    packages=["liblemmer-python-binding"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
