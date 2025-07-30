0c17873d771755915c6119c4edb8bb51c4a36125
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:29 +0000

    Add package autoreg-learn/0.0.1/autoreg-learn-0.0.1.tar.gz

---
+++ packages/autoreg-learn/autoreg-learn-0.0.1.tar.gz/autoreg-learn-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="autoreg-learn",
+    version="0.0.1",
+    packages=["autoreg-learn"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
