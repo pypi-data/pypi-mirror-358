8f0ce7d029c94afad18be9d0b2fb436467c8acf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:21 +0000

    Add package geventmysql/0.0.1/geventmysql-0.0.1.tar.gz

---
+++ packages/geventmysql/geventmysql-0.0.1.tar.gz/geventmysql-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="geventmysql",
+    version="0.0.1",
+    packages=["geventmysql"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
