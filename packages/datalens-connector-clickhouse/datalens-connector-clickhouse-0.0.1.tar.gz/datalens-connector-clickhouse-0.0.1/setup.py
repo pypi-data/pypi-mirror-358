98d6fc0ea804cf5673b653f8cbfe2a3d022c115c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:35 +0000

    Add package datalens-connector-clickhouse/0.0.1/datalens-connector-clickhouse-0.0.1.tar.gz

---
+++ packages/datalens-connector-clickhouse/datalens-connector-clickhouse-0.0.1.tar.gz/datalens-connector-clickhouse-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="datalens-connector-clickhouse",
+    version="0.0.1",
+    packages=["datalens-connector-clickhouse"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
