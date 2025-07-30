210251602a9c8bb099f345818b2c522246c6c8a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:22 +0000

    Add package frankwolfetopython/0.3.1/frankwolfetopython-0.3.1-py3-none-any.whl

---
+++ packages/frankwolfetopython/frankwolfetopython-0.3.1-py3-none-any.whl/frankwolfetopython/julia_env.py
@@ -0,0 +1,8 @@
+import juliacall as jc 
+jl = jc.newmodule("FrankWolfeModule")
+jl.seval("using PythonCall")
+jl.seval("using FrankWolfe")
+jl.seval("using LinearAlgebra")
+jl.seval("using GLPK")
+jl.seval("using MathOptInterface")
+jl.seval("import MathOptInterface as MOI")
