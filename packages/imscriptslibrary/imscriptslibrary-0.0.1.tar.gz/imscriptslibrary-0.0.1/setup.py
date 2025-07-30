b3831993cb7119d5d7e059e09e0954ae524d35b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:20 +0000

    Add package imscriptslibrary/0.0.1/imscriptslibrary-0.0.1.tar.gz

---
+++ packages/imscriptslibrary/imscriptslibrary-0.0.1.tar.gz/imscriptslibrary-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="imscriptslibrary",
+    version="0.0.1",
+    packages=["imscriptslibrary"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
