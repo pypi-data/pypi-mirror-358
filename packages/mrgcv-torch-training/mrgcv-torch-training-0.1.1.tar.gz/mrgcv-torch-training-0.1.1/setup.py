3f768f5eee1adea8439e3de2c67e61fa5f3b4a48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:22 +0000

    Add package mrgcv-torch-training/0.1.1/mrgcv-torch-training-0.1.1.tar.gz

---
+++ packages/mrgcv-torch-training/mrgcv-torch-training-0.1.1.tar.gz/mrgcv-torch-training-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="mrgcv-torch-training",
+    version="0.1.1",
+    packages=["mrgcv-torch-training"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
