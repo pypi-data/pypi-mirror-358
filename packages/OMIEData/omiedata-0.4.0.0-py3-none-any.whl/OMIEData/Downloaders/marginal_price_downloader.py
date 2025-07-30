de4895dd52de873d4f714bbcaf3b3b8394f80b57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:20:59 +0000

    Add package OMIEData/0.4.0.0/omiedata-0.4.0.0-py3-none-any.whl

---
+++ packages/OMIEData/omiedata-0.4.0.0-py3-none-any.whl/OMIEData/Downloaders/marginal_price_downloader.py
@@ -0,0 +1,15 @@
+
+from OMIEData.Downloaders.general_omie_downloader import GeneralOMIEDownloader
+
+
+class MarginalPriceDownloader(GeneralOMIEDownloader):
+
+    url_year = 'AGNO_YYYY'
+    url_month = '/MES_MM/TXT/'
+    url_name = 'INT_PBC_EV_H_1_DD_MM_YYYY_DD_MM_YYYY.TXT'
+    output_mask = 'PMD_YYYYMMDD.txt'
+
+    def __init__(self):
+
+        url1 = self.url_year + self.url_month + self.url_name
+        GeneralOMIEDownloader.__init__(self, url_mask=url1, output_mask=self.output_mask)
---
+++ packages/OMIEData/omiedata-0.4.0.0-py3-none-any.whl/OMIEData/FileReaders/energy_by_technology_files_reader.py
@@ -0,0 +1,49 @@
+import pandas as pd
+from requests import Response
+from io import BytesIO
+
+from OMIEData.FileReaders.omie_file_reader import OMIEFileReader
+from OMIEData.Enums.all_enums import TechnologyType
+
+
+class EnergyByTechnologyHourlyFileReader(OMIEFileReader):
+
+    def __init__(self, types=None):
+
+        self.conceptsToLoad = [v for v in TechnologyType] if not types else types
+
+        self._dict_column_concept = {'Fecha': 'DATE',
+                                     'Hora': 'HOUR',
+                                     'CARBÓN': 'COAL',
+                                     'FUEL-GAS': 'FUEL_GAS',
+                                     'AUTOPRODUCTOR': 'SELF_PRODUCER',
+                                     'NUCLEAR': 'NUCLEAR',
+                                     'HIDRÁULICA': 'HYDRO',
+                                     'CICLO COMBINADO': 'COMBINED_CYCLE',
+                                     'EÓLICA': 'WIND',
+                                     'SOLAR TÉRMICA': 'THERMAL_SOLAR',
+                                     'SOLAR FOTOVOLTAICA': 'PHOTOVOLTAIC_SOLAR',
+                                     'COGENERACIÓN/RESIDUOS/MINI HIDRA': 'RESIDUALS',
+                                     'IMPORTACIÓN INTER.': 'IMPORT',
+                                     'IMPORTACIÓN INTER. SIN MIBEL': 'IMPORT_WITHOUT_MIBEL'}
+
+    def get_keys(self) -> list:
+
+        key_list_retrieve = ['DATE', 'HOUR']
+        key_list_retrieve.extend([str(v) for v in self.conceptsToLoad])
+        return key_list_retrieve
+
+    def get_data_from_response(self, response: Response) -> pd.DataFrame:
+        return self._get_data_from_file_like(file_like=BytesIO(response.content))
+
+    def get_data_from_file(self, filename: str) -> pd.DataFrame:
+        return self._get_data_from_file_like(file_like=filename)
+
+    def _get_data_from_file_like(self, file_like) -> pd.DataFrame:
+        df = pd.read_csv(file_like, sep=';', skiprows=2, header=0, encoding='latin-1', skipfooter=1, engine='python',
+                         decimal=",", thousands='.')
+        df = df.rename({k: v for k, v in self._dict_column_concept.items()}, axis=1)
+        df = df[[x for x in self.get_keys()]]
+
+        return df
+
