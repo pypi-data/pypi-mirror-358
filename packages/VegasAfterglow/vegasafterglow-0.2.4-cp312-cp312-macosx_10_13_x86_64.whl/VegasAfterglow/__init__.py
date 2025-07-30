9bd5f6ba7e6160b2b42ff032180185e6f8af8901
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:07:29 +0000

    Add package VegasAfterglow/0.2.4/vegasafterglow-0.2.4-cp312-cp312-macosx_10_13_x86_64.whl

---
+++ packages/VegasAfterglow/vegasafterglow-0.2.4-cp312-cp312-macosx_10_13_x86_64.whl/VegasAfterglow/__init__.py
@@ -0,0 +1,24 @@
+from .types import ModelParams, Setups, ObsData, VegasMC, FitResult, ParamDef, Scale, TophatJet, GaussianJet, PowerLawJet, ISM, Wind, Medium, Ejecta, Model, Radiation, Observer, Magnetar
+from .runner import Fitter
+
+__all__ = [
+    "ModelParams",
+    "Setups",
+    "ObsData",
+    "VegasMC",
+    "FitResult",
+    "Fitter",
+    "ParamDef",
+    "Scale",
+    "ISM",
+    "Wind",
+    "Medium",
+    "TophatJet",
+    "GaussianJet",
+    "PowerLawJet",
+    "Ejecta",
+    "Model",
+    "Radiation",
+    "Observer",
+    "Magnetar",
+]
