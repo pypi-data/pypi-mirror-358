fe67ea411e400077c008ce987088bae0fd409ccc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:19 +0000

    Add package tochvision/0.11.1/tochvision-0.11.1.tar.gz

---
+++ packages/tochvision/tochvision-0.11.1.tar.gz/tochvision-0.11.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="tochvision",
+    version="0.11.1",
+    packages=["tochvision"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
