d1a996f7b70129b8efc2aae5f4a47c45ac600e79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:56 +0000

    Add package pypo-social/0.0.1/pypo-social-0.0.1.tar.gz

---
+++ packages/pypo-social/pypo-social-0.0.1.tar.gz/pypo-social-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pypo-social",
+    version="0.0.1",
+    packages=["pypo-social"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
