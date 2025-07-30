8ac4545dc32c3bfe18b9000f8dd4fe22a2f6934f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:22 +0000

    Add package mrgcv-media/0.0.4/mrgcv-media-0.0.4.tar.gz

---
+++ packages/mrgcv-media/mrgcv-media-0.0.4.tar.gz/mrgcv-media-0.0.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="mrgcv-media",
+    version="0.0.4",
+    packages=["mrgcv-media"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
