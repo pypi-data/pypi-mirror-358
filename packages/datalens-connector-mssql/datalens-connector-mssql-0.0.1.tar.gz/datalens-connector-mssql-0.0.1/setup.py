3f63457baba92edccf9d9a9dd02fefedfca930cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:35 +0000

    Add package datalens-connector-mssql/0.0.1/datalens-connector-mssql-0.0.1.tar.gz

---
+++ packages/datalens-connector-mssql/datalens-connector-mssql-0.0.1.tar.gz/datalens-connector-mssql-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="datalens-connector-mssql",
+    version="0.0.1",
+    packages=["datalens-connector-mssql"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
