aa16f7339420398e42a7056c9c957257f04db36a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:07 +0000

    Add package mras-django-tastypie-jqgrid/0.1.2/mras-django-tastypie-jqgrid-0.1.2.tar.gz

---
+++ packages/mras-django-tastypie-jqgrid/mras-django-tastypie-jqgrid-0.1.2.tar.gz/mras-django-tastypie-jqgrid-0.1.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="mras-django-tastypie-jqgrid",
+    version="0.1.2",
+    packages=["mras-django-tastypie-jqgrid"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
