a932db3f2e87485858a21695be398299369dbd26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:07:01 +0000

    Add package dmgrinder/0.0.1/dmgrinder-0.0.1.tar.gz

---
+++ packages/dmgrinder/dmgrinder-0.0.1.tar.gz/dmgrinder-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dmgrinder",
+    version="0.0.1",
+    packages=["dmgrinder"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
