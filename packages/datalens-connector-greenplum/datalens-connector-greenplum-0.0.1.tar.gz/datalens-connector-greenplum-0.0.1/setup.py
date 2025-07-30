20b76abfc0991d78257e3cf24a88c97cf291588d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:35 +0000

    Add package datalens-connector-greenplum/0.0.1/datalens-connector-greenplum-0.0.1.tar.gz

---
+++ packages/datalens-connector-greenplum/datalens-connector-greenplum-0.0.1.tar.gz/datalens-connector-greenplum-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="datalens-connector-greenplum",
+    version="0.0.1",
+    packages=["datalens-connector-greenplum"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
