a769e73d33393246a222e55cc3b31cfc213ec71b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:15 +0000

    Add package oauth2mock/0.0.1/oauth2mock-0.0.1.tar.gz

---
+++ packages/oauth2mock/oauth2mock-0.0.1.tar.gz/oauth2mock-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="oauth2mock",
+    version="0.0.1",
+    packages=["oauth2mock"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
