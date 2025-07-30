08c9e62dbd9ec00ff42dac4b8e0a0f11c81df478
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:11 +0000

    Add package spyt/0.0.1/spyt-0.0.1.tar.gz

---
+++ packages/spyt/spyt-0.0.1.tar.gz/spyt-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="spyt",
+    version="0.0.1",
+    packages=["spyt"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
