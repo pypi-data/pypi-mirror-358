0854604500a040474c3ffa22dcca0c3be6f444b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:58 +0000

    Add package guestfs/0.0.1/guestfs-0.0.1.tar.gz

---
+++ packages/guestfs/guestfs-0.0.1.tar.gz/guestfs-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="guestfs",
+    version="0.0.1",
+    packages=["guestfs"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
