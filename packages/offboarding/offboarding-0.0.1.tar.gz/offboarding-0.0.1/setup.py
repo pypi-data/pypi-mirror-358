beb22281f18f22486d1e4c3f3c18a36e4f846ffb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:27 +0000

    Add package offboarding/0.0.1/offboarding-0.0.1.tar.gz

---
+++ packages/offboarding/offboarding-0.0.1.tar.gz/offboarding-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="offboarding",
+    version="0.0.1",
+    packages=["offboarding"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
