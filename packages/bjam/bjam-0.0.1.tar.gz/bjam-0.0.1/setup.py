ae67f281097bdab1434bd6b154f3f2bb8cb28f6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:22 +0000

    Add package bjam/0.0.1/bjam-0.0.1.tar.gz

---
+++ packages/bjam/bjam-0.0.1.tar.gz/bjam-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="bjam",
+    version="0.0.1",
+    packages=["bjam"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
