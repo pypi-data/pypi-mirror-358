b5fe15cf16fd74a67524738fdaae473520d4cace
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:12 +0000

    Add package poetry-plugin-dotenv/2.13.0/poetry_plugin_dotenv-2.13.0-py3-none-any.whl

---
+++ packages/poetry-plugin-dotenv/poetry_plugin_dotenv-2.13.0-py3-none-any.whl/poetry_plugin_dotenv/__init__.py
@@ -0,0 +1,23 @@
+"""poetry-plugin-dotenv - is the plugin that automatically loads environment variables from a dotenv file into the environment before poetry commands are run."""
+
+from __future__ import annotations
+
+
+__title__ = "poetry-plugin-dotenv"
+__summary__ = "poetry-plugin-dotenv - is the plugin that automatically loads environment variables from a dotenv file into the environment before poetry commands are run."
+__uri__ = "https://github.com/pivoshenko/poetry-plugin-dotenv"
+
+__version__ = "2.13.0"
+
+__author__ = "Volodymyr Pivoshenko"
+__email__ = "volodymyr.pivoshenko@gmail.com"
+
+__license__ = "MIT"
+__copyright__ = "Copyright 2025, Volodymyr Pivoshenko"
+
+from poetry_plugin_dotenv import config
+from poetry_plugin_dotenv import dotenv
+from poetry_plugin_dotenv import plugin
+
+
+__all__ = ["config", "dotenv", "plugin"]
