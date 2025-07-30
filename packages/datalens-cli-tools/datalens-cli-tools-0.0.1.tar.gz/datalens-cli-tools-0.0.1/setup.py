d56cb548f9ff0a89fb2906ae64378bb28efcd6e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:05:09 +0000

    Add package datalens-cli-tools/0.0.1/datalens-cli-tools-0.0.1.tar.gz

---
+++ packages/datalens-cli-tools/datalens-cli-tools-0.0.1.tar.gz/datalens-cli-tools-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="datalens-cli-tools",
+    version="0.0.1",
+    packages=["datalens-cli-tools"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
