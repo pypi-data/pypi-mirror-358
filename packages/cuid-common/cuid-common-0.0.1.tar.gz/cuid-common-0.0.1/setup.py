7c44e56fac3329ca7fe963debe15f11ee18de27e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:41 +0000

    Add package cuid-common/0.0.1/cuid-common-0.0.1.tar.gz

---
+++ packages/cuid-common/cuid-common-0.0.1.tar.gz/cuid-common-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="cuid-common",
+    version="0.0.1",
+    packages=["cuid-common"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
