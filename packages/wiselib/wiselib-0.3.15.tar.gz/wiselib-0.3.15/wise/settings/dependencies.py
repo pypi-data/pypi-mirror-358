92b1e692670e16cb8333b7f89d459b265e4bea77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:09:23 +0000

    Add package wiselib/0.3.15/wiselib-0.3.15.tar.gz

---
+++ packages/wiselib/wiselib-0.3.15.tar.gz/wiselib-0.3.15/wise/settings/dependencies.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+
+
+class DelphinusSettings(BaseModel):
+    url: str
+
+    timeout: int = 10  # request timeout in seconds
+
+    candles_by_slug_path: str = "/v1/candles-by-slugs"
+    last_candle_by_slug_path: str = "/v1/candles-by-slugs/last"
+
+    candles_by_pair_path: str = "/v2/candles-by-pair"
+    last_candle_by_pair_path: str = "/v2/candles-by-pair/last"
+
+
+class DelphiSettings(BaseModel):
+    url: str
