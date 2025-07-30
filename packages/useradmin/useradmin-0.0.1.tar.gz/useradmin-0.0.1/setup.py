da0ce3e5592cc4492aef36e905651cc6cba352e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:21 +0000

    Add package useradmin/0.0.1/useradmin-0.0.1.tar.gz

---
+++ packages/useradmin/useradmin-0.0.1.tar.gz/useradmin-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="useradmin",
+    version="0.0.1",
+    packages=["useradmin"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
