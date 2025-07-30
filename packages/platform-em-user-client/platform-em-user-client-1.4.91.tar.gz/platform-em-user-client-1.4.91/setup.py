8073811f3368d565a0a508df4b30a7b0adc83fea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:20 +0000

    Add package platform-em-user-client/1.4.91/platform-em-user-client-1.4.91.tar.gz

---
+++ packages/platform-em-user-client/platform-em-user-client-1.4.91.tar.gz/platform-em-user-client-1.4.91/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="platform-em-user-client",
+    version="1.4.91",
+    packages=["platform-em-user-client"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
