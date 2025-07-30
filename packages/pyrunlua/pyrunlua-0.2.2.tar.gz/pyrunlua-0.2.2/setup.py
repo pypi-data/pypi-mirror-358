f738758306af606a0140dd6e95d3f663da6312f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:33 +0000

    Add package pyrunlua/0.2.2/pyrunlua-0.2.2.tar.gz

---
+++ packages/pyrunlua/pyrunlua-0.2.2.tar.gz/pyrunlua-0.2.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pyrunlua",
+    version="0.2.2",
+    packages=["pyrunlua"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
