ee56067c7efaa50cdb845106ab4126de95cde2ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:21 +0000

    Add package vk-async-factory-boy/1.1.2/vk-async-factory-boy-1.1.2.tar.gz

---
+++ packages/vk-async-factory-boy/vk-async-factory-boy-1.1.2.tar.gz/vk-async-factory-boy-1.1.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="vk-async-factory-boy",
+    version="1.1.2",
+    packages=["vk-async-factory-boy"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
