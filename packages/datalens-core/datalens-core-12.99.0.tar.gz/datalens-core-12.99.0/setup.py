fbc9e343824fb60553125d920bd9e5f50deee9db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:05:12 +0000

    Add package datalens-core/12.99.0/datalens-core-12.99.0.tar.gz

---
+++ packages/datalens-core/datalens-core-12.99.0.tar.gz/datalens-core-12.99.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="datalens-core",
+    version="12.99.0",
+    packages=["datalens-core"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
