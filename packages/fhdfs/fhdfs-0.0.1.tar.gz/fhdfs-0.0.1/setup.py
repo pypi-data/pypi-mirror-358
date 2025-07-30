21f8bd48d0b09138cb14bdd99fadfa2ad7d9c1c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:14 +0000

    Add package fhdfs/0.0.1/fhdfs-0.0.1.tar.gz

---
+++ packages/fhdfs/fhdfs-0.0.1.tar.gz/fhdfs-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="fhdfs",
+    version="0.0.1",
+    packages=["fhdfs"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
