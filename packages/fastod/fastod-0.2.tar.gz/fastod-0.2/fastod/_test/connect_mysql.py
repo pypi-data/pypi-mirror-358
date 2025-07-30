1679e334ea149ebf0fc2d8cd58e24e84dfcac9df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:10 +0000

    Add package fastod/0.2/fastod-0.2.tar.gz

---
+++ packages/fastod/fastod-0.2.tar.gz/fastod-0.2/fastod/_test/connect_mysql.py
@@ -0,0 +1,22 @@
+from fastod import MySQL
+
+cfg = {
+    'host': 'localhost',
+    'port': 3306,
+    'user': 'root',
+    'password': 'root@0',
+    'db': 'test'
+}
+
+db = MySQL(**cfg)
+
+# 生成测试表（默认1w条数据）
+# db.gen_test_table('test_20250622')
+
+t = db['test_20250622']
+# 删除测试表
+# t.remove()
+
+lines = t.query(limit=10)
+for line in lines:
+    print(line)
