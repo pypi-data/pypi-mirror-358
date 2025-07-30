81268ba1a1046570435f08b22097f95244c661be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:39 +0000

    Add package ydb-proto-stubs-import/0.0.1/ydb-proto-stubs-import-0.0.1.tar.gz

---
+++ packages/ydb-proto-stubs-import/ydb-proto-stubs-import-0.0.1.tar.gz/ydb-proto-stubs-import-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ydb-proto-stubs-import",
+    version="0.0.1",
+    packages=["ydb-proto-stubs-import"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
