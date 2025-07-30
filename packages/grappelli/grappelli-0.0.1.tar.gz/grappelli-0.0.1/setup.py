fddb6b2db3e16556d63a139df7ad6cc2f5fc6968
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:55 +0000

    Add package grappelli/0.0.1/grappelli-0.0.1.tar.gz

---
+++ packages/grappelli/grappelli-0.0.1.tar.gz/grappelli-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="grappelli",
+    version="0.0.1",
+    packages=["grappelli"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
