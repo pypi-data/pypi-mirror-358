106e94ed1464710830471759ec3ea82c7d5ed8ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:08 +0000

    Add package equine/0.1.6rc1/equine-0.1.6rc1.tar.gz

---
+++ packages/equine/equine-0.1.6rc1.tar.gz/equine-0.1.6rc1/tests/test_equine_gp.py
@@ -0,0 +1,194 @@
+# Copyright 2024, MASSACHUSETTS INSTITUTE OF TECHNOLOGY
+# Subject to FAR 52.227-11 – Patent Rights – Ownership by the Contractor (May 2014).
+# SPDX-License-Identifier: MIT
+
+import os
+import torch
+from conftest import (
+    generate_random_string_list,
+    random_dataset,
+    use_basic_embedding_model,
+    use_save_load_model_tests,
+)
+from hypothesis import given, settings
+
+import equine as eq
+
+
+@given(random_dataset=random_dataset())
+@settings(deadline=None, max_examples=10)
+def test_equine_gp_train_from_scratch(random_dataset) -> None:
+    dataset, num_classes, X, embedding_model = use_basic_embedding_model(random_dataset)
+
+    model = eq.EquineGP(embedding_model, num_classes, num_classes)
+    loss_fn = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(
+        model.parameters(),
+        lr=0.001,
+        momentum=0.9,
+        weight_decay=0.0001,
+    )
+    model.train_model(dataset, loss_fn, optimizer, num_epochs=10)
+
+    model.predict(X[1:10])  # Contracts should fire asserts on errors
+
+
+@given(random_dataset=random_dataset())
+@settings(deadline=None, max_examples=10)
+def test_equine_gp_train_from_scratch_with_temperature(random_dataset) -> None:
+    dataset, num_classes, X, embedding_model = use_basic_embedding_model(random_dataset)
+
+    model = eq.EquineGP(embedding_model, num_classes, num_classes, use_temperature=True)
+    loss_fn = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(
+        model.parameters(),
+        lr=0.001,
+        momentum=0.9,
+        weight_decay=0.0001,
+    )
+    train_dict = model.train_model(dataset, loss_fn, optimizer, num_epochs=10)
+    assert "calibration_loader" in train_dict
+    assert train_dict["calibration_loader"] is not None
+    cal_loader = train_dict["calibration_loader"]
+
+    model.calibrate_temperature(cal_loader, 1, 0.01)
+
+    model.predict(X[1:10])  # Contracts should fire asserts on errors
+
+
+@given(random_dataset=random_dataset())
+@settings(deadline=None, max_examples=2)
+def test_equine_gp_save_load(random_dataset) -> None:
+    dataset, num_classes, X, embedding_model = use_basic_embedding_model(random_dataset)
+
+    model = eq.EquineGP(embedding_model, num_classes, num_classes)
+    loss_fn = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(
+        model.parameters(),
+        lr=0.001,
+        momentum=0.9,
+        weight_decay=0.0001,
+    )
+    model.train_model(dataset, loss_fn, optimizer, num_epochs=2)
+
+    new_model, tmp_filename = use_save_load_model_tests(
+        model, X, tmp_filename="gp_save_load.eq"
+    )
+
+    if os.path.exists(tmp_filename):
+        os.remove(tmp_filename)  # Cleanup
+
+
+@given(random_dataset=random_dataset())
+@settings(deadline=None, max_examples=1)
+def test_equine_gp_save_load_with_temperature(random_dataset) -> None:
+    dataset, num_classes, X, embedding_model = use_basic_embedding_model(random_dataset)
+
+    model = eq.EquineGP(embedding_model, num_classes, num_classes, use_temperature=True)
+    loss_fn = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(
+        model.parameters(),
+        lr=0.001,
+        momentum=0.9,
+        weight_decay=0.0001,
+    )
+    model.train_model(dataset, loss_fn, optimizer, num_epochs=2)
+
+    new_model, tmp_filename = use_save_load_model_tests(
+        model, X, tmp_filename="gp_save_load_with_temperature.eq"
+    )
+
+    if os.path.exists(tmp_filename):
+        os.remove(tmp_filename)  # Cleanup
+
+
+@given(random_dataset=random_dataset())
+@settings(deadline=None, max_examples=1)
+def test_equine_gp_save_load_with_vis(random_dataset) -> None:
+    dataset, num_classes, X, embedding_model = use_basic_embedding_model(random_dataset)
+
+    model = eq.EquineGP(embedding_model, num_classes, num_classes)
+    loss_fn = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(
+        model.parameters(),
+        lr=0.001,
+        momentum=0.9,
+        weight_decay=0.0001,
+    )
+    model.train_model(dataset, loss_fn, optimizer, num_epochs=2, vis_support=True)
+
+    new_model, tmp_filename = use_save_load_model_tests(
+        model, X, tmp_filename="gp_save_load_with_vis.eq"
+    )
+
+    assert new_model.support is not None, "support was not saved"
+    assert new_model.prototypes is not None, "prototypes were not saved"
+    assert (
+        model.support.keys() == new_model.get_support().keys()
+    ), "Support keys changed on reload"
+    assert (
+        torch.nn.functional.mse_loss(model.prototypes, new_model.get_prototypes())
+        <= 1e-7
+    ), "Prototypes changed on reload"
+
+    if os.path.exists(tmp_filename):
+        os.remove(tmp_filename)  # Cleanup
+
+
+@given(random_dataset=random_dataset())
+@settings(deadline=None, max_examples=1)
+def test_equine_gp_save_load_with_feature_and_label_names(random_dataset) -> None:
+    dataset, num_classes, X, embedding_model = use_basic_embedding_model(random_dataset)
+
+    # without feature and label names
+    model = eq.EquineGP(embedding_model, num_classes, num_classes)
+    loss_fn = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(
+        model.parameters(),
+        lr=0.001,
+        momentum=0.9,
+        weight_decay=0.0001,
+    )
+    model.train_model(dataset, loss_fn, optimizer, num_epochs=2)
+
+    new_model, tmp_filename = use_save_load_model_tests(
+        model, X, tmp_filename="gp_save_load_no_feature_and_label_names.eq"
+    )
+
+    assert new_model.get_feature_names() is None, "feature_names changed on reload"
+    assert new_model.get_label_names() is None, "label_names changed on reload"
+
+    if os.path.exists(tmp_filename):
+        os.remove(tmp_filename)  # Cleanup
+
+    # with feature and label names
+    feature_names = generate_random_string_list(X.shape[1])
+    label_names = generate_random_string_list(num_classes)
+
+    model = eq.EquineGP(
+        embedding_model,
+        num_classes,
+        num_classes,
+        feature_names=feature_names,
+        label_names=label_names,
+    )
+    loss_fn = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(
+        model.parameters(),
+        lr=0.001,
+        momentum=0.9,
+        weight_decay=0.0001,
+    )
+    model.train_model(dataset, loss_fn, optimizer, num_epochs=2)
+
+    new_model, tmp_filename = use_save_load_model_tests(
+        model, X, tmp_filename="gp_save_load_with_feature_and_label_names.eq"
+    )
+
+    assert (
+        new_model.get_feature_names() == feature_names
+    ), "feature_names changed on reload"
+    assert new_model.get_label_names() == label_names, "label_names changed on reload"
+
+    if os.path.exists(tmp_filename):
+        os.remove(tmp_filename)  # Cleanup
