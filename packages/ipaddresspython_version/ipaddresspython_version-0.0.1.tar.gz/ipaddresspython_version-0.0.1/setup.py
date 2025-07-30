b2f16b4647b121f1eab8a2d2a7b7f6ec29fe4428
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:59 +0000

    Add package ipaddresspython_version/0.0.1/ipaddresspython_version-0.0.1.tar.gz

---
+++ packages/ipaddresspython_version/ipaddresspython_version-0.0.1.tar.gz/ipaddresspython_version-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ipaddresspython_version",
+    version="0.0.1",
+    packages=["ipaddresspython_version"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
