aa4e3f648391e1723806100d371c3e9aa8819459
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:05:09 +0000

    Add package datalens-api-connector/0.0.1/datalens-api-connector-0.0.1.tar.gz

---
+++ packages/datalens-api-connector/datalens-api-connector-0.0.1.tar.gz/datalens-api-connector-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="datalens-api-connector",
+    version="0.0.1",
+    packages=["datalens-api-connector"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
