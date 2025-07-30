6937cae9d625a47a56f5c4d5fdd578401e51f26b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:30 +0000

    Add package notifyng-plugins/0.0.1/notifyng-plugins-0.0.1.tar.gz

---
+++ packages/notifyng-plugins/notifyng-plugins-0.0.1.tar.gz/notifyng-plugins-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="notifyng-plugins",
+    version="0.0.1",
+    packages=["notifyng-plugins"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
