c36543d1550606d68619e36572db59fcaaf741c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:18 +0000

    Add package sonya_ml/0.0.1/sonya_ml-0.0.1.tar.gz

---
+++ packages/sonya_ml/sonya_ml-0.0.1.tar.gz/sonya_ml-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="sonya_ml",
+    version="0.0.1",
+    packages=["sonya_ml"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
