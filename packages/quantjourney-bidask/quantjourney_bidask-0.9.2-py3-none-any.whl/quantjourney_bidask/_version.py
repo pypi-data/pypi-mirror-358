df02553a6230a3a3108fe365cef43dc9233af865
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:44:55 +0000

    Add package quantjourney-bidask/0.9.2/quantjourney_bidask-0.9.2-py3-none-any.whl

---
+++ packages/quantjourney-bidask/quantjourney_bidask-0.9.2-py3-none-any.whl/quantjourney_bidask/_version.py
@@ -0,0 +1,7 @@
+"""Version information for quantjourney_bidask."""
+
+__version__ = "0.9.2"
+__author__ = "Jakub Polec"
+__email__ = "jakub@quantjourney.pro"
+__license__ = "MIT"
+__copyright__ = "Copyright (c) 2025 Jakub Polec, QuantJourney"
