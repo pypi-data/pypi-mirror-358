c38b4d8ec574aae63d9a34a0a761cea253a00f82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:53 +0000

    Add package citrusleaf/0.0.1/citrusleaf-0.0.1.tar.gz

---
+++ packages/citrusleaf/citrusleaf-0.0.1.tar.gz/citrusleaf-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="citrusleaf",
+    version="0.0.1",
+    packages=["citrusleaf"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
