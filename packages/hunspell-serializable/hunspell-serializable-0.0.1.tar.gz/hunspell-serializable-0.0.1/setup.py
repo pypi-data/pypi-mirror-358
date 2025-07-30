22ff80a5edc76ba93e08aee5a26d333cb061037b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:09 +0000

    Add package hunspell-serializable/0.0.1/hunspell-serializable-0.0.1.tar.gz

---
+++ packages/hunspell-serializable/hunspell-serializable-0.0.1.tar.gz/hunspell-serializable-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="hunspell-serializable",
+    version="0.0.1",
+    packages=["hunspell-serializable"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
