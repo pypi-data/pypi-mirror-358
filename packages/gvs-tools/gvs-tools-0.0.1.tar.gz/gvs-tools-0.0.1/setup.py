ffd6b91e4055385b29e768867ce2211dfd3e77f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:21 +0000

    Add package gvs-tools/0.0.1/gvs-tools-0.0.1.tar.gz

---
+++ packages/gvs-tools/gvs-tools-0.0.1.tar.gz/gvs-tools-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="gvs-tools",
+    version="0.0.1",
+    packages=["gvs-tools"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
