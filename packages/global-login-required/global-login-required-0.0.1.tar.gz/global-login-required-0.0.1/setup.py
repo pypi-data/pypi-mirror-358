d90fcc65adf6fe4c38b6af4e489eb8d078d8b885
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:20 +0000

    Add package global-login-required/0.0.1/global-login-required-0.0.1.tar.gz

---
+++ packages/global-login-required/global-login-required-0.0.1.tar.gz/global-login-required-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="global-login-required",
+    version="0.0.1",
+    packages=["global-login-required"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
