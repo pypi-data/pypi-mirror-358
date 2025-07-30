e205a4a4976831c12848574b9c78983c574ecd43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:21 +0000

    Add package librdbms/0.0.1/librdbms-0.0.1.tar.gz

---
+++ packages/librdbms/librdbms-0.0.1.tar.gz/librdbms-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="librdbms",
+    version="0.0.1",
+    packages=["librdbms"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
