b255155d976c86a14b78f3839ce8d959c8f1131c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:05:19 +0000

    Add package datalens-sqlalchemy-common/0.2/datalens-sqlalchemy-common-0.2.tar.gz

---
+++ packages/datalens-sqlalchemy-common/datalens-sqlalchemy-common-0.2.tar.gz/datalens-sqlalchemy-common-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="datalens-sqlalchemy-common",
+    version="0.2",
+    packages=["datalens-sqlalchemy-common"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
