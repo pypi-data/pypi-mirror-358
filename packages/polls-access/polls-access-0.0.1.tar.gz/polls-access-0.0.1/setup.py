2e90eb09377c5dfe3d1aac7fee25e5fbfe832580
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:21 +0000

    Add package polls-access/0.0.1/polls-access-0.0.1.tar.gz

---
+++ packages/polls-access/polls-access-0.0.1.tar.gz/polls-access-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="polls-access",
+    version="0.0.1",
+    packages=["polls-access"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
