ffcd571a161aea7e765e9137ae6ac999f5895d6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:20 +0000

    Add package toolup-voip-bot/2024.7.4/toolup-voip-bot-2024.7.4.tar.gz

---
+++ packages/toolup-voip-bot/toolup-voip-bot-2024.7.4.tar.gz/toolup-voip-bot-2024.7.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="toolup-voip-bot",
+    version="2024.7.4",
+    packages=["toolup-voip-bot"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
