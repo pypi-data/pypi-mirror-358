1f05cfc8802983036d6f4bb07654158b0a0c280c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:19 +0000

    Add package window-normalization/0.0.1/window-normalization-0.0.1.tar.gz

---
+++ packages/window-normalization/window-normalization-0.0.1.tar.gz/window-normalization-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="window-normalization",
+    version="0.0.1",
+    packages=["window-normalization"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
