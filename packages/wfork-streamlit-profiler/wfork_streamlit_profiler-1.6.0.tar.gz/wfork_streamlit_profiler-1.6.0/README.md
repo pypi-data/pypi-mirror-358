7852f49a8b68063de57dd81fe601d653f180d561
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:08:42 +0000

    Add package wfork-streamlit-profiler/1.6.0/wfork_streamlit_profiler-1.6.0.tar.gz

---
+++ packages/wfork-streamlit-profiler/wfork_streamlit_profiler-1.6.0.tar.gz/wfork_streamlit_profiler-1.6.0/README.md
@@ -0,0 +1,59 @@
+# wfork-streamlit-profiler &nbsp;🏄🏼
+
+[![PyPi](https://img.shields.io/pypi/v/wfork-streamlit-profiler)](https://pypi.org/project/wfork-streamlit-profiler/) [![GitHub latest tag](https://img.shields.io/github/v/tag/wyattscarpenter/streamlit-profiler?label=GitHub&logo=github)](https://github.com/wyattscarpenter/streamlit-profiler/)
+
+**Runtime profiler for Streamlit, powered by [pyinstrument](https://github.com/joerick/pyinstrument).**
+
+*wfork-streamlit-profiler is a fork of streamlit-profiler 0.2.4. If there are any newer versions of the original streamlit-profiler, you should probably use those instead. See the original project's github for more: https://github.com/jrieke/streamlit-profiler*
+
+streamlit-profiler is a [Streamlit component](https://streamlit.io/components) that
+helps you find out which parts of your app are slow. It profiles the code via
+[pyinstrument](https://github.com/joerick/pyinstrument) and shows the results right
+within your Streamlit app.
+
+---
+
+<h3 align="center">
+  ⏱️ <a href="https://share.streamlit.io/jrieke/streamlit-profiler/main/examples/basic.py">Live demo</a> ⏱️
+</h3>
+
+---
+
+<p align="center">
+    <a href="https://share.streamlit.io/jrieke/streamlit-profiler/main/examples/basic.py"><img src="images/demo.png" width=600></a>
+</p>
+
+## Installation
+
+```bash
+pip install wfork-streamlit-profiler
+```
+
+## Usage
+
+```python3
+import streamlit as st
+from wfork_streamlit_profiler import Profiler
+
+with Profiler():
+    st.title("My app")
+    # ... other code
+```
+
+Or:
+```python3
+import streamlit as st
+from wfork_streamlit_profiler import Profiler
+
+p = Profiler()
+p.start()
+# ... other code
+p.stop()
+```
+
+Then start your app as usual: `streamlit run my_app.py`
+
+The `Profiler` class is an extension of `pyinstrument.Profiler`, so you can use
+[all of its functions](https://pyinstrument.readthedocs.io/en/latest/reference.html#pyinstrument.Profiler).
+
+Don't want the profiler to immediately display when you stop profiling? Initialize it with `Profiler(auto_output_on_stop=False)` instead, and call the `.output_streamlit()` method whenever you want it to display, instead. You can also set the async_mode of the profiler in the pyinstrument in the same constructor, whatever that means.
---
+++ packages/wfork-streamlit-profiler/wfork_streamlit_profiler-1.6.0.tar.gz/wfork_streamlit_profiler-1.6.0/wfork_streamlit_profiler/__init__.py
@@ -0,0 +1,104 @@
+import streamlit.components.v1 as components
+from pyinstrument import Profiler as OriginalProfiler
+from pyinstrument.profiler import AsyncMode
+from pyinstrument.session import Session
+from typing_extensions import override
+
+# pyright: strict
+
+class Profiler(OriginalProfiler):
+    _auto_output_on_stop: bool
+
+    def __init__(
+        self,
+        interval: float = 0.001,
+        async_mode: AsyncMode = "enabled",
+        auto_output_on_stop: bool = True,
+    ) -> None:
+        super().__init__(interval, async_mode)
+        self._auto_output_on_stop = auto_output_on_stop
+
+    @override
+    def stop(self) -> Session: #pytype: disable=override-error #workaround for: https://github.com/google/pytype/issues/1917
+        session = super().stop()
+
+        if self._auto_output_on_stop:
+            self.output_streamlit()
+
+        return session
+
+    def output_streamlit(self) -> None:
+        # Create HTML report of profiling results.
+        html = self.output_html()
+
+        # Adapt style of html report to match Streamlit's design.
+
+        # Default colors for Streamlit's white theme.
+        # TODO: Use streamlit's theme colors instead of hardcoding colors. Doesn't work 
+        #   currently, see https://github.com/streamlit/streamlit/issues/4198.
+        text_color = "#31333f"
+        background_color = "#ffffff"
+        secondary_background_color = "#f0f2f6"
+        caption_color = "#84858c"
+
+        # Default font color
+        html = html.replace("#fff", text_color)
+        # Background color
+        html = html.replace("#303538", background_color)
+        # Line background when hovered
+        html = html.replace(
+            "background-color:#354759;opacity:.5",
+            f"background-color:{secondary_background_color}",
+        )
+        # Header background
+        html = html.replace("#292f32", secondary_background_color)
+        # Expander background
+        html = html.replace("#3b4043", secondary_background_color)
+        # Expander background when hovered
+        html = html.replace("#4a4f54", "#c1c1c5")
+        # Expander text + triangle
+        html = html.replace("hsla(0,0%,100%,.58)", caption_color)
+        html = html.replace('fill:"#FFF"', f'fill:"{caption_color}"')
+        html = html.replace('"fill-opacity":".582"', '"fill-opacity":"1"')
+        # Vertical guiding lines
+        html = html.replace("opacity:.08", "opacity:.4")
+        # File name and line (right side)
+        html = html.replace("hsla(0,0%,100%,.5)", caption_color)
+        # Metrics top right
+        html = html.replace("#737779", text_color)
+        # Metrics top right labels
+        html = html.replace("#a9abad", caption_color)
+
+        # Blue -> blue 80
+        html = html.replace("#5db3ff", "#1c83e1")
+        # Red -> red 80
+        html = html.replace("#FF4159", "#ff2b2b")
+        # Yellow -> yellow 80
+        # html = html.replace("#D8CB2A", "#faca2b")
+        # Green -> green 70
+        html = html.replace("#7ED321", "#21c354")
+        # Font weight of green color from 500 to 600 (to match other colors).
+        # html = html.replace("e=500", "e=600")
+
+        # Add border around body. Make it stick to the component iframe. 
+        html = html.replace(
+            "body,html{",
+            "body{position:absolute;top:0;right:0;bottom:0;left:0;border:1px solid #d6d6d8;border-radius:0.25rem}body,html{",  # padding-bottom: 20px !important
+        )
+
+        # Make header top corners rounded to match border.
+        html = html.replace(
+            ".header[data-v-0183f483]{",
+            ".header[data-v-0183f483]{border-radius:0.25rem 0.25rem 0 0;",
+        )
+
+        # Make timeline content scrollable, but keep header fixed. Adapt padding.
+        html = html.replace("#app{", "#app{display:flex;flex-flow:column;height:100%;")
+        html = html.replace(
+            "#app{", "#app > div:nth-child(3){padding: 20px 30px;overflow:scroll}#app{"
+        )
+        html = html.replace('height:"20px"', 'height:"0"')  # remove spacer
+        # html = html.replace(".margins{", ".margins{overflow:scroll;")
+
+        # Display modified HTML report in iframe.
+        components.html(html, height=600)
