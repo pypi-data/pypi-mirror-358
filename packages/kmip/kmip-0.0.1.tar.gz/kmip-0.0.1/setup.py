9bfc9774a6573af01b00b231dd7d2463e1bc7203
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:22 +0000

    Add package kmip/0.0.1/kmip-0.0.1.tar.gz

---
+++ packages/kmip/kmip-0.0.1.tar.gz/kmip-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="kmip",
+    version="0.0.1",
+    packages=["kmip"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
