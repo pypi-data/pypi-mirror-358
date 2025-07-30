d0069c7de5ec63e6e637582783ef8d0e862bafe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:37 +0000

    Add package passport-token-validator/0.0.1/passport-token-validator-0.0.1.tar.gz

---
+++ packages/passport-token-validator/passport-token-validator-0.0.1.tar.gz/passport-token-validator-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="passport-token-validator",
+    version="0.0.1",
+    packages=["passport-token-validator"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
