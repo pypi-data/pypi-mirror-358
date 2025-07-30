28d6921a35ea051eb9d890f7fb6317334bcb6d82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:46 +0000

    Add package libusb/1.0.29/libusb-1.0.29.tar.gz

---
+++ packages/libusb/libusb-1.0.29.tar.gz/libusb-1.0.29/tests/test_main.py
@@ -0,0 +1,15 @@
+# Copyright (c) 2016 Adam Karpierz
+# SPDX-License-Identifier: Zlib
+
+import unittest
+
+import libusb
+
+
+class MainTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_main(self):
+        pass
