b4274aec773c490d51f4150081d1eec19de4d24b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:56 +0000

    Add package images-tests/0.0.1/images-tests-0.0.1.tar.gz

---
+++ packages/images-tests/images-tests-0.0.1.tar.gz/images-tests-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="images-tests",
+    version="0.0.1",
+    packages=["images-tests"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
