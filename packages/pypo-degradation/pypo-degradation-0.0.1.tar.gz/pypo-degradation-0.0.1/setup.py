95b86e801485f97b2742b354cfa7a363c8902a7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:58 +0000

    Add package pypo-degradation/0.0.1/pypo-degradation-0.0.1.tar.gz

---
+++ packages/pypo-degradation/pypo-degradation-0.0.1.tar.gz/pypo-degradation-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pypo-degradation",
+    version="0.0.1",
+    packages=["pypo-degradation"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
