ad0a227188fc760b4526ecd534b02adb5cf78263
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:05:15 +0000

    Add package datalens-formula/7.47.0/datalens-formula-7.47.0.tar.gz

---
+++ packages/datalens-formula/datalens-formula-7.47.0.tar.gz/datalens-formula-7.47.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="datalens-formula",
+    version="7.47.0",
+    packages=["datalens-formula"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
