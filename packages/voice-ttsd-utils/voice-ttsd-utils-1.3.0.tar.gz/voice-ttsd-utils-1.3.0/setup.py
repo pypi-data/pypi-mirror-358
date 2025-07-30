b3971fcb3536d5226cdc39c18bedb76bb493dad3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:18 +0000

    Add package voice-ttsd-utils/1.3.0/voice-ttsd-utils-1.3.0.tar.gz

---
+++ packages/voice-ttsd-utils/voice-ttsd-utils-1.3.0.tar.gz/voice-ttsd-utils-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="voice-ttsd-utils",
+    version="1.3.0",
+    packages=["voice-ttsd-utils"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
