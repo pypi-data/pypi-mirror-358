99cc9be2b0a85e730f0fee8d29d8c8dbb11e9dcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:08 +0000

    Add package mlearning/0.0.1/mlearning-0.0.1.tar.gz

---
+++ packages/mlearning/mlearning-0.0.1.tar.gz/mlearning-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="mlearning",
+    version="0.0.1",
+    packages=["mlearning"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
