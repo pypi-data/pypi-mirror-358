3c722787b3e45945f4f9d7f184f732d6d2306bbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:07:39 +0000

    Add package discordai-lib/1.2.0/discordai_lib-1.2.0-py3-none-any.whl

---
+++ packages/discordai-lib/discordai_lib-1.2.0-py3-none-any.whl/discordailib/__init__.py
@@ -0,0 +1,11 @@
+
+"""
+DiscordAI Library - Easy AI integration for Discord bots using Pollination.ai
+"""
+
+from .client import DiscordAI
+from .models import AIModel, AIFeature
+
+__version__ = "1.2.0"
+__author__ = "DiscordAI Library"
+__all__ = ["DiscordAI", "AIModel", "AIFeature"]
