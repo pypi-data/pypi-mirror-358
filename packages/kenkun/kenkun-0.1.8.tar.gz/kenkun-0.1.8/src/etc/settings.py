969335cf7cef198a3200a12d8a91387d3ea2b3d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:07 +0000

    Add package kenkun/0.1.8/kenkun-0.1.8.tar.gz

---
+++ packages/kenkun/kenkun-0.1.8.tar.gz/kenkun-0.1.8/src/etc/settings.py
@@ -0,0 +1,4 @@
+# Github access properties
+GITHUB_USER='sylvioazevedo'
+GITHUB_TOKEN='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'
+GITHUB_URL=f'https://{GITHUB_USER}:{GITHUB_TOKEN}@github.com/{GITHUB_USER}/'
