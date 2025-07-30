36ca1cee5ff4d7b708058d836d24b79a66887df1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:05:18 +0000

    Add package datalens-query-processing/0.0.1/datalens-query-processing-0.0.1.tar.gz

---
+++ packages/datalens-query-processing/datalens-query-processing-0.0.1.tar.gz/datalens-query-processing-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="datalens-query-processing",
+    version="0.0.1",
+    packages=["datalens-query-processing"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
