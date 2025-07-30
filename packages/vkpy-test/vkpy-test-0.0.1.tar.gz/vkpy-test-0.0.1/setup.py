b5fd1c81211b7f31f28c917bf87eed9232a960ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:59 +0000

    Add package vkpy-test/0.0.1/vkpy-test-0.0.1.tar.gz

---
+++ packages/vkpy-test/vkpy-test-0.0.1.tar.gz/vkpy-test-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="vkpy-test",
+    version="0.0.1",
+    packages=["vkpy-test"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
