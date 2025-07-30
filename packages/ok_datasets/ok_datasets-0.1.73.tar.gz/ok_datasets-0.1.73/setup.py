79a98e60c849edbd5c27ef9a88eb62baa68ad221
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:12 +0000

    Add package ok_datasets/0.1.73/ok_datasets-0.1.73.tar.gz

---
+++ packages/ok_datasets/ok_datasets-0.1.73.tar.gz/ok_datasets-0.1.73/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ok_datasets",
+    version="0.1.73",
+    packages=["ok_datasets"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
