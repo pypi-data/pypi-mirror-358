18961a3377366db8e4572d5b5fe0600aa5dc62c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:21 +0000

    Add package jakarta.transaction-api/2.0.1/jakarta.transaction-api-2.0.1.tar.gz

---
+++ packages/jakarta.transaction-api/jakarta.transaction-api-2.0.1.tar.gz/jakarta.transaction-api-2.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="jakarta.transaction-api",
+    version="2.0.1",
+    packages=["jakarta.transaction-api"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
