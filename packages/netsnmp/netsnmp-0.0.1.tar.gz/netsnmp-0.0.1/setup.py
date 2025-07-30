9a9df799f6f93662a9e3034d611d63e852d96f03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:30 +0000

    Add package netsnmp/0.0.1/netsnmp-0.0.1.tar.gz

---
+++ packages/netsnmp/netsnmp-0.0.1.tar.gz/netsnmp-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="netsnmp",
+    version="0.0.1",
+    packages=["netsnmp"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
