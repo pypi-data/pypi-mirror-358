27d3db83efbfcbf03fb022c762504be541d30c0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:50 +0000

    Add package pypoligram/0.1.0/pypoligram-0.1.0.tar.gz

---
+++ packages/pypoligram/pypoligram-0.1.0.tar.gz/pypoligram-0.1.0/pypoligram/sync.py
@@ -0,0 +1,21 @@
+import inspect
+
+try:
+	from pyrogram.sync import async_to_sync #type: ignore
+except ImportError:
+	def async_to_sync(source, name):
+		"""if we can't import it, we will just pass it.
+		"""
+		pass
+
+from .clients import ClientManager
+
+def wrap(source):
+	for name in dir(source):
+		method = getattr(source, name)
+
+		if not name.startswith("_"):
+			if inspect.iscoroutinefunction(method) or inspect.isasyncgenfunction(method):
+				async_to_sync(source, name)
+
+wrap(ClientManager)
