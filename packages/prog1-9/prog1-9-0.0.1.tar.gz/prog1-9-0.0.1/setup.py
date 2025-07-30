481dc66f587c0c1a6ad3eea0f22605f7c76a2b94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:14 +0000

    Add package prog1-9/0.0.1/prog1-9-0.0.1.tar.gz

---
+++ packages/prog1-9/prog1-9-0.0.1.tar.gz/prog1-9-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="prog1-9",
+    version="0.0.1",
+    packages=["prog1-9"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
