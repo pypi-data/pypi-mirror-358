c3279762eacdde0c6c19c430aa07d1710d1dafab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:36 +0000

    Add package python-tx-tftp/0.0.1/python-tx-tftp-0.0.1.tar.gz

---
+++ packages/python-tx-tftp/python-tx-tftp-0.0.1.tar.gz/python-tx-tftp-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="python-tx-tftp",
+    version="0.0.1",
+    packages=["python-tx-tftp"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
