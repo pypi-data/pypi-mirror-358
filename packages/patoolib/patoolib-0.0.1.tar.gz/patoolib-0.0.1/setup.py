c1d139861e2bd5f8e296f1d5f080e179e8dc9b36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:19 +0000

    Add package patoolib/0.0.1/patoolib-0.0.1.tar.gz

---
+++ packages/patoolib/patoolib-0.0.1.tar.gz/patoolib-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="patoolib",
+    version="0.0.1",
+    packages=["patoolib"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
