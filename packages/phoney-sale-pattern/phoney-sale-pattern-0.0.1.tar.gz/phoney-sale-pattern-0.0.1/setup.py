c1cc9089d97a51b11944034953764338e79075a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:30 +0000

    Add package phoney-sale-pattern/0.0.1/phoney-sale-pattern-0.0.1.tar.gz

---
+++ packages/phoney-sale-pattern/phoney-sale-pattern-0.0.1.tar.gz/phoney-sale-pattern-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="phoney-sale-pattern",
+    version="0.0.1",
+    packages=["phoney-sale-pattern"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
