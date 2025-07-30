a9e198da9becfdf09b5ffa2dd56784e57cadb92e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:56 +0000

    Add package ner_package/0.3.4/ner_package-0.3.4.tar.gz

---
+++ packages/ner_package/ner_package-0.3.4.tar.gz/ner_package-0.3.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ner_package",
+    version="0.3.4",
+    packages=["ner_package"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
