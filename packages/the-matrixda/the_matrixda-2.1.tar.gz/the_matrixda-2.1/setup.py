8ac6073e6cf8bfcc07d5178800ea18c4fc99514d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:04 +0000

    Add package the-matrixda/2.1/the_matrixda-2.1.tar.gz

---
+++ packages/the-matrixda/the_matrixda-2.1.tar.gz/the_matrixda-2.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+    name='the-matrixda',
+    version='2.1',
+    packages = ['the-matrixda'],
+    install_requires=[],
+    python_requires=">=3.3",
+    author="DavidYdin",
+    author_email="David.2280@yandex.ru"
+)
