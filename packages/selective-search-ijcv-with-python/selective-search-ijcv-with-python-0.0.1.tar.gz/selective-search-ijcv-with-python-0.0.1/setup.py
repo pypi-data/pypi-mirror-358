fcc766742f266eee5d92ea3c2bbdcac3e3a83b33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:20 +0000

    Add package selective-search-ijcv-with-python/0.0.1/selective-search-ijcv-with-python-0.0.1.tar.gz

---
+++ packages/selective-search-ijcv-with-python/selective-search-ijcv-with-python-0.0.1.tar.gz/selective-search-ijcv-with-python-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="selective-search-ijcv-with-python",
+    version="0.0.1",
+    packages=["selective-search-ijcv-with-python"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
