5e38ecc66f1de55b5def9d390ed81a6bda741e75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:29 +0000

    Add package bluer-algo/4.73.1/bluer_algo-4.73.1.tar.gz

---
+++ packages/bluer-algo/bluer_algo-4.73.1.tar.gz/bluer_algo-4.73.1/setup.py
@@ -0,0 +1,26 @@
+from blueness.pypi import setup
+
+from bluer_algo import NAME, VERSION, DESCRIPTION, REPO_NAME
+
+setup(
+    filename=__file__,
+    repo_name=REPO_NAME,
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    packages=[
+        NAME,
+        f"{NAME}.help",
+        f"{NAME}.help.image_classifier",
+        f"{NAME}.image_classifier",
+        f"{NAME}.image_classifier.ingest",
+        f"{NAME}.image_classifier.ingest.fruits_360",
+    ],
+    include_package_data=True,
+    package_data={
+        NAME: [
+            "config.env",
+            ".abcli/**/*.sh",
+        ],
+    },
+)
