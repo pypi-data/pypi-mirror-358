d5cfb3e0a245ead25af65ae3415ddddaf2a5d951
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:46:19 +0000

    Add package tap-bitso/0.5.1/tap_bitso-0.5.1-py3-none-any.whl

---
+++ packages/tap-bitso/tap_bitso-0.5.1-py3-none-any.whl/tap_bitso/streams.py
@@ -0,0 +1,68 @@
+"""Stream type classes for tap-bitso."""
+
+from __future__ import annotations
+
+import importlib.resources
+
+from tap_bitso import schemas
+from tap_bitso.client import AuthenticatedBitsoStream, BitsoStream
+
+SCHEMAS_DIR = importlib.resources.files(schemas)
+
+
+class LedgerStream(AuthenticatedBitsoStream):
+    """Ledger stream.
+
+    DEPRECATED.
+    """
+
+    name = "ledger"
+    path = "/v3/ledger"
+    replication_key = "eid"
+    primary_keys = ("eid",)
+    next_page_token_jsonpath = f"$.payload[-1].{replication_key}"
+    schema_filepath = SCHEMAS_DIR / "ledger.json"
+
+
+class TradesStream(BitsoStream):
+    """Trades stream."""
+
+    name = "trades"
+    path = "/v3/trades"
+    book_based = True
+    replication_key = "tid"
+    primary_keys = ("tid",)
+    next_page_token_jsonpath = f"$.payload[-1].{replication_key}"
+    schema_filepath = SCHEMAS_DIR / "trade.json"
+
+
+class UserTradesStream(AuthenticatedBitsoStream):
+    """User trades stream."""
+
+    name = "user_trades"
+    path = "/v3/user_trades"
+    book_based = True
+    replication_key = "tid"
+    primary_keys = ("tid",)
+    next_page_token_jsonpath = f"$.payload[-1].{replication_key}"
+    schema_filepath = SCHEMAS_DIR / "trade.json"
+
+
+class TickersStream(BitsoStream):
+    """Tickers stream."""
+
+    name = "tickers"
+    path = "/v3/ticker"
+    book_based = True
+    records_jsonpath = "$.payload"
+    primary_keys = ("book", "created_at")
+    schema_filepath = SCHEMAS_DIR / "ticker.json"
+
+
+class BooksStream(BitsoStream):
+    """Books stream."""
+
+    name = "books"
+    path = "/v3/available_books"
+    primary_keys = ("book",)
+    schema_filepath = SCHEMAS_DIR / "book.json"
---
+++ packages/tap-bitso/tap_bitso-0.5.1-py3-none-any.whl/tap_bitso/tap.py
@@ -0,0 +1,101 @@
+"""Bitso tap class."""
+
+from __future__ import annotations
+
+import structlog
+from singer_sdk import Stream, Tap
+from singer_sdk import typing as th
+
+from tap_bitso import streams
+
+structlog.configure(
+    wrapper_class=structlog.stdlib.BoundLogger,
+    logger_factory=structlog.stdlib.LoggerFactory(),
+    processors=[
+        structlog.processors.JSONRenderer(),
+    ],
+)
+
+
+def console_formatter(*, colors: bool = True) -> structlog.stdlib.ProcessorFormatter:
+    """Return a console formatter for structlog.
+
+    Args:
+        colors: Whether to use colors in the console output.
+
+    Returns:
+        A structlog formatter.
+    """
+    return structlog.stdlib.ProcessorFormatter(
+        processor=structlog.dev.ConsoleRenderer(colors=colors),
+        foreign_pre_chain=[
+            structlog.stdlib.add_log_level,
+            structlog.processors.TimeStamper(fmt="iso"),
+        ],
+    )
+
+
+def json_formatter() -> structlog.stdlib.ProcessorFormatter:
+    """Return a JSON formatter for structlog.
+
+    Returns:
+        A structlog formatter.
+    """
+    return structlog.stdlib.ProcessorFormatter(
+        processor=structlog.processors.JSONRenderer(),
+        foreign_pre_chain=[
+            structlog.stdlib.add_logger_name,
+            structlog.stdlib.add_log_level,
+            structlog.processors.TimeStamper(fmt="iso"),
+            structlog.stdlib.PositionalArgumentsFormatter(),
+        ],
+    )
+
+
+class TapBitso(Tap):
+    """Bitso tap class."""
+
+    name = "tap-bitso"
+
+    config_jsonschema = th.PropertiesList(
+        th.Property(
+            "key",
+            th.StringType,
+            required=True,
+            description="Bitso API Key",
+        ),
+        th.Property(
+            "secret",
+            th.StringType,
+            required=True,
+            description="Bitso API Secret",
+        ),
+        th.Property(
+            "base_url",
+            th.StringType,
+            default="https://api.bitso.com",
+            description="Bitso API base URL",
+        ),
+        th.Property(
+            "books",
+            th.ArrayType(th.StringType),
+            default=["btc_mxn"],
+            description=(
+                "Specifies which book to use for `tickers` and other endpoints"
+            ),
+        ),
+    ).to_dict()
+
+    def discover_streams(self) -> list[Stream]:
+        """Return a list of discovered streams.
+
+        Returns:
+            A list of streams.
+        """
+        return [
+            streams.BooksStream(tap=self),
+            # streams.LedgerStream(tap=self),  # Removed upstream?  # noqa: ERA001
+            streams.TickersStream(tap=self),
+            streams.TradesStream(tap=self),
+            streams.UserTradesStream(tap=self),
+        ]
