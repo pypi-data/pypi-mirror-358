11e4b39b1577bd1151b64a07721f170227733f66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:12 +0000

    Add package mmcv-full-ok/1.2.6/mmcv-full-ok-1.2.6.tar.gz

---
+++ packages/mmcv-full-ok/mmcv-full-ok-1.2.6.tar.gz/mmcv-full-ok-1.2.6/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="mmcv-full-ok",
+    version="1.2.6",
+    packages=["mmcv-full-ok"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
