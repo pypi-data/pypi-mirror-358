f5aa20e2ee928bfe7647c2dd1f295bbfa7223b4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:07:01 +0000

    Add package mailru/0.0.1/mailru-0.0.1.tar.gz

---
+++ packages/mailru/mailru-0.0.1.tar.gz/mailru-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="mailru",
+    version="0.0.1",
+    packages=["mailru"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
