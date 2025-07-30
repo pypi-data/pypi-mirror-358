0bc49efc0a6c6ddd7b45f1f16c6bae8979dc71ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:55 +0000

    Add package oshit/0.12.5/oshit-0.12.5-py3-none-any.whl

---
+++ packages/oshit/oshit-0.12.5-py3-none-any.whl/oshit/__init__.py
@@ -0,0 +1,17 @@
+"""A terminal-based HackerNews reader."""
+
+######################################################################
+# Main app information.
+__author__ = "Dave Pearson"
+__copyright__ = "Copyright 2024-2025, Dave Pearson"
+__credits__ = ["Dave Pearson"]
+__maintainer__ = "Dave Pearson"
+__email__ = "davep@davep.org"
+__version__ = "0.12.5"
+__licence__ = "GPLv3+"
+
+##############################################################################
+# Public symbols.
+__all__: list[str] = []
+
+### __init__.py ends here
