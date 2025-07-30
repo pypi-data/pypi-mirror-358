6e4bc29c6c8f8403261f857e6c9485741083731a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:21 +0000

    Add package kubespawner/0.0.1/kubespawner-0.0.1.tar.gz

---
+++ packages/kubespawner/kubespawner-0.0.1.tar.gz/kubespawner-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="kubespawner",
+    version="0.0.1",
+    packages=["kubespawner"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
