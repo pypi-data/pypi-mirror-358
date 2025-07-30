e94dee7b93780a194ad61d569e002313bc80e377
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:23 +0000

    Add package pytest-yt-local/0.0.1/pytest-yt-local-0.0.1.tar.gz

---
+++ packages/pytest-yt-local/pytest-yt-local-0.0.1.tar.gz/pytest-yt-local-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pytest-yt-local",
+    version="0.0.1",
+    packages=["pytest-yt-local"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
