c52b039413dc3d28d97d7af28c6461a6316b5e42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:21 +0000

    Add package nlp-data-nltk/1.0.4/nlp-data-nltk-1.0.4.tar.gz

---
+++ packages/nlp-data-nltk/nlp-data-nltk-1.0.4.tar.gz/nlp-data-nltk-1.0.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="nlp-data-nltk",
+    version="1.0.4",
+    packages=["nlp-data-nltk"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
