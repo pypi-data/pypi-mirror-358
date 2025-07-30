84ff593f16c79e82a0565a1a01b462612645fb3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:25 +0000

    Add package netboxlabs-netbox-branching/0.5.7/netboxlabs_netbox_branching-0.5.7-py3-none-any.whl

---
+++ packages/netboxlabs-netbox-branching/netboxlabs_netbox_branching-0.5.7-py3-none-any.whl/netbox_branching/constants.py
@@ -0,0 +1,46 @@
+# Name of the main (non-branch) PostgreSQL schema
+MAIN_SCHEMA = 'public'
+
+# HTTP cookie
+COOKIE_NAME = 'active_branch'
+
+# HTTP header for API requests
+BRANCH_HEADER = 'X-NetBox-Branch'
+
+# URL query parameter name
+QUERY_PARAM = '_branch'
+
+# Models which do not support change logging, but whose database tables
+# must be replicated for each branch to ensure proper functionality
+INCLUDE_MODELS = (
+    'dcim.cablepath',
+    'extras.cachedvalue',
+)
+
+# Models for which branching support is explicitly disabled
+EXEMPT_MODELS = (
+    # Exempt applicable core NetBox models
+    'core.*',
+    'extras.branch',
+    'extras.customfield',
+    'extras.customfieldchoiceset',
+    'extras.customlink',
+    'extras.eventrule',
+    'extras.exporttemplate',
+    'extras.notificationgroup',
+    'extras.savedfilter',
+    'extras.webhook',
+
+    # Exempt all models from this plugin and from netbox-changes
+    'netbox_branching.*',
+    'netbox_changes.*',
+)
+
+# Indexes to ignore as they are removed in a NetBox v4.3 migration, but might be present
+# in earlier NetBox releases.
+# TODO: Remove in v0.6.0
+SKIP_INDEXES = (
+    'dcim_cabletermination_termination_type_id_termination_id_idx',     # Removed in dcim.0207_remove_redundant_indexes
+    'vpn_l2vpntermination_assigned_object_type_id_assigned_objec_idx',  # Removed in vpn.0009_remove_redundant_indexes
+    'vpn_tunneltermination_termination_type_id_termination_id_idx',     # Removed in vpn.0009_remove_redundant_indexes
+)
