a235e61d03164ad24c9265a8a2dda5359fdf7fa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:54 +0000

    Add package fast-layer-norm/0.0.1/fast-layer-norm-0.0.1.tar.gz

---
+++ packages/fast-layer-norm/fast-layer-norm-0.0.1.tar.gz/fast-layer-norm-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="fast-layer-norm",
+    version="0.0.1",
+    packages=["fast-layer-norm"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
