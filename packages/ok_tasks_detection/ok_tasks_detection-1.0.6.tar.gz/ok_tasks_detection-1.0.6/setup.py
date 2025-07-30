014344fa2cb6e03bdb7f50187d21c556317e55ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:15 +0000

    Add package ok_tasks_detection/1.0.6/ok_tasks_detection-1.0.6.tar.gz

---
+++ packages/ok_tasks_detection/ok_tasks_detection-1.0.6.tar.gz/ok_tasks_detection-1.0.6/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ok_tasks_detection",
+    version="1.0.6",
+    packages=["ok_tasks_detection"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
