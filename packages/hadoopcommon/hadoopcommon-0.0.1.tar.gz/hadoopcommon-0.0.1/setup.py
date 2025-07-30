87707a3f589002d9cbe104ee2ceefa5d41faa47a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:07:01 +0000

    Add package hadoopcommon/0.0.1/hadoopcommon-0.0.1.tar.gz

---
+++ packages/hadoopcommon/hadoopcommon-0.0.1.tar.gz/hadoopcommon-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="hadoopcommon",
+    version="0.0.1",
+    packages=["hadoopcommon"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
