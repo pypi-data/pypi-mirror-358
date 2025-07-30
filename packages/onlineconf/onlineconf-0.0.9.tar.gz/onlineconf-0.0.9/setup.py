9502c8b8baeb20d8fcd809e002e8c09ff2217cfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:09 +0000

    Add package onlineconf/0.0.9/onlineconf-0.0.9.tar.gz

---
+++ packages/onlineconf/onlineconf-0.0.9.tar.gz/onlineconf-0.0.9/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="onlineconf",
+    version="0.0.9",
+    packages=["onlineconf"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
