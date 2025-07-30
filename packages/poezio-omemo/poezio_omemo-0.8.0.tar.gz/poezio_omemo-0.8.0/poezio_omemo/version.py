5e17625b269715d3c2aec1847a9ac4557cd9be30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:32 +0000

    Add package poezio-omemo/0.8.0/poezio_omemo-0.8.0.tar.gz

---
+++ packages/poezio-omemo/poezio_omemo-0.8.0.tar.gz/poezio_omemo-0.8.0/poezio_omemo/version.py
@@ -0,0 +1,17 @@
+# Copyright © 2019 Maxime “pep” Buquet <pep@bouah.net>
+# Copyright © 2025 Tim “Syndace” Henkes <me@syndace.dev>
+#
+# Distributed under terms of the GPLv3 license.
+
+"""
+    OMEMO plugin for Poezio
+    Copyright © 2021 Maxime “pep” Buquet <pep@bouah.net>
+    Copyright © 2025 Tim “Syndace” Henkes <me@syndace.dev>
+    This file is part of poezio-omemo.
+
+    See the file LICENSE for copying permission.
+"""
+
+__all__ = [ "__version__" ]
+
+__version__ = "0.8.0"
