0b2f87705af0530d53fa4382b28b4292523c7e0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:06 +0000

    Add package pyrsyslog/0.0.1/pyrsyslog-0.0.1.tar.gz

---
+++ packages/pyrsyslog/pyrsyslog-0.0.1.tar.gz/pyrsyslog-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pyrsyslog",
+    version="0.0.1",
+    packages=["pyrsyslog"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
