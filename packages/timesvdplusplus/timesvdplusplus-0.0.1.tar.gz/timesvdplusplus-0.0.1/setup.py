ba472979b4082f9c1f7f363c825b57ca6a025137
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:41 +0000

    Add package timesvdplusplus/0.0.1/timesvdplusplus-0.0.1.tar.gz

---
+++ packages/timesvdplusplus/timesvdplusplus-0.0.1.tar.gz/timesvdplusplus-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="timesvdplusplus",
+    version="0.0.1",
+    packages=["timesvdplusplus"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
