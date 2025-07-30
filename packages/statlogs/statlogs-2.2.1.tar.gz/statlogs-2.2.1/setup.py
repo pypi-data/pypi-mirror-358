20e750613dba0da835b98c83c6db580af07dfd05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:58 +0000

    Add package statlogs/2.2.1/statlogs-2.2.1.tar.gz

---
+++ packages/statlogs/statlogs-2.2.1.tar.gz/statlogs-2.2.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="statlogs",
+    version="2.2.1",
+    packages=["statlogs"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
