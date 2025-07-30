70f556973461b37b8e219feb84e506af174b3184
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:58 +0000

    Add package vkpy-engines/1.0.3/vkpy-engines-1.0.3.tar.gz

---
+++ packages/vkpy-engines/vkpy-engines-1.0.3.tar.gz/vkpy-engines-1.0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="vkpy-engines",
+    version="1.0.3",
+    packages=["vkpy-engines"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
