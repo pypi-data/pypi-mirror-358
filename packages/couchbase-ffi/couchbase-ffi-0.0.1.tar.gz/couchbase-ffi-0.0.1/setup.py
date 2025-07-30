9fae7797d6e88deed94e6be28615b16e92c7654c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:21 +0000

    Add package couchbase-ffi/0.0.1/couchbase-ffi-0.0.1.tar.gz

---
+++ packages/couchbase-ffi/couchbase-ffi-0.0.1.tar.gz/couchbase-ffi-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="couchbase-ffi",
+    version="0.0.1",
+    packages=["couchbase-ffi"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
