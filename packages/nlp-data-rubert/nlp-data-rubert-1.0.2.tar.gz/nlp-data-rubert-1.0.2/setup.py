7af25d1e2b6fbda127aba5abfd3895be33bce2f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:57 +0000

    Add package nlp-data-rubert/1.0.2/nlp-data-rubert-1.0.2.tar.gz

---
+++ packages/nlp-data-rubert/nlp-data-rubert-1.0.2.tar.gz/nlp-data-rubert-1.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="nlp-data-rubert",
+    version="1.0.2",
+    packages=["nlp-data-rubert"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
