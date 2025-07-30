cdf04fe45f211c7da4beb81aa6a06261d1a46628
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:39 +0000

    Add package mycalculator2025/0.1.1/mycalculator2025-0.1.1-py3-none-any.whl

---
+++ packages/mycalculator2025/mycalculator2025-0.1.1-py3-none-any.whl/operations.py
@@ -0,0 +1,24 @@
+def add(a=0, b=0):
+    return a + b
+
+
+def subtract(a=0, b=0):
+    return a - b
+
+
+def multiply(a=0, b=0):
+    return a * b
+
+
+def divide(a=0, b=0):
+    if b == 0:
+        return "infinity"
+    return a / b
+
+
+def power(a=0, b=0):
+    return a ** b
+
+
+def mod(a=0, b=0):
+    return a % b
