00cb1bf6ebe5d38a628ce2cfb5c0297f25eb6fd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:09:34 +0000

    Add package aio-kong/3.7.0/aio_kong-3.7.0.tar.gz

---
+++ packages/aio-kong/aio_kong-3.7.0.tar.gz/aio_kong-3.7.0/kong/auths.py
@@ -0,0 +1,80 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Any
+
+from .components import CrudComponent, KongEntity
+
+if TYPE_CHECKING:
+    from .consumers import Consumer
+
+
+def auth_factory(consumer: Consumer, auth_type: str) -> ConsumerAuth:
+    known_types = {"basic-auth": BasicAuth, "key-auth": KeyAuth}
+    constructor = known_types.get(auth_type, ConsumerAuth)
+    return constructor(consumer, Auth, auth_type)
+
+
+class Auth(KongEntity):
+    pass
+
+
+class ConsumerAuth(CrudComponent[Auth]):
+    unique_field: str = ""
+
+    @property
+    def url(self) -> str:
+        return f"{self.root.url}/{self.name}"
+
+    async def get_existing_id(self, creds_config: dict) -> str | None:
+        if not self.unique_field:
+            raise NotImplementedError(
+                "Existence check not implemented for this type of\
+                 authentication"
+            )
+        cur_unique = creds_config[self.unique_field]
+        try:
+            return next(
+                cred
+                for cred in await self.get_list()
+                if cred[self.unique_field] == cur_unique
+            )["id"]
+        except StopIteration:
+            return None
+
+    async def create_or_update_credentials(self, creds_config: dict) -> Auth:
+        if existing_id := await self.get_existing_id(creds_config):
+            return await self.update_credentials(existing_id, data=creds_config)
+        else:
+            return await self.create_credentials(data=creds_config)
+
+    async def update_credentials(self, id_: str, **kw: Any) -> Auth:
+        url = f"{self.url}/{id_}"
+
+        return await self.cli.execute(
+            url,
+            "patch",
+            headers={"Content-Type": "application/x-www-form-urlencoded"},
+            wrap=self.wrap,
+            **kw,
+        )
+
+    async def create_credentials(self, **kw: Any) -> Auth:
+        return await self.cli.execute(
+            self.url,
+            "post",
+            headers={"Content-Type": "application/x-www-form-urlencoded"},
+            wrap=self.wrap,
+            **kw,
+        )
+
+    async def get_or_create(self) -> Auth:
+        secrets = await self.get_list(limit=1)
+        return secrets[0] if secrets else await self.create()
+
+
+class BasicAuth(ConsumerAuth):
+    unique_field = "username"
+
+
+class KeyAuth(ConsumerAuth):
+    unique_field = "key"
---
+++ packages/aio-kong/aio_kong-3.7.0.tar.gz/aio_kong-3.7.0/kong/utils.py
@@ -0,0 +1,42 @@
+import socket
+from typing import Any
+from uuid import UUID
+
+from multidict import MultiDict
+
+
+def as_list(key: str, data: dict) -> dict:
+    if key in data:
+        v = data[key]
+        if isinstance(v, str):
+            v = [v]
+        data[key] = list(v)
+    return data
+
+
+def as_dict(data: Any, key: str = "data") -> dict:
+    return {key: data} if not isinstance(data, dict) else data
+
+
+def local_ip() -> str:
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    try:
+        return s.getsockname()[0]
+    finally:
+        s.close()
+
+
+def as_params(*, params: dict | None = None, **kwargs: Any) -> MultiDict:
+    mp: MultiDict = MultiDict(params if params is not None else {})
+    mp.update(kwargs)
+    return mp
+
+
+def uid(id_: str | UUID) -> str:
+    if isinstance(id_, UUID):
+        return str(id_)
+    try:
+        return str(UUID(id_))
+    except ValueError:
+        return str(id_)
