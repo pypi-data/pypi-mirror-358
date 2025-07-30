5b72be1348ff21d4388893f871a7c223a24eb20c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:39 +0000

    Add package dependency-decoding/0.0.1/dependency-decoding-0.0.1.tar.gz

---
+++ packages/dependency-decoding/dependency-decoding-0.0.1.tar.gz/dependency-decoding-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dependency-decoding",
+    version="0.0.1",
+    packages=["dependency-decoding"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
