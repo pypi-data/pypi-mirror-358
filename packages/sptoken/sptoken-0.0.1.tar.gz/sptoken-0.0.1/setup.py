3f07d24885e9a7f03bf20cca9dfb9051b76ff5af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:31 +0000

    Add package sptoken/0.0.1/sptoken-0.0.1.tar.gz

---
+++ packages/sptoken/sptoken-0.0.1.tar.gz/sptoken-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="sptoken",
+    version="0.0.1",
+    packages=["sptoken"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
