90b9b1994d947843697e4c2ae3cbb9b730f76a33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:37 +0000

    Add package mlp-cuda/0.0.1/mlp-cuda-0.0.1.tar.gz

---
+++ packages/mlp-cuda/mlp-cuda-0.0.1.tar.gz/mlp-cuda-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="mlp-cuda",
+    version="0.0.1",
+    packages=["mlp-cuda"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
