99a5be5c5a701292cec86ce329e654d720113934
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:21 +0000

    Add package istack-commons-runtime/4.1.2/istack-commons-runtime-4.1.2.tar.gz

---
+++ packages/istack-commons-runtime/istack-commons-runtime-4.1.2.tar.gz/istack-commons-runtime-4.1.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="istack-commons-runtime",
+    version="4.1.2",
+    packages=["istack-commons-runtime"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
