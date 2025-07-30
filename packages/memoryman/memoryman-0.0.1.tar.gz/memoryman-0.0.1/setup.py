ceebb16880901cb8ed3f693efb1126f23c962d57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:07:01 +0000

    Add package memoryman/0.0.1/memoryman-0.0.1.tar.gz

---
+++ packages/memoryman/memoryman-0.0.1.tar.gz/memoryman-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="memoryman",
+    version="0.0.1",
+    packages=["memoryman"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
