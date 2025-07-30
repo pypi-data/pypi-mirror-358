33882a9c3f82e8f41b535ab8b3cb59ce8d4c1a17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:13 +0000

    Add package pygwan/0.2.9/pygwan-0.2.9.tar.gz

---
+++ packages/pygwan/pygwan-0.2.9.tar.gz/pygwan-0.2.9/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pygwan",
+    version="0.2.9",
+    packages=find_packages(),
+    description="WhatsApp Cloud API is complicated, Twilio is expensive, PyGwan is the opposite of both. Free & Intuitive! ~ Tarmica Chiwara.",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    author="Tarmica Chiwara",
+    author_email="tarimicac+pypi@gmail.com",
+    url="https://github.com/lordskyzw/pygwan",
+)
