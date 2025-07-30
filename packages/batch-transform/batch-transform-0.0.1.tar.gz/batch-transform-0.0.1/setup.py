0374fcb2280a5234cba2498e51331446d23aa752
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:37 +0000

    Add package batch-transform/0.0.1/batch-transform-0.0.1.tar.gz

---
+++ packages/batch-transform/batch-transform-0.0.1.tar.gz/batch-transform-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="batch-transform",
+    version="0.0.1",
+    packages=["batch-transform"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
