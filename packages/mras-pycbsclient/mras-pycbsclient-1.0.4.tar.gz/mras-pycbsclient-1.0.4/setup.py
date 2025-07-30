ed7a58b20c00d61beb72730e7399172d0ea14e93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:38 +0000

    Add package mras-pycbsclient/1.0.4/mras-pycbsclient-1.0.4.tar.gz

---
+++ packages/mras-pycbsclient/mras-pycbsclient-1.0.4.tar.gz/mras-pycbsclient-1.0.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="mras-pycbsclient",
+    version="1.0.4",
+    packages=["mras-pycbsclient"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
