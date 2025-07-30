00a0ed8cb26a4ab61c81a7edfa53992a5ce4759a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:38 +0000

    Add package requirements.in/0.0.1/requirements.in-0.0.1.tar.gz

---
+++ packages/requirements.in/requirements.in-0.0.1.tar.gz/requirements.in-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="requirements.in",
+    version="0.0.1",
+    packages=["requirements.in"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
