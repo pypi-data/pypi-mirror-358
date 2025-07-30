e27f7bb2764e32e6d5dce5d0b87657dbc7f63454
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:21 +0000

    Add package vk-hate-speech/0.0.1/vk-hate-speech-0.0.1.tar.gz

---
+++ packages/vk-hate-speech/vk-hate-speech-0.0.1.tar.gz/vk-hate-speech-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="vk-hate-speech",
+    version="0.0.1",
+    packages=["vk-hate-speech"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
