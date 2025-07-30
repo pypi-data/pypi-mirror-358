e208c4d5bb22aa2a9c870e00fe107e34da1a15ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:54 +0000

    Add package fast-encdec-multihead-attn-norm-add/0.0.1/fast-encdec-multihead-attn-norm-add-0.0.1.tar.gz

---
+++ packages/fast-encdec-multihead-attn-norm-add/fast-encdec-multihead-attn-norm-add-0.0.1.tar.gz/fast-encdec-multihead-attn-norm-add-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="fast-encdec-multihead-attn-norm-add",
+    version="0.0.1",
+    packages=["fast-encdec-multihead-attn-norm-add"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
