b08703b06f90decab8b1201cdf55def7687bafdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:58 +0000

    Add package pypo-solder/0.0.1/pypo-solder-0.0.1.tar.gz

---
+++ packages/pypo-solder/pypo-solder-0.0.1.tar.gz/pypo-solder-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pypo-solder",
+    version="0.0.1",
+    packages=["pypo-solder"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
