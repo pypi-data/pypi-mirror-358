137306c22a186cb6261f5452a1037ee9d6db9497
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:30 +0000

    Add package bigdata-dpp/0.0.1/bigdata-dpp-0.0.1.tar.gz

---
+++ packages/bigdata-dpp/bigdata-dpp-0.0.1.tar.gz/bigdata-dpp-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="bigdata-dpp",
+    version="0.0.1",
+    packages=["bigdata-dpp"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
