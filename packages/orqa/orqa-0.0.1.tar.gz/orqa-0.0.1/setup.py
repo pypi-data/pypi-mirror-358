e8668b8b89f4035921706088edd42bcd72de7d23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:12 +0000

    Add package orqa/0.0.1/orqa-0.0.1.tar.gz

---
+++ packages/orqa/orqa-0.0.1.tar.gz/orqa-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="orqa",
+    version="0.0.1",
+    packages=["orqa"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
