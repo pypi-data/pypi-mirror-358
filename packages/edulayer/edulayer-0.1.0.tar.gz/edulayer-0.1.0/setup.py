1afa02124e067b8d37a5147da607eaf91b2855c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:37 +0000

    Add package edulayer/0.1.0/edulayer-0.1.0.tar.gz

---
+++ packages/edulayer/edulayer-0.1.0.tar.gz/edulayer-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='edulayer',
+    version='0.1.0',
+    packages=find_packages(),
+    author='fswair',
+    description='edulayer is a Python package.',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
