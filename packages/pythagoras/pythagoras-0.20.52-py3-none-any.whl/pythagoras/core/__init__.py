d35d83168e8b000c6be352e50c01ca0ca3447c17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:07:14 +0000

    Add package pythagoras/0.20.52/pythagoras-0.20.52-py3-none-any.whl

---
+++ packages/pythagoras/pythagoras-0.20.52-py3-none-any.whl/pythagoras/core/__init__.py
@@ -0,0 +1,5 @@
+from .._030_data_portals import ready, get
+from .._060_autonomous_code_portals import autonomous
+from .._080_pure_code_portals import pure
+from .._090_swarming_portals import SwarmingPortal
+from .._100_top_level_API import get_portal
