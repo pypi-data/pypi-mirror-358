311070ab77c5fa7c8f05e6b6c86c35fa1780191e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:05:29 +0000

    Add package django-bulk-drf/0.1.1/django_bulk_drf-0.1.1.tar.gz

---
+++ packages/django-bulk-drf/django_bulk_drf-0.1.1.tar.gz/django_bulk_drf-0.1.1/django_bulk_drf/__init__.py
@@ -0,0 +1,29 @@
+"""
+Django Bulk DRF - Asynchronous bulk operations for Django REST Framework
+
+Provides mixins for ViewSets to handle bulk create, update, and delete operations
+asynchronously using Celery workers with Redis for progress tracking.
+"""
+
+__version__ = "0.1.62"
+__author__ = "Konrad Beck"
+__email__ = "konrad.beck@merchantcapital.co.za"
+
+# Make common imports available at package level
+from .bulk_mixins import (
+    BulkCreateMixin,
+    BulkDeleteMixin, 
+    BulkOperationsMixin,
+    BulkReplaceMixin,
+    BulkUpdateMixin,
+)
+from .bulk_views import BulkOperationStatusView
+
+__all__ = [
+    "BulkCreateMixin",
+    "BulkDeleteMixin",
+    "BulkOperationsMixin",
+    "BulkReplaceMixin", 
+    "BulkUpdateMixin",
+    "BulkOperationStatusView",
+]
