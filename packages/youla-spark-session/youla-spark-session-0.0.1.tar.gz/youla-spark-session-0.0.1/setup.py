96c625e368ab2d31c42c43dca793b6c9fcf09226
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:05 +0000

    Add package youla-spark-session/0.0.1/youla-spark-session-0.0.1.tar.gz

---
+++ packages/youla-spark-session/youla-spark-session-0.0.1.tar.gz/youla-spark-session-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="youla-spark-session",
+    version="0.0.1",
+    packages=["youla-spark-session"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
