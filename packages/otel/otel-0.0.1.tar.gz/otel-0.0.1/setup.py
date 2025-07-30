aa6ee1a8caa6ef24b44b7e4c41bcd74b665f9613
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:09 +0000

    Add package otel/0.0.1/otel-0.0.1.tar.gz

---
+++ packages/otel/otel-0.0.1.tar.gz/otel-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="otel",
+    version="0.0.1",
+    packages=["otel"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
