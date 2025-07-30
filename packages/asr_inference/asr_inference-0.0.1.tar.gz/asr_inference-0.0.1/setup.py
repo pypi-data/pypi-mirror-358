8e8860cd02eff6051743d0751764e52085b96093
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:22 +0000

    Add package asr_inference/0.0.1/asr_inference-0.0.1.tar.gz

---
+++ packages/asr_inference/asr_inference-0.0.1.tar.gz/asr_inference-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="asr_inference",
+    version="0.0.1",
+    packages=["asr_inference"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
