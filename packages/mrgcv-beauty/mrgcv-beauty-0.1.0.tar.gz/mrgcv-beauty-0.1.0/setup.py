5de5178aee954f4758bf52aa9ce560d79b050cbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:21 +0000

    Add package mrgcv-beauty/0.1.0/mrgcv-beauty-0.1.0.tar.gz

---
+++ packages/mrgcv-beauty/mrgcv-beauty-0.1.0.tar.gz/mrgcv-beauty-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="mrgcv-beauty",
+    version="0.1.0",
+    packages=["mrgcv-beauty"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
