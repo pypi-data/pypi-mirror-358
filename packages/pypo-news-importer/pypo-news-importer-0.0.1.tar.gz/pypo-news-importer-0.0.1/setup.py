66670af1075fdcfbbb3f5eaa6e9aab782c0775f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:55 +0000

    Add package pypo-news-importer/0.0.1/pypo-news-importer-0.0.1.tar.gz

---
+++ packages/pypo-news-importer/pypo-news-importer-0.0.1.tar.gz/pypo-news-importer-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pypo-news-importer",
+    version="0.0.1",
+    packages=["pypo-news-importer"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
