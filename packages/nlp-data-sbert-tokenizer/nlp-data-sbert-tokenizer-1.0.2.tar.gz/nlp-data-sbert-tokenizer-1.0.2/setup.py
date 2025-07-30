b0b083b9d5f8e0d1b936a91b1f8f57813b249c66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:57 +0000

    Add package nlp-data-sbert-tokenizer/1.0.2/nlp-data-sbert-tokenizer-1.0.2.tar.gz

---
+++ packages/nlp-data-sbert-tokenizer/nlp-data-sbert-tokenizer-1.0.2.tar.gz/nlp-data-sbert-tokenizer-1.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="nlp-data-sbert-tokenizer",
+    version="1.0.2",
+    packages=["nlp-data-sbert-tokenizer"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
