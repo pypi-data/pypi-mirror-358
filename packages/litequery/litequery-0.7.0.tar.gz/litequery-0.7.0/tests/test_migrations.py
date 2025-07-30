a58cdc5cbea02e7acd24fefc458890a86bb1af57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:19:45 +0000

    Add package litequery/0.7.0/litequery-0.7.0.tar.gz

---
+++ packages/litequery/litequery-0.7.0.tar.gz/litequery-0.7.0/tests/test_migrations.py
@@ -0,0 +1,83 @@
+import os
+import shutil
+import sqlite3
+import tempfile
+import textwrap
+from pathlib import Path
+
+import pytest
+
+from litequery.config import get_config
+from litequery.migrations import migrate
+
+
+@pytest.fixture
+def temp_db():
+    fd, path = tempfile.mkstemp()
+    yield Path(path)
+    os.close(fd)
+    os.unlink(path)
+
+
+@pytest.fixture
+def temp_migrations_dir(temp_db):
+    migrations_path = temp_db.parent / "migrations"
+    migrations_path.mkdir(exist_ok=True)
+    yield migrations_path
+    shutil.rmtree(migrations_path)
+
+
+def create_migration_file(dir_path, filename, content):
+    migration_path = dir_path / filename
+    with open(migration_path, "w") as f:
+        f.write(content)
+    return migration_path
+
+
+def test_migrate(temp_db: Path, temp_migrations_dir: Path):
+    migrations = {
+        "001_initial.sql": "create table users (id integer primary key autoincrement);",
+        "002_add_name.sql": "alter table users add column name text;",
+    }
+    for filename, content in migrations.items():
+        create_migration_file(temp_migrations_dir, filename, content)
+
+    migrate(get_config(str(temp_db)))
+
+    conn = sqlite3.connect(temp_db)
+    cursor = conn.cursor()
+
+    applied_migrations = cursor.execute(
+        "select filename from migrations order by run_at"
+    ).fetchall()
+    assert len(applied_migrations) == 2
+    assert applied_migrations[0][0] == "001_initial.sql"
+    assert applied_migrations[1][0] == "002_add_name.sql"
+
+    table_info = cursor.execute("pragma table_info(users)").fetchall()
+    assert table_info[0][1] == "id"
+    assert table_info[1][1] == "name"
+
+    conn.close()
+
+
+def test_creates_schema(temp_db, temp_migrations_dir):
+    create_migration_file(
+        temp_migrations_dir,
+        "001_initial.sql",
+        "create table users (id integer primary key autoincrement);",
+    )
+
+    migrate(get_config(str(temp_db)))
+
+    with open(os.path.join(os.path.dirname(temp_db), "schema.sql")) as f:
+        schema_content = """
+            CREATE TABLE migrations (
+                id integer primary key autoincrement,
+                filename text not null,
+                run_at text not null default current_timestamp
+            );
+            CREATE TABLE sqlite_sequence(name,seq);
+            CREATE TABLE users (id integer primary key autoincrement);
+        """
+        assert f.read().strip() == textwrap.dedent(schema_content).strip()
