41e039896b4e87892d836cf4fe1b6492a6471b06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:07:01 +0000

    Add package landmarking/0.0.1/landmarking-0.0.1.tar.gz

---
+++ packages/landmarking/landmarking-0.0.1.tar.gz/landmarking-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="landmarking",
+    version="0.0.1",
+    packages=["landmarking"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
