266a10151e46f77f039f1b049d7b9b7a9bd27d13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:28 +0000

    Add package ubsender/0.0.1/ubsender-0.0.1.tar.gz

---
+++ packages/ubsender/ubsender-0.0.1.tar.gz/ubsender-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ubsender",
+    version="0.0.1",
+    packages=["ubsender"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
