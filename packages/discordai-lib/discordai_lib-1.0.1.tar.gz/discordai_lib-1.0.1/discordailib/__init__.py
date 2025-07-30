18e439b6f99646e3304d1258ff8150060d24654b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:14 +0000

    Add package discordai-lib/1.0.1/discordai_lib-1.0.1.tar.gz

---
+++ packages/discordai-lib/discordai_lib-1.0.1.tar.gz/discordai_lib-1.0.1/discordailib/__init__.py
@@ -0,0 +1,11 @@
+
+"""
+DiscordAI Library - Easy AI integration for Discord bots using Pollination.ai
+"""
+
+from .client import DiscordAI
+from .models import AIModel, AIFeature
+
+__version__ = "1.0.0"
+__author__ = "DiscordAI Library"
+__all__ = ["DiscordAI", "AIModel", "AIFeature"]
---
+++ packages/discordai-lib/discordai_lib-1.0.1.tar.gz/discordai_lib-1.0.1/setup.py
@@ -0,0 +1,47 @@
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = [line.strip() for line in fh if line.strip() and not line.startswith("#")]
+
+setup(
+    name="discordai-lib",
+    version="1.0.1",
+    author="VyeronServices",
+    author_email="vyeronservices.official@gmail.com",
+    description="Easy AI integration for Discord bots using Pollination.ai",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/VyeronServices/discordai-lib",
+    packages=find_packages(),
+    package_data={
+        'discordailib': ['*.py'],
+    },
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Communications :: Chat",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+    python_requires=">=3.8",
+    install_requires=requirements,
+    keywords="discord, bot, ai, pollination, deepseek, grok, openai, chatbot, image generation",
+    project_urls={
+        "Bug Reports": "https://github.com/VyeronServices/discordai-lib/issues",
+        "Source": "https://github.com/VyeronServices/discordai-lib",
+        "Documentation": "https://github.com/VyeronServices/discordai-lib#readme",
+        "Discord Support": "https://discord.gg/zsBNWvDu96",
+    },
+)
