c6334dbdff1a284943460509e3c517f3279061d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:36 +0000

    Add package datalens-connector-promql/0.0.1/datalens-connector-promql-0.0.1.tar.gz

---
+++ packages/datalens-connector-promql/datalens-connector-promql-0.0.1.tar.gz/datalens-connector-promql-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="datalens-connector-promql",
+    version="0.0.1",
+    packages=["datalens-connector-promql"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
