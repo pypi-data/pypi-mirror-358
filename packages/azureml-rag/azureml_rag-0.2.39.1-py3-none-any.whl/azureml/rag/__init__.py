fba8c27c699091e26debd4358b27dd16ffc83175
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:54 +0000

    Add package azureml-rag/0.2.39.1/azureml_rag-0.2.39.1-py3-none-any.whl

---
+++ packages/azureml-rag/azureml_rag-0.2.39.1-py3-none-any.whl/azureml/rag/__init__.py
@@ -0,0 +1,10 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+"""AzureML Retrieval Augmented Generation (RAG) utilities."""
+
+from azureml.rag.mlindex import MLIndex
+
+__all__ = [
+    "MLIndex",
+]
