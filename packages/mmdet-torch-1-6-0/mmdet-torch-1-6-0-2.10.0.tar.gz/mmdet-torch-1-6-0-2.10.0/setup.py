79928b8b7cc4737e21d2a06ab6bd57cef7ba1e41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:12 +0000

    Add package mmdet-torch-1-6-0/2.10.0/mmdet-torch-1-6-0-2.10.0.tar.gz

---
+++ packages/mmdet-torch-1-6-0/mmdet-torch-1-6-0-2.10.0.tar.gz/mmdet-torch-1-6-0-2.10.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="mmdet-torch-1-6-0",
+    version="2.10.0",
+    packages=["mmdet-torch-1-6-0"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
