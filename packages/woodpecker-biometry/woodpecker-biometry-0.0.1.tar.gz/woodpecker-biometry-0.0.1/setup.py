68b6c8d4e7cccaebc6b5bc4701265c6b2831bcfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:08 +0000

    Add package woodpecker-biometry/0.0.1/woodpecker-biometry-0.0.1.tar.gz

---
+++ packages/woodpecker-biometry/woodpecker-biometry-0.0.1.tar.gz/woodpecker-biometry-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="woodpecker-biometry",
+    version="0.0.1",
+    packages=["woodpecker-biometry"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
