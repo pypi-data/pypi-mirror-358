2a3f53e16b9b054b246a8e91be80f98209a65c11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:09 +0000

    Add package main-device-clf/0.0.1/main-device-clf-0.0.1.tar.gz

---
+++ packages/main-device-clf/main-device-clf-0.0.1.tar.gz/main-device-clf-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="main-device-clf",
+    version="0.0.1",
+    packages=["main-device-clf"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
