854be002cae896f6daaec8fc88ee5c57edacbb75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:08 +0000

    Add package tifrgba/0.1.2/tifrgba-0.1.2.tar.gz

---
+++ packages/tifrgba/tifrgba-0.1.2.tar.gz/tifrgba-0.1.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="tifrgba",
+    version="0.1.2",
+    packages=["tifrgba"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
