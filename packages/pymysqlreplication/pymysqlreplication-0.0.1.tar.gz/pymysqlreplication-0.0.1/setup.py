3a3193cb554f59596f94c28aa2f772bcf7a154e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:04 +0000

    Add package pymysqlreplication/0.0.1/pymysqlreplication-0.0.1.tar.gz

---
+++ packages/pymysqlreplication/pymysqlreplication-0.0.1.tar.gz/pymysqlreplication-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pymysqlreplication",
+    version="0.0.1",
+    packages=["pymysqlreplication"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
