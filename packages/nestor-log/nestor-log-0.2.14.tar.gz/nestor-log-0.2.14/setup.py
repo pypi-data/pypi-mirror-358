6d513a948d35389dc5b86eb462a3184c6a280954
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:22 +0000

    Add package nestor-log/0.2.14/nestor-log-0.2.14.tar.gz

---
+++ packages/nestor-log/nestor-log-0.2.14.tar.gz/nestor-log-0.2.14/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="nestor-log",
+    version="0.2.14",
+    packages=["nestor-log"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
