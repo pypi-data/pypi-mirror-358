6b2ae0dc7b03709104db9dc852eb8bb7f220897e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:04 +0000

    Add package fused-weight-gradient-mlp-cuda/0.0.1/fused-weight-gradient-mlp-cuda-0.0.1.tar.gz

---
+++ packages/fused-weight-gradient-mlp-cuda/fused-weight-gradient-mlp-cuda-0.0.1.tar.gz/fused-weight-gradient-mlp-cuda-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="fused-weight-gradient-mlp-cuda",
+    version="0.0.1",
+    packages=["fused-weight-gradient-mlp-cuda"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
