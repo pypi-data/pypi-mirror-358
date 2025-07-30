f9c85e78dceecc3e74c8d593758143e8c5e3de7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:24 +0000

    Add package splunk-soar-sdk/1.0.0b36/splunk_soar_sdk-1.0.0b36-py3-none-any.whl

---
+++ packages/splunk-soar-sdk/splunk_soar_sdk-1.0.0b36-py3-none-any.whl/soar_sdk/paths.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+SDK_ROOT = Path(__file__).parent
+
+# SDK templates (built into the SDK)
+SDK_TEMPLATES = SDK_ROOT / "templates"
+
+# App's templates
+APP_TEMPLATES = Path("templates")
