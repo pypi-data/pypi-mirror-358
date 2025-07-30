f556d9371f1f359dbbdfed51384ec9d953a9a79b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:28 +0000

    Add package jakarta.xml.bind-api/4.0.2/jakarta.xml.bind-api-4.0.2.tar.gz

---
+++ packages/jakarta.xml.bind-api/jakarta.xml.bind-api-4.0.2.tar.gz/jakarta.xml.bind-api-4.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="jakarta.xml.bind-api",
+    version="4.0.2",
+    packages=["jakarta.xml.bind-api"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
