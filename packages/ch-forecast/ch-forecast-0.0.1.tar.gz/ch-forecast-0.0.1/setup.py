36741f64c3edf12e16cebb2640ea57a710495767
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:33 +0000

    Add package ch-forecast/0.0.1/ch-forecast-0.0.1.tar.gz

---
+++ packages/ch-forecast/ch-forecast-0.0.1.tar.gz/ch-forecast-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ch-forecast",
+    version="0.0.1",
+    packages=["ch-forecast"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
