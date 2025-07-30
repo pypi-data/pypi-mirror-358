68a0882f6eacd34fce49ba9b6f10a6eac7b2c29b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:20 +0000

    Add package sparkfly-api-client/1.1.0/sparkfly_api_client-1.1.0-py3-none-any.whl

---
+++ packages/sparkfly-api-client/sparkfly_api_client-1.1.0-py3-none-any.whl/openapi_client/api/__init__.py
@@ -0,0 +1,22 @@
+# flake8: noqa
+
+# import apis into api package
+from openapi_client.api.audiences_api import AudiencesApi
+from openapi_client.api.bi_store_lists_api import BIStoreListsApi
+from openapi_client.api.campaigns_api import CampaignsApi
+from openapi_client.api.ctm_api import CtmApi
+from openapi_client.api.eligible_item_sets_api import EligibleItemSetsApi
+from openapi_client.api.email_opt_in_api import EmailOptInApi
+from openapi_client.api.impressions_api import ImpressionsApi
+from openapi_client.api.items_api import ItemsApi
+from openapi_client.api.member_privacy_api import MemberPrivacyApi
+from openapi_client.api.members_api import MembersApi
+from openapi_client.api.offer_lists_api import OfferListsApi
+from openapi_client.api.offer_pos_offer_codes_api import OfferPOSOfferCodesApi
+from openapi_client.api.offer_states_api import OfferStatesApi
+from openapi_client.api.offers_api import OffersApi
+from openapi_client.api.pos_offer_codes_api import POSOfferCodesApi
+from openapi_client.api.store_lists_api import StoreListsApi
+from openapi_client.api.stores_api import StoresApi
+from openapi_client.api.templates_api import TemplatesApi
+
