ff58bb18748894b7bb259487c86e3abf06a9fba4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:58 +0000

    Add package rest-flex-fields/0.0.1/rest-flex-fields-0.0.1.tar.gz

---
+++ packages/rest-flex-fields/rest-flex-fields-0.0.1.tar.gz/rest-flex-fields-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="rest-flex-fields",
+    version="0.0.1",
+    packages=["rest-flex-fields"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
