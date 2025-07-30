aeb6e847cdd299642efc5fea21f471e4da31820d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:06:21 +0000

    Add package misfits/0.0.9/misfits-0.0.9.tar.gz

---
+++ packages/misfits/misfits-0.0.9.tar.gz/misfits-0.0.9/misfits/headers.py
@@ -0,0 +1,53 @@
+from rich.text import Text
+from textual.app import ComposeResult
+from textual.containers import Horizontal
+from textual.widgets import Label
+from textual.widgets import Static
+from textualeffects.widgets import EffectLabel
+
+from misfits import __version__
+
+
+def labelize(arg: Label | EffectLabel | str | None):
+    if isinstance(arg, Label) or isinstance(arg, EffectLabel):
+        return arg
+    elif isinstance(arg, str):
+        return Label(arg)
+    elif arg is None:
+        return None
+    else:
+        raise ValueError()
+
+
+class Header(Static):
+    def __init__(
+        self,
+        *,
+        left_label: Label | EffectLabel | str | None = None,
+        mid_label: Label | EffectLabel | str | None = None,
+        right_label: Label | EffectLabel | str | None = None,
+    ):
+        self.left_label = labelize(left_label)
+        self.mid_label = labelize(mid_label)
+        self.right_label = labelize(right_label)
+        super().__init__()
+
+    def compose(self) -> ComposeResult:
+        with Horizontal():
+            if self.left_label:
+                yield self.left_label
+            yield Static()
+            if self.mid_label:
+                yield self.mid_label
+            yield Static()
+            if self.right_label:
+                yield self.right_label
+
+
+class MainHeader(Header):
+    def __init__(self):
+        self.has_run_before = False
+        super().__init__(
+            left_label=" misfits",
+            right_label=Label(Text.from_markup(f"[italic dim]v.{__version__} ")),
+        )
