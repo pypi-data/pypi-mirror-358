0aef651bc98653077c280680733091278c16f1e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:13 +0000

    Add package ok_tasks/0.0.171/ok_tasks-0.0.171.tar.gz

---
+++ packages/ok_tasks/ok_tasks-0.0.171.tar.gz/ok_tasks-0.0.171/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ok_tasks",
+    version="0.0.171",
+    packages=["ok_tasks"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
