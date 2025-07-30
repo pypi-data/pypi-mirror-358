918e57be8a65d77633b8d93ffcb5551c2783c79d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:44:37 +0000

    Add package pyhausbus/1.0.5/pyhausbus-1.0.5-py2.py3-none-any.whl

---
+++ packages/pyhausbus/pyhausbus-1.0.5-py2.py3-none-any.whl/pyhausbus/de/hausbus/homeassistant/proxy/DaliLine.py
@@ -0,0 +1,123 @@
+import logging
+from pyhausbus.HausBusCommand import HausBusCommand
+from pyhausbus.ABusFeature import *
+from pyhausbus.ResultWorker import ResultWorker
+import pyhausbus.HausBusUtils as HausBusUtils
+from pyhausbus.de.hausbus.homeassistant.proxy.daliLine.data.Configuration import Configuration
+from pyhausbus.de.hausbus.homeassistant.proxy.daliLine.params.EErrorCode import EErrorCode
+
+class DaliLine(ABusFeature):
+  CLASS_ID:int = 160
+
+  def __init__ (self,objectId:int):
+    super().__init__(objectId)
+
+  @staticmethod
+  def create(deviceId:int, instanceId:int):
+    return DaliLine(HausBusUtils.getObjectId(deviceId, 160, instanceId))
+
+  """
+  @param address0 .
+  @param address1 .
+  @param address2 .
+  @param address3 .
+  """
+  def getConfiguration(self, address0:int, address1:int, address2:int, address3:int):
+    logging.info("getConfiguration"+" address0 = "+str(address0)+" address1 = "+str(address1)+" address2 = "+str(address2)+" address3 = "+str(address3))
+    hbCommand = HausBusCommand(self.objectId, 0, "getConfiguration")
+    hbCommand.addByte(address0)
+    hbCommand.addByte(address1)
+    hbCommand.addByte(address2)
+    hbCommand.addByte(address3)
+    ResultWorker()._setResultInfo(Configuration,self.getObjectId())
+    hbCommand.send()
+    logging.info("returns")
+
+  """
+  @param address0 .
+  @param address1 .
+  @param address2 .
+  @param address3 .
+  """
+  def setConfiguration(self, address0:int, address1:int, address2:int, address3:int):
+    logging.info("setConfiguration"+" address0 = "+str(address0)+" address1 = "+str(address1)+" address2 = "+str(address2)+" address3 = "+str(address3))
+    hbCommand = HausBusCommand(self.objectId, 1, "setConfiguration")
+    hbCommand.addByte(address0)
+    hbCommand.addByte(address1)
+    hbCommand.addByte(address2)
+    hbCommand.addByte(address3)
+    ResultWorker()._setResultInfo(None,self.getObjectId())
+    hbCommand.send()
+    logging.info("returns")
+
+  """
+  """
+  def allOff(self):
+    logging.info("allOff")
+    hbCommand = HausBusCommand(self.objectId, 2, "allOff")
+    ResultWorker()._setResultInfo(None,self.getObjectId())
+    hbCommand.send()
+    logging.info("returns")
+
+  """
+  """
+  def allOn(self):
+    logging.info("allOn")
+    hbCommand = HausBusCommand(self.objectId, 3, "allOn")
+    ResultWorker()._setResultInfo(None,self.getObjectId())
+    hbCommand.send()
+    logging.info("returns")
+
+  """
+  @param command Dali Kommando s.Spezifikation.
+  @param address Kurz- oder Gruppenadresse YAAA AAAS\r\n64 Kurzadressen           0AAA AAAS\r\n16 Gruppenadressen        100A AAAS\r\nSammelaufruf              1111 111S\r\n\r\nY: Adressenart: Y=?  ? ? ??  ?? ??? ??  ?0?  ? ? ??  ?? ??? ??  ? ? Kurzadresse.
+  """
+  def sendCommand(self, command:int, address:int):
+    logging.info("sendCommand"+" command = "+str(command)+" address = "+str(address))
+    hbCommand = HausBusCommand(self.objectId, 4, "sendCommand")
+    hbCommand.addByte(command)
+    hbCommand.addByte(address)
+    ResultWorker()._setResultInfo(None,self.getObjectId())
+    hbCommand.send()
+    logging.info("returns")
+
+  """
+  @param address0 .
+  @param address1 .
+  @param address2 .
+  @param address3 .
+  """
+  def Configuration(self, address0:int, address1:int, address2:int, address3:int):
+    logging.info("Configuration"+" address0 = "+str(address0)+" address1 = "+str(address1)+" address2 = "+str(address2)+" address3 = "+str(address3))
+    hbCommand = HausBusCommand(self.objectId, 128, "Configuration")
+    hbCommand.addByte(address0)
+    hbCommand.addByte(address1)
+    hbCommand.addByte(address2)
+    hbCommand.addByte(address3)
+    ResultWorker()._setResultInfo(None,self.getObjectId())
+    hbCommand.send()
+    logging.info("returns")
+
+  """
+  @param status .
+  """
+  def Status(self, status:int):
+    logging.info("Status"+" status = "+str(status))
+    hbCommand = HausBusCommand(self.objectId, 129, "Status")
+    hbCommand.addByte(status)
+    ResultWorker()._setResultInfo(None,self.getObjectId())
+    hbCommand.send()
+    logging.info("returns")
+
+  """
+  @param errorCode .
+  """
+  def evError(self, errorCode:EErrorCode):
+    logging.info("evError"+" errorCode = "+str(errorCode))
+    hbCommand = HausBusCommand(self.objectId, 255, "evError")
+    hbCommand.addByte(errorCode.value)
+    ResultWorker()._setResultInfo(None,self.getObjectId())
+    hbCommand.send()
+    logging.info("returns")
+
+
---
+++ packages/pyhausbus/pyhausbus-1.0.5-py2.py3-none-any.whl/pyhausbus/de/hausbus/homeassistant/proxy/TcpClient.py
@@ -0,0 +1,72 @@
+import logging
+from pyhausbus.HausBusCommand import HausBusCommand
+from pyhausbus.ABusFeature import *
+from pyhausbus.ResultWorker import ResultWorker
+import pyhausbus.HausBusUtils as HausBusUtils
+from pyhausbus.de.hausbus.homeassistant.proxy.tcpClient.data.CurrentIp import CurrentIp
+
+class TcpClient(ABusFeature):
+  CLASS_ID:int = 91
+
+  def __init__ (self,objectId:int):
+    super().__init__(objectId)
+
+  @staticmethod
+  def create(deviceId:int, instanceId:int):
+    return TcpClient(HausBusUtils.getObjectId(deviceId, 91, instanceId))
+
+  """
+  @param IP0 .
+  @param IP1 .
+  @param IP2 .
+  @param IP3 .
+  @param port .
+  """
+  def announceServer(self, IP0:int, IP1:int, IP2:int, IP3:int, port:int):
+    logging.info("announceServer"+" IP0 = "+str(IP0)+" IP1 = "+str(IP1)+" IP2 = "+str(IP2)+" IP3 = "+str(IP3)+" port = "+str(port))
+    hbCommand = HausBusCommand(self.objectId, 1, "announceServer")
+    hbCommand.addByte(IP0)
+    hbCommand.addByte(IP1)
+    hbCommand.addByte(IP2)
+    hbCommand.addByte(IP3)
+    hbCommand.addWord(port)
+    ResultWorker()._setResultInfo(None,self.getObjectId())
+    hbCommand.send()
+    logging.info("returns")
+
+  """
+  """
+  def getCurrentIp(self):
+    logging.info("getCurrentIp")
+    hbCommand = HausBusCommand(self.objectId, 2, "getCurrentIp")
+    ResultWorker()._setResultInfo(CurrentIp,self.getObjectId())
+    hbCommand.send()
+    logging.info("returns")
+
+  """
+  @param IP0 .
+  @param IP1 .
+  @param IP2 .
+  @param IP3 .
+  """
+  def CurrentIp(self, IP0:int, IP1:int, IP2:int, IP3:int):
+    logging.info("CurrentIp"+" IP0 = "+str(IP0)+" IP1 = "+str(IP1)+" IP2 = "+str(IP2)+" IP3 = "+str(IP3))
+    hbCommand = HausBusCommand(self.objectId, 128, "CurrentIp")
+    hbCommand.addByte(IP0)
+    hbCommand.addByte(IP1)
+    hbCommand.addByte(IP2)
+    hbCommand.addByte(IP3)
+    ResultWorker()._setResultInfo(None,self.getObjectId())
+    hbCommand.send()
+    logging.info("returns")
+
+  """
+  """
+  def evWhoIsServer(self):
+    logging.info("evWhoIsServer")
+    hbCommand = HausBusCommand(self.objectId, 200, "evWhoIsServer")
+    ResultWorker()._setResultInfo(None,self.getObjectId())
+    hbCommand.send()
+    logging.info("returns")
+
+
