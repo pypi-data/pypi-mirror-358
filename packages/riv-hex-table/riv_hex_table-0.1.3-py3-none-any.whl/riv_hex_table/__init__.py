73a06ba151552b6fd128c13bba37abc5534f904b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:34:06 +0000

    Add package riv-hex-table/0.1.3/riv_hex_table-0.1.3-py3-none-any.whl

---
+++ packages/riv-hex-table/riv_hex_table-0.1.3-py3-none-any.whl/riv_hex_table/__init__.py
@@ -0,0 +1,7 @@
+# my_table_package/__init__.py
+
+from .hex_table import StyledHexTableGenerator
+from .hex_table import StyledHexCardGenerator
+
+# Optionally define a version for your package
+__version__ = "0.1.0"
