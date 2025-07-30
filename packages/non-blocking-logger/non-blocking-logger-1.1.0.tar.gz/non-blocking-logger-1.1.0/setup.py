a47cb1ae9d267e8357b656a5b6b8ac5a13187a2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:08 +0000

    Add package non-blocking-logger/1.1.0/non-blocking-logger-1.1.0.tar.gz

---
+++ packages/non-blocking-logger/non-blocking-logger-1.1.0.tar.gz/non-blocking-logger-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="non-blocking-logger",
+    version="1.1.0",
+    packages=["non-blocking-logger"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
