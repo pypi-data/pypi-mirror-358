94a447392b137656cb07ee6a1d61aa9c2aa21356
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:19 +0000

    Add package tinymce/0.0.1/tinymce-0.0.1.tar.gz

---
+++ packages/tinymce/tinymce-0.0.1.tar.gz/tinymce-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="tinymce",
+    version="0.0.1",
+    packages=["tinymce"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
