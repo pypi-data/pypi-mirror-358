e460391be7db974be511c9a877c76ddaf2dc4930
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:03 +0000

    Add package simera/0.2.1/simera-0.2.1.tar.gz

---
+++ packages/simera/simera-0.2.1.tar.gz/simera-0.2.1/simera/__init__.py
@@ -0,0 +1,7 @@
+__version__ = '0.2.1'
+
+from simera.config import Config
+from simera.zipcode import ZipcodeManager
+from simera.ratesheet import Ratesheet
+from simera.shipment import Shipment
+from simera.cost import Cost, calculate_cost
