9b26629b4eb54feb0933597c09aa29b12122ad30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:19 +0000

    Add package curtin/0.0.1/curtin-0.0.1.tar.gz

---
+++ packages/curtin/curtin-0.0.1.tar.gz/curtin-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="curtin",
+    version="0.0.1",
+    packages=["curtin"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
