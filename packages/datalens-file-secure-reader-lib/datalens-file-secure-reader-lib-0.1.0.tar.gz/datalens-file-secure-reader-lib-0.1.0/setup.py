8fdd2a32a6826fbbe35a11b68515596782a3cde3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:05:13 +0000

    Add package datalens-file-secure-reader-lib/0.1.0/datalens-file-secure-reader-lib-0.1.0.tar.gz

---
+++ packages/datalens-file-secure-reader-lib/datalens-file-secure-reader-lib-0.1.0.tar.gz/datalens-file-secure-reader-lib-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="datalens-file-secure-reader-lib",
+    version="0.1.0",
+    packages=["datalens-file-secure-reader-lib"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
