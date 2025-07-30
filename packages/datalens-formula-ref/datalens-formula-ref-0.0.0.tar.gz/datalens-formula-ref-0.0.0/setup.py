8565a0e30d74398b965bc531d40c24f069800794
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:37 +0000

    Add package datalens-formula-ref/0.0.0/datalens-formula-ref-0.0.0.tar.gz

---
+++ packages/datalens-formula-ref/datalens-formula-ref-0.0.0.tar.gz/datalens-formula-ref-0.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="datalens-formula-ref",
+    version="0.0.0",
+    packages=["datalens-formula-ref"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
