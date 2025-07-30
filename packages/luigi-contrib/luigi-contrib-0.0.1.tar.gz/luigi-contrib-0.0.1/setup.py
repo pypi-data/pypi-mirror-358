9e78e86cf117ea9f83ac086d0b4a6ced28db8476
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:41 +0000

    Add package luigi-contrib/0.0.1/luigi-contrib-0.0.1.tar.gz

---
+++ packages/luigi-contrib/luigi-contrib-0.0.1.tar.gz/luigi-contrib-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="luigi-contrib",
+    version="0.0.1",
+    packages=["luigi-contrib"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
