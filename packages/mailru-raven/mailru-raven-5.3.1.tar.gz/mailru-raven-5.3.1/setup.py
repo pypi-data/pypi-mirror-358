60c10a285c1b32f7f5891d07a489c90e746c35de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:08 +0000

    Add package mailru-raven/5.3.1/mailru-raven-5.3.1.tar.gz

---
+++ packages/mailru-raven/mailru-raven-5.3.1.tar.gz/mailru-raven-5.3.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="mailru-raven",
+    version="5.3.1",
+    packages=["mailru-raven"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
