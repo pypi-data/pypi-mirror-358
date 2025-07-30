0bd5a529aff6b17cf3eb1a5c7bcc02c800e511fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:07 +0000

    Add package text-utils-ex/0.0.1/text-utils-ex-0.0.1.tar.gz

---
+++ packages/text-utils-ex/text-utils-ex-0.0.1.tar.gz/text-utils-ex-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="text-utils-ex",
+    version="0.0.1",
+    packages=["text-utils-ex"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
