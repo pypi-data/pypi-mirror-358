ce2e4e4de5858e3dfa9f5dfed770c1d2ad7e55c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:31 +0000

    Add package check-on-pr/0.0.1/check-on-pr-0.0.1.tar.gz

---
+++ packages/check-on-pr/check-on-pr-0.0.1.tar.gz/check-on-pr-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="check-on-pr",
+    version="0.0.1",
+    packages=["check-on-pr"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
