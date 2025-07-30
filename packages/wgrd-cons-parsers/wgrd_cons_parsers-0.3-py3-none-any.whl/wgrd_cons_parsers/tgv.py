cbf49d4158ee89f214f3555e306a7889971ae9a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:09:09 +0000

    Add package wgrd-cons-parsers/0.3/wgrd_cons_parsers-0.3-py3-none-any.whl

---
+++ packages/wgrd-cons-parsers/wgrd_cons_parsers-0.3-py3-none-any.whl/wgrd_cons_parsers/tgv.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+from wgrd_cons_parsers.common import *
+
+from conshex import *
+from conshex.string import *
+
+from wgrd_cons_parsers.utils import align
+from wgrd_cons_parsers.cons_utils import File
+
+TGV = Struct(
+    "version" / Int32ul,
+    "compressed" / Enum(Int32ul, uncompressed=0, compressed=1),
+    Check(lambda ctx: ctx.compressed in ["uncompressed", "compressed"]),
+    "width" / Int32ul,
+    "height" / Int32ul,
+    "imageWidth" / Int32ul,
+    "imageHeight" / Int32ul,
+    Check(lambda ctx: ctx.imageWidth == ctx.width),
+    Check(lambda ctx: ctx.imageHeight == ctx.height),
+    "mipmapCount" / Rebuild(Int16ul, lambda ctx: len(ctx.images)),
+    "pixelFormatName" / Aligned(7, PascalString(Int16ul, "utf-8")),
+    "checksum" / Bytes(16),
+    "offsets" / Rebuild(Array(this.mipmapCount, Int32ul), lambda ctx: [ctx._checksum_meta._endoffset + 2*ctx.mipmapCount*4 + sum([align(s, 4) for s in ctx.sizes[0:x]]) for x in range(ctx.mipmapCount)]),
+    "sizes" / Rebuild(Array(this.mipmapCount, Int32ul), lambda ctx: [len(ctx.images[x].data) for x in range(ctx.mipmapCount)]),
+    "images" / Array(this.mipmapCount,
+                     "mipmap" / Pointer(lambda ctx: ctx.offsets[ctx._index],
+                                        Struct("data" / Aligned(4, File(Bytes(lambda ctx: ctx._.sizes[ctx._._index]), lambda ctx: f"tgv/tgv_{ctx._index}_{ctx._.compressed}.tgvformat")))
+                                        )
+                     ),
+    )
+
+
+if __name__ == "__main__":
+    main = CommonMain(TGV, "TGV")
+    main.main()
