cc6e890d0ae5814e643d3c23fe08527ff4f3ec7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:07:01 +0000

    Add package dmcontrol-client/0.0.1/dmcontrol-client-0.0.1.tar.gz

---
+++ packages/dmcontrol-client/dmcontrol-client-0.0.1.tar.gz/dmcontrol-client-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dmcontrol-client",
+    version="0.0.1",
+    packages=["dmcontrol-client"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
