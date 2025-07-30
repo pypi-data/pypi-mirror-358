a555b2d86d981faecef2001423adc1514fc9c495
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:10 +0000

    Add package bazel-tools/0.0.1/bazel-tools-0.0.1.tar.gz

---
+++ packages/bazel-tools/bazel-tools-0.0.1.tar.gz/bazel-tools-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="bazel-tools",
+    version="0.0.1",
+    packages=["bazel-tools"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
