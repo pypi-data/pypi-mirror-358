e6f8223bbecb1f453dee70f7be897d396b6470e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:20 +0000

    Add package vk-observeit-statsd/0.0.5/vk-observeit-statsd-0.0.5.tar.gz

---
+++ packages/vk-observeit-statsd/vk-observeit-statsd-0.0.5.tar.gz/vk-observeit-statsd-0.0.5/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="vk-observeit-statsd",
+    version="0.0.5",
+    packages=["vk-observeit-statsd"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
