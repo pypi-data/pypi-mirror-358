bfcb569bdc8c3757fdf17d3b61a9843699a3822c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:05:08 +0000

    Add package qacore/0.20.4/qacore-0.20.4.tar.gz

---
+++ packages/qacore/qacore-0.20.4.tar.gz/qacore-0.20.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="qacore",
+    version="0.20.4",
+    packages=["qacore"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
