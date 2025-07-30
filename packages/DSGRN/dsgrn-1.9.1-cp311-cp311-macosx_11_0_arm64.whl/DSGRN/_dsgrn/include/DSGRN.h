a3eaf6d1c384b05442c38e934eb1cae89dc1f549
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:56 +0000

    Add package DSGRN/1.9.1/dsgrn-1.9.1-cp311-cp311-macosx_11_0_arm64.whl

---
+++ packages/DSGRN/dsgrn-1.9.1-cp311-cp311-macosx_11_0_arm64.whl/DSGRN/_dsgrn/include/DSGRN.h
@@ -0,0 +1,33 @@
+/// DSGRN.h
+/// Shaun Harker
+/// 2015-05-29
+
+#pragma once
+
+#include "Parameter/Network.h"
+#include "Parameter/Parameter.h"
+#include "Parameter/ParameterGraph.h"
+#include "Parameter/OrderParameter.h"
+#include "Parameter/LogicParameter.h"
+#include "Parameter/ParameterSampler.h"
+#include "Phase/Domain.h"
+#include "Phase/DomainGraph.h"
+#include "Phase/Wall.h"
+#include "Phase/WallGraph.h"
+#include "Dynamics/Annotation.h"
+#include "Dynamics/MorseDecomposition.h"
+#include "Dynamics/MorseGraph.h"
+#include "Graph/Digraph.h"
+#include "Graph/Poset.h"
+#include "Graph/Components.h"
+#include "Graph/StrongComponents.h"
+#include "Graph/LabelledMultidigraph.h"
+#include "Pattern/MatchingGraph.h"
+#include "Pattern/MatchingRelation.h"
+#include "Pattern/Pattern.h"
+#include "Pattern/PatternGraph.h"
+#include "Pattern/PatternMatch.h"
+#include "Pattern/SearchGraph.h"
+#include "Query/ComputeSingleGeneQuery.h"
+#include "Query/NFA.h"
+#include "Query/ThompsonsConstruction.h"
