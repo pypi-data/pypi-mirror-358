2b3e867b5fdc01c3fa28ccb96fe3cbdac0531001
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:25 +0000

    Add package key1/0.0.1/key1-0.0.1.tar.gz

---
+++ packages/key1/key1-0.0.1.tar.gz/key1-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="key1",
+    version="0.0.1",
+    packages=["key1"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
