3fb8457241e65078dd10c890e17832ac4fd64e70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:35:46 +0000

    Add package uaibot/1.2.6/uaibot-1.2.6.tar.gz

---
+++ packages/uaibot/uaibot-1.2.6.tar.gz/uaibot-1.2.6/uaibot/c_implementation/build/CMakeFiles/uaibot_cpp_bind.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/uaibot_cpp_bind.dir/link.d"
+  "/home/fbartelt/Documents/Projetos/UAIbotPy/uaibot/c_implementation/uaibot_cpp_bind.cpython-313-x86_64-linux-gnu.so"
+  "/home/fbartelt/Documents/Projetos/UAIbotPy/uaibot/c_implementation/uaibot_cpp_bind.pdb"
+  "CMakeFiles/uaibot_cpp_bind.dir/gjk.c.o"
+  "CMakeFiles/uaibot_cpp_bind.dir/gjk.c.o.d"
+  "CMakeFiles/uaibot_cpp_bind.dir/manipulator.cpp.o"
+  "CMakeFiles/uaibot_cpp_bind.dir/manipulator.cpp.o.d"
+  "CMakeFiles/uaibot_cpp_bind.dir/pybind_main.cpp.o"
+  "CMakeFiles/uaibot_cpp_bind.dir/pybind_main.cpp.o.d"
+  "CMakeFiles/uaibot_cpp_bind.dir/utils.cpp.o"
+  "CMakeFiles/uaibot_cpp_bind.dir/utils.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/uaibot_cpp_bind.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ packages/uaibot/uaibot-1.2.6.tar.gz/uaibot-1.2.6/uaibot/c_implementation/build/CMakeFiles/uaibot_cpp_bind.dir/gjk.c.o.d
@@ -0,0 +1,9 @@
+CMakeFiles/uaibot_cpp_bind.dir/gjk.c.o: \
+ /home/fbartelt/Documents/Projetos/UAIbotPy/uaibot/c_implementation/gjk.c \
+ /usr/include/stdc-predef.h \
+ /home/fbartelt/Documents/Projetos/UAIbotPy/uaibot/c_implementation/gjk.h \
+ /usr/include/assert.h /usr/include/features.h \
+ /usr/include/features-time64.h /usr/include/bits/wordsize.h \
+ /usr/include/bits/timesize.h /usr/include/sys/cdefs.h \
+ /usr/include/bits/long-double.h /usr/include/gnu/stubs.h \
+ /usr/include/gnu/stubs-64.h
