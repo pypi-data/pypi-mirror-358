c621cf9bcfcedaab92519f4db534b6e8ac0e5d75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:38 +0000

    Add package datalens-model-tools/0.0.1/datalens-model-tools-0.0.1.tar.gz

---
+++ packages/datalens-model-tools/datalens-model-tools-0.0.1.tar.gz/datalens-model-tools-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="datalens-model-tools",
+    version="0.0.1",
+    packages=["datalens-model-tools"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
