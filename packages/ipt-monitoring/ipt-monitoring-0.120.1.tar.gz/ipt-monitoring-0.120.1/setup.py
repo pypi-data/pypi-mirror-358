c8a5024310959438e0d404100f8a71b4324f35c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:29 +0000

    Add package ipt-monitoring/0.120.1/ipt-monitoring-0.120.1.tar.gz

---
+++ packages/ipt-monitoring/ipt-monitoring-0.120.1.tar.gz/ipt-monitoring-0.120.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ipt-monitoring",
+    version="0.120.1",
+    packages=["ipt-monitoring"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
