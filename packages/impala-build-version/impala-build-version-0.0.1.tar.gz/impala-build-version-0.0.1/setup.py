5109e0f083196d4c54a8680ca29067ed466ab7e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:22 +0000

    Add package impala-build-version/0.0.1/impala-build-version-0.0.1.tar.gz

---
+++ packages/impala-build-version/impala-build-version-0.0.1.tar.gz/impala-build-version-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="impala-build-version",
+    version="0.0.1",
+    packages=["impala-build-version"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
