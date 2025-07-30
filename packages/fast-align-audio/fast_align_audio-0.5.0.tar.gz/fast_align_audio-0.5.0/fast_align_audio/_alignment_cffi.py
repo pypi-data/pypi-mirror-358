6b30fbd0eb8f94e13903345482e78e6243f2fb72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:35:58 +0000

    Add package fast-align-audio/0.5.0/fast_align_audio-0.5.0.tar.gz

---
+++ packages/fast-align-audio/fast_align_audio-0.5.0.tar.gz/fast_align_audio-0.5.0/fast_align_audio/_alignment_cffi.py
@@ -0,0 +1,32 @@
+import os
+from cffi import FFI
+
+
+ffibuilder = FFI()
+ffibuilder.cdef("""
+    typedef struct {
+        int min_idx;
+        float min_val;
+    } MinResult;
+""")
+ffibuilder.cdef(
+    "MinResult fast_find_alignment(size_t, float *, size_t, float *, size_t, size_t);"
+)
+
+script_dir = os.path.dirname(os.path.realpath(__file__))
+c_file_path = os.path.join(script_dir, "_faa.c")
+
+with open(c_file_path, "r") as file:
+    c_code = file.read()
+
+extra_compile_args = []
+if os.name == "posix":
+    extra_compile_args = ["-mavx", "-Wall", "-Wextra"]
+
+ffibuilder.set_source(
+    "_fast_align_audio", c_code, extra_compile_args=extra_compile_args
+)
+
+
+if __name__ == "__main__":
+    ffibuilder.compile(verbose=True)
---
+++ packages/fast-align-audio/fast_align_audio-0.5.0.tar.gz/fast_align_audio-0.5.0/setup.py
@@ -0,0 +1,54 @@
+import codecs
+import os
+import re
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    long_description = readme_file.read()
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*parts):
+    with codecs.open(os.path.join(here, *parts), "r") as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+setup(
+    name="fast-align-audio",
+    version=find_version("fast_align_audio", "__init__.py"),
+    description=(
+        "A fast python library for aligning similar audio snippets passed in as NumPy"
+        " arrays."
+    ),
+    license="ISC",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(exclude=["test_fixtures", "tests"]),
+    setup_requires=["cffi>=1.0.0"],
+    tests_require=["soundfile", "pytest"],
+    cffi_modules=["fast_align_audio/_alignment_cffi.py:ffibuilder"],
+    install_requires=["cffi>=1.0.0", "numpy>=2,<3"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Multimedia :: Sound/Audio",
+        "Topic :: Scientific/Engineering",
+        "License :: OSI Approved :: ISC License (ISCL)",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    url="https://github.com/nomonosound/fast-align-audio",
+)
