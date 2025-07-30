94e26244fcc83c5d94ad0562463f53316ca47116
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:11 +0000

    Add package swagger-annotations-jakarta/2.2.15/swagger-annotations-jakarta-2.2.15.tar.gz

---
+++ packages/swagger-annotations-jakarta/swagger-annotations-jakarta-2.2.15.tar.gz/swagger-annotations-jakarta-2.2.15/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="swagger-annotations-jakarta",
+    version="2.2.15",
+    packages=["swagger-annotations-jakarta"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
