f734d3451386f2966195589098d1f109ca6a4e8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:27 +0000

    Add package voice-kws-inference/4.4.0/voice-kws-inference-4.4.0.tar.gz

---
+++ packages/voice-kws-inference/voice-kws-inference-4.4.0.tar.gz/voice-kws-inference-4.4.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="voice-kws-inference",
+    version="4.4.0",
+    packages=["voice-kws-inference"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
