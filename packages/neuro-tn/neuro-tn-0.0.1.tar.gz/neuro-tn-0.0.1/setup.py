a9b6d1d0c1e6ad2a11f559e21adeddf1b1f9730e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:27 +0000

    Add package neuro-tn/0.0.1/neuro-tn-0.0.1.tar.gz

---
+++ packages/neuro-tn/neuro-tn-0.0.1.tar.gz/neuro-tn-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="neuro-tn",
+    version="0.0.1",
+    packages=["neuro-tn"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
