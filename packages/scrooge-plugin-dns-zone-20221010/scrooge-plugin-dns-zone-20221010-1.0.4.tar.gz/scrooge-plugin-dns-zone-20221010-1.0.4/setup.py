7fb21cff4441c3e1d5214d67f20a5231f9496781
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:42 +0000

    Add package scrooge-plugin-dns-zone-20221010/1.0.4/scrooge-plugin-dns-zone-20221010-1.0.4.tar.gz

---
+++ packages/scrooge-plugin-dns-zone-20221010/scrooge-plugin-dns-zone-20221010-1.0.4.tar.gz/scrooge-plugin-dns-zone-20221010-1.0.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="scrooge-plugin-dns-zone-20221010",
+    version="1.0.4",
+    packages=["scrooge-plugin-dns-zone-20221010"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
