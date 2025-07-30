6ca6115d959fc086ccdd61905f18875714a55e84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:21:00 +0000

    Add package particula/0.2.6/particula-0.2.6.tar.gz

---
+++ packages/particula/particula-0.2.6.tar.gz/particula-0.2.6/particula/particles/properties/kelvin_effect_module.py
@@ -0,0 +1,128 @@
+"""Module to calculate the Kelvin effect on vapor pressure."""
+
+from typing import Union
+
+import numpy as np
+from numpy.typing import NDArray
+
+from particula.util.constants import GAS_CONSTANT
+from particula.util.machine_limit import get_safe_exp
+from particula.util.validate_inputs import validate_inputs
+
+
+@validate_inputs(
+    {
+        "effective_surface_tension": "positive",
+        "effective_density": "positive",
+        "molar_mass": "positive",
+        "temperature": "positive",
+    }
+)
+def get_kelvin_radius(
+    effective_surface_tension: Union[float, NDArray[np.float64]],
+    effective_density: Union[float, NDArray[np.float64]],
+    molar_mass: Union[float, NDArray[np.float64]],
+    temperature: float,
+) -> Union[float, NDArray[np.float64]]:
+    """Compute the Kelvin radius (rₖ) to account for curvature effects on vapor
+    pressure.
+
+    The Kelvin radius is defined by:
+
+    - rₖ = (2 × σ × M) / (R × T × ρ)
+        - rₖ is Kelvin radius in meters (m).
+        - σ is the effective surface tension in N/m.
+        - M is the molar mass in kg/mol.
+        - R is the universal gas constant in J/(mol·K).
+        - T is the temperature in Kelvin (K).
+        - ρ is the effective density in kg/m³.
+
+    Arguments:
+        - effective_surface_tension : Surface tension of the mixture (N/m).
+        - effective_density : Effective density of the mixture (kg/m³).
+        - molar_mass : Molar mass (kg/mol).
+        - temperature : Temperature of the system (K).
+
+    Returns:
+        - Kelvin radius in meters (float or NDArray[np.float64]).
+
+    Examples:
+        ``` py title="Example"
+        import numpy as np
+        import particula as par
+        par.particles.get_kelvin_radius(
+            effective_surface_tension=0.072,
+            effective_density=1000.0,
+            molar_mass=0.018,
+            temperature=298.15
+        )
+        # Output: ...
+        ```
+
+    References:
+        - "Kelvin equation," Wikipedia,
+          https://en.wikipedia.org/wiki/Kelvin_equation
+    """
+    return (2 * effective_surface_tension * molar_mass) / (
+        GAS_CONSTANT * temperature * effective_density
+    )
+
+
+@validate_inputs(
+    {
+        "particle_radius": "nonnegative",
+        "kelvin_radius_value": "nonnegative",
+    }
+)
+def get_kelvin_term(
+    particle_radius: Union[float, NDArray[np.float64]],
+    kelvin_radius_value: Union[float, NDArray[np.float64]],
+) -> Union[float, NDArray[np.float64]]:
+    """Compute the Kelvin exponential term to account for curvature effects.
+
+    The Kelvin term (K) is given by:
+
+    - K = exp(rₖ / rₚ)
+        - K is dimensionless.
+        - rₖ is the Kelvin radius (m).
+        - rₚ is the particle radius (m).
+
+    Arguments:
+        - particle_radius : Radius of the particle (m).
+        - kelvin_radius_value : Precomputed Kelvin radius (m).
+
+    Returns:
+        - Dimensionless exponential factor adjusting vapor pressure.
+
+    Examples:
+        ``` py title="Example"
+        import particula as par
+        par.particles.get_kelvin_term(
+            particle_radius=1e-7,
+            kelvin_radius_value=2e-7
+        )
+        print(kv_term)
+        # Output: ...
+        ```
+
+    References:
+        - Donahue, N. M., et al. (2013). "How do organic vapors contribute to
+          new-particle formation?" Faraday Discussions, 165, 91–104.
+          https://doi.org/10.1039/C3FD00046J. [check]
+    """
+    kelvin_expand = False
+    # Broadcast the arrays if necessary np.isscalar(kelvin_radius_value)
+    if isinstance(kelvin_radius_value, np.ndarray) and (
+        kelvin_radius_value.size > 1
+    ):
+        kelvin_expand = True
+        kelvin_radius_value = kelvin_radius_value[np.newaxis, :]
+    if isinstance(particle_radius, np.ndarray) and not kelvin_expand:
+        return get_safe_exp(kelvin_radius_value / particle_radius)
+    if (
+        isinstance(particle_radius, np.ndarray)
+        and (particle_radius.size > 1)
+        and kelvin_expand
+    ):
+        particle_radius = particle_radius[:, np.newaxis]
+    return get_safe_exp(kelvin_radius_value / particle_radius)
---
+++ packages/particula/particula-0.2.6.tar.gz/particula-0.2.6/readme.md
@@ -0,0 +1,19 @@
+# Overview
+
+Particula is a Python-based aerosol particle simulator. Its goal is to provide a robust aerosol simulation (including both gas and particle phases) that can be used to answer scientific questions arising from experiments and research endeavors.
+
+The Particula website [https://uncscode.github.io/particula](https://uncscode.github.io/particula) contains the API reference, how-to guides, and tutorials.
+
+## PyPI Installation
+
+If your Python environment is already set up, you can install [`particula` via pip](https://pypi.org/project/particula/) using the following command:
+
+``` bash
+pip install particula
+```
+
+Or install via conda:
+
+```
+conda install -c conda-forge particula
+```
