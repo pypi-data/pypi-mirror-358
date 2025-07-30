2ad11f8f771ec8a323a112d8ccb4c07e8caf5406
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:45 +0000

    Add package scrooge-plugin-vm-complex-20200310-alpha/1.0.4/scrooge-plugin-vm-complex-20200310-alpha-1.0.4.tar.gz

---
+++ packages/scrooge-plugin-vm-complex-20200310-alpha/scrooge-plugin-vm-complex-20200310-alpha-1.0.4.tar.gz/scrooge-plugin-vm-complex-20200310-alpha-1.0.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="scrooge-plugin-vm-complex-20200310-alpha",
+    version="1.0.4",
+    packages=["scrooge-plugin-vm-complex-20200310-alpha"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
