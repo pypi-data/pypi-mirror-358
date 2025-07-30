946ab32ce9de22142c8357e53bc65ef6419fc3d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:55 +0000

    Add package oshit/0.12.5/oshit-0.12.5.tar.gz

---
+++ packages/oshit/oshit-0.12.5.tar.gz/oshit-0.12.5/oshit/app/oshit.py
@@ -0,0 +1,55 @@
+"""The main application class."""
+
+##############################################################################
+# Textual imports.
+from textual.app import App
+
+##############################################################################
+# Local imports.
+from .data import load_configuration, save_configuration
+from .screens import Main
+
+
+##############################################################################
+class OSHit(App[None]):
+    """The Orange Site Hit application."""
+
+    ENABLE_COMMAND_PALETTE = False
+
+    CSS = """
+    Header {
+        HeaderIcon {
+            visibility: hidden;
+        }
+
+        &.-tall {
+            height: 1;
+        }
+    }
+
+    Footer, Footer * {
+        background: $primary;
+    }
+
+    FooterKey .footer-key--key {
+        color: $text;
+    }
+    """
+
+    def __init__(self) -> None:
+        """Initialise the application."""
+        super().__init__()
+        self.dark = load_configuration().dark_mode
+
+    def on_mount(self) -> None:
+        """Get things going once the app is up and running."""
+        self.push_screen(Main())
+
+    def _watch_dark(self) -> None:
+        """Save the light/dark mode configuration choice."""
+        configuration = load_configuration()
+        configuration.dark_mode = self.dark
+        save_configuration(configuration)
+
+
+### oshit.py ends here
