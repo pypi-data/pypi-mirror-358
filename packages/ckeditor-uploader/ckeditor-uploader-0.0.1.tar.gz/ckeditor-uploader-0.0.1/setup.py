9023d16c5277b160b74eb1be0039771a37c61ad6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:20 +0000

    Add package ckeditor-uploader/0.0.1/ckeditor-uploader-0.0.1.tar.gz

---
+++ packages/ckeditor-uploader/ckeditor-uploader-0.0.1.tar.gz/ckeditor-uploader-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ckeditor-uploader",
+    version="0.0.1",
+    packages=["ckeditor-uploader"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
