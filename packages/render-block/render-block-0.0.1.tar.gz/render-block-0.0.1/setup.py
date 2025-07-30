c005d84668347a4879e8756cc3661f2fa2ad7955
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:20 +0000

    Add package render-block/0.0.1/render-block-0.0.1.tar.gz

---
+++ packages/render-block/render-block-0.0.1.tar.gz/render-block-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="render-block",
+    version="0.0.1",
+    packages=["render-block"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
