1dde6e1c5d0019a488c706b0457349b9e0da4f77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:20 +0000

    Add package auditlogclient/1.0.4/auditlogclient-1.0.4.tar.gz

---
+++ packages/auditlogclient/auditlogclient-1.0.4.tar.gz/auditlogclient-1.0.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="auditlogclient",
+    version="1.0.4",
+    packages=["auditlogclient"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
