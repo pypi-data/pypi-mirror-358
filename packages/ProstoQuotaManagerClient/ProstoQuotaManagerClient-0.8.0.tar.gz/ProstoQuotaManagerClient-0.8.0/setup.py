d374c7f13454d4ea85ce29a5cd0a356974ffef1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:37 +0000

    Add package ProstoQuotaManagerClient/0.8.0/ProstoQuotaManagerClient-0.8.0.tar.gz

---
+++ packages/ProstoQuotaManagerClient/ProstoQuotaManagerClient-0.8.0.tar.gz/ProstoQuotaManagerClient-0.8.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ProstoQuotaManagerClient",
+    version="0.8.0",
+    packages=["ProstoQuotaManagerClient"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
