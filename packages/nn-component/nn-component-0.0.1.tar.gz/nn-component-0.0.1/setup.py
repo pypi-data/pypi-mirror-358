418da41d39969916862c461b1606f4b99bcf5a7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:42 +0000

    Add package nn-component/0.0.1/nn-component-0.0.1.tar.gz

---
+++ packages/nn-component/nn-component-0.0.1.tar.gz/nn-component-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="nn-component",
+    version="0.0.1",
+    packages=["nn-component"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
