c805d67ed19d3c3602725609ccf4f7662064ea5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:08 +0000

    Add package pybullet-envs/0.0.1/pybullet-envs-0.0.1.tar.gz

---
+++ packages/pybullet-envs/pybullet-envs-0.0.1.tar.gz/pybullet-envs-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pybullet-envs",
+    version="0.0.1",
+    packages=["pybullet-envs"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
