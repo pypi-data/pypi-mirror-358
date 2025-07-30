6028deb903927e0a2bd86a7ded386b0e8aad3757
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:07 +0000

    Add package lstmAtten-datautils/2.2.10/lstmatten_datautils-2.2.10.tar.gz

---
+++ packages/lstmAtten-datautils/lstmatten_datautils-2.2.10.tar.gz/lstmatten_datautils-2.2.10/setup.py
@@ -0,0 +1,22 @@
+# lstmAtten_datautils/setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="lstmAtten_datautils",
+    version="2.2.10",
+    author="DT.L",
+    description="A utility package for processing data for LSTM Attention models in relation classification",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        "torch>=1.7.0",
+        "shutup>=0.2.0"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
