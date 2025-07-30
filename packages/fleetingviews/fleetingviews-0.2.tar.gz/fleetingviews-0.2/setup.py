cfc66aa371838a4eeb9341fff9390cd4ef569525
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:37 +0000

    Add package fleetingviews/0.2/fleetingviews-0.2.tar.gz

---
+++ packages/fleetingviews/fleetingviews-0.2.tar.gz/fleetingviews-0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name="fleetingviews",
+    version="0.2",
+    author="Bruno Arellano",
+    author_email="arellanobruno@hotmail.com",
+    py_modules=['FleetingViews'],
+    license="MIT",
+    maintainer="Bruno Arellano",
+    maintainer_email="arellanobruno@hotmail.com",
+    keywords=["flet", "views", "flet views", "view creation", "flet app"],
+    description="Facilitates view creation and management in Flet applications.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ArellanoBrunoc/FleetingViews",
+    include_package_data=True,
+    install_requires=[
+        "flet"
+    ],
+)
