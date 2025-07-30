1dbbbc15f13d5d86098f4ab31fb6b6a99c0cad8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:13 +0000

    Add package ok_tasks_vanilla/1.0.20/ok_tasks_vanilla-1.0.20.tar.gz

---
+++ packages/ok_tasks_vanilla/ok_tasks_vanilla-1.0.20.tar.gz/ok_tasks_vanilla-1.0.20/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ok_tasks_vanilla",
+    version="1.0.20",
+    packages=["ok_tasks_vanilla"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
