ce406e162e43e78f9e6129e3e417f060a0e1d9e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:17 +0000

    Add package dynamic-components/0.0.1/dynamic-components-0.0.1.tar.gz

---
+++ packages/dynamic-components/dynamic-components-0.0.1.tar.gz/dynamic-components-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dynamic-components",
+    version="0.0.1",
+    packages=["dynamic-components"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
