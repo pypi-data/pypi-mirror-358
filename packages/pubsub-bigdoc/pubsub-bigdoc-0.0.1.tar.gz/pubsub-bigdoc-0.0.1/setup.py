39b20bf8863f0c27e65c1149fcd5ff566e8b9be4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:42 +0000

    Add package pubsub-bigdoc/0.0.1/pubsub-bigdoc-0.0.1.tar.gz

---
+++ packages/pubsub-bigdoc/pubsub-bigdoc-0.0.1.tar.gz/pubsub-bigdoc-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pubsub-bigdoc",
+    version="0.0.1",
+    packages=["pubsub-bigdoc"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
