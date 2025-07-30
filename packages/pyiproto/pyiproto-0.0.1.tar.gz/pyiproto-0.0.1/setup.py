83f94160fcedd8e178fda0fea9c78f2395aa3a8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:09 +0000

    Add package pyiproto/0.0.1/pyiproto-0.0.1.tar.gz

---
+++ packages/pyiproto/pyiproto-0.0.1.tar.gz/pyiproto-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pyiproto",
+    version="0.0.1",
+    packages=["pyiproto"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
