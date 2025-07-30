141158d50126c3835871f4e3d68170cdebcc0ec9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:13 +0000

    Add package ajax-datatable/0.0.1/ajax-datatable-0.0.1.tar.gz

---
+++ packages/ajax-datatable/ajax-datatable-0.0.1.tar.gz/ajax-datatable-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ajax-datatable",
+    version="0.0.1",
+    packages=["ajax-datatable"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
