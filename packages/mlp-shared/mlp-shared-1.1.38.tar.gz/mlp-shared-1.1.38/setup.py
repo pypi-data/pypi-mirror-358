69d85ae0eb6f678f96f7fb4e686f5d6fcccaad34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:10 +0000

    Add package mlp-shared/1.1.38/mlp-shared-1.1.38.tar.gz

---
+++ packages/mlp-shared/mlp-shared-1.1.38.tar.gz/mlp-shared-1.1.38/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="mlp-shared",
+    version="1.1.38",
+    packages=["mlp-shared"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
