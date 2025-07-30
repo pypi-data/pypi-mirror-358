97ea2a127107f1568a4a00be8a2ad8781fb4555d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:20 +0000

    Add package webdav-storage/0.0.1/webdav-storage-0.0.1.tar.gz

---
+++ packages/webdav-storage/webdav-storage-0.0.1.tar.gz/webdav-storage-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="webdav-storage",
+    version="0.0.1",
+    packages=["webdav-storage"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
