e82138f3324d5976543510bd885882cb14255bd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:28 +0000

    Add package acryl/0.0.1/acryl-0.0.1.tar.gz

---
+++ packages/acryl/acryl-0.0.1.tar.gz/acryl-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="acryl",
+    version="0.0.1",
+    packages=["acryl"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
