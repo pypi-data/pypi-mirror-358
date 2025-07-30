d961f21b0d8599aa1b37f50474e8519fe2801008
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:29 +0000

    Add package notify-service/0.0.1/notify-service-0.0.1.tar.gz

---
+++ packages/notify-service/notify-service-0.0.1.tar.gz/notify-service-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="notify-service",
+    version="0.0.1",
+    packages=["notify-service"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
