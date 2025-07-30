56adfde6eff5ea46d49b81e324a1f732ad7c3a1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:41 +0000

    Add package beeai-cli/0.2.14/beeai_cli-0.2.14-py3-none-any.whl

---
+++ packages/beeai-cli/beeai_cli-0.2.14-py3-none-any.whl/beeai_cli/configuration.py
@@ -0,0 +1,32 @@
+# Copyright 2025 © BeeAI a Series of LF Projects, LLC
+# SPDX-License-Identifier: Apache-2.0
+
+import functools
+import pathlib
+
+import pydantic
+import pydantic_settings
+from pydantic import SecretStr
+
+
+@functools.cache
+class Configuration(pydantic_settings.BaseSettings):
+    model_config = pydantic_settings.SettingsConfigDict(
+        env_file=None, env_prefix="BEEAI__", env_nested_delimiter="__", extra="allow"
+    )
+    host: pydantic.AnyUrl = "http://localhost:8333"
+    playground: str = "playground"
+    debug: bool = False
+    home: pathlib.Path = pathlib.Path.home() / ".beeai"
+    agent_registry: pydantic.AnyUrl = (
+        "https://github.com/i-am-bee/beeai-platform@release-v0.2.14#path=agent-registry.yaml"
+    )
+    admin_password: SecretStr | None = None
+
+    @property
+    def lima_home(self) -> pathlib.Path:
+        return self.home / "lima"
+
+    @property
+    def docker_home(self) -> pathlib.Path:
+        return self.home / "docker"
