83ea1f14b26cd0a68da04d32b311b3aa79cf3bd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:32 +0000

    Add package log4j-api/2.23.1/log4j-api-2.23.1.tar.gz

---
+++ packages/log4j-api/log4j-api-2.23.1.tar.gz/log4j-api-2.23.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="log4j-api",
+    version="2.23.1",
+    packages=["log4j-api"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
