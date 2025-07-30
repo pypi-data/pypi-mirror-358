26aaa24c13500864931612f260e2ad055eb2eb7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:05:08 +0000

    Add package scrooge-sqlalchemy-orm-models/0.0.18/scrooge-sqlalchemy-orm-models-0.0.18.tar.gz

---
+++ packages/scrooge-sqlalchemy-orm-models/scrooge-sqlalchemy-orm-models-0.0.18.tar.gz/scrooge-sqlalchemy-orm-models-0.0.18/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="scrooge-sqlalchemy-orm-models",
+    version="0.0.18",
+    packages=["scrooge-sqlalchemy-orm-models"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
