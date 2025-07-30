c6050799dda714b6bf7e198bb94dff691ae10c9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:06 +0000

    Add package ProstoQuotaManagerClientSync/0.9.0/ProstoQuotaManagerClientSync-0.9.0.tar.gz

---
+++ packages/ProstoQuotaManagerClientSync/ProstoQuotaManagerClientSync-0.9.0.tar.gz/ProstoQuotaManagerClientSync-0.9.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ProstoQuotaManagerClientSync",
+    version="0.9.0",
+    packages=["ProstoQuotaManagerClientSync"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
