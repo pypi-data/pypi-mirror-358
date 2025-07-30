dbbfdc49ded2ab76bc6df21254158dcfc6bb9138
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:17 +0000

    Add package pypo-old/0.0.1/pypo-old-0.0.1.tar.gz

---
+++ packages/pypo-old/pypo-old-0.0.1.tar.gz/pypo-old-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pypo-old",
+    version="0.0.1",
+    packages=["pypo-old"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
