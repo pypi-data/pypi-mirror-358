2f04f04571ccd35d63e674bac0c9861e9de93570
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:35:55 +0000

    Add package calendar_event_engine/0.1.2/calendar_event_engine-0.1.2.tar.gz

---
+++ packages/calendar_event_engine/calendar_event_engine-0.1.2.tar.gz/calendar_event_engine-0.1.2/calendar_event_engine/publishers/mobilizon/uploader.py
@@ -0,0 +1,137 @@
+import json
+import os
+
+import validators
+from calendar_event_engine.db_cache import UploadedEventRow, UploadSource, SQLiteDB
+from calendar_event_engine.logger import create_logger_from_designated_logger
+from calendar_event_engine.publishers.abc_publisher import Publisher
+from calendar_event_engine.publishers.mobilizon.api import MobilizonAPI
+from calendar_event_engine.publishers.mobilizon.types import MobilizonEvent, EventParameters
+from calendar_event_engine.types.generics import GenericEvent
+from calendar_event_engine.types.submission import EventsToUploadFromCalendarID
+
+logger = create_logger_from_designated_logger(__name__)
+
+def none_if_not_present(x, dictionary):
+    return None if x not in dictionary else dictionary[x]
+
+class MobilizonUploader(Publisher):
+
+    mobilizonAPI: MobilizonAPI
+    cache_db: SQLiteDB
+    test_mode: bool
+    fakeUUIDForTests = 0
+
+    def __init__(self, test_mode, cache_db):
+        self.cache_db = cache_db
+        self.testMode = test_mode
+
+    def close(self):
+        if not self.testMode:
+            self.mobilizonAPI.logout()
+
+    def update(self) -> None:
+        pass
+
+    def monitor(self) -> None:
+        pass
+
+    def upload(self, groups_events_to_upload: list[EventsToUploadFromCalendarID]):
+        for events_to_upload in groups_events_to_upload:
+            all_events = events_to_upload.events
+            event_kernel = events_to_upload.eventKernel
+            source_id = events_to_upload.calendar_id
+            for generic_event in all_events:
+                event: MobilizonEvent = self.generic_event_converter(generic_event)
+                upload_response: dict = {}
+                try:
+                    if not self.cache_db.entry_already_in_cache(event.beginsOn, event.title, source_id):
+                        if self.testMode:
+                            self.fakeUUIDForTests += 1
+                            upload_response = {"id": 1, "uuid": self.fakeUUIDForTests}
+                        else:
+                            if event.picture is not None and validators.url(event.picture.mediaId):
+                                potential_id = self.mobilizonAPI.upload_file(event.picture.mediaId)
+                                if potential_id != "":
+                                    event.picture.mediaId = potential_id
+                            upload_response = self.mobilizonAPI.bot_created_event(event)
+                        logger.info(f"{event.title}: {upload_response}")
+
+                        upload_row = UploadedEventRow(uuid=upload_response["uuid"], id=upload_response["id"],
+                                                      title=event.title, date=event.beginsOn,
+                                                      group_id=event.attributedToId, group_name=event_kernel.group_name)
+                        upload_source = UploadSource(uuid=upload_response["uuid"], website_url=event.onlineAddress,
+                                                     source=source_id, source_type=event_kernel.scraper_type.value)
+                        self.cache_db.insert_uploaded_event(upload_row, upload_source)
+                except Exception as e:
+                    logger.error(f"Unable to upload the following event: {event}", e)
+
+
+    def connect(self):
+        if not self.testMode:
+            endpoint = os.environ.get("MOBILIZON_ENDPOINT")
+            email = os.environ.get("MOBILIZON_EMAIL")
+            passwd = os.environ.get("MOBILIZON_PASSWORD")
+
+            if email is None and passwd is None:
+                login_file_path = os.environ.get("MOBILIZON_LOGIN_FILE")
+                with open(login_file_path, "r") as f:
+                    secrets = json.load(f)
+                    email = secrets["email"]
+                    passwd = secrets["password"]
+
+            self.mobilizonAPI = MobilizonAPI(endpoint, email, passwd)
+
+    def generic_event_converter(self, generic_event: GenericEvent):
+        mobilizon_metadata = generic_event.publisher_specific_info["mobilizon"]
+        category = None if "defaultCategory" not in mobilizon_metadata else EventParameters.Categories[mobilizon_metadata["defaultCategory"]]
+
+        if validators.url(generic_event.picture):
+            mobilizon_picture = EventParameters.MediaInput(generic_event.picture)
+        else:
+            mobilizon_picture = None if "defaultImageID" not in mobilizon_metadata else EventParameters.MediaInput(mobilizon_metadata["defaultImageID"])
+
+
+        mobilizon_tags = None if "defaultTags" not in mobilizon_metadata else mobilizon_metadata["defaultTags"]
+        generic_physical_address = generic_event.physical_address
+        mobilizon_physical_address = None if generic_physical_address == None else EventParameters.Address(locality=generic_physical_address.locality,
+                                                             postalCode=generic_physical_address.postalCode,
+                                                             street=generic_physical_address.street,
+                                                             geom=generic_physical_address.geom,
+                                                             country=generic_physical_address.country,
+                                                             region=generic_physical_address.region,
+                                                             timezone=generic_physical_address.timezone,
+                                                             description=generic_physical_address.description)
+        mobilizon_event = MobilizonEvent(mobilizon_metadata["groupID"], generic_event.title,
+                                         description=generic_event.description, beginsOn=generic_event.begins_on,
+                                         endsOn=generic_event.ends_on, tags=mobilizon_tags,
+                                         onlineAddress=generic_event.online_address, physicalAddress=mobilizon_physical_address,
+                                         category=category, picture=mobilizon_picture)
+        return mobilizon_event
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ packages/calendar_event_engine/calendar_event_engine-0.1.2.tar.gz/calendar_event_engine-0.1.2/calendar_event_engine/scrapers/google_calendar/api.py
@@ -0,0 +1,178 @@
+import copy
+import os
+from datetime import datetime, timedelta
+
+import google.auth
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build, Resource
+from googleapiclient.errors import HttpError
+
+from calendar_event_engine.logger import create_logger_from_designated_logger
+from calendar_event_engine.types.generics import GenericAddress, GenericEvent
+from calendar_event_engine.utils.location import find_geolocation_from_address
+
+logger = create_logger_from_designated_logger(__name__)
+
+# Subscribe to the calendars
+# https://webapps.stackexchange.com/questions/5217/how-can-i-find-the-subscribe-url-from-the-google-calendar-embed-source-code
+# http://www.google.com/calendar/feeds/HERE/public/basic
+
+# Bike Shop Source 
+# bsbc.co_c4dt5esnmutedv7p3nu01aerhk@group.calendar.google.com
+
+# Save The Sound Source
+# ctenvironment@gmail.com
+
+# Google API Documentation
+# https://console.cloud.google.com/apis/credentials/consent
+# https://developers.google.com/calendar/api/quickstart/python
+# https://developers.google.com/resources/api-libraries/documentation/calendar/v3/python/latest/calendar_v3.events.html#list
+
+
+class ExpiredToken(Exception):
+    pass
+
+
+class GCalAPI:
+    _apiClient: Resource
+    
+    def __init__(self):
+        pass
+    
+    def init_calendar_read_client_browser(self, token_path: str):
+        SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
+        credentialTokens = None
+        if os.path.exists(token_path):
+            credentialTokens = Credentials.from_authorized_user_file(token_path, SCOPES)
+        
+        if not credentialTokens or not credentialTokens.valid:
+            try: 
+                if credentialTokens and credentialTokens.expired and credentialTokens.refresh_token:
+                    credentialTokens.refresh(Request())
+                else:
+                    flow = InstalledAppFlow.from_client_secrets_file(
+                            f"{os.getcwd()}/config/OAuthClientApp.json", SCOPES
+                        )
+                    credentialTokens = flow.run_local_server(port=9000)
+                
+                # When refreshed authentication token needs to be re-written, and if authenticating for the first time it needs to be just written
+                with open(token_path, "w") as tokenFile:
+                    tokenFile.write(credentialTokens.to_json())
+            except Exception:
+                raise ExpiredToken
+        self._apiClient = build("calendar", "v3", credentials=credentialTokens)
+        logger.info("Logged in Google Cal Browser")
+
+    
+    def init_calendar_read_client_adc(self):
+        logger.info("Logged In Google Cal ADC")
+        credentials, projectID = google.auth.default()
+        
+        self._apiClient = build("calendar", "v3", credentials=credentials)
+    
+
+    def getAllEventsAWeekFromNow(self, eventKernel: GenericEvent, calendarId: str,
+                                 checkCacheFunction,
+                                 dateOfLastEventScraped: datetime = None) -> list[GenericEvent]:
+        """Get events all events for that specific calender a week from today.
+
+        Args:
+            service (Resource): Calender resource available from creating a client with 'read calender' scope
+            calendarId (str): UUID of calender. Same ID used to subscribe to a calender
+            dateOfLastEventScraped (str, optional): UTC date with timedelta following isoformat. Have to add 'Z' to isoformat to indicate its UTC time. Defaults to None.
+        """
+        try:
+            # Call the Calendar API, 'Z' indicates UTC time
+            stringDateLastEvent = datetime.utcnow().astimezone().isoformat()
+            if dateOfLastEventScraped is not None: 
+                stringDateLastEvent = dateOfLastEventScraped.isoformat()
+            logger.debug(f"Time of last event: {stringDateLastEvent}")
+            weekFromNow = datetime.utcnow().astimezone() + timedelta(days=7)
+            weekFromNow = weekFromNow.isoformat()
+            
+            events_result = (
+                self._apiClient.events()
+                .list(
+                    calendarId=calendarId,
+                    timeMin=stringDateLastEvent,
+                    timeMax=weekFromNow,
+                    singleEvents=True,
+                    orderBy="startTime",
+                )
+                .execute()
+            )
+            googleEvents = events_result.get("items", [])
+            if len(googleEvents) == 0:
+                logger.info(f"No upcoming events for calendarID {calendarId}\n")
+                return googleEvents
+
+            events = []
+            for googleEvent in googleEvents:
+                _process_google_event(googleEvent=googleEvent, eventsToUpload=events,
+                                    checkCacheForEvent=checkCacheFunction,calendarId=calendarId,
+                                    eventKernel=copy.deepcopy(eventKernel))
+            
+            return events
+        except HttpError as error:
+            logger.error(f"An error occurred: {error}")
+    
+    def close(self):
+        self._apiClient.close()
+
+
+
+
+
+def _process_google_event(googleEvent: dict, eventsToUpload: [], checkCacheForEvent, 
+                          calendarId: str, eventKernel: GenericEvent):
+    
+    starTimeGoogleEvent = googleEvent["start"].get("dateTime")
+    endTimeGooglEvent = googleEvent["end"].get("dateTime")
+    title = googleEvent.get("summary")
+    description = googleEvent.get("description")
+
+    
+    if None not in [starTimeGoogleEvent, endTimeGooglEvent, title, description]:
+        startDateTime = datetime.fromisoformat(starTimeGoogleEvent.replace('Z', '+00:00')).astimezone()
+        endDateTime = datetime.fromisoformat(endTimeGooglEvent.replace('Z', '+00:00')).astimezone()
+        if not checkCacheForEvent(startDateTime.isoformat(), title, calendarId):
+            eventAddress, default_location_notif = find_geolocation_from_address(parse_google_location(googleEvent.get("location"), eventKernel.physical_address), eventKernel.physical_address, title)
+            eventKernel.begins_on = startDateTime.isoformat()
+            eventKernel.ends_on = endDateTime.isoformat()
+            eventKernel.physical_address = eventAddress
+            eventKernel.title = title
+            eventKernel.description = f"Automatically scraped by Event Bot {default_location_notif}: \n\n{description}"
+            eventsToUpload.append(eventKernel)
+            
+
+def parse_google_location(location:str, default_location: GenericAddress):
+    if location is None:
+        logger.debug("No location provided, using default")
+        return default_location
+    tokens = location.split(",")
+    address: GenericAddress
+    match len(tokens):
+        case 1:
+            return default_location
+        case 2:
+            return default_location
+        case 3:
+            address = GenericAddress(locality=tokens[0], postalCode=tokens[1], street="", country=tokens[2])
+        case 4:
+            address = GenericAddress(locality=tokens[1], postalCode=tokens[2], street=tokens[0], country=tokens[3])
+        case 5:
+            address = GenericAddress(locality=tokens[2], postalCode=tokens[3], street=tokens[1], country=tokens[4])
+        case _:
+            return default_location
+
+    return address
+
+    
+
+if __name__ == "__main__":
+    gcal = GCalAPI()
+    google_token_path = os.environ.get("GOOGLE_API_TOKEN_PATH")
+    gcal.init_calendar_read_client_browser(google_token_path)
+
