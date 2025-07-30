76f3721c1e2c8e97200e0db652def346f42083a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:05:48 +0000

    Add package scrooge-plugin-volume-snapshot-20220628/1.0.4/scrooge-plugin-volume-snapshot-20220628-1.0.4.tar.gz

---
+++ packages/scrooge-plugin-volume-snapshot-20220628/scrooge-plugin-volume-snapshot-20220628-1.0.4.tar.gz/scrooge-plugin-volume-snapshot-20220628-1.0.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="scrooge-plugin-volume-snapshot-20220628",
+    version="1.0.4",
+    packages=["scrooge-plugin-volume-snapshot-20220628"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
