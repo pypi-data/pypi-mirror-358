90e5030573b2b2d44c75d9a0da31f0c9bc273abb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:13 +0000

    Add package ok_tasks_text/1.0.16/ok_tasks_text-1.0.16.tar.gz

---
+++ packages/ok_tasks_text/ok_tasks_text-1.0.16.tar.gz/ok_tasks_text-1.0.16/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ok_tasks_text",
+    version="1.0.16",
+    packages=["ok_tasks_text"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
