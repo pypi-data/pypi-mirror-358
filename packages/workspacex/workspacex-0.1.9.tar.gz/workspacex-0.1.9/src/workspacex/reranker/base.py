6625546a1160c2d4ece0f56685b29fb7c0b7a621
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:19:36 +0000

    Add package workspacex/0.1.9/workspacex-0.1.9.tar.gz

---
+++ packages/workspacex/workspacex-0.1.9.tar.gz/workspacex-0.1.9/src/workspacex/reranker/base.py
@@ -0,0 +1,38 @@
+from abc import ABC, abstractmethod
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
+from workspacex.artifact import Artifact
+
+class RerankConfig(BaseModel):
+    base_url: str = Field(..., description="Base URL")
+    api_key: Optional[str] = Field(default=None, description="API key")
+    model_name: Optional[str] = Field(default=None, description="Model name")
+    score_threshold: Optional[float] = Field(default=0.5, description="Score threshold")
+    top_n: Optional[int] = Field(default=10, description="Top n")
+
+class RerankResult(BaseModel):
+    artifact: Artifact = Field(..., description="Artifact")
+    score: float = Field(..., description="Score")
+
+class BaseRerankRunner(ABC):
+    @abstractmethod
+    def run(
+        self,
+        query: str,
+        documents: list[Artifact],
+        score_threshold: Optional[float] = None,
+        top_n: Optional[int] = None,
+        user: Optional[str] = None,
+    ) -> list[RerankResult]:
+        """
+        Run rerank model
+        :param query: search query
+        :param documents: documents for reranking
+        :param score_threshold: score threshold
+        :param top_n: top n
+        :param user: unique user id if needed
+        :return:
+        """
+        raise NotImplementedError
