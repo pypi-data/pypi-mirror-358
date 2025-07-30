8a435a7535a93fadca993030c0068febe6f937bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:37 +0000

    Add package pybind-fclr/0.0.1/pybind-fclr-0.0.1.tar.gz

---
+++ packages/pybind-fclr/pybind-fclr-0.0.1.tar.gz/pybind-fclr-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pybind-fclr",
+    version="0.0.1",
+    packages=["pybind-fclr"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
