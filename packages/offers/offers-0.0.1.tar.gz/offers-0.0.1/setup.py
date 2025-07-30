7308286315f19c2685610c34605f01699557f559
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:27 +0000

    Add package offers/0.0.1/offers-0.0.1.tar.gz

---
+++ packages/offers/offers-0.0.1.tar.gz/offers-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="offers",
+    version="0.0.1",
+    packages=["offers"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
