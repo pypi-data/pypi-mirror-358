7b2338169b227eaaf5d84b6b61e798e8dc1d9696
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:07 +0000

    Add package pl-bolts/0.0.1/pl-bolts-0.0.1.tar.gz

---
+++ packages/pl-bolts/pl-bolts-0.0.1.tar.gz/pl-bolts-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pl-bolts",
+    version="0.0.1",
+    packages=["pl-bolts"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
