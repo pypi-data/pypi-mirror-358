871ac6128f4cd09901675c9de7af4fc2a18176f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:41 +0000

    Add package pybind-gbot/0.0.1/pybind-gbot-0.0.1.tar.gz

---
+++ packages/pybind-gbot/pybind-gbot-0.0.1.tar.gz/pybind-gbot-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pybind-gbot",
+    version="0.0.1",
+    packages=["pybind-gbot"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
