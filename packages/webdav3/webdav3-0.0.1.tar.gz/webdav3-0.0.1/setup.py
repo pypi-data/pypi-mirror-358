318f92f21fef6f32aa4cea86bf06d57b8d971e6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:18 +0000

    Add package webdav3/0.0.1/webdav3-0.0.1.tar.gz

---
+++ packages/webdav3/webdav3-0.0.1.tar.gz/webdav3-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="webdav3",
+    version="0.0.1",
+    packages=["webdav3"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
