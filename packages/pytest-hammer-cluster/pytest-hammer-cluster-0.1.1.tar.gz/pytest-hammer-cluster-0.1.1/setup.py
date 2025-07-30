b1362cfa0b76bb2ff0d0bcbb832a76fd2346fbaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:10 +0000

    Add package pytest-hammer-cluster/0.1.1/pytest-hammer-cluster-0.1.1.tar.gz

---
+++ packages/pytest-hammer-cluster/pytest-hammer-cluster-0.1.1.tar.gz/pytest-hammer-cluster-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pytest-hammer-cluster",
+    version="0.1.1",
+    packages=["pytest-hammer-cluster"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
