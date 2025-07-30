3170e866b3bd49a6fb7025781c45e24fa9a79b66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:39 +0000

    Add package dl-db-testing/0.0.1/dl-db-testing-0.0.1.tar.gz

---
+++ packages/dl-db-testing/dl-db-testing-0.0.1.tar.gz/dl-db-testing-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dl-db-testing",
+    version="0.0.1",
+    packages=["dl-db-testing"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
