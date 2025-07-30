351b8e24f97a583221b585eb7ff843130d0d64a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:13 +0000

    Add package julep/2.15.2/julep-2.15.2.tar.gz

---
+++ packages/julep/julep-2.15.2.tar.gz/julep-2.15.2/src/julep/types/shared_params/api_call_def.py
@@ -0,0 +1,40 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import Dict, Union, Optional
+from typing_extensions import Literal, Required, TypedDict
+
+from .secret_ref import SecretRef
+
+__all__ = ["APICallDef"]
+
+
+class APICallDef(TypedDict, total=False):
+    method: Required[Literal["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS", "CONNECT", "TRACE"]]
+
+    url: Required[str]
+
+    content: Optional[str]
+
+    cookies: Optional[Dict[str, str]]
+
+    data: Optional[object]
+
+    files: Optional[object]
+
+    follow_redirects: Optional[bool]
+
+    headers: Optional[Dict[str, str]]
+
+    include_response_content: bool
+
+    json: Optional[object]
+
+    params: Union[str, object, None]
+
+    schema: Optional[object]
+
+    secrets: Optional[Dict[str, SecretRef]]
+
+    timeout: Optional[int]
