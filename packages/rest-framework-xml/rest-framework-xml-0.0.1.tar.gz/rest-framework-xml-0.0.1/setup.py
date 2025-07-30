b55f32994294c4fa78d5e44bbb3f7bf52edbd74b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:09 +0000

    Add package rest-framework-xml/0.0.1/rest-framework-xml-0.0.1.tar.gz

---
+++ packages/rest-framework-xml/rest-framework-xml-0.0.1.tar.gz/rest-framework-xml-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="rest-framework-xml",
+    version="0.0.1",
+    packages=["rest-framework-xml"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
