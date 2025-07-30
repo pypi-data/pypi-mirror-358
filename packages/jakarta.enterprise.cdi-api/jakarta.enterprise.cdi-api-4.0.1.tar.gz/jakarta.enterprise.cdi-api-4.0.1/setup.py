b89243b7fad478750846ae517fec347967271d1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:09 +0000

    Add package jakarta.enterprise.cdi-api/4.0.1/jakarta.enterprise.cdi-api-4.0.1.tar.gz

---
+++ packages/jakarta.enterprise.cdi-api/jakarta.enterprise.cdi-api-4.0.1.tar.gz/jakarta.enterprise.cdi-api-4.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="jakarta.enterprise.cdi-api",
+    version="4.0.1",
+    packages=["jakarta.enterprise.cdi-api"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
