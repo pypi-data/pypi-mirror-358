40a4e50a3553a6a514f77992c6dc975b4bc30840
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:28 +0000

    Add package jakarta.activation-api/2.1.3/jakarta.activation-api-2.1.3.tar.gz

---
+++ packages/jakarta.activation-api/jakarta.activation-api-2.1.3.tar.gz/jakarta.activation-api-2.1.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="jakarta.activation-api",
+    version="2.1.3",
+    packages=["jakarta.activation-api"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
