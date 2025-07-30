2699db7beedf8891046493483023ae5b19e18e32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:57 +0000

    Add package pypo-ya-market/0.0.1/pypo-ya-market-0.0.1.tar.gz

---
+++ packages/pypo-ya-market/pypo-ya-market-0.0.1.tar.gz/pypo-ya-market-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pypo-ya-market",
+    version="0.0.1",
+    packages=["pypo-ya-market"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
