bd85c4f5e24feae7bc30efeba9695eee8ec59c7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:20 +0000

    Add package dppclient/1.0.5/dppclient-1.0.5.tar.gz

---
+++ packages/dppclient/dppclient-1.0.5.tar.gz/dppclient-1.0.5/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dppclient",
+    version="1.0.5",
+    packages=["dppclient"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
