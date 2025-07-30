6a49db00191069fbf9846de0520c1ca2da1a45e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:36 +0000

    Add package datalens-connector-oracle/0.0.1/datalens-connector-oracle-0.0.1.tar.gz

---
+++ packages/datalens-connector-oracle/datalens-connector-oracle-0.0.1.tar.gz/datalens-connector-oracle-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="datalens-connector-oracle",
+    version="0.0.1",
+    packages=["datalens-connector-oracle"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
