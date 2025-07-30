73eed2416cef3dbcbe18e7d917f0990e008c2419
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:07:00 +0000

    Add package vkpy-ranges/1.0.0/vkpy-ranges-1.0.0.tar.gz

---
+++ packages/vkpy-ranges/vkpy-ranges-1.0.0.tar.gz/vkpy-ranges-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="vkpy-ranges",
+    version="1.0.0",
+    packages=["vkpy-ranges"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
