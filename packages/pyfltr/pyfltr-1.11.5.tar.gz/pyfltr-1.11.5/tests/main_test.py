bf8de30f5c247ded2f2bb1c428618c8e99c1ee2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:31 +0000

    Add package pyfltr/1.11.5/pyfltr-1.11.5.tar.gz

---
+++ packages/pyfltr/pyfltr-1.11.5.tar.gz/pyfltr-1.11.5/tests/main_test.py
@@ -0,0 +1,25 @@
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-function-docstring
+
+import pathlib
+import subprocess
+
+import pytest
+
+import pyfltr.main
+
+
+@pytest.mark.parametrize("mode", ["run", "ci", "pre-commit"])
+def test_success(mocker, mode):
+    proc = subprocess.CompletedProcess(["test"], returncode=0, stdout="test")
+    mocker.patch("subprocess.run", return_value=proc)
+    returncode = pyfltr.main.run([mode, str(pathlib.Path(__file__).parent.parent)])
+    assert returncode == 0
+
+
+@pytest.mark.parametrize("mode", ["run", "ci", "pre-commit"])
+def test_fail(mocker, mode):
+    proc = subprocess.CompletedProcess(["test"], returncode=-1, stdout="test")
+    mocker.patch("subprocess.run", return_value=proc)
+    returncode = pyfltr.main.run([mode, str(pathlib.Path(__file__).parent.parent)])
+    assert returncode == 1
