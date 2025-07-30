a9db74ea86586af53407108f56206a86a72c0b56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:23 +0000

    Add package fuse-gtest-files/0.0.1/fuse-gtest-files-0.0.1.tar.gz

---
+++ packages/fuse-gtest-files/fuse-gtest-files-0.0.1.tar.gz/fuse-gtest-files-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="fuse-gtest-files",
+    version="0.0.1",
+    packages=["fuse-gtest-files"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
