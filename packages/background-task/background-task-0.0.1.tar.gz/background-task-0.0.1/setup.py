ffcde7437d4b0148f799676d1b2f00d08e4b78e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:31 +0000

    Add package background-task/0.0.1/background-task-0.0.1.tar.gz

---
+++ packages/background-task/background-task-0.0.1.tar.gz/background-task-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="background-task",
+    version="0.0.1",
+    packages=["background-task"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
