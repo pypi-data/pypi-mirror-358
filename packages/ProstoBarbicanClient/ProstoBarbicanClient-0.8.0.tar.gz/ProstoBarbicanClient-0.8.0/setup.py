02f31d4e43fe6f4da7c564650fb14fa427a722c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:52 +0000

    Add package ProstoBarbicanClient/0.8.0/ProstoBarbicanClient-0.8.0.tar.gz

---
+++ packages/ProstoBarbicanClient/ProstoBarbicanClient-0.8.0.tar.gz/ProstoBarbicanClient-0.8.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ProstoBarbicanClient",
+    version="0.8.0",
+    packages=["ProstoBarbicanClient"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
