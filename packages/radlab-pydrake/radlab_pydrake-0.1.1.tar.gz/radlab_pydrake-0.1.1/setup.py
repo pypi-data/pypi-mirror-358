ff3bc9f928d6e64b725da25d1a7e208b14071eb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:45:02 +0000

    Add package radlab-pydrake/0.1.1/radlab_pydrake-0.1.1.tar.gz

---
+++ packages/radlab-pydrake/radlab_pydrake-0.1.1.tar.gz/radlab_pydrake-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='radlab_pydrake',
+    version='0.1.1',
+    packages=find_packages(),
+    install_requires=[],
+    author='Your Name',
+    author_email='your@email.com',
+    description='A simple custom package',
+    url='https://github.com/yourusername/my_package',  # Optional
+)
