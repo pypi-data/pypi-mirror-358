ae4bd7f590d13ab59cfe6cf143e57a9cbbdfce7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:57 +0000

    Add package pyqclassify/1.5.8/pyqclassify-1.5.8.tar.gz

---
+++ packages/pyqclassify/pyqclassify-1.5.8.tar.gz/pyqclassify-1.5.8/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pyqclassify",
+    version="1.5.8",
+    packages=["pyqclassify"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
