4b35fc2201f9de269fbde4481d111f6c135966d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:04 +0000

    Add package nccl-p2p-cuda/0.0.1/nccl-p2p-cuda-0.0.1.tar.gz

---
+++ packages/nccl-p2p-cuda/nccl-p2p-cuda-0.0.1.tar.gz/nccl-p2p-cuda-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="nccl-p2p-cuda",
+    version="0.0.1",
+    packages=["nccl-p2p-cuda"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
