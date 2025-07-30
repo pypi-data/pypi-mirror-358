4af8e3b7709a1430e89f17e61ec163f2de512936
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:31 +0000

    Add package jobset/0.0.1/jobset-0.0.1.tar.gz

---
+++ packages/jobset/jobset-0.0.1.tar.gz/jobset-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="jobset",
+    version="0.0.1",
+    packages=["jobset"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
