3ab637ceeb83d86eb0ace7c6dc453db1e5ba3980
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:05:15 +0000

    Add package datalens-maintenance/0.0.1/datalens-maintenance-0.0.1.tar.gz

---
+++ packages/datalens-maintenance/datalens-maintenance-0.0.1.tar.gz/datalens-maintenance-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="datalens-maintenance",
+    version="0.0.1",
+    packages=["datalens-maintenance"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
