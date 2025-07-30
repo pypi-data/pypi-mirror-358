aab3b99e9188c3a46c61af6c93e66b515696f17a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:07:00 +0000

    Add package it-shop/0.0.1/it-shop-0.0.1.tar.gz

---
+++ packages/it-shop/it-shop-0.0.1.tar.gz/it-shop-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="it-shop",
+    version="0.0.1",
+    packages=["it-shop"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
