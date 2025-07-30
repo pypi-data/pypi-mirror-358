0c62572be900ac4367510cb2d8071dd01aef3daf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:49 +0000

    Add package nitypes/0.1.0.dev3/nitypes-0.1.0.dev3.tar.gz

---
+++ packages/nitypes/nitypes-0.1.0.dev3.tar.gz/nitypes-0.1.0.dev3/src/nitypes/waveform/_digital/_types.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+from typing import TypeVar, Union
+
+import numpy as np
+from typing_extensions import TypeAlias
+
+__all__ = [
+    "_AnyPort",
+    "_AnyState",
+    "_TState",
+    "_TOtherState",
+    "_DIGITAL_PORT_DTYPES",
+    "_DIGITAL_STATE_DTYPES",
+]
+
+_AnyPort: TypeAlias = Union[np.uint8, np.uint16, np.uint32]
+
+# np.byte == np.int8, np.ubyte == np.uint8
+_AnyState: TypeAlias = Union[np.bool, np.int8, np.uint8]
+_TState = TypeVar("_TState", bound=_AnyState)
+_TOtherState = TypeVar("_TOtherState", bound=_AnyState)
+
+_DIGITAL_PORT_DTYPES = (np.uint8, np.uint16, np.uint32)
+_DIGITAL_STATE_DTYPES = (np.bool, np.int8, np.uint8)
