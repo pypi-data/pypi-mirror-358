43580e8110af358a7a2c41d4e3352a6d8326d014
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:30 +0000

    Add package walytis-beta-api/2.4.8/walytis_beta_api-2.4.8-py3-none-any.whl

---
+++ packages/walytis-beta-api/walytis_beta_api-2.4.8-py3-none-any.whl/walytis_beta_api/versions.py
@@ -0,0 +1,8 @@
+"""The versions of the the various Walytis components."""
+from walytis_beta_tools.versions import (
+    WALYTIS_BETA_PROTOCOL_VERSION,
+    WALYTIS_BETA_API_PROTOCOL_VERSION,
+    WALYTIS_BETA_API_VERSION,
+    WALYTIS_BETA_CORE_VERSION,
+)
+
---
+++ packages/walytis-beta-api/walytis_beta_api-2.4.8-py3-none-any.whl/walytis_beta_tools/_experimental/config.py
@@ -0,0 +1,34 @@
+import os
+from ipfs_tk_generics import IpfsClient
+from walytis_beta_tools.log import logger
+
+# initialise IPFS
+USE_IPFS_NODE = os.environ.get("USE_IPFS_NODE", "").lower() in ["true", "1"]
+IPFS_REPO_DIR = os.environ.get("IPFS_REPO_DIR", "")
+ipfs:IpfsClient
+# print("IPFS embedded node:", USE_IPFS_NODE)
+if USE_IPFS_NODE:
+    if IPFS_REPO_DIR:
+        if not os.path.exists(IPFS_REPO_DIR):
+            raise Exception(
+                "The path specified in the environment variable IPFS_REPO_DIR "
+                f"doesn't exist: {IPFS_REPO_DIR}"
+            )
+    else:
+        IPFS_REPO_DIR = os.path.abspath(os.path.join(".ipfs_repo"))
+        if not os.path.exists(IPFS_REPO_DIR):
+            os.makedirs(IPFS_REPO_DIR)
+        os.environ["IPFS_REPO_DIR"]=IPFS_REPO_DIR
+    print(f"IPFS repo: {IPFS_REPO_DIR}")
+    from ipfs_node import IpfsNode
+
+    ipfs = IpfsNode(IPFS_REPO_DIR)
+else:
+    from ipfs_remote import IpfsRemote
+    ipfs = IpfsRemote("localhost:5001")
+    try:
+        ipfs.wait_till_ipfs_is_running(timeout_sec=5)
+    except TimeoutError:
+        logger.warning("IPFS isn't running. Waiting for IPFS to start...")
+        ipfs.wait_till_ipfs_is_running()
+        logger.warning("IPFS running now.")
