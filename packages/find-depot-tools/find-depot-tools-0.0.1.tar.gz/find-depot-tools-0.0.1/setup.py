5d0f5c44024d50d8b6a2cae06095770bc6c28736
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:10 +0000

    Add package find-depot-tools/0.0.1/find-depot-tools-0.0.1.tar.gz

---
+++ packages/find-depot-tools/find-depot-tools-0.0.1.tar.gz/find-depot-tools-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="find-depot-tools",
+    version="0.0.1",
+    packages=["find-depot-tools"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
