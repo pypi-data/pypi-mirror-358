7de7361589e4c22bac64a70c2ba82d4ceaab783b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:06 +0000

    Add package pybind-tree/0.0.1/pybind-tree-0.0.1.tar.gz

---
+++ packages/pybind-tree/pybind-tree-0.0.1.tar.gz/pybind-tree-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pybind-tree",
+    version="0.0.1",
+    packages=["pybind-tree"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
