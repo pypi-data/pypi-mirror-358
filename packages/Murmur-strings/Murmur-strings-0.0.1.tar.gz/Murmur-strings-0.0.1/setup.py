ae8bd96e84d5eb7345520ff1ce0bce49e8e03e98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:41 +0000

    Add package Murmur-strings/0.0.1/Murmur-strings-0.0.1.tar.gz

---
+++ packages/Murmur-strings/Murmur-strings-0.0.1.tar.gz/Murmur-strings-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="Murmur-strings",
+    version="0.0.1",
+    packages=["Murmur-strings"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
