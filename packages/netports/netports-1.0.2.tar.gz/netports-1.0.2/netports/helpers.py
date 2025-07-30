0f3596ae85fb59298947256856dabe143b7cc9d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:10:19 +0000

    Add package netports/1.0.2/netports-1.0.2.tar.gz

---
+++ packages/netports/netports-1.0.2.tar.gz/netports-1.0.2/netports/helpers.py
@@ -0,0 +1,247 @@
+"""Helper functions."""
+
+import re
+import time
+from typing import Any, Iterable
+
+from netports.static import BRIEF_ALL_I, BRIEF_ALL_S, SPLITTER
+from netports.types_ import LAny, LStr, StrInt, IStrInt, LInt, T2Str, T3Str, T4Str
+
+
+# =============================== str ================================
+
+
+def findall1(pattern: str, string: str, flags=0) -> str:
+    """Parse 1st item of re.findall(). If nothing is found, returns an empty string.
+
+    Group with parentheses in pattern is required.
+
+    :return: Interested substring.
+
+    :example:
+        pattern: "a(b)cde"
+        string: "abcde"
+        return: "b"
+    """
+    result = (re.findall(pattern=pattern, string=string, flags=flags) or [""])[0]
+    if isinstance(result, str):
+        return result
+    if isinstance(result, tuple):
+        return result[0]
+    return ""
+
+
+def findall2(pattern: str, string: str, flags=0) -> T2Str:
+    """Parse 2 items of re.findall(). If nothing is found, returns 2 empty strings.
+
+    Group with parentheses in pattern is required.
+
+    :return: Two interested substrings.
+
+    :example:
+        pattern: "a(b)(c)de"
+        string: "abcde"
+        return: "b", "c"
+    """
+    result = (re.findall(pattern=pattern, string=string, flags=flags) or [("", "")])[0]
+    if isinstance(result, tuple) and len(result) >= 2:
+        return result[0], result[1]
+    return "", ""
+
+
+def findall3(pattern: str, string: str, flags=0) -> T3Str:
+    """Parse 3 items of re.findall(). If nothing is found, returns 3 empty strings.
+
+    Group with parentheses in pattern is required.
+
+    :return: Three interested substrings.
+
+    :example:
+        pattern: "a(b)(c)(d)e"
+        string: "abcde"
+        return: "b", "c", "d"
+    """
+    result = (re.findall(pattern=pattern, string=string, flags=flags) or [("", "", "")])[0]
+    if isinstance(result, tuple) and len(result) >= 3:
+        return result[0], result[1], result[2]
+    return "", "", ""
+
+
+def findall4(pattern: str, string: str, flags=0) -> T4Str:
+    """Parse 4 items of re.findall(). If nothing is found, returns 4 empty strings.
+
+    :param pattern: Regex pattern, where 4 groups with parentheses in pattern are required.
+    :param string: String where need to find pattern.
+    :param flags: findall flags.
+    :return: Three interested substrings.
+
+    :example:
+        pattern = "a(b)(c)(d)(e)f"
+        string = "abcdef"
+        return: "b", "c", "d", "e"
+    """
+    result = (re.findall(pattern=pattern, string=string, flags=flags) or [("", "", "", "")])[0]
+    if isinstance(result, tuple) and len(result) >= 4:
+        return result[0], result[1], result[2], result[3]
+    return "", "", "", ""
+
+
+def repr_params(*args, **kwargs) -> str:
+    """Make params for __repr__() method."""
+    args_ = ", ".join([f"{v!r}" for v in args if v])
+    kwargs_ = ", ".join([f"{k}={v!r}" for k, v in kwargs.items() if v])
+    params = [s for s in (args_, kwargs_) if s]
+    return ", ".join(params)
+
+
+def join(items: LAny) -> str:
+    """Join items by ","."""
+    return SPLITTER.join([str(i) for i in items])
+
+
+# =============================== bool ===============================
+
+
+def is_all(**kwargs) -> bool:
+    """Get param `all` from `kwargs`."""
+    return bool(kwargs.get("all"))
+
+
+def is_brief(**kwargs) -> bool:
+    """Get param `verbose` from `kwargs` and reverse the result."""
+    return not is_verbose(**kwargs)
+
+
+def is_brief_in_items(items: Any) -> bool:
+    """Check is "-1" or -1 in `items`, used for verbose=False.
+
+    :param items: str, int, List[str], List[int].
+    :return: True - if "-1" or -1 present in `items`.
+    """
+    if isinstance(items, (str, int)):
+        items = [items]
+    if not isinstance(items, Iterable):
+        raise TypeError(f"{items=} expected {str}")
+    for item in items:
+        if isinstance(item, str):
+            if item == BRIEF_ALL_S:
+                return True
+            continue
+        if isinstance(item, int):
+            if item == BRIEF_ALL_I:
+                return True
+            continue
+        raise TypeError(f"{item=} expected {str}")
+    return False
+
+
+def is_verbose(**kwargs) -> bool:
+    """Get param `verbose` from `kwargs`."""
+    verbose = kwargs.get("verbose")
+    if verbose is None:
+        return False
+    return bool(verbose)
+
+
+def is_strict(**kwargs) -> bool:
+    """Get param `strict` from `kwargs`."""
+    strict = kwargs.get("strict")
+    if strict is None:
+        return True
+    return bool(strict)
+
+
+# =============================== int ================================
+
+
+def to_int(number: StrInt) -> int:
+    """Convert str to int.
+
+    :param number: int or digit as str/
+    :return: int.
+
+    :raises TypeError: If number is not digit.
+    """
+    if isinstance(number, int):
+        return number
+    if isinstance(number, str) and number.isdigit():
+        return int(number)
+    raise TypeError(f"{number=} {int} expected")
+
+
+def to_lint(numbers: IStrInt) -> LInt:
+    """Convert List[str] to List[int].
+
+    :param numbers: List[str].
+    :return: List[int].
+    :raises TypeError: If number is not digit.
+    """
+    if not isinstance(numbers, (list, set, tuple)):
+        raise TypeError(f"{numbers=} {list} expected")
+    return [to_int(s) for s in numbers]
+
+
+# =============================== list ===============================
+
+
+def lstr(items: Any) -> LStr:
+    """Convert any types of items to List[str]."""
+    if not isinstance(items, (bytes, dict, float, int, list, set, str, tuple)):
+        raise TypeError(f"{items=} {list} expected")
+    if isinstance(items, (bytes, float, int, str)):
+        items = [items]
+    items_ = []
+    for item in items:
+        if isinstance(item, bytes):
+            items_.append(item.decode("utf-8"))
+        elif isinstance(item, float):
+            items_.append(str(int(item)))
+        else:
+            items_.append(str(item))
+    return items_
+
+
+def no_dupl(items: Iterable) -> list:
+    """Remove duplicate items from list."""
+    items_ = []
+    for item in items:
+        if item not in items_:
+            items_.append(item)
+    return items_
+
+
+def remove_brief_items(items: Any) -> list:
+    """Remove "-1", -1 from items.
+
+    :param items: str, int, List[str], List[int].
+    :return: Items without "-1", -1.
+    """
+    if isinstance(items, (str, int)):
+        items = [items]
+    return [i for i in items if i not in (BRIEF_ALL_S, BRIEF_ALL_I)]
+
+
+def split(items: Any) -> LStr:
+    """Split items str, List[str] by "," " "."""
+    results: LStr = []
+    for items_s in lstr(items):
+        for item_s in items_s.split():
+            results.extend(item_s.split(SPLITTER))
+    return results
+
+
+# ============================= wrapper ==============================
+
+
+def time_spent(func):
+    """Wrapper measure function execution time."""
+
+    def wrap(*args, **kwargs):
+        """Wrap."""
+        started = time.time()
+        return_ = func(*args, **kwargs)
+        elapsed = time.time() - started
+        print(f"=== {func.__name__}, spent {elapsed:.3f}s ===")
+        return return_
+
+    return wrap
---
+++ packages/netports/netports-1.0.2.tar.gz/netports-1.0.2/netports/ipv4.py
@@ -0,0 +1,213 @@
+"""IPv4 address representation in CIDR notation with host data under mask bits."""
+
+from __future__ import annotations
+
+import re
+from functools import total_ordering
+from ipaddress import IPv4Interface, IPv4Network, IPv4Address
+from typing import List
+
+from pydantic import BaseModel, Field
+from vhelpers import vre
+
+RE_IP = r"\d+\.\d+\.\d+\.\d+"
+
+
+@total_ordering
+class IPv4(BaseModel):
+    """IPv4 address representation in CIDR notation with host data under mask bits."""
+
+    addr: str = Field(description="IPv4 address with host data under mask bits. A.B.C.D/LEN")
+    ip: str = Field(default="", description="IPv4 address without prefixlen, A.B.C.D")
+    prefix: str = Field(default="", description="IPv4 prefix without host data, A.B.C.D/LEN")
+    len: int = Field(default="", description="Prefix length")
+
+    def __init__(self, *args, **kwargs):
+        """Initialize IPv4.
+
+        :param addr: IP address in CIDR notation with host data under mask bits.
+        :param strict: If True, IP must be valid network address (not host address).
+        :raises ValueError: If the CIDR address is invalid
+            or cannot be converted from network with mask format.
+        """
+        addr = _validate_addr(*args, **kwargs)
+        super().__init__(addr=addr)
+        self._interface = IPv4Interface(address=addr)
+        self.ip = str(self._interface.ip)
+        self.prefix = str(self._interface.network)
+        self.len = int(self._interface.network.prefixlen)
+
+    def __repr__(self) -> str:
+        """Representation of the object."""
+        class_ = self.__class__.__name__
+        return f"{class_}('{self.addr}')"
+
+    def __str__(self) -> str:
+        """String representation."""
+        return str(self.addr)
+
+    def __hash__(self) -> int:
+        """Hash value of the object."""
+        return hash(self._interface.network)
+
+    def __eq__(self, other) -> bool:
+        """Check if two objects are equal.
+
+        :param other: Another object to compare.
+        :return: True if objects are equal, False otherwise.
+        """
+        if not isinstance(other, IPv4):
+            return False
+        return self._interface == other._interface
+
+    def __lt__(self, other) -> bool:
+        """Compare two objects.
+
+        :param other: Another object to compare with.
+        """
+        if not isinstance(other, IPv4):
+            return False
+        return self._interface < other._interface
+
+    def __contains__(self, other: IPv4) -> bool:
+        """Check if all IPs in the other subnet are part of this network."""
+        return other._interface.network.subnet_of(self._interface.network)
+
+    # ================================ is ================================
+
+    def is_address(self) -> bool:
+        """True if the address with host data under mask bits, False if the prefix."""
+        if self.len == 32:
+            return True
+        try:
+            IPv4Network(self.addr, strict=True)
+        except ValueError:
+            return True
+        return False
+
+    def is_prefix(self) -> bool:
+        """True if the prefix without host data under mask bits, False if the address."""
+        return not self.is_address()
+
+    def is_global(self) -> bool:
+        """True if the address is defined as globally reachable iana-ipv4-special-registry."""
+        return bool(self._interface.ip.is_global)
+
+    def is_link_local(self) -> bool:
+        """True if the address is reserved for link-local usage. See RFC 3927."""
+        return bool(self._interface.ip.is_link_local)
+
+    def is_loopback(self) -> bool:
+        """True if this is a loopback address. See RFC 3330."""
+        return bool(self._interface.ip.is_loopback)
+
+    def is_multicast(self) -> bool:
+        """True if the address is reserved for multicast use. See RFC 3171."""
+        return bool(self._interface.ip.is_multicast)
+
+    def is_private(self) -> bool:
+        """True if the address is defined as not globally reachable iana-ipv4-special-registry."""
+        return bool(self._interface.ip.is_private)
+
+    def is_reserved(self) -> bool:
+        """True if the address is otherwise IETF reserved."""
+        return bool(self._interface.ip.is_reserved)
+
+    def is_unspecified(self) -> bool:
+        """True if the address is unspecified. See RFC 5735."""
+        return bool(self._interface.ip.is_unspecified)
+
+    # ============================= objects ==============================
+
+    def hostmask(self) -> IPv4Address:
+        """The host mask, as an IPv4Address object."""
+        return self._interface.hostmask
+
+    def netmask(self) -> IPv4Address:
+        """The net mask, as an IPv4Address object."""
+        return self._interface.netmask
+
+    def network(self) -> IPv4Network:
+        """The network, as an IPv4Network object."""
+        return self._interface.network
+
+    # ============================== masks ===============================
+
+    def addr_mask(self, splitter: str = " ") -> str:
+        """Address with the mask in net mask notation, A.B.C.D 255.255.255.0.
+
+        :param splitter: String to split network address and hostmask.
+        :return: Address with hostmask in net mask notation.
+        """
+        return f"{self._interface.ip}{splitter}{self._interface.netmask}"
+
+    def addr_wildcard(self, splitter: str = " ") -> str:
+        """Address with the mask in wildcard mask notation, A.B.C.D 0.0.0.255.
+
+        :param splitter: String to split network address and hostmask.
+        :return: Address with hostmask in wildcard mask notation.
+        """
+        return f"{self._interface.ip}{splitter}{self._interface.hostmask}"
+
+    def net_mask(self, splitter: str = " ") -> str:
+        """Network with the mask in net mask notation, A.B.C.D 255.255.255.0.
+
+        :param splitter: String to split network address and hostmask.
+        :return: Network address with hostmask in net mask notation.
+        """
+        network_address = self._interface.network.network_address
+        netmask = self._interface.network.netmask
+        return f"{network_address}{splitter}{netmask}"
+
+    def net(self) -> str:
+        """Network without prefixlen, A.B.C.D."""
+        return str(self._interface.network.network_address)
+
+    def net_wildcard(self, splitter: str = " ") -> str:
+        """Network with the mask in wildcard mask notation, A.B.C.D 0.0.0.255.
+
+        :param splitter: String to split network address and hostmask.
+        :return: Network address with hostmask in wildcard mask notation.
+        """
+        network_address = self._interface.network.network_address
+        hostmask = self._interface.network.hostmask
+        return f"{network_address}{splitter}{hostmask}"
+
+
+LIPv4 = List[IPv4]
+
+
+def _validate_addr(*args, **kwargs) -> str:
+    """Validate IPv4 address, convert address or network with mask to CIDR A.B.C.D/LEN format.
+
+    :param args: Arguments containing the address value.
+    :param addr: IP address in CIDR notation with host data under mask bits.
+    :param strict: If True, IP must be valid network address (not host address).
+    :return: IPv4 address representation in CIDR notation with host data under mask bits.
+    :raises ValueError: If the address is invalid or cannot be converted from network/mask format.
+    """
+    addr = ""
+    if args:
+        addr = str(args[0]).strip()
+    if not addr:
+        addr = str(kwargs.get("addr") or "").strip()
+    if not addr:
+        addr = "0.0.0.0"
+
+    strict = bool(kwargs.get("strict"))
+
+    # addr
+    if re.search(rf"^{RE_IP}/\d+$", addr):
+        pass
+    elif re.search(rf"^{RE_IP}$", addr):
+        addr = f"{addr}/32"
+    else:
+        prefix, mask = vre.find2(rf"^({RE_IP})\D({RE_IP})$", addr)
+        if not (prefix and mask):
+            raise ValueError("Invalid address format")
+        network = IPv4Network(f"0.0.0.0/{mask}")
+        prefixlen = network.prefixlen
+        addr = f"{prefix}/{prefixlen}"
+
+    _ = IPv4Network(addr, strict=strict)
+    return addr
