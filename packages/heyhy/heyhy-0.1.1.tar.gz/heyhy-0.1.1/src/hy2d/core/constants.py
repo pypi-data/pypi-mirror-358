b87c64fcb334f251bc2fe1224f64f60c775c1400
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:16 +0000

    Add package heyhy/0.1.1/heyhy-0.1.1.tar.gz

---
+++ packages/heyhy/heyhy-0.1.1.tar.gz/heyhy-0.1.1/src/hy2d/core/constants.py
@@ -0,0 +1,14 @@
+"""全局常量配置"""
+
+from pathlib import Path
+
+BASE_DIR = Path("/home/hysteria2")
+DOCKER_COMPOSE_PATH = BASE_DIR / "docker-compose.yaml"
+CONFIG_PATH = BASE_DIR / "config.yaml"
+
+LISTEN_PORT = 4433
+SERVICE_IMAGE = "metacubex/mihomo:latest"
+
+MIHOMO_PROXIES_DOCS = "https://wiki.metacubex.one/config/proxies/hysteria2/#hysteria2"
+
+MASQUERADE_WEBSITE = "https://cocodataset.org/"
