b6651e37ed018f031de5f910e710b6f5a0bbfba3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:36 +0000

    Add package fused-dense-cuda/0.0.1/fused-dense-cuda-0.0.1.tar.gz

---
+++ packages/fused-dense-cuda/fused-dense-cuda-0.0.1.tar.gz/fused-dense-cuda-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="fused-dense-cuda",
+    version="0.0.1",
+    packages=["fused-dense-cuda"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
