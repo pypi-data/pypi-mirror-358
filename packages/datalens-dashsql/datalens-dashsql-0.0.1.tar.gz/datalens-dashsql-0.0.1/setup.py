983ba91379cac9aed725de91b01f20454a81dbbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:05:12 +0000

    Add package datalens-dashsql/0.0.1/datalens-dashsql-0.0.1.tar.gz

---
+++ packages/datalens-dashsql/datalens-dashsql-0.0.1.tar.gz/datalens-dashsql-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="datalens-dashsql",
+    version="0.0.1",
+    packages=["datalens-dashsql"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
