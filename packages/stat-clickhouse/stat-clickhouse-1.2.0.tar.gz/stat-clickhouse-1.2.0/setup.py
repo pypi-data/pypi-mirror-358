b287a4b17eb72f1cb6f6163e38faa872cef875a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:09 +0000

    Add package stat-clickhouse/1.2.0/stat-clickhouse-1.2.0.tar.gz

---
+++ packages/stat-clickhouse/stat-clickhouse-1.2.0.tar.gz/stat-clickhouse-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="stat-clickhouse",
+    version="1.2.0",
+    packages=["stat-clickhouse"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
