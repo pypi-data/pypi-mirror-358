d847d4775960278ea0aca46d80dc82628ec7fea2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:13 +0000

    Add package voice-omar/0.0.1/voice-omar-0.0.1.tar.gz

---
+++ packages/voice-omar/voice-omar-0.0.1.tar.gz/voice-omar-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="voice-omar",
+    version="0.0.1",
+    packages=["voice-omar"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
