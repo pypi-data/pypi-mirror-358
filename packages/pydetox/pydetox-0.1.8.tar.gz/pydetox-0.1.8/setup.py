c2e3ac8960caa12f5f56a296ba64a57e8d168cf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:11 +0000

    Add package pydetox/0.1.8/pydetox-0.1.8.tar.gz

---
+++ packages/pydetox/pydetox-0.1.8.tar.gz/pydetox-0.1.8/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pydetox",
+    version="0.1.8",
+    packages=["pydetox"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
