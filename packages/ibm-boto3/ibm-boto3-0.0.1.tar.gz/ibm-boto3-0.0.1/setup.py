746b9be626008606884c6d97ba57b32c5ef82d82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:25 +0000

    Add package ibm-boto3/0.0.1/ibm-boto3-0.0.1.tar.gz

---
+++ packages/ibm-boto3/ibm-boto3-0.0.1.tar.gz/ibm-boto3-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ibm-boto3",
+    version="0.0.1",
+    packages=["ibm-boto3"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
