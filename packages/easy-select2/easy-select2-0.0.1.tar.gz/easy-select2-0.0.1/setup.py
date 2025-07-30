35498021939e2d395195faf7d448fd5f96fbf239
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:06 +0000

    Add package easy-select2/0.0.1/easy-select2-0.0.1.tar.gz

---
+++ packages/easy-select2/easy-select2-0.0.1.tar.gz/easy-select2-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="easy-select2",
+    version="0.0.1",
+    packages=["easy-select2"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
