d044a3bd788ae528e5a59ea935955594795adda4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:23 +0000

    Add package voice-music-recommender-common/1.1.2/voice-music-recommender-common-1.1.2.tar.gz

---
+++ packages/voice-music-recommender-common/voice-music-recommender-common-1.1.2.tar.gz/voice-music-recommender-common-1.1.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="voice-music-recommender-common",
+    version="1.1.2",
+    packages=["voice-music-recommender-common"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
