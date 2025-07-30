db773138cb6843129c2314313dc9618a9f1e4949
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:27 +0000

    Add package kws-inference/0.0.1/kws-inference-0.0.1.tar.gz

---
+++ packages/kws-inference/kws-inference-0.0.1.tar.gz/kws-inference-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="kws-inference",
+    version="0.0.1",
+    packages=["kws-inference"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
