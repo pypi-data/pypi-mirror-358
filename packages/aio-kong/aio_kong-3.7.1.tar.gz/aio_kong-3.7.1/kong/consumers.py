6f805aea09f46e951764a3677fa3c7dc8e6dbb3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:09:37 +0000

    Add package aio-kong/3.7.1/aio_kong-3.7.1.tar.gz

---
+++ packages/aio-kong/aio_kong-3.7.1.tar.gz/aio_kong-3.7.1/kong/consumers.py
@@ -0,0 +1,82 @@
+from typing import cast
+
+from .acls import Acl, Acls
+from .auths import ConsumerAuth, auth_factory
+from .components import CrudComponent, JsonType, KongError, KongResponseError
+from .plugins import KongEntityWithPlugins
+
+
+class Consumer(KongEntityWithPlugins):
+    @property
+    def username(self) -> str:
+        return self.data.get("username", "")
+
+    @property
+    def acls(self) -> Acls:
+        return Acls(self, Acl)
+
+    @property
+    def jwts(self) -> ConsumerAuth:
+        return auth_factory(self, "jwt")
+
+    @property
+    def keyauths(self) -> ConsumerAuth:
+        return auth_factory(self, "key-auth")
+
+    @property
+    def basicauths(self) -> ConsumerAuth:
+        return auth_factory(self, "basic-auth")
+
+
+class Consumers(CrudComponent[Consumer]):
+
+    async def apply_credentials(self, auths: list[dict], consumer: Consumer) -> None:
+        for auth_data in auths:
+            auth = auth_factory(consumer, auth_data["type"])
+            await auth.create_or_update_credentials(auth_data["config"])
+
+    async def apply_json(self, data: JsonType, clear: bool = True) -> list:
+        if not isinstance(data, list):
+            data = [data]
+        result = []
+        for entry in data:
+            if not isinstance(entry, dict):
+                raise KongError("dictionary required")
+            entry = entry.copy()
+            groups = entry.pop("groups", [])
+            auths = entry.pop("auths", [])
+            udata = entry.copy()
+            id_ = udata.pop("id", None)
+            username = None
+            if not id_:
+                username = udata.pop("username", None)
+                if not username:
+                    raise KongError("Consumer username or id is required")
+            uid = cast(str, id_ or username)
+            try:
+                entity = await self.get(uid)
+            except KongResponseError as exc:
+                if exc.status == 404:
+                    entity = await self.create(**entry)
+                else:
+                    raise
+            else:
+                if entry:
+                    entity = await self.update(uid, **udata)
+            consumer = cast(Consumer, entity)
+            acls = await consumer.acls.get_list()
+            current_groups = dict(((a["group"], a) for a in acls))
+            for group in groups:
+                if group not in current_groups:
+                    await consumer.acls.create(group=group)
+                else:
+                    current_groups.pop(group)
+
+            for acl in current_groups.values():
+                await consumer.acls.delete(acl["id"])
+
+            await self.apply_credentials(auths, consumer)
+
+            result.append(consumer.data)
+
+        return result
---
+++ packages/aio-kong/aio_kong-3.7.1.tar.gz/aio_kong-3.7.1/readme.md
@@ -0,0 +1,93 @@
+# Async Python Client for Kong
+
+[![PyPI version](https://badge.fury.io/py/aio-kong.svg)](https://badge.fury.io/py/aio-kong)
+[![Python versions](https://img.shields.io/pypi/pyversions/aio-kong.svg)](https://pypi.org/project/aio-kong)
+[![Build](https://github.com/quantmind/aio-kong/workflows/build/badge.svg)](https://github.com/quantmind/aio-kong/actions?query=workflow%3Abuild)
+[![codecov](https://codecov.io/gh/quantmind/aio-kong/branch/main/graph/badge.svg?token=JF5L0PEkW6)](https://codecov.io/gh/quantmind/aio-kong)
+[![Downloads](https://img.shields.io/pypi/dd/aio-kong.svg)](https://pypi.org/project/aio-kong/)
+
+
+Tested with [kong][] v3.8
+
+## Installation
+
+To install the package
+
+```
+pip install aio-kong
+```
+
+## Testing
+
+To run tests, clone the repository and
+
+```
+make install
+make test
+```
+
+:warning: If you don't have Kong or postgres running locally, run the services first
+
+```bash
+make services
+```
+
+test certificates were generated using the command
+
+```
+openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -nodes -subj '/CN=localhost'
+```
+
+## Client
+
+The client can be imported via
+
+```python
+from kong.client import Kong
+```
+
+In a coroutine:
+
+```python
+async with Kong() as cli:
+    services = await cli.services.get_list()
+    print(json.dumps([s.data for s in services], indent=2))
+```
+
+By default the url is obtained from the "KONG_ADMIN_URL" environment variable which defaults to http://127.0.0.1:8001.
+
+The client has handlers for all Kong objects
+
+- [cli.services](./kong/services.py) CRUD operations on services
+- [cli.routes](./kong/routes.py) CRUD operations on routes
+- [cli.plugins](./kong/plugins.py) CRUD operations on plugins
+- [cli.consumers](./kong/consumers.py) CRUD operations on consumers
+- [cli.certificates](./kong/certificates.py) CRUD operations on TLS certificates
+- [cli.snis](./kong/snis.py) CRUD operations on SNIs
+- `cli.acls` To list all ACLs
+
+### Apply a configuration
+
+The client allow to apply a configuration object to kong:
+
+```python
+await cli.apply_json(config)
+```
+
+## Command line tool
+
+The library can install the `kongfig` command line tool for uploading kong configuration files.
+
+For the command line tool to work, you need to install the package as:
+
+```bash
+pip install aio-kong[cli]
+```
+
+and to run the cli tool, you can use the following command:
+
+```
+kongfig --help
+```
+
+[kong]: https://github.com/Kong/kong
