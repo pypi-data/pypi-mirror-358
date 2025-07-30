e755971429e6b70119b42ac3031c3daad37354a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:40 +0000

    Add package settings-private/0.0.1/settings-private-0.0.1.tar.gz

---
+++ packages/settings-private/settings-private-0.0.1.tar.gz/settings-private-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="settings-private",
+    version="0.0.1",
+    packages=["settings-private"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
