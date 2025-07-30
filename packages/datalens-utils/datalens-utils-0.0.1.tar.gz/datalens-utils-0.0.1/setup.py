31b083ec226ac8360dcabfa1c4172a44c72a753e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:38 +0000

    Add package datalens-utils/0.0.1/datalens-utils-0.0.1.tar.gz

---
+++ packages/datalens-utils/datalens-utils-0.0.1.tar.gz/datalens-utils-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="datalens-utils",
+    version="0.0.1",
+    packages=["datalens-utils"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
