a630db8935eabc1058a585bd81d759e836aa9966
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:09 +0000

    Add package jakarta.enterprise.lang-model/4.0.1/jakarta.enterprise.lang-model-4.0.1.tar.gz

---
+++ packages/jakarta.enterprise.lang-model/jakarta.enterprise.lang-model-4.0.1.tar.gz/jakarta.enterprise.lang-model-4.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="jakarta.enterprise.lang-model",
+    version="4.0.1",
+    packages=["jakarta.enterprise.lang-model"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
