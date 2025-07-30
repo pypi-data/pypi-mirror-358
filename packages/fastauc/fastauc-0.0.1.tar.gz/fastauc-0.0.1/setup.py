47a1671d4a7c36bce57bed8dcec768f52dbd2b79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:30 +0000

    Add package fastauc/0.0.1/fastauc-0.0.1.tar.gz

---
+++ packages/fastauc/fastauc-0.0.1.tar.gz/fastauc-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="fastauc",
+    version="0.0.1",
+    packages=["fastauc"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
