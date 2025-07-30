3f0aa5532b1e7b490f7fca17fa53fb847f341700
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:01 +0000

    Add package owlt-mcp-server/0.1.6/owlt_mcp_server-0.1.6-py3-none-any.whl

---
+++ packages/owlt-mcp-server/owlt_mcp_server-0.1.6-py3-none-any.whl/owlt_mcp_server/tools/csv_tools.py
@@ -0,0 +1,18 @@
+from owlt_mcp_server.server import mcp
+from owlt_mcp_server.utils.trans_func import uni_translate
+from owlt_mcp_server.utils.trans_func import dseek_translate
+from owlt_mcp_server.utils.trans_func import lara_translate
+
+@mcp.tool()
+def translate_text(text: str, language: str) ->str:
+    return uni_translate(text, language)
+    
+    
+    
+@mcp.tool()
+def use_deepseek_translate(text: str, language: str) ->str:
+    return dseek_translate(text, language)
+    
+@mcp.tool()
+def use_lara(text: str, language: str) ->str:
+    return lara_translate(text, language)
