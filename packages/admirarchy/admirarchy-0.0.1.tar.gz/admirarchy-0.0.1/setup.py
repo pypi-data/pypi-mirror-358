baf0799d0024c39d32e0dbd32330a5bd698f6f8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:20 +0000

    Add package admirarchy/0.0.1/admirarchy-0.0.1.tar.gz

---
+++ packages/admirarchy/admirarchy-0.0.1.tar.gz/admirarchy-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="admirarchy",
+    version="0.0.1",
+    packages=["admirarchy"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
