fd48b2f9892385035233840af00ad2cc7d5ef97b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:07 +0000

    Add package resource-releaser/0.1.1/resource-releaser-0.1.1.tar.gz

---
+++ packages/resource-releaser/resource-releaser-0.1.1.tar.gz/resource-releaser-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="resource-releaser",
+    version="0.1.1",
+    packages=["resource-releaser"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
