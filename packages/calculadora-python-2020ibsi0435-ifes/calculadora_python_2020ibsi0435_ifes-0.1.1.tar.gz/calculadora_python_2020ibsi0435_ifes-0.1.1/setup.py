79c28f00b9e07bee2bd6aeddec1f42f77f50c11b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:35:39 +0000

    Add package calculadora-python-2020ibsi0435-ifes/0.1.1/calculadora_python_2020ibsi0435_ifes-0.1.1.tar.gz

---
+++ packages/calculadora-python-2020ibsi0435-ifes/calculadora_python_2020ibsi0435_ifes-0.1.1.tar.gz/calculadora_python_2020ibsi0435_ifes-0.1.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="calculadora-python-2020ibsi0435-ifes",
+    version="0.1.1",
+    description="Uma calculadora com API FastAPI",
+    author="Matthew A.",
+    author_email="ecshestagioclik@gmail.com",
+    packages=find_packages(),  # encontra automaticamente o pacote calculadora
+    install_requires=[
+        "fastapi",
+        "uvicorn",
+        "pydantic",
+    ],
+    python_requires=">=3.8",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Framework :: FastAPI",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,
+)
