8be0b4d4d22937cc123376557854892c9fe1061a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:20 +0000

    Add package pylipd/1.5.0/pylipd-1.5.0.tar.gz

---
+++ packages/pylipd/pylipd-1.5.0.tar.gz/pylipd-1.5.0/pylipd/classes/compilation.py
@@ -0,0 +1,293 @@
+
+##############################
+# Auto-generated. Do not Edit
+##############################
+
+import re
+from pylipd.utils import uniqid
+
+class Compilation:
+    """Auto-generated LinkedEarth class representing `Compilation`."""
+    def __init__(self):
+        """Initialize a new Compilation instance."""
+        
+        self.name: str = None
+        self.version: str = None
+        self.misc = {}
+        self.ontns = "http://linked.earth/ontology#"
+        self.ns = "http://linked.earth/lipd"
+        self.type = "http://linked.earth/ontology#Compilation"
+        self.id = self.ns + "/" + uniqid("Compilation.")
+
+    @staticmethod
+    def from_data(id, data) -> 'Compilation':
+        """Instantiate `Compilation` from an ontology-style data graph.
+
+        Parameters
+        ----------
+        id : str
+            The node identifier for this object.
+        data : dict
+            Dictionary mapping node ids to their predicate lists.
+
+        Returns
+        -------
+        Compilation
+            The populated `Compilation` instance.
+        """
+        self = Compilation()
+        self.id = id
+        mydata = data[id]
+        for key in mydata:
+            value = mydata[key]
+            obj = None
+            if key == "type":
+                for val in value:
+                    self.type = val["@id"]
+        
+            elif key == "hasName":
+                for val in value:
+                    if "@value" in val:
+                        obj = val["@value"]                        
+                    self.name = obj
+        
+            elif key == "hasVersion":
+                for val in value:
+                    if "@value" in val:
+                        obj = val["@value"]                        
+                    self.version = obj
+            else:
+                for val in value:
+                    obj = None
+                    if "@id" in val:
+                        obj = data[val["@id"]]
+                    elif "@value" in val:
+                        obj = val["@value"]
+                    self.set_non_standard_property(key, obj)
+            
+        return self
+
+    def to_data(self, data={}):
+        """Serialize the object into a JSON-LD compatible dictionary.
+
+        Parameters
+        ----------
+        data : dict, optional
+            Existing data dictionary to extend.
+
+        Returns
+        -------
+        dict
+            The updated data dictionary.
+        """
+        data[self.id] = {}
+        data[self.id]["type"] = [
+            {
+                "@id": self.type,
+                "@type": "uri"
+            }
+        ]
+
+        if self.name:
+            value_obj = self.name
+            obj = {
+                "@value": value_obj,
+                "@type": "literal",
+                "@datatype": "http://www.w3.org/2001/XMLSchema#string"
+            }
+            data[self.id]["hasName"] = [obj]
+                
+
+        if self.version:
+            value_obj = self.version
+            obj = {
+                "@value": value_obj,
+                "@type": "literal",
+                "@datatype": "http://www.w3.org/2001/XMLSchema#string"
+            }
+            data[self.id]["hasVersion"] = [obj]
+                
+        for key in self.misc:
+            value = self.misc[key]
+            data[self.id][key] = []
+            ptype = None
+            tp = type(value).__name__
+            if tp == "int":
+                ptype = "http://www.w3.org/2001/XMLSchema#integer"
+            elif tp == "float" or tp == "double":
+                ptype = "http://www.w3.org/2001/XMLSchema#float"
+            elif tp == "str":
+                if re.match(r"\d{4}-\d{2}-\d{2}( |T)\d{2}:\d{2}:\d{2}", value):
+                    ptype = "http://www.w3.org/2001/XMLSchema#datetime"   
+                elif re.match(r"\d{4}-\d{2}-\d{2}", value):
+                    ptype = "http://www.w3.org/2001/XMLSchema#date"
+                else:
+                    ptype = "http://www.w3.org/2001/XMLSchema#string"
+            elif tp == "bool":
+                ptype = "http://www.w3.org/2001/XMLSchema#boolean"
+
+            data[self.id][key].append({
+                "@value": value,
+                "@type": "literal",
+                "@datatype": ptype
+            })
+        
+        return data
+
+    def to_json(self):
+        """Return a lightweight JSON representation (used by LiPD).
+
+        Returns
+        -------
+        dict
+            A dictionary representation of this object.
+        """
+        data = {
+            "@id": self.id
+        }
+
+        if self.name:
+            value_obj = self.name
+            obj = value_obj
+            data["compilationName"] = obj
+
+        if self.version:
+            value_obj = self.version
+            obj = value_obj
+            data["compilationVersion"] = obj
+
+        for key in self.misc:
+            value = self.misc[key]
+            data[key] = value
+                   
+        return data
+
+    @staticmethod
+    def from_json(data) -> 'Compilation':
+        """Instantiate `Compilation` from its lightweight JSON representation.
+
+        Parameters
+        ----------
+        data : dict
+            The JSON dictionary to parse.
+
+        Returns
+        -------
+        Compilation
+            The populated `Compilation` instance.
+        """
+        self = Compilation()
+        for key in data:
+            pvalue = data[key]
+            if key == "@id":
+                self.id = pvalue
+            elif key == "compilationName":
+                    value = pvalue
+                    obj = value
+                    self.name = obj
+            elif key == "compilationVersion":
+                    value = pvalue
+                    obj = value
+                    self.version = obj
+            else:
+                self.set_non_standard_property(key, pvalue)
+                   
+        return self
+
+    def set_non_standard_property(self, key, value):
+        """Store a predicate that is not defined in the ontology schema.
+
+        This is useful for forward-compatibility with new properties that are
+        not yet part of the official schema.
+
+        Parameters
+        ----------
+        key : str
+            The property name.
+        value : any
+            The property value.
+        """
+        if key not in self.misc:
+            self.misc[key] = value
+    
+    def get_non_standard_property(self, key):
+        """Return a single non-standard property by key.
+
+        Parameters
+        ----------
+        key : str
+            The property name.
+
+        Returns
+        -------
+        any
+            The property value.
+        """
+        return self.misc[key]
+                
+    def get_all_non_standard_properties(self):
+        """Return the dictionary of all non-standard properties.
+
+        Returns
+        -------
+        dict
+            Dictionary of all non-standard properties.
+        """
+        return self.misc
+
+    def add_non_standard_property(self, key, value):
+        """Append a value to a list-valued non-standard property.
+
+        Parameters
+        ----------
+        key : str
+            The property name.
+        value : any
+            The value to append.
+        """
+        if key not in self.misc:
+            self.misc[key] = []
+        self.misc[key].append(value)
+        
+    def getName(self) -> str:
+        """Get name.
+
+        Returns
+        -------
+        str
+            The current value of name.
+        """
+        return self.name
+
+    def setName(self, name:str):
+        """Set name.
+
+        Parameters
+        ----------
+        name : str
+            The value to assign.
+        """
+        assert isinstance(name, str), f"Error: '{name}' is not of type str"
+        self.name = name
+    
+    def getVersion(self) -> str:
+        """Get version.
+
+        Returns
+        -------
+        str
+            The current value of version.
+        """
+        return self.version
+
+    def setVersion(self, version:str):
+        """Set version.
+
+        Parameters
+        ----------
+        version : str
+            The value to assign.
+        """
+        assert isinstance(version, str), f"Error: '{version}' is not of type str"
+        self.version = version
+    
