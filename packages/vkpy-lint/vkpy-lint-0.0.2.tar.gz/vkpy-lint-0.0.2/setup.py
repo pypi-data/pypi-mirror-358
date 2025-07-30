d54436541b3b9f56555943bf760663fe75d4bb77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:59 +0000

    Add package vkpy-lint/0.0.2/vkpy-lint-0.0.2.tar.gz

---
+++ packages/vkpy-lint/vkpy-lint-0.0.2.tar.gz/vkpy-lint-0.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="vkpy-lint",
+    version="0.0.2",
+    packages=["vkpy-lint"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
