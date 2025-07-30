36e3704a074d4afaaa3c5811335c052236bbda00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:38 +0000

    Add package piegy/2.1.8/piegy-2.1.8-pp310-pypy310_pp73-macosx_11_0_arm64.whl

---
+++ packages/piegy/piegy-2.1.8-pp310-pypy310_pp73-macosx_11_0_arm64.whl/piegy/build_info.py
@@ -0,0 +1,12 @@
+"""
+Contains build info, whether it's local built, or a pre-compiled wheel.
+Auto-generated at compile time.
+"""
+
+build_info = {
+    "version": "2.1.8",
+    "built from": "wheel",
+    "build date": "2025-06-28 08:03:58",
+    "python version": "3.10.16",
+    "platform": "darwin"
+}
