bfa31b798b4a8c359f55692c9d5ce6aa914ab861
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:05 +0000

    Add package mailru-im-async-bot/0.0.1/mailru-im-async-bot-0.0.1.tar.gz

---
+++ packages/mailru-im-async-bot/mailru-im-async-bot-0.0.1.tar.gz/mailru-im-async-bot-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="mailru-im-async-bot",
+    version="0.0.1",
+    packages=["mailru-im-async-bot"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
