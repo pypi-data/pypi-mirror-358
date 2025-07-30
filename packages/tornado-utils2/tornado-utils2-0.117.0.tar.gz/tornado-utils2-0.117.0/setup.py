b488ae82591360f48f8ee37b4f177f5e803892ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:29 +0000

    Add package tornado-utils2/0.117.0/tornado-utils2-0.117.0.tar.gz

---
+++ packages/tornado-utils2/tornado-utils2-0.117.0.tar.gz/tornado-utils2-0.117.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="tornado-utils2",
+    version="0.117.0",
+    packages=["tornado-utils2"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
