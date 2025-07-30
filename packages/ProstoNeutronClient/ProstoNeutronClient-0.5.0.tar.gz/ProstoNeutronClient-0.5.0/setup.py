40b373e831a93e283f4101186c241958a3223f7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:37 +0000

    Add package ProstoNeutronClient/0.5.0/ProstoNeutronClient-0.5.0.tar.gz

---
+++ packages/ProstoNeutronClient/ProstoNeutronClient-0.5.0.tar.gz/ProstoNeutronClient-0.5.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ProstoNeutronClient",
+    version="0.5.0",
+    packages=["ProstoNeutronClient"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
