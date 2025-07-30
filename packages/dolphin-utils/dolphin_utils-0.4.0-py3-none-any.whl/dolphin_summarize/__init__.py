7c8d3c7b8ba40664b38db42e22c325b1e9aa0f43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:06 +0000

    Add package dolphin-utils/0.4.0/dolphin_utils-0.4.0-py3-none-any.whl

---
+++ packages/dolphin-utils/dolphin_utils-0.4.0-py3-none-any.whl/dolphin_summarize/__init__.py
@@ -0,0 +1,8 @@
+"""
+Model Architecture Summarizer for Safetensors
+--------------------------------------------
+This package provides tools to analyze safetensors files and generate 
+a condensed summary of the model architecture.
+"""
+
+__version__ = "0.3.0"
