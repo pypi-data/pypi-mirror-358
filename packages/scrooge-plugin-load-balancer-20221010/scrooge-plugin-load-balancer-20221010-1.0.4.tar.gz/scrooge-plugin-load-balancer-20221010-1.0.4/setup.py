f7a29182f667382ab00af7a641fe8a1e3b95248a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:44 +0000

    Add package scrooge-plugin-load-balancer-20221010/1.0.4/scrooge-plugin-load-balancer-20221010-1.0.4.tar.gz

---
+++ packages/scrooge-plugin-load-balancer-20221010/scrooge-plugin-load-balancer-20221010-1.0.4.tar.gz/scrooge-plugin-load-balancer-20221010-1.0.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="scrooge-plugin-load-balancer-20221010",
+    version="1.0.4",
+    packages=["scrooge-plugin-load-balancer-20221010"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
