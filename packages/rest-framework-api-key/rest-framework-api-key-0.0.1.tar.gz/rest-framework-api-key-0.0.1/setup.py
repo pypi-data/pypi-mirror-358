42b9c38cf3d6156b660e519d82fba5edde523a7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:22 +0000

    Add package rest-framework-api-key/0.0.1/rest-framework-api-key-0.0.1.tar.gz

---
+++ packages/rest-framework-api-key/rest-framework-api-key-0.0.1.tar.gz/rest-framework-api-key-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="rest-framework-api-key",
+    version="0.0.1",
+    packages=["rest-framework-api-key"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
