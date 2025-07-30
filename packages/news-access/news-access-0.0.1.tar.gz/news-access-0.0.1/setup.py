c17b00e578ce40e89d8333fd7bc299ea6cbb4428
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:13 +0000

    Add package news-access/0.0.1/news-access-0.0.1.tar.gz

---
+++ packages/news-access/news-access-0.0.1.tar.gz/news-access-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="news-access",
+    version="0.0.1",
+    packages=["news-access"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
