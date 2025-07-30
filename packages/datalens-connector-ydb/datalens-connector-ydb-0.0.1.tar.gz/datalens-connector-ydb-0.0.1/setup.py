4c391e05c32cb45c9e4c70e5307e4f0900cbb060
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:37 +0000

    Add package datalens-connector-ydb/0.0.1/datalens-connector-ydb-0.0.1.tar.gz

---
+++ packages/datalens-connector-ydb/datalens-connector-ydb-0.0.1.tar.gz/datalens-connector-ydb-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="datalens-connector-ydb",
+    version="0.0.1",
+    packages=["datalens-connector-ydb"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
