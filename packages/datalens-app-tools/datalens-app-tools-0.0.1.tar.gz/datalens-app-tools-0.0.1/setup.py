e6cf5411adb669d9d0454eb0b7f584bd29cf4d15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:05:10 +0000

    Add package datalens-app-tools/0.0.1/datalens-app-tools-0.0.1.tar.gz

---
+++ packages/datalens-app-tools/datalens-app-tools-0.0.1.tar.gz/datalens-app-tools-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="datalens-app-tools",
+    version="0.0.1",
+    packages=["datalens-app-tools"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
