3dd31622e3cf5a5658ca5fd24507054e21af0480
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:05:18 +0000

    Add package datalens-sqlalchemy-bitrix/0.0.1/datalens-sqlalchemy-bitrix-0.0.1.tar.gz

---
+++ packages/datalens-sqlalchemy-bitrix/datalens-sqlalchemy-bitrix-0.0.1.tar.gz/datalens-sqlalchemy-bitrix-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="datalens-sqlalchemy-bitrix",
+    version="0.0.1",
+    packages=["datalens-sqlalchemy-bitrix"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
