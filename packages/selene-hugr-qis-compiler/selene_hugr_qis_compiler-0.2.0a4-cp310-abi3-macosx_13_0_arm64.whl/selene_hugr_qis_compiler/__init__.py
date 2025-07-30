00301b350b8b5e358118e5be934cf8d050341a9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:30 +0000

    Add package selene-hugr-qis-compiler/0.2.0a4/selene_hugr_qis_compiler-0.2.0a4-cp310-abi3-macosx_13_0_arm64.whl

---
+++ packages/selene-hugr-qis-compiler/selene_hugr_qis_compiler-0.2.0a4-cp310-abi3-macosx_13_0_arm64.whl/selene_hugr_qis_compiler/__init__.py
@@ -0,0 +1,5 @@
+from .selene_hugr_qis_compiler import *
+
+__doc__ = selene_hugr_qis_compiler.__doc__
+if hasattr(selene_hugr_qis_compiler, "__all__"):
+    __all__ = selene_hugr_qis_compiler.__all__
