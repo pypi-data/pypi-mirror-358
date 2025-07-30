e69cf1519aa75e850bda1366e1a077a5fda769f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:42 +0000

    Add package browser-sync-proto/0.1.7/browser-sync-proto-0.1.7.tar.gz

---
+++ packages/browser-sync-proto/browser-sync-proto-0.1.7.tar.gz/browser-sync-proto-0.1.7/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="browser-sync-proto",
+    version="0.1.7",
+    packages=["browser-sync-proto"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
