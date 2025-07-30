58369b14a6cd24a02f11df0d655f43b8e4759623
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:07:00 +0000

    Add package registry-sdk/0.0.1/registry-sdk-0.0.1.tar.gz

---
+++ packages/registry-sdk/registry-sdk-0.0.1.tar.gz/registry-sdk-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="registry-sdk",
+    version="0.0.1",
+    packages=["registry-sdk"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
