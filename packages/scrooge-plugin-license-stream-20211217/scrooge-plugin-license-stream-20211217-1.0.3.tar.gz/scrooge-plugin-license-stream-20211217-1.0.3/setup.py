1c0b2a30d4065344833d50ae932ce3a39147044c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:44 +0000

    Add package scrooge-plugin-license-stream-20211217/1.0.3/scrooge-plugin-license-stream-20211217-1.0.3.tar.gz

---
+++ packages/scrooge-plugin-license-stream-20211217/scrooge-plugin-license-stream-20211217-1.0.3.tar.gz/scrooge-plugin-license-stream-20211217-1.0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="scrooge-plugin-license-stream-20211217",
+    version="1.0.3",
+    packages=["scrooge-plugin-license-stream-20211217"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
