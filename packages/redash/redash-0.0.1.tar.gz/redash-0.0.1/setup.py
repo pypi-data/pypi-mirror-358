b0231ca1d79a0c2a28a74e126980e292db81819e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:41 +0000

    Add package redash/0.0.1/redash-0.0.1.tar.gz

---
+++ packages/redash/redash-0.0.1.tar.gz/redash-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="redash",
+    version="0.0.1",
+    packages=["redash"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
