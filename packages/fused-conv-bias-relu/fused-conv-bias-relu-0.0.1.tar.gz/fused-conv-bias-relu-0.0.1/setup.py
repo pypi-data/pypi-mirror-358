7a6a76a6583af1e9bdfa847f8b5c2c9ea7ad137d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:03 +0000

    Add package fused-conv-bias-relu/0.0.1/fused-conv-bias-relu-0.0.1.tar.gz

---
+++ packages/fused-conv-bias-relu/fused-conv-bias-relu-0.0.1.tar.gz/fused-conv-bias-relu-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="fused-conv-bias-relu",
+    version="0.0.1",
+    packages=["fused-conv-bias-relu"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
