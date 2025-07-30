ca7f076a01178f836a584241f75b3e59e7d1b393
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:09:02 +0000

    Add package universalinit/0.1.58/universalinit-0.1.58.tar.gz

---
+++ packages/universalinit/universalinit-0.1.58.tar.gz/universalinit-0.1.58/src/universalinit/templates/express/src/app.js
@@ -0,0 +1,43 @@
+const cors = require('cors');
+const express = require('express');
+const routes = require('./routes');
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('../swagger');
+
+// Initialize express app
+const app = express();
+
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+app.use('/docs', swaggerUi.serve, (req, res, next) => {
+  const dynamicSpec = {
+    ...swaggerSpec,
+    servers: [
+      {
+        url: `${req.protocol}://${req.get('host')}`,
+      },
+    ],
+  };
+  swaggerUi.setup(dynamicSpec)(req, res, next);
+});
+
+// Parse JSON request body
+app.use(express.json());
+
+// Mount routes
+app.use('/', routes);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({
+    status: 'error',
+    message: 'Internal Server Error',
+  });
+});
+
+module.exports = app;
