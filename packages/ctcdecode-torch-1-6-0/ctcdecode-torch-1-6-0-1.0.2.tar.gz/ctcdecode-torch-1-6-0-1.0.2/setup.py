e117c3d7cf0ec376a89ea441f878e31646667a1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:12 +0000

    Add package ctcdecode-torch-1-6-0/1.0.2/ctcdecode-torch-1-6-0-1.0.2.tar.gz

---
+++ packages/ctcdecode-torch-1-6-0/ctcdecode-torch-1-6-0-1.0.2.tar.gz/ctcdecode-torch-1-6-0-1.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ctcdecode-torch-1-6-0",
+    version="1.0.2",
+    packages=["ctcdecode-torch-1-6-0"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
