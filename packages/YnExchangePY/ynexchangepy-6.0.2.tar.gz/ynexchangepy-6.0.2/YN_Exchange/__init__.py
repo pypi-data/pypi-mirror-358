e64c46f045bfc248189922f305b3a7dd382197a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:15 +0000

    Add package YnExchangePY/6.0.2/ynexchangepy-6.0.2.tar.gz

---
+++ packages/YnExchangePY/ynexchangepy-6.0.2.tar.gz/ynexchangepy-6.0.2/YN_Exchange/__init__.py
@@ -0,0 +1,11 @@
+from .Exchange import CryptoManager
+from .Exchange import Calculator
+from .Exchange import ExchangeAPI
+from .Exchange import MetalManager
+from .Exchange import NaturalResourcesManager
+
+from .AsyncExchange import AsyncCryptoManager
+from .AsyncExchange import AsyncCalculator
+from .AsyncExchange import AsyncMetalManager
+from .AsyncExchange import AsyncNaturalResourcesManager
+from .AsyncExchange import ExchangeAPI
---
+++ packages/YnExchangePY/ynexchangepy-6.0.2.tar.gz/ynexchangepy-6.0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '6.0.2'
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    LONG_DESCRIPTION = "\n" + fh.read()
+
+
+# Setting up
+setup(
+    name="YnExchangePY",
+    version=VERSION,
+    author="Yasha Najafi(YN)",
+    author_email="najafiyasha@gmail.com",
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['bs4','plotly','matplotlib','mplfinance','ccxt','ccxt','pandas',"urllib3","asyncio","aiohttp"],
+    keywords=['python', 'crypto', 'prices', 'crypto calculations', 'api', 'calculations',"metal"],
+    license="MIT",
+    description="OpenSource Crypto Library",
+)
