58369545683e13bb0cae8927a7b00ecc7fc983c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:29 +0000

    Add package ipa-client-install/0.0.1/ipa-client-install-0.0.1.tar.gz

---
+++ packages/ipa-client-install/ipa-client-install-0.0.1.tar.gz/ipa-client-install-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ipa-client-install",
+    version="0.0.1",
+    packages=["ipa-client-install"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
