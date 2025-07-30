e2d8b32417b2ccfc2da120a19f54d3b10a1f5c7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:31 +0000

    Add package dockerjob/0.0.1/dockerjob-0.0.1.tar.gz

---
+++ packages/dockerjob/dockerjob-0.0.1.tar.gz/dockerjob-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dockerjob",
+    version="0.0.1",
+    packages=["dockerjob"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
