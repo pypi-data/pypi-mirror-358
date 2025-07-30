f7a20924cdfa882e98c6e37d4e4a6a61f02ed2d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:10:06 +0000

    Add package clickup-mcp/0.1.4/clickup_mcp-0.1.4-py3-none-any.whl

---
+++ packages/clickup-mcp/clickup_mcp-0.1.4-py3-none-any.whl/clickup_mcp/main.py
@@ -0,0 +1,42 @@
+# server.py
+from mcp.server.fastmcp import FastMCP
+import requests
+from .utilities import validate_api_token
+
+from typing import Optional, Tuple
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Create an MCP server
+mcp = FastMCP("Demo")
+
+
+# getTask tool
+@mcp.tool()
+def get_task(taskID: str) -> str:
+
+    """ This tool is capable of retreiving the task details for a specific task ID and return it to the user"""
+
+    # Validate API token
+    is_valid, token_or_error = validate_api_token()
+    if not is_valid:
+        return token_or_error
+    
+    api_token = token_or_error
+
+    url = f"https://api.clickup.com/api/v2/task/{taskID}"
+    headers = {
+        "accept": "application/json",
+        "Authorization": api_token
+    }
+
+    response = requests.get(url, headers=headers)
+    return response.text
+    
+def main():
+    mcp.run()
+
+if __name__ == "__main__":
+    main()
