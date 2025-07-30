76c0b8d64537420983d7401e6936b2533f56ed04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:29 +0000

    Add package ipt-periodic-task/0.92.1/ipt-periodic-task-0.92.1.tar.gz

---
+++ packages/ipt-periodic-task/ipt-periodic-task-0.92.1.tar.gz/ipt-periodic-task-0.92.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ipt-periodic-task",
+    version="0.92.1",
+    packages=["ipt-periodic-task"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
