60a43477716c8b7f5a423548c9b352414d3c7eb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:09:35 +0000

    Add package docxblocks/1.6.3/docxblocks-1.6.3-py3-none-any.whl

---
+++ packages/docxblocks/docxblocks-1.6.3-py3-none-any.whl/docxblocks/constants.py
@@ -0,0 +1,6 @@
+# Default values for empty/missing content
+DEFAULT_EMPTY_VALUE_TEXT = "VALUE NOT FOUND"
+DEFAULT_EMPTY_VALUE_STYLE = {
+    "font_color": "FF0000",  # Red color in hex
+    "bold": True
+} 
