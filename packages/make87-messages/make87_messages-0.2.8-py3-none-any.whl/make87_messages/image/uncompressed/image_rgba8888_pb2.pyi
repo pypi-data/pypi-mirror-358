7ce93cf2af1e43208ce1cfb2e527aaf276f7e71a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:33:48 +0000

    Add package make87-messages/0.2.8/make87_messages-0.2.8-py3-none-any.whl

---
+++ packages/make87-messages/make87_messages-0.2.8-py3-none-any.whl/make87_messages/image/uncompressed/image_rgba8888_pb2.pyi
@@ -0,0 +1,18 @@
+from make87_messages.core import header_pb2 as _header_pb2
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Mapping as _Mapping, Optional as _Optional, Union as _Union
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class ImageRGBA8888(_message.Message):
+    __slots__ = ["header", "width", "height", "data"]
+    HEADER_FIELD_NUMBER: _ClassVar[int]
+    WIDTH_FIELD_NUMBER: _ClassVar[int]
+    HEIGHT_FIELD_NUMBER: _ClassVar[int]
+    DATA_FIELD_NUMBER: _ClassVar[int]
+    header: _header_pb2.Header
+    width: int
+    height: int
+    data: bytes
+    def __init__(self, header: _Optional[_Union[_header_pb2.Header, _Mapping]] = ..., width: _Optional[int] = ..., height: _Optional[int] = ..., data: _Optional[bytes] = ...) -> None: ...
