ca0e36a20a03756e131a2df76d01efd491b20501
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:05 +0000

    Add package voice-airflow-qa/1.1.146/voice-airflow-qa-1.1.146.tar.gz

---
+++ packages/voice-airflow-qa/voice-airflow-qa-1.1.146.tar.gz/voice-airflow-qa-1.1.146/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="voice-airflow-qa",
+    version="1.1.146",
+    packages=["voice-airflow-qa"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
