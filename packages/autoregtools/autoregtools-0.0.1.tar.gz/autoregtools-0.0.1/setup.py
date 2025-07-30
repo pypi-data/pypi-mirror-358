1a47d4066609660707eae7b2aa56f0733b42e0de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:07:01 +0000

    Add package autoregtools/0.0.1/autoregtools-0.0.1.tar.gz

---
+++ packages/autoregtools/autoregtools-0.0.1.tar.gz/autoregtools-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="autoregtools",
+    version="0.0.1",
+    packages=["autoregtools"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
