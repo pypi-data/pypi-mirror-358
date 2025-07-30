da4c4e131a4ae67fe988f2665fd22bd65b8c120d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:36 +0000

    Add package telepai/0.1.0/telepai-0.1.0.tar.gz

---
+++ packages/telepai/telepai-0.1.0.tar.gz/telepai-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='telepai',
+    version='0.1.0',
+    packages=find_packages(),
+    author='fswair',
+    description='telepai is a Python package.',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
