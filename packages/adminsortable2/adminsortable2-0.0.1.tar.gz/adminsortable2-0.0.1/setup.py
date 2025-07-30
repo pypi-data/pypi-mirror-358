0c79305a81a8a6a7b02f52f6491fa649f823639a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:38 +0000

    Add package adminsortable2/0.0.1/adminsortable2-0.0.1.tar.gz

---
+++ packages/adminsortable2/adminsortable2-0.0.1.tar.gz/adminsortable2-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="adminsortable2",
+    version="0.0.1",
+    packages=["adminsortable2"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
