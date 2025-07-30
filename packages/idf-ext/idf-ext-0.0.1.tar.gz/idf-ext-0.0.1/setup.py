50ec3054950eb659748c921f05be7e6cf5135ef8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:12 +0000

    Add package idf-ext/0.0.1/idf-ext-0.0.1.tar.gz

---
+++ packages/idf-ext/idf-ext-0.0.1.tar.gz/idf-ext-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="idf-ext",
+    version="0.0.1",
+    packages=["idf-ext"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
