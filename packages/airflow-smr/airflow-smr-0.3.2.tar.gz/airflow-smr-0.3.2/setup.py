c002925493ed57690608947e8b5de2066ccb38b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:17 +0000

    Add package airflow-smr/0.3.2/airflow-smr-0.3.2.tar.gz

---
+++ packages/airflow-smr/airflow-smr-0.3.2.tar.gz/airflow-smr-0.3.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="airflow-smr",
+    version="0.3.2",
+    packages=["airflow-smr"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
