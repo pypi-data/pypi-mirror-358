3adcaaea8c66dbccf60257e531eaf5aab9bc580e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:22 +0000

    Add package pulse-layer-client/1.8/pulse-layer-client-1.8.tar.gz

---
+++ packages/pulse-layer-client/pulse-layer-client-1.8.tar.gz/pulse-layer-client-1.8/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pulse-layer-client",
+    version="1.8",
+    packages=["pulse-layer-client"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
