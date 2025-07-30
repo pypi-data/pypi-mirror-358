f871b7c4eabaf608b3cc9d0fb67210821002bf31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:07:01 +0000

    Add package vkpy-mockspy/0.0.1/vkpy-mockspy-0.0.1.tar.gz

---
+++ packages/vkpy-mockspy/vkpy-mockspy-0.0.1.tar.gz/vkpy-mockspy-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="vkpy-mockspy",
+    version="0.0.1",
+    packages=["vkpy-mockspy"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
