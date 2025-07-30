26c7784b5e973edf68cabd45c787a576d06efdd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:37 +0000

    Add package pytest-hammer-artifacts/0.2.2/pytest-hammer-artifacts-0.2.2.tar.gz

---
+++ packages/pytest-hammer-artifacts/pytest-hammer-artifacts-0.2.2.tar.gz/pytest-hammer-artifacts-0.2.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pytest-hammer-artifacts",
+    version="0.2.2",
+    packages=["pytest-hammer-artifacts"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
