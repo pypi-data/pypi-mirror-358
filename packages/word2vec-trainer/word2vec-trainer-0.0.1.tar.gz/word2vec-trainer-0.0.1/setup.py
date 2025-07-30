efef92cb618ee5b1ba8e52536af89c5ab2886f87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:06 +0000

    Add package word2vec-trainer/0.0.1/word2vec-trainer-0.0.1.tar.gz

---
+++ packages/word2vec-trainer/word2vec-trainer-0.0.1.tar.gz/word2vec-trainer-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="word2vec-trainer",
+    version="0.0.1",
+    packages=["word2vec-trainer"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
