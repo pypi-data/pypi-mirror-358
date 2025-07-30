2ff9cefb648895850c4de7926f21e78b55bc8790
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:05 +0000

    Add package mraspy-tarantool-connector/0.0.5/mraspy-tarantool-connector-0.0.5.tar.gz

---
+++ packages/mraspy-tarantool-connector/mraspy-tarantool-connector-0.0.5.tar.gz/mraspy-tarantool-connector-0.0.5/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="mraspy-tarantool-connector",
+    version="0.0.5",
+    packages=["mraspy-tarantool-connector"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
