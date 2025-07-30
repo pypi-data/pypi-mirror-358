bdfabdadccc9b1fda898ebd2374590e1bf9d214e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:21 +0000

    Add package python35-pymwm_diff/0.0.1/python35-pymwm_diff-0.0.1.tar.gz

---
+++ packages/python35-pymwm_diff/python35-pymwm_diff-0.0.1.tar.gz/python35-pymwm_diff-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="python35-pymwm_diff",
+    version="0.0.1",
+    packages=["python35-pymwm_diff"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
