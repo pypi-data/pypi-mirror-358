4d94586640f45f57058e0fc68fc4183bcb62f6f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:05:15 +0000

    Add package datalens-formula-testing/0.0.1/datalens-formula-testing-0.0.1.tar.gz

---
+++ packages/datalens-formula-testing/datalens-formula-testing-0.0.1.tar.gz/datalens-formula-testing-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="datalens-formula-testing",
+    version="0.0.1",
+    packages=["datalens-formula-testing"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
