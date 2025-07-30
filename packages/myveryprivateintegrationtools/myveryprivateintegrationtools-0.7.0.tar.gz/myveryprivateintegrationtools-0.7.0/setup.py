1e5db3459da3197a08da5d4c6be16a03ad74c500
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:11 +0000

    Add package myveryprivateintegrationtools/0.7.0/myveryprivateintegrationtools-0.7.0.tar.gz

---
+++ packages/myveryprivateintegrationtools/myveryprivateintegrationtools-0.7.0.tar.gz/myveryprivateintegrationtools-0.7.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="myveryprivateintegrationtools",            # The name of your package
+    version="0.7.0",                     # Version number
+    python_requires='>=3.13.3',            # Python version requirement
+    packages=find_packages()
+)
