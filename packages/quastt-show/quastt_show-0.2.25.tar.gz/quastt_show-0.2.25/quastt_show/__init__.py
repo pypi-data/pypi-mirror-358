95ad716055408723020c3506b18e2c92fa6accb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:08:32 +0000

    Add package quastt-show/0.2.25/quastt_show-0.2.25.tar.gz

---
+++ packages/quastt-show/quastt_show-0.2.25.tar.gz/quastt_show-0.2.25/quastt_show/__init__.py
@@ -0,0 +1,3 @@
+from .game import TriviaShow   
+from .logger import SimpleQLogger
+from .resources import LFMTracker,AutoRefactoringPrompter
---
+++ packages/quastt-show/quastt_show-0.2.25.tar.gz/quastt_show-0.2.25/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="quastt_show",
+    version="0.2.25",
+    description="Useful tools for monitoring,tracking,learning from mistakes.Just check our docs: https://quastt.com/",
+    author="RVA",
+    packages=find_packages(),
+    install_requires=["requests","traceback","time","threading","concurrent"],
+    python_requires=">=3.7",
+    entry_points={
+        "console_scripts": [
+            "quastt-show=quastt_show.game:main",
+        ],
+    },
+    include_package_data=True,
+)
