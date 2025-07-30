d2e6be4d3cc19bbbbbc2240ed88228560fdab1c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:05:11 +0000

    Add package datalens-constants/0.0.1/datalens-constants-0.0.1.tar.gz

---
+++ packages/datalens-constants/datalens-constants-0.0.1.tar.gz/datalens-constants-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="datalens-constants",
+    version="0.0.1",
+    packages=["datalens-constants"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
