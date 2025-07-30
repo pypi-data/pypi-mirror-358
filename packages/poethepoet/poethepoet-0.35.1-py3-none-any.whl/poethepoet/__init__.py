cf0bd24e6024755e08c83d0311aa2a3bbe3f4866
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:08:35 +0000

    Add package poethepoet/0.35.1/poethepoet-0.35.1-py3-none-any.whl

---
+++ packages/poethepoet/poethepoet-0.35.1-py3-none-any.whl/poethepoet/__init__.py
@@ -0,0 +1,77 @@
+from typing import Optional
+
+from .__version__ import __version__
+
+__all__ = ["__version__", "main"]
+
+
+def main():
+    import sys
+    from pathlib import Path
+
+    if len(sys.argv) > 1 and sys.argv[1].startswith("_"):
+        first_arg = sys.argv[1]  # built in task name
+        second_arg = next(iter(sys.argv[2:]), "")  # config path or poe alias
+        third_arg = next(iter(sys.argv[3:]), "")  # path to alias config for bash
+
+        if first_arg in ("_list_tasks", "_describe_tasks"):
+            target_path = (
+                str(Path(second_arg).expanduser().resolve()) if second_arg else None
+            )
+            _list_tasks(target_path=target_path)
+            return
+
+        target_path = ""
+        if second_arg:
+            if not second_arg.isalnum():
+                raise ValueError(f"Invalid alias: {second_arg!r}")
+
+            if third_arg:
+                if not Path(third_arg).expanduser().resolve().exists():
+                    raise ValueError(f"Invalid path: {third_arg!r}")
+
+                target_path = str(Path(third_arg).resolve())
+
+        if first_arg == "_zsh_completion":
+            from .completion.zsh import get_zsh_completion_script
+
+            print(get_zsh_completion_script(name=second_arg))
+            return
+
+        if first_arg == "_bash_completion":
+            from .completion.bash import get_bash_completion_script
+
+            print(get_bash_completion_script(name=second_arg, target_path=target_path))
+            return
+
+        if first_arg == "_fish_completion":
+            from .completion.fish import get_fish_completion_script
+
+            print(get_fish_completion_script(name=second_arg))
+            return
+
+    from .app import PoeThePoet
+
+    app = PoeThePoet(cwd=Path().resolve(), output=sys.stdout)
+    result = app(cli_args=sys.argv[1:])
+    if result:
+        raise SystemExit(result)
+
+
+def _list_tasks(target_path: Optional[str] = None):
+    """
+    A special task accessible via `poe _list_tasks` for use in shell completion
+
+    Note this code path should include minimal imports to avoid slowing down the shell
+    """
+
+    try:
+        from .config import PoeConfig
+
+        config = PoeConfig()
+        config.load(target_path, strict=False)
+        task_names = (task for task in config.task_names if task and task[0] != "_")
+        print(" ".join(task_names))
+    except Exception:
+        # this happens if there's no pyproject.toml present
+        pass
---
+++ packages/poethepoet/poethepoet-0.35.1-py3-none-any.whl/poethepoet/env/cache.py
@@ -0,0 +1,61 @@
+from os import environ
+from pathlib import Path
+from typing import TYPE_CHECKING, Optional, Union
+
+from ..exceptions import ExecutionError
+
+if TYPE_CHECKING:
+    from .ui import PoeUi
+
+POE_DEBUG = environ.get("POE_DEBUG", "0") == "1"
+
+
+class EnvFileCache:
+    _cache: dict[str, dict[str, str]] = {}
+    _ui: Optional["PoeUi"]
+    _project_dir: Path
+
+    def __init__(self, project_dir: Path, ui: Optional["PoeUi"]):
+        self._project_dir = project_dir
+        self._ui = ui
+
+    def get(self, envfile: Union[str, Path]) -> dict[str, str]:
+        """
+        Parse, cache, and return the environment variables from the envfile at the
+        given path. The path is used as the cache key.
+        """
+        from .parse import parse_env_file
+
+        envfile_path = self._project_dir.joinpath(Path(envfile).expanduser()).absolute()
+        envfile_path_str = str(envfile_path)
+
+        if envfile_path_str in self._cache:
+            return self._cache[envfile_path_str]
+
+        result = {}
+
+        if envfile_path.is_file():
+            try:
+                with envfile_path.open(encoding="utf-8") as envfile_file:
+                    result = parse_env_file(envfile_file.readlines())
+                if POE_DEBUG:
+                    print(f" + Loaded Envfile from {envfile_path}")
+            except ValueError as error:
+                message = error.args[0]
+                raise ExecutionError(
+                    f"Syntax error in referenced envfile: {envfile_path_str!r};"
+                    f" {message}"
+                ) from error
+
+        else:
+            if POE_DEBUG:
+                print(f" ! Envfile not found at {envfile_path}")
+
+            if self._ui is not None:
+                self._ui.print_msg(
+                    f"Warning: Poe failed to locate envfile at {envfile_path_str!r}",
+                    verbosity=1,
+                )
+
+        self._cache[envfile_path_str] = result
+        return result
