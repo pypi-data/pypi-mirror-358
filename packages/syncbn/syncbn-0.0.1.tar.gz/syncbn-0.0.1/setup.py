ae33327d2c4945bd5f9a65a89e5f2c71d8889bd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:37 +0000

    Add package syncbn/0.0.1/syncbn-0.0.1.tar.gz

---
+++ packages/syncbn/syncbn-0.0.1.tar.gz/syncbn-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="syncbn",
+    version="0.0.1",
+    packages=["syncbn"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
