14347943b6bbde534ebe6e44c7fcda44b3d720db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:37 +0000

    Add package datalens-compeng-pg/0.0.1/datalens-compeng-pg-0.0.1.tar.gz

---
+++ packages/datalens-compeng-pg/datalens-compeng-pg-0.0.1.tar.gz/datalens-compeng-pg-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="datalens-compeng-pg",
+    version="0.0.1",
+    packages=["datalens-compeng-pg"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
