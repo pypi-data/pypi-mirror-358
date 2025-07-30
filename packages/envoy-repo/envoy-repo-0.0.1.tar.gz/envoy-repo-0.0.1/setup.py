c95ce46b14c2a68c1df807a7a03de1b26e7e15d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:11 +0000

    Add package envoy-repo/0.0.1/envoy-repo-0.0.1.tar.gz

---
+++ packages/envoy-repo/envoy-repo-0.0.1.tar.gz/envoy-repo-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="envoy-repo",
+    version="0.0.1",
+    packages=["envoy-repo"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
