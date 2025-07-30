8fb65fb47e91f0c1380a54f5aae9a96c6bad6445
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:20 +0000

    Add package win32com/0.0.1/win32com-0.0.1.tar.gz

---
+++ packages/win32com/win32com-0.0.1.tar.gz/win32com-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="win32com",
+    version="0.0.1",
+    packages=["win32com"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
