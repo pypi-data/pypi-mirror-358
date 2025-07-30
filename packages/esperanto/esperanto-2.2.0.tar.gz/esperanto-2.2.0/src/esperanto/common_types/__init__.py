0d2cfbe22c7812e50e5a2e0c511605e9a8188e3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:27 +0000

    Add package esperanto/2.2.0/esperanto-2.2.0.tar.gz

---
+++ packages/esperanto/esperanto-2.2.0.tar.gz/esperanto-2.2.0/src/esperanto/common_types/__init__.py
@@ -0,0 +1,31 @@
+"""Types module for Esperanto."""
+
+from .model import Model
+from .response import (
+    ChatCompletion,
+    Choice,
+    ChatCompletionChunk,
+    Choice,
+    DeltaMessage,
+    Message,
+    StreamChoice,
+    Usage,
+)
+from .stt import TranscriptionResponse
+from .task_type import EmbeddingTaskType
+from .tts import AudioResponse
+
+__all__ = [
+    "Usage",
+    "Message",
+    "DeltaMessage",
+    "Choice",
+    "Choice",
+    "StreamChoice",
+    "ChatCompletion",
+    "ChatCompletionChunk",
+    "TranscriptionResponse",
+    "AudioResponse",
+    "Model",
+    "EmbeddingTaskType",
+]
