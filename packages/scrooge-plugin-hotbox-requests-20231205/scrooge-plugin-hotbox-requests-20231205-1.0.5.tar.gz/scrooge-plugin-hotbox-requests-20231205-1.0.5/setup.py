efec51098245601f1a4da04420b6f5e070045ab8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:42 +0000

    Add package scrooge-plugin-hotbox-requests-20231205/1.0.5/scrooge-plugin-hotbox-requests-20231205-1.0.5.tar.gz

---
+++ packages/scrooge-plugin-hotbox-requests-20231205/scrooge-plugin-hotbox-requests-20231205-1.0.5.tar.gz/scrooge-plugin-hotbox-requests-20231205-1.0.5/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="scrooge-plugin-hotbox-requests-20231205",
+    version="1.0.5",
+    packages=["scrooge-plugin-hotbox-requests-20231205"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
