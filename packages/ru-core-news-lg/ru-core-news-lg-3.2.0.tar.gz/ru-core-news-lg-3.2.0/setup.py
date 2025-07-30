f97fcc2e2e5b4a537ef1b5f9c6a793a86ccb5866
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:09 +0000

    Add package ru-core-news-lg/3.2.0/ru-core-news-lg-3.2.0.tar.gz

---
+++ packages/ru-core-news-lg/ru-core-news-lg-3.2.0.tar.gz/ru-core-news-lg-3.2.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ru-core-news-lg",
+    version="3.2.0",
+    packages=["ru-core-news-lg"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
