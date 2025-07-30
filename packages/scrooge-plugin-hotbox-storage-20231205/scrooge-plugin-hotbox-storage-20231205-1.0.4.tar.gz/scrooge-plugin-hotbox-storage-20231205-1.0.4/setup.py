c335744710d0f7090219a8d9453c8c4903d58fc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:43 +0000

    Add package scrooge-plugin-hotbox-storage-20231205/1.0.4/scrooge-plugin-hotbox-storage-20231205-1.0.4.tar.gz

---
+++ packages/scrooge-plugin-hotbox-storage-20231205/scrooge-plugin-hotbox-storage-20231205-1.0.4.tar.gz/scrooge-plugin-hotbox-storage-20231205-1.0.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="scrooge-plugin-hotbox-storage-20231205",
+    version="1.0.4",
+    packages=["scrooge-plugin-hotbox-storage-20231205"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
