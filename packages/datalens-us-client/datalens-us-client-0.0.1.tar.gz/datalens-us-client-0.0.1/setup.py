360bf1c362d85d23aaa6b2c68d91113f3193b10b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:38 +0000

    Add package datalens-us-client/0.0.1/datalens-us-client-0.0.1.tar.gz

---
+++ packages/datalens-us-client/datalens-us-client-0.0.1.tar.gz/datalens-us-client-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="datalens-us-client",
+    version="0.0.1",
+    packages=["datalens-us-client"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
