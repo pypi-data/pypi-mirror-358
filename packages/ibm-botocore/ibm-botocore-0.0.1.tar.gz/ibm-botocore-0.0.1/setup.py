2bb2eba7c247b6e9c63e74dbf344f843a141e2d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:25 +0000

    Add package ibm-botocore/0.0.1/ibm-botocore-0.0.1.tar.gz

---
+++ packages/ibm-botocore/ibm-botocore-0.0.1.tar.gz/ibm-botocore-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ibm-botocore",
+    version="0.0.1",
+    packages=["ibm-botocore"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
