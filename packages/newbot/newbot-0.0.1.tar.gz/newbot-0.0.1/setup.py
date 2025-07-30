24c503005193ba377406338f9dc4b0a096c1641a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:21 +0000

    Add package newbot/0.0.1/newbot-0.0.1.tar.gz

---
+++ packages/newbot/newbot-0.0.1.tar.gz/newbot-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="newbot",
+    version="0.0.1",
+    packages=["newbot"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
