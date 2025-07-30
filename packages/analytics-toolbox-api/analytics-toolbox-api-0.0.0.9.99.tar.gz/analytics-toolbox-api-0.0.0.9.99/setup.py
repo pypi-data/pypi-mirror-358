23d1db098df605106da8217048344e40670a402d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:28 +0000

    Add package analytics-toolbox-api/0.0.0.9.99/analytics-toolbox-api-0.0.0.9.99.tar.gz

---
+++ packages/analytics-toolbox-api/analytics-toolbox-api-0.0.0.9.99.tar.gz/analytics-toolbox-api-0.0.0.9.99/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="analytics-toolbox-api",
+    version="0.0.0.9.99",
+    packages=["analytics-toolbox-api"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
