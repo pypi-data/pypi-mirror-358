80c9bfa7fd7c61332682f089f49ce55725d03eb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:45:14 +0000

    Add package uxarray/2025.6.0/uxarray-2025.6.0.tar.gz

---
+++ packages/uxarray/uxarray-2025.6.0.tar.gz/uxarray-2025.6.0/uxarray/cross_sections/dataarray_accessor.py
@@ -0,0 +1,211 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, List, Set, Tuple, Union
+
+if TYPE_CHECKING:
+    pass
+
+
+class UxDataArrayCrossSectionAccessor:
+    """Accessor for cross-section operations on a ``UxDataArray``"""
+
+    def __init__(self, uxda) -> None:
+        self.uxda = uxda
+
+    def __repr__(self):
+        prefix = "<uxarray.UxDataArray.cross_section>\n"
+        methods_heading = "Supported Methods:\n"
+
+        methods_heading += "  * constant_latitude(lat, inverse_indices)\n"
+        methods_heading += "  * constant_longitude(lon, inverse_indices)\n"
+        methods_heading += "  * constant_latitude_interval(lats, inverse_indices)\n"
+        methods_heading += "  * constant_longitude_interval(lons, inverse_indices)\n"
+
+        return prefix + methods_heading
+
+    def constant_latitude(
+        self, lat: float, inverse_indices: Union[List[str], Set[str], bool] = False
+    ):
+        """Extracts a cross-section of the data array by selecting all faces that
+        intersect with a specified line of constant latitude.
+
+        Parameters
+        ----------
+        lat : float
+            The latitude at which to extract the cross-section, in degrees.
+            Must be between -90.0 and 90.0
+        inverse_indices : Union[List[str], Set[str], bool], optional
+            Controls storage of original grid indices. Options:
+            - True: Stores original face indices
+            - List/Set of strings: Stores specified index types (valid values: "face", "edge", "node")
+            - False: No index storage (default)
+
+        Returns
+        -------
+        uxarray.UxDataArray
+            A subset of the original data array containing only the faces that intersect
+            with the specified latitude.
+
+        Raises
+        ------
+        ValueError
+            If no intersections are found at the specified longitude or the data variable is not face-centered.
+
+        Examples
+        --------
+        >>> # Extract data at 15.5°S latitude
+        >>> cross_section = uxda.cross_section.constant_latitude(lat=-15.5)
+
+        Notes
+        -----
+        The initial execution time may be significantly longer than subsequent runs
+        due to Numba's just-in-time compilation. Subsequent calls will be faster due to caching.
+        """
+        if not self.uxda._face_centered():
+            raise ValueError(
+                "Cross sections are only supported for face-centered data variables."
+            )
+
+        faces = self.uxda.uxgrid.get_faces_at_constant_latitude(lat)
+
+        return self.uxda.isel(n_face=faces, inverse_indices=inverse_indices)
+
+    def constant_longitude(
+        self, lon: float, inverse_indices: Union[List[str], Set[str], bool] = False
+    ):
+        """Extracts a cross-section of the data array by selecting all faces that
+        intersect with a specified line of constant longitude.
+
+        Parameters
+        ----------
+        lon : float
+            The latitude at which to extract the cross-section, in degrees.
+            Must be between -180.0 and 180.0
+        inverse_indices : Union[List[str], Set[str], bool], optional
+            Controls storage of original grid indices. Options:
+            - True: Stores original face indices
+            - List/Set of strings: Stores specified index types (valid values: "face", "edge", "node")
+            - False: No index storage (default)
+
+        Returns
+        -------
+        uxarray.UxDataArray
+            A subset of the original data array containing only the faces that intersect
+            with the specified longitude.
+
+        Raises
+        ------
+        ValueError
+            If no intersections are found at the specified longitude or the data variable is not face-centered.
+
+        Examples
+        --------
+        >>> # Extract data at 0° longitude
+        >>> cross_section = uxda.cross_section.constant_latitude(lon=0.0)
+
+        Notes
+        -----
+        The initial execution time may be significantly longer than subsequent runs
+        due to Numba's just-in-time compilation. Subsequent calls will be faster due to caching.
+        """
+        if not self.uxda._face_centered():
+            raise ValueError(
+                "Cross sections are only supported for face-centered data variables."
+            )
+
+        faces = self.uxda.uxgrid.get_faces_at_constant_longitude(
+            lon,
+        )
+
+        return self.uxda.isel(n_face=faces, inverse_indices=inverse_indices)
+
+    def constant_latitude_interval(
+        self,
+        lats: Tuple[float, float],
+        inverse_indices: Union[List[str], Set[str], bool] = False,
+    ):
+        """Extracts a cross-section of data by selecting all faces that
+        are within a specified latitude interval.
+
+        Parameters
+        ----------
+        lats : Tuple[float, float]
+            The latitude interval (min_lat, max_lat) at which to extract the cross-section,
+            in degrees. Values must be between -90.0 and 90.0
+        inverse_indices : Union[List[str], Set[str], bool], optional
+            Controls storage of original grid indices. Options:
+            - True: Stores original face indices
+            - List/Set of strings: Stores specified index types (valid values: "face", "edge", "node")
+            - False: No index storage (default)
+
+        Returns
+        -------
+        uxarray.UxDataArray
+            A subset of the original data array containing only the faces that are within a specified latitude interval.
+
+        Raises
+        ------
+        ValueError
+            If no faces are found within the specified latitude interval.
+
+        Examples
+        --------
+        >>> # Extract data between 30°S and 30°N latitude
+        >>> cross_section = uxda.cross_section.constant_latitude_interval(
+        ...     lats=(-30.0, 30.0)
+        ... )
+
+
+        Notes
+        -----
+        The initial execution time may be significantly longer than subsequent runs
+        due to Numba's just-in-time compilation. Subsequent calls will be faster due to caching.
+        """
+        faces = self.uxda.uxgrid.get_faces_between_latitudes(lats)
+
+        return self.uxda.isel(n_face=faces, inverse_indices=inverse_indices)
+
+    def constant_longitude_interval(
+        self,
+        lons: Tuple[float, float],
+        inverse_indices: Union[List[str], Set[str], bool] = False,
+    ):
+        """Extracts a cross-section of data by selecting all faces are within a specifed longitude interval.
+
+        Parameters
+        ----------
+        lons : Tuple[float, float]
+            The longitude interval (min_lon, max_lon) at which to extract the cross-section,
+            in degrees. Values must be between -180.0 and 180.0
+        inverse_indices : Union[List[str], Set[str], bool], optional
+            Controls storage of original grid indices. Options:
+            - True: Stores original face indices
+            - List/Set of strings: Stores specified index types (valid values: "face", "edge", "node")
+            - False: No index storage (default)
+
+        Returns
+        -------
+        uxarray.UxDataArray
+            A subset of the original data array containing only the faces that intersect
+            with the specified longitude interval.
+
+        Raises
+        ------
+        ValueError
+            If no faces are found within the specified longitude interval.
+
+        Examples
+        --------
+        >>> # Extract data between 0° and 45° longitude
+        >>> cross_section = uxda.cross_section.constant_longitude_interval(
+        ...     lons=(0.0, 45.0)
+        ... )
+
+        Notes
+        -----
+        The initial execution time may be significantly longer than subsequent runs
+        due to Numba's just-in-time compilation. Subsequent calls will be faster due to caching.
+        """
+        faces = self.uxda.uxgrid.get_faces_between_longitudes(lons)
+
+        return self.uxda.isel(n_face=faces, inverse_indices=inverse_indices)
---
+++ packages/uxarray/uxarray-2025.6.0.tar.gz/uxarray-2025.6.0/uxarray/subset/dataarray_accessor.py
@@ -0,0 +1,128 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, List, Optional, Set, Tuple, Union
+
+import numpy as np
+
+if TYPE_CHECKING:
+    pass
+
+
+class DataArraySubsetAccessor:
+    """Accessor for performing unstructured grid subsetting with a data
+    variable, accessed through ``UxDataArray.subset``"""
+
+    def __init__(self, uxda) -> None:
+        self.uxda = uxda
+
+    def __repr__(self):
+        prefix = "<uxarray.UxDataArray.subset>\n"
+        methods_heading = "Supported Methods:\n"
+
+        methods_heading += "  * nearest_neighbor(center_coord, k, element, inverse_indices, **kwargs)\n"
+        methods_heading += (
+            "  * bounding_circle(center_coord, r, element, inverse_indices, **kwargs)\n"
+        )
+        methods_heading += (
+            "  * bounding_box(lon_bounds, lat_bounds, inverse_indices, **kwargs)\n"
+        )
+
+        return prefix + methods_heading
+
+    def bounding_box(
+        self,
+        lon_bounds: Union[Tuple, List, np.ndarray],
+        lat_bounds: Union[Tuple, List, np.ndarray],
+        inverse_indices: Union[List[str], Set[str], bool] = False,
+    ):
+        """Subsets an unstructured grid between two latitude and longitude
+        points which form a bounding box.
+
+        A bounding box may span the antimeridian, when the pair of longitude points is given in descending order (
+        i.e. the first longitude point is greater than the second).
+
+        Parameters
+        ----------
+        lon_bounds: tuple, list, np.ndarray
+            (lon_left, lon_right) where lon_left < lon_right when the bounding box does not span
+            the antimeridian, otherwise lon_left > lon_right, both between [-180, 180]
+        lat_bounds: tuple, list, np.ndarray
+            (lat_bottom, lat_top) where lat_top > lat_bottom and between [-90, 90]
+        method: str
+            Bounding Box Method, currently supports 'coords', which ensures the coordinates of the corner nodes,
+            face centers, or edge centers lie within the bounds.
+        element: str
+            Element for use with `coords` comparison, one of `nodes`, `face centers`, or `edge centers`
+        inverse_indices : Union[List[str], Set[str], bool], optional
+            Controls storage of original grid indices. Options:
+            - True: Stores original face indices
+            - List/Set of strings: Stores specified index types (valid values: "face", "edge", "node")
+            - False: No index storage (default)
+        """
+        grid = self.uxda.uxgrid.subset.bounding_box(
+            lon_bounds, lat_bounds, inverse_indices=inverse_indices
+        )
+
+        return self.uxda._slice_from_grid(grid)
+
+    def bounding_circle(
+        self,
+        center_coord: Union[Tuple, List, np.ndarray],
+        r: Union[float, int],
+        element: Optional[str] = "face centers",
+        inverse_indices: Union[List[str], Set[str], bool] = False,
+        **kwargs,
+    ):
+        """Subsets an unstructured grid by returning all elements within some
+        radius (in degrees) from a center coord.
+
+        Parameters
+        ----------
+        center_coord : tuple, list, np.ndarray
+            Longitude and latitude of the center of the bounding circle
+        r: scalar, int, float
+            Radius of bounding circle (in degrees)
+        element: str
+            Element for use with `coords` comparison, one of `nodes`, `face centers`, or `edge centers`
+        inverse_indices : Union[List[str], Set[str], bool], optional
+            Controls storage of original grid indices. Options:
+            - True: Stores original face indices
+            - List/Set of strings: Stores specified index types (valid values: "face", "edge", "node")
+            - False: No index storage (default)
+        """
+        grid = self.uxda.uxgrid.subset.bounding_circle(
+            center_coord, r, element, inverse_indices=inverse_indices, **kwargs
+        )
+        return self.uxda._slice_from_grid(grid)
+
+    def nearest_neighbor(
+        self,
+        center_coord: Union[Tuple, List, np.ndarray],
+        k: int,
+        element: Optional[str] = "face centers",
+        inverse_indices: Union[List[str], Set[str], bool] = False,
+        **kwargs,
+    ):
+        """Subsets an unstructured grid by returning the ``k`` closest
+        neighbors from a center coordinate.
+
+        Parameters
+        ----------
+        center_coord : tuple, list, np.ndarray
+            Longitude and latitude of the center of the bounding circle
+        k: int
+            Number of neighbors to query
+        element: str
+            Element for use with `coords` comparison, one of `nodes`, `face centers`, or `edge centers`
+        inverse_indices : Union[List[str], Set[str], bool], optional
+            Controls storage of original grid indices. Options:
+            - True: Stores original face indices
+            - List/Set of strings: Stores specified index types (valid values: "face", "edge", "node")
+            - False: No index storage (default)
+        """
+
+        grid = self.uxda.uxgrid.subset.nearest_neighbor(
+            center_coord, k, element, inverse_indices=inverse_indices, **kwargs
+        )
+
+        return self.uxda._slice_from_grid(grid)
