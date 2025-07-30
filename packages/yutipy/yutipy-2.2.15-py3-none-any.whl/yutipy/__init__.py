59b7aa7c578965e6f27c9b6e33c20b13065ee700
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:03 +0000

    Add package yutipy/2.2.15/yutipy-2.2.15-py3-none-any.whl

---
+++ packages/yutipy/yutipy-2.2.15-py3-none-any.whl/yutipy/__init__.py
@@ -0,0 +1,23 @@
+from yutipy import (
+    deezer,
+    exceptions,
+    itunes,
+    kkbox,
+    lastfm,
+    logger,
+    musicyt,
+    spotify,
+    yutipy_music,
+)
+
+__all__ = [
+    "deezer",
+    "exceptions",
+    "itunes",
+    "kkbox",
+    "lastfm",
+    "logger",
+    "musicyt",
+    "spotify",
+    "yutipy_music",
+]
