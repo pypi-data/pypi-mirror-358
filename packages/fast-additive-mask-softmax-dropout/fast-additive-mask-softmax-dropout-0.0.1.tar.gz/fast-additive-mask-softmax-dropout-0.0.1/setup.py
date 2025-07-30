4f6dfcaf463d6384dc2761526e495f8ba2c7a953
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:54 +0000

    Add package fast-additive-mask-softmax-dropout/0.0.1/fast-additive-mask-softmax-dropout-0.0.1.tar.gz

---
+++ packages/fast-additive-mask-softmax-dropout/fast-additive-mask-softmax-dropout-0.0.1.tar.gz/fast-additive-mask-softmax-dropout-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="fast-additive-mask-softmax-dropout",
+    version="0.0.1",
+    packages=["fast-additive-mask-softmax-dropout"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
