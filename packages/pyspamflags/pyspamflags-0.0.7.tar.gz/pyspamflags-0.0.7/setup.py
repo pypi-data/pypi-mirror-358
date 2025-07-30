8c2a7a487e2831b2395322c888a0100c673fb73d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:05:08 +0000

    Add package pyspamflags/0.0.7/pyspamflags-0.0.7.tar.gz

---
+++ packages/pyspamflags/pyspamflags-0.0.7.tar.gz/pyspamflags-0.0.7/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pyspamflags",
+    version="0.0.7",
+    packages=["pyspamflags"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
