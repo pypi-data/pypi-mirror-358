662d2bbb2e87a7058ea15dbec8c7286d831d5b61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:55 +0000

    Add package pypo-mediastat/0.0.1/pypo-mediastat-0.0.1.tar.gz

---
+++ packages/pypo-mediastat/pypo-mediastat-0.0.1.tar.gz/pypo-mediastat-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pypo-mediastat",
+    version="0.0.1",
+    packages=["pypo-mediastat"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
