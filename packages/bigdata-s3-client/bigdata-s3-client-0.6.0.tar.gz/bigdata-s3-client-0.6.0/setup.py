ad9077cd571bee0181f9d4cd78641ba07c6f243f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:39 +0000

    Add package bigdata-s3-client/0.6.0/bigdata-s3-client-0.6.0.tar.gz

---
+++ packages/bigdata-s3-client/bigdata-s3-client-0.6.0.tar.gz/bigdata-s3-client-0.6.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="bigdata-s3-client",
+    version="0.6.0",
+    packages=["bigdata-s3-client"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
