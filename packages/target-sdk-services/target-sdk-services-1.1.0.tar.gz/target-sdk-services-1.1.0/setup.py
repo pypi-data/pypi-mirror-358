28bdd350e7c4aef5bbcc5877d7cf3647a8a2caac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:06 +0000

    Add package target-sdk-services/1.1.0/target-sdk-services-1.1.0.tar.gz

---
+++ packages/target-sdk-services/target-sdk-services-1.1.0.tar.gz/target-sdk-services-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="target-sdk-services",
+    version="1.1.0",
+    packages=["target-sdk-services"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
