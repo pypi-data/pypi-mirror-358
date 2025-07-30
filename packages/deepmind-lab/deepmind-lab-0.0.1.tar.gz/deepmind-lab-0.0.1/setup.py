42052784bf7931fc8cfab0676b32352d15763732
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:07 +0000

    Add package deepmind-lab/0.0.1/deepmind-lab-0.0.1.tar.gz

---
+++ packages/deepmind-lab/deepmind-lab-0.0.1.tar.gz/deepmind-lab-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="deepmind-lab",
+    version="0.0.1",
+    packages=["deepmind-lab"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
