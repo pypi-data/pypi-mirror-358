4536b918c0c347077e1066392dab03deebeb6bb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:09 +0000

    Add package jakarta.annotation-api/2.1.1/jakarta.annotation-api-2.1.1.tar.gz

---
+++ packages/jakarta.annotation-api/jakarta.annotation-api-2.1.1.tar.gz/jakarta.annotation-api-2.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="jakarta.annotation-api",
+    version="2.1.1",
+    packages=["jakarta.annotation-api"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
