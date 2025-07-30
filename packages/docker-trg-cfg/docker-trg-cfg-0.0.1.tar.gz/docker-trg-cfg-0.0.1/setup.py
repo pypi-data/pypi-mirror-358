e624300d6444e2851cce02b0173b913fe1fec801
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:10 +0000

    Add package docker-trg-cfg/0.0.1/docker-trg-cfg-0.0.1.tar.gz

---
+++ packages/docker-trg-cfg/docker-trg-cfg-0.0.1.tar.gz/docker-trg-cfg-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="docker-trg-cfg",
+    version="0.0.1",
+    packages=["docker-trg-cfg"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
