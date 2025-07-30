64cbca25fe3f450525876f937ca6bcb927e7caa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:05 +0000

    Add package freestyle/0.0.16/freestyle-0.0.16-py3-none-any.whl

---
+++ packages/freestyle/freestyle-0.0.16-py3-none-any.whl/freestyle/__init__.py
@@ -0,0 +1,9 @@
+from .client import Freestyle
+from .dev_server import (
+    FreestyleDevServer,
+    FreestyleDevServerFilesystem,
+    FreestyleDevServerProcess,
+)
+
+
+from _openapi_client.models import *
