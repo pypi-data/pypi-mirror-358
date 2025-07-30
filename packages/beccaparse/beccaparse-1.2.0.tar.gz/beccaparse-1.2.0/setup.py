d374dba8ea3951152332af8b71178daa1f537db0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:29 +0000

    Add package beccaparse/1.2.0/beccaparse-1.2.0.tar.gz

---
+++ packages/beccaparse/beccaparse-1.2.0.tar.gz/beccaparse-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="beccaparse",
+    version="1.2.0",
+    packages=["beccaparse"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
