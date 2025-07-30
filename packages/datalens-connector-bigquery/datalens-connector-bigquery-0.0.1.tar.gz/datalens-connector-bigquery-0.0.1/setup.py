ad5f134dcd6c67c864f63678dacdfb00f9e3e1be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:37 +0000

    Add package datalens-connector-bigquery/0.0.1/datalens-connector-bigquery-0.0.1.tar.gz

---
+++ packages/datalens-connector-bigquery/datalens-connector-bigquery-0.0.1.tar.gz/datalens-connector-bigquery-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="datalens-connector-bigquery",
+    version="0.0.1",
+    packages=["datalens-connector-bigquery"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
