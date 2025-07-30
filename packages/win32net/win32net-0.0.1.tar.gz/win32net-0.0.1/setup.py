e21d13aeeb27bc29e83ae58f2839a36be9c1177f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:20 +0000

    Add package win32net/0.0.1/win32net-0.0.1.tar.gz

---
+++ packages/win32net/win32net-0.0.1.tar.gz/win32net-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="win32net",
+    version="0.0.1",
+    packages=["win32net"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
