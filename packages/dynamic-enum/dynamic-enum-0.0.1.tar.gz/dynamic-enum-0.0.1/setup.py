707c430b251e6095c085dea295bdcd58831e4b20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:38 +0000

    Add package dynamic-enum/0.0.1/dynamic-enum-0.0.1.tar.gz

---
+++ packages/dynamic-enum/dynamic-enum-0.0.1.tar.gz/dynamic-enum-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dynamic-enum",
+    version="0.0.1",
+    packages=["dynamic-enum"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
