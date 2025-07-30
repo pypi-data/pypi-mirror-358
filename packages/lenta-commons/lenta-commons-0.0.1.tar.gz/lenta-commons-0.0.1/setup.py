2dad2a400a85f41c858dfaaec0bc02b74f741e68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:18 +0000

    Add package lenta-commons/0.0.1/lenta-commons-0.0.1.tar.gz

---
+++ packages/lenta-commons/lenta-commons-0.0.1.tar.gz/lenta-commons-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="lenta-commons",
+    version="0.0.1",
+    packages=["lenta-commons"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
