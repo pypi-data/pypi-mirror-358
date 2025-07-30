ad199aff5dac1e8ca84643d3d4ee8c4367cd9203
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:08:19 +0000

    Add package python3-cyberfusion-pagerduty-voys-webhook/1.1.2/python3_cyberfusion_pagerduty_voys_webhook-1.1.2.tar.gz

---
+++ packages/python3-cyberfusion-pagerduty-voys-webhook/python3_cyberfusion_pagerduty_voys_webhook-1.1.2.tar.gz/python3_cyberfusion_pagerduty_voys_webhook-1.1.2/src/cyberfusion/PagerDutyVoysWebhook/settings.py
@@ -0,0 +1,19 @@
+from pydantic import (
+    BaseSettings,
+)
+
+
+class Settings(BaseSettings):
+    """Settings."""
+
+    api_key: str = "change_me"
+    secret_key: str = "change_me"
+    escalation_policy_id: str = "AB1CD2E"
+
+    class Config:
+        """Config."""
+
+        secrets_dir = "/etc/pagerduty-voys-webhook"
+
+
+settings = Settings()
