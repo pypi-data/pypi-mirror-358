280768c7c4b930b7a337372ff6fef6d324efaeef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:07:14 +0000

    Add package Ryzenth/2.0.8/ryzenth-2.0.8-py3-none-any.whl

---
+++ packages/Ryzenth/ryzenth-2.0.8-py3-none-any.whl/Ryzenth/tests/test_ryzenthapiclient.py
@@ -0,0 +1,17 @@
+from Ryzenth import RyzenthApiClient
+from Ryzenth.enums import ResponseType
+
+clients = RyzenthApiClient(
+    tools_name=["itzpire"],
+    api_key={"itzpire": [{}]},
+    rate_limit=100,
+    use_httpx=True # Fixed Aiohttp RuntimeError: no running event loop
+)
+
+def test_itzpire():
+    result = clients.sync_get(
+        tool="itzpire",
+        path="/games/siapakah-aku",
+        use_type=ResponseType.JSON
+    )
+    assert result is not None
