f2f2105ea3472eddfa6fc6039f4a4bc39722025c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:37 +0000

    Add package datalens-attrs-model-mapper/0.0.1/datalens-attrs-model-mapper-0.0.1.tar.gz

---
+++ packages/datalens-attrs-model-mapper/datalens-attrs-model-mapper-0.0.1.tar.gz/datalens-attrs-model-mapper-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="datalens-attrs-model-mapper",
+    version="0.0.1",
+    packages=["datalens-attrs-model-mapper"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
