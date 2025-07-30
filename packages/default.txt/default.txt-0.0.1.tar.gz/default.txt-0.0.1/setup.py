e785eeabeffdc22d461d950afe522099b2d654f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:06 +0000

    Add package default.txt/0.0.1/default.txt-0.0.1.tar.gz

---
+++ packages/default.txt/default.txt-0.0.1.tar.gz/default.txt-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="default.txt",
+    version="0.0.1",
+    packages=["default.txt"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
