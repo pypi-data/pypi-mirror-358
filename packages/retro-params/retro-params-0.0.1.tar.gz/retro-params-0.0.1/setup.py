bcce501f6f05034d424723e2ca68ed5e46c6f5ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:41 +0000

    Add package retro-params/0.0.1/retro-params-0.0.1.tar.gz

---
+++ packages/retro-params/retro-params-0.0.1.tar.gz/retro-params-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="retro-params",
+    version="0.0.1",
+    packages=["retro-params"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
