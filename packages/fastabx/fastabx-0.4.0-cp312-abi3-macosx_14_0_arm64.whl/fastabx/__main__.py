1d9b39aac2536f20a22401489e579c19177176ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:07:03 +0000

    Add package fastabx/0.4.0/fastabx-0.4.0-cp312-abi3-macosx_14_0_arm64.whl

---
+++ packages/fastabx/fastabx-0.4.0-cp312-abi3-macosx_14_0_arm64.whl/fastabx/__main__.py
@@ -0,0 +1,39 @@
+"""Entry point for the ZeroSpeech ABX evaluation."""
+
+import argparse
+from argparse import ArgumentDefaultsHelpFormatter
+
+from fastabx.distance import available_distances
+from fastabx.zerospeech import zerospeech_abx
+
+
+def main() -> None:
+    """ZeroSpeech ABX evaluation."""
+    parser = argparse.ArgumentParser(description="ZeroSpeech ABX", formatter_class=ArgumentDefaultsHelpFormatter)
+    parser.add_argument("item", help="Path to the item file")
+    parser.add_argument("features", help="Path to the features directory")
+    parser.add_argument("--frequency", type=int, default=50, help="Feature frequency (in Hz)")
+    parser.add_argument("--speaker", choices=["within", "across"], default="within", help="Speaker mode")
+    parser.add_argument("--context", choices=["within", "any"], default="within", help="Context mode")
+    parser.add_argument("--distance", choices=available_distances(), default="angular", help="Distance")
+    parser.add_argument("--max-size-group", type=int, default=10, help="Maximum number of A, B, or X in a cell")
+    parser.add_argument("--max-x-across", type=int, default=5, help="With 'across', maximum number of X given (A, B)")
+    parser.add_argument("--seed", type=int, default=0, help="Random seed")
+    args = parser.parse_args()
+
+    score = zerospeech_abx(
+        args.item,
+        args.features,
+        speaker=args.speaker,
+        context=args.context,
+        distance=args.distance,
+        frequency=args.frequency,
+        max_size_group=args.max_size_group,
+        max_x_across=args.max_x_across,
+        seed=args.seed,
+    )
+    print(f"ABX error rate: {score:.3%}")  # noqa: T201
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/fastabx/fastabx-0.4.0-cp312-abi3-macosx_14_0_arm64.whl/fastabx/zerospeech.py
@@ -0,0 +1,70 @@
+"""ZeroSpeech ABX evaluation. Reproduces ZeroSpeech 2021."""
+
+from collections.abc import Callable
+from pathlib import Path
+from typing import Literal
+
+import torch
+
+from fastabx.dataset import Dataset
+from fastabx.distance import DistanceName
+from fastabx.score import Score
+from fastabx.subsample import Subsampler
+from fastabx.task import Task
+
+
+class InvalidSpeakerOrContextError(ValueError):
+    """The speaker or context conditions are not set correctly."""
+
+
+def zerospeech_abx(
+    item: str | Path,
+    root: str | Path,
+    *,
+    speaker: Literal["within", "across"] = "within",
+    context: Literal["within", "any"] = "within",
+    distance: DistanceName = "angular",
+    frequency: int = 50,
+    feature_maker: Callable[[str | Path], torch.Tensor] = torch.load,
+    max_size_group: int | None = 10,
+    max_x_across: int | None = 5,
+    extension: str = ".pt",
+    seed: int = 0,
+) -> float:
+    """Compute the ABX similarly to the ZeroSpeech 2021 challenge.
+
+    On triphone or phoneme, described by an item file.
+    Within or across speaker, and within context or ignoring context.
+
+    :param item: the item file
+    :param root: the root directory containing either the features or the audio files
+    :param speaker: the speaker mode, either "within" or "across"
+    :param context: the context mode, either "within" or "any"
+    :param distance: the distance metric, "angular" (same as "cosine"), "euclidean", "kl_symmetric" or "identical"
+    :param frequency: the feature frequency of the features / the output of the feature maker, in Hz. Default is 50 Hz
+    :param feature_maker: the feature maker. Defaults to just loading the file with ``torch.load``
+    :param max_size_group: maximum number of instances of A, B, or X in each :py:class:`.Cell`. Default is 10.
+        Passed to the :py:class:`.Subsampler` of the :py:class:`.Task`. Disabled if set to ``None``
+    :param max_x_across: in the "across" speaker mode, maximum number of X considered for given values of A and B.
+        Default is 5. Passed to the :py:class:`.Subsampler` of the :py:class:`.Task`. Disabled if set to ``None``
+    :param extension: the filename extension of the files to process in ``root``, default is ".pt"
+    :param seed: the random seed for the subsampling, default is 0
+    """
+    dataset = Dataset.from_item(item, root, frequency, feature_maker=feature_maker, extension=extension)
+    by: list[str] | None
+    across: list[str] | None
+    match (speaker, context):
+        case ("within", "within"):
+            by, across = ["prev-phone", "next-phone", "speaker"], None
+        case ("within", "any"):
+            by, across = ["speaker"], None
+        case ("across", "within"):
+            by, across = ["prev-phone", "next-phone"], ["speaker"]
+        case ("across", "any"):
+            by, across = None, ["speaker"]
+        case _:
+            raise InvalidSpeakerOrContextError
+    subsampler = Subsampler(max_size_group, max_x_across, seed)
+    task = Task(dataset, on="#phone", by=by, across=across, subsampler=subsampler)
+    levels = ([("next-phone", "prev-phone")] if context == "within" else []) + ["speaker"]
+    return Score(task, distance).collapse(levels=levels)
