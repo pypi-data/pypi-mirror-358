c0f7a2a86661058b74e3bb4a06631c7a839913a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:22 +0000

    Add package yandex-tokenizer/0.0.1/yandex-tokenizer-0.0.1.tar.gz

---
+++ packages/yandex-tokenizer/yandex-tokenizer-0.0.1.tar.gz/yandex-tokenizer-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="yandex-tokenizer",
+    version="0.0.1",
+    packages=["yandex-tokenizer"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
