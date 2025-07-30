f16d55732db6581a634e4a3ac275e094c29038fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:07:08 +0000

    Add package mobius-mpd/0.1.1/mobius_mpd-0.1.1-py3-none-any.whl

---
+++ packages/mobius-mpd/mobius_mpd-0.1.1-py3-none-any.whl/mobius_mpd/__init__.py
@@ -0,0 +1,19 @@
+
+"""mobius_mpd
+
+A friction‑free Python package that exposes the Möbius Perspective‑Distortion (MPD)
+data‑augmentation transform for PyTorch **and** Albumentations.
+
+Paper to cite
+-------------
+Chhipa, Prakash Chandra, et al. *"Möbius transform for mitigating perspective distortions in representation learning."* European Conference on Computer Vision (ECCV) 2024.
+
+If you use this package in academic work, **please cite the paper above**.
+"""
+
+from .torch_transform import MobiusMPDTransform
+from .albumentations_transform import A_MobiusMPD
+
+__all__ = ["MobiusMPDTransform", "A_MobiusMPD"]
+
+__version__ = "0.1.1"
