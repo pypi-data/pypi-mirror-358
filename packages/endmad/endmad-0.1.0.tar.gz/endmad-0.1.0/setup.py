5d2b26bdf703765382214674f1ba62fcb57a83b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:08:56 +0000

    Add package endmad/0.1.0/endmad-0.1.0.tar.gz

---
+++ packages/endmad/endmad-0.1.0.tar.gz/endmad-0.1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="endmad",
+    version="0.1.0",
+    packages=find_packages(),
+    author="Alpha Go",
+    description="My package with folders",
+)
