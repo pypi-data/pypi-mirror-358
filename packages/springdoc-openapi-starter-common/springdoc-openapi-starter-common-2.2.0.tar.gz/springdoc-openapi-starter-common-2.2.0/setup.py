0b429a26f5474efc4212008dbbff7b87b45e90a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:10 +0000

    Add package springdoc-openapi-starter-common/2.2.0/springdoc-openapi-starter-common-2.2.0.tar.gz

---
+++ packages/springdoc-openapi-starter-common/springdoc-openapi-starter-common-2.2.0.tar.gz/springdoc-openapi-starter-common-2.2.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="springdoc-openapi-starter-common",
+    version="2.2.0",
+    packages=["springdoc-openapi-starter-common"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
