f48b55d77be173d6c02f525b0d37d039f6eb2416
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:10 +0000

    Add package conf-prod/0.0.1/conf-prod-0.0.1.tar.gz

---
+++ packages/conf-prod/conf-prod-0.0.1.tar.gz/conf-prod-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="conf-prod",
+    version="0.0.1",
+    packages=["conf-prod"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
