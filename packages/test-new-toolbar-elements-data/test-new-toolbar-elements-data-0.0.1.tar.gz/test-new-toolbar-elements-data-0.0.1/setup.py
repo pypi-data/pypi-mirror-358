a974beb2e22ef83364bd8b03bf8c02ceb225a300
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:58 +0000

    Add package test-new-toolbar-elements-data/0.0.1/test-new-toolbar-elements-data-0.0.1.tar.gz

---
+++ packages/test-new-toolbar-elements-data/test-new-toolbar-elements-data-0.0.1.tar.gz/test-new-toolbar-elements-data-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="test-new-toolbar-elements-data",
+    version="0.0.1",
+    packages=["test-new-toolbar-elements-data"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
