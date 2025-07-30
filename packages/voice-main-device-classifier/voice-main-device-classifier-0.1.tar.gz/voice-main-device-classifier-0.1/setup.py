a98f328c73e58226fc6cc1b9d672f969d484ad8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:09 +0000

    Add package voice-main-device-classifier/0.1/voice-main-device-classifier-0.1.tar.gz

---
+++ packages/voice-main-device-classifier/voice-main-device-classifier-0.1.tar.gz/voice-main-device-classifier-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="voice-main-device-classifier",
+    version="0.1",
+    packages=["voice-main-device-classifier"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
