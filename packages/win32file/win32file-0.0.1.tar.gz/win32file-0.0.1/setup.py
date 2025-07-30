28f1065d25b51e51d703f1a1bd64512a971ce156
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:31 +0000

    Add package win32file/0.0.1/win32file-0.0.1.tar.gz

---
+++ packages/win32file/win32file-0.0.1.tar.gz/win32file-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="win32file",
+    version="0.0.1",
+    packages=["win32file"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
