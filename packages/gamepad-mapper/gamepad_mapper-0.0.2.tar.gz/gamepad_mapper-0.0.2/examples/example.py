1001d0af5fe4ed81897260d620ca944ae15a6b51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:08:40 +0000

    Add package gamepad-mapper/0.0.2/gamepad_mapper-0.0.2.tar.gz

---
+++ packages/gamepad-mapper/gamepad_mapper-0.0.2.tar.gz/gamepad_mapper-0.0.2/examples/example.py
@@ -0,0 +1,13 @@
+import sys, pygame
+from gamepad_mapper import load_or_map
+
+pygame.init()
+pygame.joystick.init()
+if pygame.joystick.get_count() == 0:
+    print("No game controller found.")
+    sys.exit(1)
+
+joystick = pygame.joystick.Joystick(0)
+joystick.init()
+
+mapping = load_or_map(joystick, ["Roll", "Pitch", "Throttle", "Yaw"], ["arm"], force=True, name="example")
