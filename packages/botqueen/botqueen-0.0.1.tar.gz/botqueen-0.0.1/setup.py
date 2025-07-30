0f8ace1ff1fdff73a8692bdc7a9f47be2846d5ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:22 +0000

    Add package botqueen/0.0.1/botqueen-0.0.1.tar.gz

---
+++ packages/botqueen/botqueen-0.0.1.tar.gz/botqueen-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="botqueen",
+    version="0.0.1",
+    packages=["botqueen"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
