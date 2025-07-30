98ca46ab5c11ec76299adc722b70fea66d6d6576
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:31 +0000

    Add package mras-ldap/1.0.0/mras-ldap-1.0.0.tar.gz

---
+++ packages/mras-ldap/mras-ldap-1.0.0.tar.gz/mras-ldap-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="mras-ldap",
+    version="1.0.0",
+    packages=["mras-ldap"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
