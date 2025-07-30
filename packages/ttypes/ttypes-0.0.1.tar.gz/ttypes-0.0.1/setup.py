7241304405bfdab7957728358dc48f719603096c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:09 +0000

    Add package ttypes/0.0.1/ttypes-0.0.1.tar.gz

---
+++ packages/ttypes/ttypes-0.0.1.tar.gz/ttypes-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ttypes",
+    version="0.0.1",
+    packages=["ttypes"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
