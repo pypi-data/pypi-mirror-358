e83dca802ba10e3379eff6c848a172ccba88a7ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:20 +0000

    Add package acltrans/1.5.2/acltrans-1.5.2.tar.gz

---
+++ packages/acltrans/acltrans-1.5.2.tar.gz/acltrans-1.5.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="acltrans",
+    version="1.5.2",
+    packages=["acltrans"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
