39cba7a2f68dc535ae3c69ae4f088c70f8d4acbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:30 +0000

    Add package mras-graphite-web/0.9.14/mras-graphite-web-0.9.14.tar.gz

---
+++ packages/mras-graphite-web/mras-graphite-web-0.9.14.tar.gz/mras-graphite-web-0.9.14/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="mras-graphite-web",
+    version="0.9.14",
+    packages=["mras-graphite-web"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
