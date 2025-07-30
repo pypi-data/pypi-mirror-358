555256bcdd300a85421a79d82c6745f4e619f492
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:54 +0000

    Add package bd-scan-yocto-via-sbom/1.1.1/bd_scan_yocto_via_sbom-1.1.1.tar.gz

---
+++ packages/bd-scan-yocto-via-sbom/bd_scan_yocto_via_sbom-1.1.1.tar.gz/bd_scan_yocto_via_sbom-1.1.1/bd_scan_yocto/ComponentClass.py
@@ -0,0 +1,53 @@
+import logging
+
+
+class Component:
+    def __init__(self, name, version, data):
+        self.name = name
+        self.version = version
+        self.data = data
+
+    def get_matchtypes(self):
+        try:
+            return self.data['matchTypes']
+        except KeyError:
+            return []
+
+    def is_dependency(self):
+        dep_types = ['FILE_DEPENDENCY_DIRECT', 'FILE_DEPENDENCY_TRANSITIVE']
+        match_types = self.get_matchtypes()
+        for m in dep_types:
+            if m in match_types:
+                return True
+        return False
+
+    def is_signature(self):
+        sig_types = ['FILE_EXACT', 'FILE_SOME_FILES_MODIFIED', 'FILE_FILES_ADDED_DELETED_AND_MODIFIED',
+                     'FILE_EXACT_FILE_MATCH']
+        match_types = self.get_matchtypes()
+        for m in sig_types:
+            if m in match_types:
+                return True
+        return False
+
+    def is_ignored(self):
+        try:
+            return self.data['ignored']
+        except KeyError:
+            return False
+
+    def get_origins(self):
+        origlist = []
+        try:
+            if 'origins' not in self.data:
+                return []
+            for origin in self.data['origins']:
+                if origin['externalNamespace'] == 'openembedded':
+                    orig = origin['externalId'].split('/')
+                    origlist.append('/'.join(orig[1:]))
+                else:
+                    origlist.append(origin['externalId'])
+            return origlist
+        except KeyError as e:
+            logging.error(f"Error in get_origin(): {e}")
+            return []
