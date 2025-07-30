848700aa3b54bdb3aebc7bd4d2a8fee2acf4578f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:05:14 +0000

    Add package datalens-file-uploader-worker-lib/0.0.0/datalens-file-uploader-worker-lib-0.0.0.tar.gz

---
+++ packages/datalens-file-uploader-worker-lib/datalens-file-uploader-worker-lib-0.0.0.tar.gz/datalens-file-uploader-worker-lib-0.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="datalens-file-uploader-worker-lib",
+    version="0.0.0",
+    packages=["datalens-file-uploader-worker-lib"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
