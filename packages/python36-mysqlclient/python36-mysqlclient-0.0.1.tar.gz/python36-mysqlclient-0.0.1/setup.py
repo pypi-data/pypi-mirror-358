3dc3f800aef1323c661cea458d8bf3e1fc437a2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:30 +0000

    Add package python36-mysqlclient/0.0.1/python36-mysqlclient-0.0.1.tar.gz

---
+++ packages/python36-mysqlclient/python36-mysqlclient-0.0.1.tar.gz/python36-mysqlclient-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="python36-mysqlclient",
+    version="0.0.1",
+    packages=["python36-mysqlclient"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
