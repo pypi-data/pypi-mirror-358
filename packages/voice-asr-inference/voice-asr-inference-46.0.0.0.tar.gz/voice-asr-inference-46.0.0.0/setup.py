9dc53a899b1562f2f0fb2fe39dee1baedb84e639
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:14 +0000

    Add package voice-asr-inference/46.0.0.0/voice-asr-inference-46.0.0.0.tar.gz

---
+++ packages/voice-asr-inference/voice-asr-inference-46.0.0.0.tar.gz/voice-asr-inference-46.0.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="voice-asr-inference",
+    version="46.0.0.0",
+    packages=["voice-asr-inference"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
