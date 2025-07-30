e99369d6f3d233d272f9dd1bcaf79e68ec8300e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:07:00 +0000

    Add package vkpy-vkuth/0.0.2/vkpy-vkuth-0.0.2.tar.gz

---
+++ packages/vkpy-vkuth/vkpy-vkuth-0.0.2.tar.gz/vkpy-vkuth-0.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="vkpy-vkuth",
+    version="0.0.2",
+    packages=["vkpy-vkuth"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
