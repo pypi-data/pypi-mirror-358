e7c7c85e41ed05d2d111a7f94db8e00ab861243c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:12 +0000

    Add package mb-mb-integration-tools/0.2.0/mb_mb_integration_tools-0.2.0.tar.gz

---
+++ packages/mb-mb-integration-tools/mb_mb_integration_tools-0.2.0.tar.gz/mb_mb_integration_tools-0.2.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mb_mb_integration_tools",            # The name of your package
+    version="0.2.0",                     # Version number
+    python_requires='>=3.13.3',            # Python version requirement
+)
