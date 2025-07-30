1f8a6afb04ca1584794331badd41b5aa6629e237
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:07:01 +0000

    Add package decorator-include/0.0.1/decorator-include-0.0.1.tar.gz

---
+++ packages/decorator-include/decorator-include-0.0.1.tar.gz/decorator-include-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="decorator-include",
+    version="0.0.1",
+    packages=["decorator-include"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
