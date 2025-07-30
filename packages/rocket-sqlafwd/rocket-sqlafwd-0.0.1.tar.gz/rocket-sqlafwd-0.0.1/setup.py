9719f2a431502ceb591e02177a1de4db87ee52e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:19 +0000

    Add package rocket-sqlafwd/0.0.1/rocket-sqlafwd-0.0.1.tar.gz

---
+++ packages/rocket-sqlafwd/rocket-sqlafwd-0.0.1.tar.gz/rocket-sqlafwd-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="rocket-sqlafwd",
+    version="0.0.1",
+    packages=["rocket-sqlafwd"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
