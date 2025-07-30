335214d58f0091f3e97c87c39e993a29edc3b8d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:10:05 +0000

    Add package nadex-cli/1.0.2/nadex_cli-1.0.2.tar.gz

---
+++ packages/nadex-cli/nadex_cli-1.0.2.tar.gz/nadex_cli-1.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    description = f.read()
+
+setup(
+    name='nadex-cli',
+    version='1.0.2',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'python-dotenv',
+        'requests',
+        'websockets',
+    ],
+    entry_points={
+        "console_scripts":[
+            "nadex_dashboard = nadex_dashboard:cli_entry"
+        ]
+    },
+    long_description=description,
+    long_description_content_type="text/markdown",
+    keywords=('nadex ', 'nadex-binary-options ', 'nadex-forex ', 'nadex-real-time ', 'nadex-market-data ', 'nadex-cli ', 'nadex-dashboard ', 'nadex-websocket ', 'nadex-trading ', 'nadex-finance ', 'nadex-python ', 'nadex-pip-install ', 'nadex-automation ', 'nadex-order-book', 'binary-options ', 'forex ', 'real-time ', 'market-data ', 'cli ', 'dashboard ', 'websocket ', 'trading ', 'finance ', 'python ', 'pip-install ', 'automation ', 'order-book', 'nadex pypi ', 'pypi nadex ', 'pip nadex ', 'pip i nadex-cli ', 'pypi nadex-cli ', 'pip nadex '),
+)
