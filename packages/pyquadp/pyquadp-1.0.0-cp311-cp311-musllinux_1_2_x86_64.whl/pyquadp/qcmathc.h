500017b11be00a26bfde17c74c76f677a243ac56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:08:24 +0000

    Add package pyquadp/1.0.0/pyquadp-1.0.0-cp311-cp311-musllinux_1_2_x86_64.whl

---
+++ packages/pyquadp/pyquadp-1.0.0-cp311-cp311-musllinux_1_2_x86_64.whl/pyquadp/qcmathc.h
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: GPL-2.0+
+#pragma once
+#include "pyquadp.h"
+
+#include "qfloat.h"
+#include "qcmplx.h"
+
+
+#define OP_cabsq  0
+#define OP_cargq  1
+#define OP_cimagq  2
+#define OP_crealq  3
+
+#define OP_cacoshq  4
+#define OP_cacosq  5
+#define OP_casinhq  6
+#define OP_casinq  7
+#define OP_catanhq  8
+#define OP_catanq  9
+#define OP_ccosq  10
+#define OP_ccoshq  11
+#define OP_cexpq  12
+#define OP_cexpiq  13
+#define OP_clogq  14
+#define OP_clog10q  15
+#define OP_conjq  16
+#define OP_cpowq  17
+#define OP_cprojq  18
+#define OP_csinq  19
+#define OP_csinhq  20
+#define OP_csqrtq  21
+#define OP_ctanq  22
+#define OP_ctanhq  23
+
+#define OP_finitecq 24
+#define OP_isinfcq 25
+#define OP_isnancq 26
+
+#define OP_phase 27
