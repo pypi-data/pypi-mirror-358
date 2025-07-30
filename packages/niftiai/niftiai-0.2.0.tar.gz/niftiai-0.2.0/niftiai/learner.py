6b115183e7c1a43105d29769071b3232c16e4162
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:50 +0000

    Add package niftiai/0.2.0/niftiai-0.2.0.tar.gz

---
+++ packages/niftiai/niftiai-0.2.0.tar.gz/niftiai-0.2.0/niftiai/learner.py
@@ -0,0 +1,23 @@
+from fastai.basics import delegates, Learner, DiceLoss
+
+from .models import ResNet3d, UNet3d, LinkNet3d
+
+
+@delegates(Learner.__init__)
+def cnn_learner3d(dls, model=None, c_in=None, n_out=None, **kwargs):
+    if model is None:
+        n_in = dls.one_batch()[0].shape[1] if c_in is None else c_in
+        n_out = dls.c if n_out is None else n_out
+        assert n_out, '`n_out` is not defined, and could not be inferred from data, set `dls.c` or pass `n_out`'
+        img_size = dls.one_batch()[0].shape[-3:]
+        model = ResNet3d(shape=img_size, n_out=n_out, c_in=n_in)
+    return Learner(dls, model, **kwargs)
+
+
+@delegates(Learner.__init__)
+def unet_learner3d(dls, c_out, linknet=False, model=None, c_in=None, loss_func=None, **kwargs):
+    if model is None:
+        c_in = dls.one_batch()[0].shape[1] if c_in is None else c_in
+        model = LinkNet3d(c_in=c_in, c_out=c_out) if linknet else UNet3d(c_in=c_in, c_out=c_out)
+    loss_func = DiceLoss() if loss_func is None else loss_func
+    return Learner(dls=dls, model=model, loss_func=loss_func, **kwargs)
---
+++ packages/niftiai/niftiai-0.2.0.tar.gz/niftiai-0.2.0/niftiai/models/__init__.py
@@ -0,0 +1,4 @@
+from unet3d.unet3d import UNet3d
+from unet3d.linknet3d import LinkNet3d
+
+from .resnet3d import ResNet3d
