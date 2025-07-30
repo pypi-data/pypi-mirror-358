fdbaf657cb9b96c968808f86df49fbe1e7c644d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:22 +0000

    Add package core-testing-extension/0.0.2.dev2/core-testing-extension-0.0.2.dev2.tar.gz

---
+++ packages/core-testing-extension/core-testing-extension-0.0.2.dev2.tar.gz/core-testing-extension-0.0.2.dev2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="core-testing-extension",
+    version="0.0.2.dev2",
+    packages=["core-testing-extension"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
