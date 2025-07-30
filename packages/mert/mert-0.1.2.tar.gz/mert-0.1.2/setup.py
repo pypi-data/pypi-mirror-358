69c3d8810944c810935a410f2d1147d3934f2b4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:10:08 +0000

    Add package mert/0.1.2/mert-0.1.2.tar.gz

---
+++ packages/mert/mert-0.1.2.tar.gz/mert-0.1.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(
+    name='mert',
+    version='0.1.2',
+    packages=['mert'],
+    description='mert is a Python package. Will be updated soon.',
+    author='Mert Sırakaya',
+)
