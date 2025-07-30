ffb234a144b791d7b69dd4034812efe21ecd2e9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:17 +0000

    Add package system-routes/0.0.1/system-routes-0.0.1.tar.gz

---
+++ packages/system-routes/system-routes-0.0.1.tar.gz/system-routes-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="system-routes",
+    version="0.0.1",
+    packages=["system-routes"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
