4f77f9b747eea370fd3e1e16a0cab9740b8b8143
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:22 +0000

    Add package bootstrap-datepicker-plus/0.0.1/bootstrap-datepicker-plus-0.0.1.tar.gz

---
+++ packages/bootstrap-datepicker-plus/bootstrap-datepicker-plus-0.0.1.tar.gz/bootstrap-datepicker-plus-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="bootstrap-datepicker-plus",
+    version="0.0.1",
+    packages=["bootstrap-datepicker-plus"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
