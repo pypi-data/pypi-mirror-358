2c76f7a8071e96a91306af34315bfb39e96d06e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:04 +0000

    Add package peer-memory-cuda/0.0.1/peer-memory-cuda-0.0.1.tar.gz

---
+++ packages/peer-memory-cuda/peer-memory-cuda-0.0.1.tar.gz/peer-memory-cuda-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="peer-memory-cuda",
+    version="0.0.1",
+    packages=["peer-memory-cuda"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
