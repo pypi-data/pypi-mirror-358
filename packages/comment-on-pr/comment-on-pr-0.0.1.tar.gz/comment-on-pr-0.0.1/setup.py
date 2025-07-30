8d8610b64501d0fb5d4c5f5212dd302d98b2c0ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:22 +0000

    Add package comment-on-pr/0.0.1/comment-on-pr-0.0.1.tar.gz

---
+++ packages/comment-on-pr/comment-on-pr-0.0.1.tar.gz/comment-on-pr-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="comment-on-pr",
+    version="0.0.1",
+    packages=["comment-on-pr"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
