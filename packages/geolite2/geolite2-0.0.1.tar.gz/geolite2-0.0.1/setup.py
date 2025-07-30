d07e938ce5cd49ebf2220ee1a3f3f67f019773e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:06 +0000

    Add package geolite2/0.0.1/geolite2-0.0.1.tar.gz

---
+++ packages/geolite2/geolite2-0.0.1.tar.gz/geolite2-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="geolite2",
+    version="0.0.1",
+    packages=["geolite2"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
