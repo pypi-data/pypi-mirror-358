ff0e66488fd1166269e1abbb7e3755d39e19f53a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:15 +0000

    Add package kanbanz/0.2.8/kanbanz-0.2.8.tar.gz

---
+++ packages/kanbanz/kanbanz-0.2.8.tar.gz/kanbanz-0.2.8/tests/test_core.py
@@ -0,0 +1,27 @@
+
+import pytest
+
+from kanbanz import Kanban,Pool
+
+@pytest.fixture
+def kanban_create():
+    kanban_path = "/Users/zhaoxuefeng/GitHub/obsidian/工作/事件看板/实验看板.md"
+    cm = Kanban(kanban_path)
+    cm.pull()
+
+    yield cm
+    cm.push()
+
+
+def test_insert(kanban_create):
+    print(kanban_create.insert('知识',Pool.预备池))
+
+def test_get_tasks_in(kanban_create):
+    print(kanban_create.get_tasks_in(Pool.预备池))
+
+def test_pop(kanban_create):
+    print(kanban_create.pop('等待反馈',Pool.预备池))
+
+def test_get_task_by_word(kanban_create):
+    print(kanban_create.get_task_by_word('知识',Pool.预备池))
+
