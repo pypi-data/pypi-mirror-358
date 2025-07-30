e703b4b8885dcde7f96211750d344b5151170309
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:43 +0000

    Add package scrooge-plugin-ip-external-20220506/1.0.4/scrooge-plugin-ip-external-20220506-1.0.4.tar.gz

---
+++ packages/scrooge-plugin-ip-external-20220506/scrooge-plugin-ip-external-20220506-1.0.4.tar.gz/scrooge-plugin-ip-external-20220506-1.0.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="scrooge-plugin-ip-external-20220506",
+    version="1.0.4",
+    packages=["scrooge-plugin-ip-external-20220506"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
