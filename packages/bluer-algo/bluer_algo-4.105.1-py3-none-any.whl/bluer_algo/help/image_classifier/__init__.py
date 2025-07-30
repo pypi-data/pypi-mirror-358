51ea5c118d15c8c17dcb101e5784ea89bd85a291
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:10 +0000

    Add package bluer-algo/4.105.1/bluer_algo-4.105.1-py3-none-any.whl

---
+++ packages/bluer-algo/bluer_algo-4.105.1-py3-none-any.whl/bluer_algo/help/image_classifier/__init__.py
@@ -0,0 +1,7 @@
+from bluer_algo.help.image_classifier.ingest import help_functions as help_ingest
+from bluer_algo.help.image_classifier.train import help_functions as help_train
+
+help_functions = {
+    "ingest": help_ingest,
+    "train": help_train,
+}
