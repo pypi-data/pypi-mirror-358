bd5fe01fd98065c415ee60609df27cf122776238
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:08 +0000

    Add package raino-mcpserver-demo/0.1.0/raino_mcpserver_demo-0.1.0.tar.gz

---
+++ packages/raino-mcpserver-demo/raino_mcpserver_demo-0.1.0.tar.gz/raino_mcpserver_demo-0.1.0/src/raino_mcpserver_demo/__init__.py
@@ -0,0 +1,28 @@
+# server.py
+from mcp.server.fastmcp import FastMCP
+
+# Create an MCP server
+mcp = FastMCP("Demo")
+
+
+# Add an addition tool
+@mcp.tool()
+def add(a: int, b: int) -> int:
+    """Add two numbers"""
+    return a + b
+
+
+# Add a dynamic greeting resource
+@mcp.resource("greeting://{name}")
+def get_greeting(name: str) -> str:
+    """Get a personalized greeting"""
+    return f"Hello, {name}!"
+
+
+
+    # mcp.settings.host = "0.0.0.0"
+    # mcp.run(transport='sse')
+
+def main() -> None:
+    mcp.run(transport='stdio')
+   
