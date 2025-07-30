e7e2c9283949d6886cc214888fa0d928f838d6a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:28 +0000

    Add package downloading/0.0.1/downloading-0.0.1.tar.gz

---
+++ packages/downloading/downloading-0.0.1.tar.gz/downloading-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="downloading",
+    version="0.0.1",
+    packages=["downloading"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
