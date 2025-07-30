873e7cee64e1a6dd32448d8772fb83336d79aea1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:19 +0000

    Add package syn-data-csv/0.2.0/syn_data_csv-0.2.0-py3-none-any.whl

---
+++ packages/syn-data-csv/syn_data_csv-0.2.0-py3-none-any.whl/syn_data_csv/main.py
@@ -0,0 +1,25 @@
+import sys
+
+from .validate_files import process_and_validate_files
+from .llm_setup import get_api_key_model
+from .generate_data import generate_synthetic_data
+from .output import generate_output
+
+
+def get_csv_data():
+
+    # Process and validate files
+    args = sys.argv[1:]
+    config, reference_file, delimiter = process_and_validate_files(args)
+
+    # Get Provider, API Key and Model
+    provider, api_key, model = get_api_key_model()
+    
+    # Generate synthetic data
+    df = generate_synthetic_data(config, reference_file, provider, api_key, model)
+    
+    # Save Synthetic data
+    generate_output(df, delimiter)
+
+if __name__ == "__main__":
+    get_csv_data()
