f1c315245200295a05a713d8ae7d76808f1abf02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:57 +0000

    Add package pypo-trend/0.0.1/pypo-trend-0.0.1.tar.gz

---
+++ packages/pypo-trend/pypo-trend-0.0.1.tar.gz/pypo-trend-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pypo-trend",
+    version="0.0.1",
+    packages=["pypo-trend"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
