3374a32915ef47aab7d738fa0f411de78569be3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:16 +0000

    Add package vggish-slim/0.0.1/vggish-slim-0.0.1.tar.gz

---
+++ packages/vggish-slim/vggish-slim-0.0.1.tar.gz/vggish-slim-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="vggish-slim",
+    version="0.0.1",
+    packages=["vggish-slim"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
