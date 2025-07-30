8d745cf9ecfd972ffc8094ba7395b326f475a429
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:56 +0000

    Add package pypo-shortcodes/0.0.1/pypo-shortcodes-0.0.1.tar.gz

---
+++ packages/pypo-shortcodes/pypo-shortcodes-0.0.1.tar.gz/pypo-shortcodes-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pypo-shortcodes",
+    version="0.0.1",
+    packages=["pypo-shortcodes"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
