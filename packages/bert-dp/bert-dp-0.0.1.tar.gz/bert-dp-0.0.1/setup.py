539145f18f5c1d7ae215b6782a213ae3c1739c79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:39 +0000

    Add package bert-dp/0.0.1/bert-dp-0.0.1.tar.gz

---
+++ packages/bert-dp/bert-dp-0.0.1.tar.gz/bert-dp-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="bert-dp",
+    version="0.0.1",
+    packages=["bert-dp"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
