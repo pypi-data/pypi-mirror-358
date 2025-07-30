52ea5cf3e0a91ca827233eed1624d89ac9c8b33e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:57 +0000

    Add package tsilva_notebook_utils/0.0.100/tsilva_notebook_utils-0.0.100-py3-none-any.whl

---
+++ packages/tsilva_notebook_utils/tsilva_notebook_utils-0.0.100-py3-none-any.whl/tsilva_notebook_utils/wandb.py
@@ -0,0 +1,16 @@
+def login(*args, **kwargs):
+    import wandb
+    return wandb.login(*args, **kwargs)
+    
+
+def render_run_iframe():
+    import wandb
+    from IPython.display import HTML
+    from IPython.display import display
+
+    run_url = wandb.run.get_url()
+    iframe_code = f"""
+    <iframe src="{run_url}" width="100%" height="1200px"></iframe>
+    """
+
+    display(HTML(iframe_code))
