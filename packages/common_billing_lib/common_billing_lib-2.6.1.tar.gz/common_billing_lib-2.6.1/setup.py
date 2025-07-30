1591e5de95bba564cc18aaa0f74966675d8d9812
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:20 +0000

    Add package common_billing_lib/2.6.1/common_billing_lib-2.6.1.tar.gz

---
+++ packages/common_billing_lib/common_billing_lib-2.6.1.tar.gz/common_billing_lib-2.6.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="common_billing_lib",
+    version="2.6.1",
+    packages=["common_billing_lib"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
