8d69a8ecfbcc450da2f280d6220c37c3d39ba012
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:05:56 +0000

    Add package acdc-py/1.1.3/acdc_py-1.1.3.tar.gz

---
+++ packages/acdc-py/acdc_py-1.1.3.tar.gz/acdc_py-1.1.3/acdc_py/__init__.py
@@ -0,0 +1,10 @@
+from .SA import *
+from .GS import *
+from .pl import *
+from .tl import *
+from .pp import *
+from .get_opt import *
+# from ._condense_diffuse_funcs import *
+
+# Want to appear as module - no direct functions
+# made __all__ = [] in each of these .py files
