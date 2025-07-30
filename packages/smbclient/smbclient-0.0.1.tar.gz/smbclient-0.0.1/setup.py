6083d0ea2d16c9cc1d3bc481284e1db5ed9db466
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:09 +0000

    Add package smbclient/0.0.1/smbclient-0.0.1.tar.gz

---
+++ packages/smbclient/smbclient-0.0.1.tar.gz/smbclient-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="smbclient",
+    version="0.0.1",
+    packages=["smbclient"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
