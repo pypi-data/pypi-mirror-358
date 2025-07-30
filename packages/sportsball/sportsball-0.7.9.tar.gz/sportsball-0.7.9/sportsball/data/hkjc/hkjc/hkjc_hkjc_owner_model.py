457c1a98cf27d0c9489e1cdaa62481432262be43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:21:29 +0000

    Add package sportsball/0.7.9/sportsball-0.7.9.tar.gz

---
+++ packages/sportsball/sportsball-0.7.9.tar.gz/sportsball-0.7.9/sportsball/data/hkjc/hkjc/hkjc_hkjc_owner_model.py
@@ -0,0 +1,25 @@
+"""HKJC HKJC owner model."""
+
+import logging
+import urllib.parse
+from urllib.parse import urlparse
+
+from ...owner_model import OwnerModel
+
+
+def create_hkjc_hkjc_owner_model(
+    url: str,
+) -> OwnerModel | None:
+    """Create an HKJC owner."""
+    o = urlparse(url)
+    query = urllib.parse.parse_qs(o.query)
+    try:
+        name = query["HorseOwner"][0]
+    except KeyError as exc:
+        logging.debug(url)
+        logging.debug(str(exc))
+        return None
+    return OwnerModel(
+        identifier=name,
+        name=name,
+    )
