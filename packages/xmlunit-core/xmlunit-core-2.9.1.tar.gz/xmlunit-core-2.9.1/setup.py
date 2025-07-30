876432c4cd4a8541c46d8bad5d10efa5217aa009
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:21 +0000

    Add package xmlunit-core/2.9.1/xmlunit-core-2.9.1.tar.gz

---
+++ packages/xmlunit-core/xmlunit-core-2.9.1.tar.gz/xmlunit-core-2.9.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="xmlunit-core",
+    version="2.9.1",
+    packages=["xmlunit-core"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
