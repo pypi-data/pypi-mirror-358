8e769387fac7d5ecee68d6e57a65a1b57bdcf013
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:10 +0000

    Add package commons-logging/1.0.4/commons-logging-1.0.4.tar.gz

---
+++ packages/commons-logging/commons-logging-1.0.4.tar.gz/commons-logging-1.0.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="commons-logging",
+    version="1.0.4",
+    packages=["commons-logging"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
