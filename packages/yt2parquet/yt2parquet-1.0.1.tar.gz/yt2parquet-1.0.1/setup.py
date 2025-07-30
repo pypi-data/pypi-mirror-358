98a60ead9931757a5b92e80104708a390d3f3b64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:04 +0000

    Add package yt2parquet/1.0.1/yt2parquet-1.0.1.tar.gz

---
+++ packages/yt2parquet/yt2parquet-1.0.1.tar.gz/yt2parquet-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="yt2parquet",
+    version="1.0.1",
+    packages=["yt2parquet"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
