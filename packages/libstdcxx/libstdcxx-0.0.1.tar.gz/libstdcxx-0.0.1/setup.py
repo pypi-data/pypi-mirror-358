502fcdbc3913f5ea03b7cb125b02b5a5a694710e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:19 +0000

    Add package libstdcxx/0.0.1/libstdcxx-0.0.1.tar.gz

---
+++ packages/libstdcxx/libstdcxx-0.0.1.tar.gz/libstdcxx-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="libstdcxx",
+    version="0.0.1",
+    packages=["libstdcxx"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
