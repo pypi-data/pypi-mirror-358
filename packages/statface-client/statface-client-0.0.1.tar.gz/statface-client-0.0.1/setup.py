107fc70d2128d01c90ff4e42fb01c2664d668305
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:20 +0000

    Add package statface-client/0.0.1/statface-client-0.0.1.tar.gz

---
+++ packages/statface-client/statface-client-0.0.1.tar.gz/statface-client-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="statface-client",
+    version="0.0.1",
+    packages=["statface-client"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
