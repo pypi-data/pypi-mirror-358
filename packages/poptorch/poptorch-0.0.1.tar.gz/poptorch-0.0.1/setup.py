814fb914abafbdeebfbc932438679fb3665c5b8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:07 +0000

    Add package poptorch/0.0.1/poptorch-0.0.1.tar.gz

---
+++ packages/poptorch/poptorch-0.0.1.tar.gz/poptorch-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="poptorch",
+    version="0.0.1",
+    packages=["poptorch"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
