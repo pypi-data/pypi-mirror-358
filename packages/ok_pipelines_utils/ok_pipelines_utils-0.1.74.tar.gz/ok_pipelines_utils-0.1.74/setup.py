90ab3d275e476eaa55cf229b8c61155ea660573b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:12 +0000

    Add package ok_pipelines_utils/0.1.74/ok_pipelines_utils-0.1.74.tar.gz

---
+++ packages/ok_pipelines_utils/ok_pipelines_utils-0.1.74.tar.gz/ok_pipelines_utils-0.1.74/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ok_pipelines_utils",
+    version="0.1.74",
+    packages=["ok_pipelines_utils"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
