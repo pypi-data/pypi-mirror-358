4d394fc6cf560f15ade01f82d2f6adee5dfbe506
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:36 +0000

    Add package fused-adam-cuda/0.0.1/fused-adam-cuda-0.0.1.tar.gz

---
+++ packages/fused-adam-cuda/fused-adam-cuda-0.0.1.tar.gz/fused-adam-cuda-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="fused-adam-cuda",
+    version="0.0.1",
+    packages=["fused-adam-cuda"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
