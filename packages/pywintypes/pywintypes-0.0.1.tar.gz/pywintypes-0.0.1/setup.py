a28951a45b155042ca3071374aa102edd7a630fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:20 +0000

    Add package pywintypes/0.0.1/pywintypes-0.0.1.tar.gz

---
+++ packages/pywintypes/pywintypes-0.0.1.tar.gz/pywintypes-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pywintypes",
+    version="0.0.1",
+    packages=["pywintypes"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
