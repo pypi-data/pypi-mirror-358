a193ad6bb9bdb4b9c3d2b8ecb30f0ef911349e32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:20 +0000

    Add package tree-queries/0.0.1/tree-queries-0.0.1.tar.gz

---
+++ packages/tree-queries/tree-queries-0.0.1.tar.gz/tree-queries-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="tree-queries",
+    version="0.0.1",
+    packages=["tree-queries"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
