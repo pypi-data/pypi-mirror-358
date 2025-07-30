4dd71d6cc50db2c1dba8fb312ebfb24c53f50fb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:06 +0000

    Add package production-local/0.0.1/production-local-0.0.1.tar.gz

---
+++ packages/production-local/production-local-0.0.1.tar.gz/production-local-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="production-local",
+    version="0.0.1",
+    packages=["production-local"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
