4788bb5656274d951e370efd252cd25240f79b50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:53 +0000

    Add package prepare-data/0.0.1/prepare-data-0.0.1.tar.gz

---
+++ packages/prepare-data/prepare-data-0.0.1.tar.gz/prepare-data-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="prepare-data",
+    version="0.0.1",
+    packages=["prepare-data"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
