a18b1282d9be29c4e8b4847833dd23313c4b3ace
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:38 +0000

    Add package datalens-package-boilerplate/0.0.1/datalens-package-boilerplate-0.0.1.tar.gz

---
+++ packages/datalens-package-boilerplate/datalens-package-boilerplate-0.0.1.tar.gz/datalens-package-boilerplate-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="datalens-package-boilerplate",
+    version="0.0.1",
+    packages=["datalens-package-boilerplate"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
