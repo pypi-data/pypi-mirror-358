3db75c3dd594fef73e3a91768936a55156a803fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:20 +0000

    Add package beeswax/0.0.1/beeswax-0.0.1.tar.gz

---
+++ packages/beeswax/beeswax-0.0.1.tar.gz/beeswax-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="beeswax",
+    version="0.0.1",
+    packages=["beeswax"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
