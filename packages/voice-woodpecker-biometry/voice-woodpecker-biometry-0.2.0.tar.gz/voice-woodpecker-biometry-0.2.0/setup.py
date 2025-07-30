2710063e369b5c24107e4a993f0b0335ed565757
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:08 +0000

    Add package voice-woodpecker-biometry/0.2.0/voice-woodpecker-biometry-0.2.0.tar.gz

---
+++ packages/voice-woodpecker-biometry/voice-woodpecker-biometry-0.2.0.tar.gz/voice-woodpecker-biometry-0.2.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="voice-woodpecker-biometry",
+    version="0.2.0",
+    packages=["voice-woodpecker-biometry"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
