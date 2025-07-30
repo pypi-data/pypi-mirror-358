7c678e9615c6acdd211255cb9ead48c2ec7cab49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:07 +0000

    Add package dropout-layer-norm/0.0.1/dropout-layer-norm-0.0.1.tar.gz

---
+++ packages/dropout-layer-norm/dropout-layer-norm-0.0.1.tar.gz/dropout-layer-norm-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dropout-layer-norm",
+    version="0.0.1",
+    packages=["dropout-layer-norm"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
