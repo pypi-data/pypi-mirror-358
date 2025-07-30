86072194b2ff6390ad786e7cad214316f9c2e421
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:16 +0000

    Add package query_filters/0.2.0/query_filters-0.2.0.tar.gz

---
+++ packages/query_filters/query_filters-0.2.0.tar.gz/query_filters-0.2.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="query_filters",
+    version="0.2.0",
+    packages=["query_filters"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
