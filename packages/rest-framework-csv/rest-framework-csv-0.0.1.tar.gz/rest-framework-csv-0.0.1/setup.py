c22498dd08c697a7383f20e7c3c5c5a185687391
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:22 +0000

    Add package rest-framework-csv/0.0.1/rest-framework-csv-0.0.1.tar.gz

---
+++ packages/rest-framework-csv/rest-framework-csv-0.0.1.tar.gz/rest-framework-csv-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="rest-framework-csv",
+    version="0.0.1",
+    packages=["rest-framework-csv"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
