b2a0f4311031c1c05821a68bdb400782509af1a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:13 +0000

    Add package vocab-build-imdocs/0.0.1/vocab-build-imdocs-0.0.1.tar.gz

---
+++ packages/vocab-build-imdocs/vocab-build-imdocs-0.0.1.tar.gz/vocab-build-imdocs-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="vocab-build-imdocs",
+    version="0.0.1",
+    packages=["vocab-build-imdocs"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
