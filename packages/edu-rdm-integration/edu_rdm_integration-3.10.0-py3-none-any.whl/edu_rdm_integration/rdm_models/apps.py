bc1ea87551dd7a14aab394d199f27e5fe57f4725
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:05 +0000

    Add package edu-rdm-integration/3.10.0/edu_rdm_integration-3.10.0-py3-none-any.whl

---
+++ packages/edu-rdm-integration/edu_rdm_integration-3.10.0-py3-none-any.whl/edu_rdm_integration/rdm_models/apps.py
@@ -0,0 +1,16 @@
+from django.apps import (
+    AppConfig,
+)
+
+
+class RDMModelsConfigApp(AppConfig):
+    """Приложение для работы с моделями РВД."""
+
+    name = 'edu_rdm_integration.rdm_models'
+    label = 'edu_rdm_integration_models'
+    verbose_name = 'Модели РВД'
+
+    def __init__(self, *args, **kwargs):
+        print('1')
+
+        super().__init__(*args, **kwargs)
