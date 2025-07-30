44848f55f0eff6e6fb2544662d8e8e7db6d9f8b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:10:06 +0000

    Add package plico/0.32.1/plico-0.32.1-py3-none-any.whl

---
+++ packages/plico/plico-0.32.1-py3-none-any.whl/plico/utils/hackerable.py
@@ -0,0 +1,24 @@
+from plico.utils.logger import Logger
+
+
+class Hackerable(object):
+
+    def __init__(self, logger=None):
+        self._hackerableExecuteCnt= 0
+        self._hackerableEvalCnt= 0
+        if logger is None:
+            self._logger= Logger.of('Hackerable')
+
+
+    def eval(self, expression):
+        self._logger.warn("Evaluating '%s' (counter: %d)" % (
+            expression, self._hackerableEvalCnt))
+        self._hackerableEvalCnt+= 1
+        return repr(eval(expression))
+
+
+    def execute(self, statement):
+        self._logger.warn("Executing '%s' (counter: %d)" % (
+            statement, self._hackerableExecuteCnt))
+        self._hackerableExecuteCnt+= 1
+        exec(statement)
