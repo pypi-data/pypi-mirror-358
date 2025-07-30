d1fdc59b26168f6e417d29bf24ad84bc063c9516
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:10 +0000

    Add package gn-helpers/0.0.1/gn-helpers-0.0.1.tar.gz

---
+++ packages/gn-helpers/gn-helpers-0.0.1.tar.gz/gn-helpers-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="gn-helpers",
+    version="0.0.1",
+    packages=["gn-helpers"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
