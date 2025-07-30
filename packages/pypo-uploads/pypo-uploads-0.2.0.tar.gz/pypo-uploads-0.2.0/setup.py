5ab9c823bfd0c0e66fb649434a348581d0a55dac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:09 +0000

    Add package pypo-uploads/0.2.0/pypo-uploads-0.2.0.tar.gz

---
+++ packages/pypo-uploads/pypo-uploads-0.2.0.tar.gz/pypo-uploads-0.2.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pypo-uploads",
+    version="0.2.0",
+    packages=["pypo-uploads"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
