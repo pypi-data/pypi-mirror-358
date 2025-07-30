b3183be2c0a377eca0ad78b6b658e1996dd07fc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:35 +0000

    Add package fast-self-multihead-attn-bias-additive-mask/0.0.1/fast-self-multihead-attn-bias-additive-mask-0.0.1.tar.gz

---
+++ packages/fast-self-multihead-attn-bias-additive-mask/fast-self-multihead-attn-bias-additive-mask-0.0.1.tar.gz/fast-self-multihead-attn-bias-additive-mask-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="fast-self-multihead-attn-bias-additive-mask",
+    version="0.0.1",
+    packages=["fast-self-multihead-attn-bias-additive-mask"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
