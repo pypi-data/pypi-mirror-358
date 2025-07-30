83caa3a3d1ad4ce936b7efe8fedea29d25f87c57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:06 +0000

    Add package apt-clone/0.2.1/apt-clone-0.2.1.tar.gz

---
+++ packages/apt-clone/apt-clone-0.2.1.tar.gz/apt-clone-0.2.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="apt-clone",
+    version="0.2.1",
+    packages=["apt-clone"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
