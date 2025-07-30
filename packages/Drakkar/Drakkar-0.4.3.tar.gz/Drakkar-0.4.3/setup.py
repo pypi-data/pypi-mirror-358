bcc037314fa36185841d1564b09c5a3ba04cc1ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:56 +0000

    Add package Drakkar/0.4.3/Drakkar-0.4.3.tar.gz

---
+++ packages/Drakkar/Drakkar-0.4.3.tar.gz/Drakkar-0.4.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="Drakkar",
+    version="0.4.3",
+    packages=["Drakkar"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
