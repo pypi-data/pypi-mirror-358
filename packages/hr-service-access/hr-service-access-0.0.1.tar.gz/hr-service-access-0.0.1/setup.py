869f42a8165b82426cad89a04dea11cdafddd4bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:14 +0000

    Add package hr-service-access/0.0.1/hr-service-access-0.0.1.tar.gz

---
+++ packages/hr-service-access/hr-service-access-0.0.1.tar.gz/hr-service-access-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="hr-service-access",
+    version="0.0.1",
+    packages=["hr-service-access"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
