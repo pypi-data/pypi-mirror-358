f5dce487064342abfa19700cd415f73cba97785b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:10 +0000

    Add package voice-markup/0.0.1/voice-markup-0.0.1.tar.gz

---
+++ packages/voice-markup/voice-markup-0.0.1.tar.gz/voice-markup-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="voice-markup",
+    version="0.0.1",
+    packages=["voice-markup"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
