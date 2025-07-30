4f323e4ad59f00e639af66e3b2be505f57ea5bbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:45:33 +0000

    Add package buildkite-sdk/0.3.1/buildkite_sdk-0.3.1.tar.gz

---
+++ packages/buildkite-sdk/buildkite_sdk-0.3.1.tar.gz/buildkite_sdk-0.3.1/tests/test_input_step.py
@@ -0,0 +1,26 @@
+from buildkite_sdk import Pipeline, InputStep, TextField
+import json
+
+def test_input_step_simple():
+    pipeline = Pipeline()
+    pipeline.add_step(InputStep(
+        input="My Input",
+        fields=[
+            TextField(
+                key="my-input-key"
+            )
+        ],
+    ))
+
+    expected = {"steps": [{ "fields": [{ "key": "my-input-key" }], "input": "My Input" }]}
+    assert pipeline.to_json() == json.dumps(expected, indent="    ")
+
+def test_input_step_typed_dict():
+    pipeline = Pipeline()
+    pipeline.add_step({
+        "fields": [{ "key": "my-input-key" }],
+        "input": "My Input"
+    })
+
+    expected = {"steps": [{ "fields": [{ "key": "my-input-key" }], "input": "My Input" }]}
+    assert pipeline.to_json() == json.dumps(expected, indent="    ")
