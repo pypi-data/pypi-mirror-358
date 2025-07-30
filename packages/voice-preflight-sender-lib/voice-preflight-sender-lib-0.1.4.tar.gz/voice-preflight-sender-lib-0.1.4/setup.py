51b8e09c5f83d1a99a0eb00e42922f538eaad0e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:09 +0000

    Add package voice-preflight-sender-lib/0.1.4/voice-preflight-sender-lib-0.1.4.tar.gz

---
+++ packages/voice-preflight-sender-lib/voice-preflight-sender-lib-0.1.4.tar.gz/voice-preflight-sender-lib-0.1.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="voice-preflight-sender-lib",
+    version="0.1.4",
+    packages=["voice-preflight-sender-lib"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
