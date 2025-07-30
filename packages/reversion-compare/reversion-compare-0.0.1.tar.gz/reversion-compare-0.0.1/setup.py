491f4a01b43582decedcb6ce89d5ab985bf46304
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:22 +0000

    Add package reversion-compare/0.0.1/reversion-compare-0.0.1.tar.gz

---
+++ packages/reversion-compare/reversion-compare-0.0.1.tar.gz/reversion-compare-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="reversion-compare",
+    version="0.0.1",
+    packages=["reversion-compare"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
