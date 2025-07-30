d40f81faf290969a5ec300e82016eeb35c72f0e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:10 +0000

    Add package testlog-parser/0.0.1/testlog-parser-0.0.1.tar.gz

---
+++ packages/testlog-parser/testlog-parser-0.0.1.tar.gz/testlog-parser-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="testlog-parser",
+    version="0.0.1",
+    packages=["testlog-parser"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
