b9ebea447caef448cf3840b43e05c97dc7039c63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:12 +0000

    Add package ambari-commons/0.0.1/ambari-commons-0.0.1.tar.gz

---
+++ packages/ambari-commons/ambari-commons-0.0.1.tar.gz/ambari-commons-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ambari-commons",
+    version="0.0.1",
+    packages=["ambari-commons"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
