d1b6368e61af278639cff18848dd19ff0d0579a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:55 +0000

    Add package pypo-golos/0.0.1/pypo-golos-0.0.1.tar.gz

---
+++ packages/pypo-golos/pypo-golos-0.0.1.tar.gz/pypo-golos-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pypo-golos",
+    version="0.0.1",
+    packages=["pypo-golos"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
