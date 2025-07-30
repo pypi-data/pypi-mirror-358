8e7428e8c47f4236452e4829b17898a948ce3c21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:13 +0000

    Add package ok_inference/1.0.37/ok_inference-1.0.37.tar.gz

---
+++ packages/ok_inference/ok_inference-1.0.37.tar.gz/ok_inference-1.0.37/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ok_inference",
+    version="1.0.37",
+    packages=["ok_inference"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
