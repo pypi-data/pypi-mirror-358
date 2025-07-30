d5f952b2cb11d079e26861751095989b7a3a67a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:07 +0000

    Add package awq-inference-engine/0.0.1/awq-inference-engine-0.0.1.tar.gz

---
+++ packages/awq-inference-engine/awq-inference-engine-0.0.1.tar.gz/awq-inference-engine-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="awq-inference-engine",
+    version="0.0.1",
+    packages=["awq-inference-engine"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
