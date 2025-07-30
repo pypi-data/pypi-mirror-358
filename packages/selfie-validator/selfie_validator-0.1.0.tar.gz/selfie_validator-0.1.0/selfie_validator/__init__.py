81703d88f77b14221be5b8d2ebc7b4602e6120df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:31 +0000

    Add package selfie-validator/0.1.0/selfie_validator-0.1.0.tar.gz

---
+++ packages/selfie-validator/selfie_validator-0.1.0.tar.gz/selfie_validator-0.1.0/selfie_validator/__init__.py
@@ -0,0 +1,17 @@
+"""
+Selfie Validator - A Python package for validating selfie image quality.
+
+This package provides tools to analyze selfie images and validate their quality
+based on various factors like lighting, angle, distance, sharpness, and face detection.
+
+Perfect for applications that need to ensure high-quality selfie input before processing.
+"""
+
+from .validator import SelfieValidator
+from .exceptions import SelfieValidationError
+
+__version__ = "0.1.0"
+__author__ = "du2x"
+__email__ = "du2x@pm.me"
+
+__all__ = ["SelfieValidator", "SelfieValidationError"]
---
+++ packages/selfie-validator/selfie_validator-0.1.0.tar.gz/selfie_validator-0.1.0/setup.py
@@ -0,0 +1,51 @@
+"""
+Setup configuration for selfie-validator package.
+"""
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="selfie-validator",
+    version="0.1.0",
+    author="du2x",
+    author_email="du2x@pm.me",
+    description="A Python package for validating selfie image quality",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/du2x/selfie-validator",
+    project_urls={
+        "Bug Tracker": "https://github.com/du2x/selfie-validator/issues",
+        "Documentation": "https://github.com/du2x/selfie-validator#readme",
+    },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Scientific/Engineering :: Image Processing",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(),
+    python_requires=">=3.8",
+    install_requires=[],
+    extras_require={
+        "dev": [
+            "pytest>=6.0",
+            "pytest-cov>=2.0",
+            "black>=21.0",
+            "flake8>=3.8",
+            "mypy>=0.910",
+        ],
+    },
+    include_package_data=True,
+    zip_safe=False,
+    keywords="selfie, image validation, face detection, opencv, computer vision",
+)
