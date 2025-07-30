f36d917cc9f5b42dbfa4ee4594d1d6d5c917d8ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:36 +0000

    Add package cloudauth/0.0.4/cloudauth-0.0.4.tar.gz

---
+++ packages/cloudauth/cloudauth-0.0.4.tar.gz/cloudauth-0.0.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="cloudauth",
+    version="0.0.4",
+    packages=["cloudauth"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
