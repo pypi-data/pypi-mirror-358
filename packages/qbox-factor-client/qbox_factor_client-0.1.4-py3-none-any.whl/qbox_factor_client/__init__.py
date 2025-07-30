30b7c7995c1c0cd29efa388c92cb7e87eab0a671
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:09:13 +0000

    Add package qbox-factor-client/0.1.4/qbox_factor_client-0.1.4-py3-none-any.whl

---
+++ packages/qbox-factor-client/qbox_factor_client-0.1.4-py3-none-any.whl/qbox_factor_client/__init__.py
@@ -0,0 +1,10 @@
+"""
+QBox Factor Client - Python client for the QBox Factor API
+
+Simple and intuitive factor data access for quantitative strategies.
+"""
+
+from .factor import FactorClient
+
+__version__ = "0.1.0"
+__all__ = ["FactorClient"]
