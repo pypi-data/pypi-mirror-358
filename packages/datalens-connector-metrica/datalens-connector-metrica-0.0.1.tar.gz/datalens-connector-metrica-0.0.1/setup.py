ecb1748d08940f2481ee69b06ba0b5f24df08713
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:05:10 +0000

    Add package datalens-connector-metrica/0.0.1/datalens-connector-metrica-0.0.1.tar.gz

---
+++ packages/datalens-connector-metrica/datalens-connector-metrica-0.0.1.tar.gz/datalens-connector-metrica-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="datalens-connector-metrica",
+    version="0.0.1",
+    packages=["datalens-connector-metrica"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
