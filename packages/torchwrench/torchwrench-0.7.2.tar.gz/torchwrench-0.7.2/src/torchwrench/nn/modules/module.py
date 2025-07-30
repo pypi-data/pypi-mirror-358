ba78b47466f792215efa4e462fe82501106b28fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:50 +0000

    Add package torchwrench/0.7.2/torchwrench-0.7.2.tar.gz

---
+++ packages/torchwrench/torchwrench-0.7.2.tar.gz/torchwrench-0.7.2/src/torchwrench/nn/modules/module.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from torchwrench.nn.modules._mixins import EModule  # noqa: F401
+from torchwrench.nn.modules._mixins import EModule as Module  # noqa: F401
