e42a69473cb4df22260506314736c781fcd16ba9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:19 +0000

    Add package bilateral-solver/0.0.1/bilateral-solver-0.0.1.tar.gz

---
+++ packages/bilateral-solver/bilateral-solver-0.0.1.tar.gz/bilateral-solver-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="bilateral-solver",
+    version="0.0.1",
+    packages=["bilateral-solver"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
