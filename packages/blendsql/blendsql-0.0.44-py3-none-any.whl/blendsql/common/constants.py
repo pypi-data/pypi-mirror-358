a3d7260d6afa0b69497497cc2ba39f7c27f7e9d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:35:37 +0000

    Add package blendsql/0.0.44/blendsql-0.0.44-py3-none-any.whl

---
+++ packages/blendsql/blendsql-0.0.44-py3-none-any.whl/blendsql/common/constants.py
@@ -0,0 +1,35 @@
+from enum import Enum, EnumMeta
+from dataclasses import dataclass
+
+HF_REPO_ID = "parkervg/blendsql-test-dbs"
+
+
+class StrInMeta(EnumMeta):
+    def __contains__(cls, item):
+        return item in cls.__members__.values()
+
+
+DEFAULT_ANS_SEP = ";"
+DEFAULT_NAN_ANS = "-"
+# INDENT = lambda n=1: "    " * n
+INDENT = lambda n=1: "\t" * n
+
+
+class IngredientType(str, Enum, metaclass=StrInMeta):
+    MAP = "MAP"
+    STRING = "STRING"
+    QA = "QA"
+    JOIN = "JOIN"
+    ALIAS = "ALIAS"
+
+
+@dataclass
+class IngredientKwarg:
+    QUESTION: str = "question"
+    CONTEXT: str = "context"
+    VALUES: str = "values"
+    OPTIONS: str = "options"
+    REGEX: str = "regex"
+    MODEL: str = "model"
+    OUTPUT_TYPE: str = "output_type"
+    EXAMPLE_OUTPUTS: str = "example_outputs"
