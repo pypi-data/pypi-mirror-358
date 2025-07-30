f34333b99ba3686e8add6aaae842dbb592a72544
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:52 +0000

    Add package xml-apis-ext/1.3.4/xml-apis-ext-1.3.04.tar.gz

---
+++ packages/xml-apis-ext/xml-apis-ext-1.3.04.tar.gz/xml-apis-ext-1.3.04/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="xml-apis-ext",
+    version="1.3.04",
+    packages=["xml-apis-ext"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
