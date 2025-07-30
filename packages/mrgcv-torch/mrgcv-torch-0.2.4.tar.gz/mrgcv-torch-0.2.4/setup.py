cebedd23003597f750d7cf46cd89be127d96a01c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:22 +0000

    Add package mrgcv-torch/0.2.4/mrgcv-torch-0.2.4.tar.gz

---
+++ packages/mrgcv-torch/mrgcv-torch-0.2.4.tar.gz/mrgcv-torch-0.2.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="mrgcv-torch",
+    version="0.2.4",
+    packages=["mrgcv-torch"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
