c34ddadb581f1504092b2e024db8c12e4f6dc7b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:19:31 +0000

    Add package hello-demo-290625/0.1/hello_demo-290625-0.1.tar.gz

---
+++ packages/hello-demo-290625/hello_demo-290625-0.1.tar.gz/hello_demo-290625-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hello_demo-290625",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=['pandas'],
+    author="Author01",
+)
---
+++ packages/hello-demo-290625/hello_demo-290625-0.1.tar.gz/hello_demo-290625-0.1/test/test.py
@@ -0,0 +1,3 @@
+from Hello_World import hello
+
+hello()
