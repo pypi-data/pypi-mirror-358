688a4902594bc34e060ed6d4a94a56de28b30ee9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:40 +0000

    Add package remote-probe/0.0.1/remote-probe-0.0.1.tar.gz

---
+++ packages/remote-probe/remote-probe-0.0.1.tar.gz/remote-probe-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="remote-probe",
+    version="0.0.1",
+    packages=["remote-probe"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
