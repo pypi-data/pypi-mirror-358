7501fb12e1c0b830b8fc9a664081f00defa5c5fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:03 +0000

    Add package focal-loss-cuda/0.0.1/focal-loss-cuda-0.0.1.tar.gz

---
+++ packages/focal-loss-cuda/focal-loss-cuda-0.0.1.tar.gz/focal-loss-cuda-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="focal-loss-cuda",
+    version="0.0.1",
+    packages=["focal-loss-cuda"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
