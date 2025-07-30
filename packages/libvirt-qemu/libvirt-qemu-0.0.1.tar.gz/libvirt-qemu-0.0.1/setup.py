a79a3e781259a8caa28edef8c5bcb538a96717a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:22 +0000

    Add package libvirt-qemu/0.0.1/libvirt-qemu-0.0.1.tar.gz

---
+++ packages/libvirt-qemu/libvirt-qemu-0.0.1.tar.gz/libvirt-qemu-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="libvirt-qemu",
+    version="0.0.1",
+    packages=["libvirt-qemu"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
