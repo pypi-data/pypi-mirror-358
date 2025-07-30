c36ac013eaf2ce3e3e5b6ad0d0b5d70d0dba186d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:21 +0000

    Add package jakarta.persistence-api/3.1.0/jakarta.persistence-api-3.1.0.tar.gz

---
+++ packages/jakarta.persistence-api/jakarta.persistence-api-3.1.0.tar.gz/jakarta.persistence-api-3.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="jakarta.persistence-api",
+    version="3.1.0",
+    packages=["jakarta.persistence-api"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
