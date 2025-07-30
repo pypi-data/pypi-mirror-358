182bc3f7ce9c95b64512de171533cfeb65bc9cda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:07:51 +0000

    Add package pxr-boost-dev/25.5.1.1/pxr_boost_dev-25.5.1.1-cp38-cp38-win_amd64.whl

---
+++ packages/pxr-boost-dev/pxr_boost_dev-25.5.1.1-cp38-cp38-win_amd64.whl/pxr-boost/cmake/pxr-boost/pxr-boost-targets-release.cmake
@@ -0,0 +1,19 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "pxr::boost-python" for configuration "Release"
+set_property(TARGET pxr::boost-python APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(pxr::boost-python PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/pxr-boost/lib/PxrBoostPython.lib"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/pxr-boost/lib/PxrBoostPython.dll"
+  )
+
+list(APPEND _cmake_import_check_targets pxr::boost-python )
+list(APPEND _cmake_import_check_files_for_pxr::boost-python "${_IMPORT_PREFIX}/pxr-boost/lib/PxrBoostPython.lib" "${_IMPORT_PREFIX}/pxr-boost/lib/PxrBoostPython.dll" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
