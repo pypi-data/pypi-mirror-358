c12737ab354330bfb85e9fbeccf872c443a86707
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:11 +0000

    Add package udpa/0.0.1/udpa-0.0.1.tar.gz

---
+++ packages/udpa/udpa-0.0.1.tar.gz/udpa-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="udpa",
+    version="0.0.1",
+    packages=["udpa"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
