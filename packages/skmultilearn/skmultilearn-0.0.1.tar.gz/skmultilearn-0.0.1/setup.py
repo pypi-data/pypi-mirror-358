2f9ffca93492ba052ffe42cb201809e02b6dd885
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:15 +0000

    Add package skmultilearn/0.0.1/skmultilearn-0.0.1.tar.gz

---
+++ packages/skmultilearn/skmultilearn-0.0.1.tar.gz/skmultilearn-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="skmultilearn",
+    version="0.0.1",
+    packages=["skmultilearn"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
