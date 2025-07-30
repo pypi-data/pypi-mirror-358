6bd60f56cf9dc0d8eae39b46c67e133b8da6b5e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:22 +0000

    Add package sortedm2m/0.0.1/sortedm2m-0.0.1.tar.gz

---
+++ packages/sortedm2m/sortedm2m-0.0.1.tar.gz/sortedm2m-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="sortedm2m",
+    version="0.0.1",
+    packages=["sortedm2m"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
