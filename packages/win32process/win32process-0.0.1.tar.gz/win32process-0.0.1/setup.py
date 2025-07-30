48f9b2ef86f2b37a0f2e50a52eaf32c33a079655
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:20 +0000

    Add package win32process/0.0.1/win32process-0.0.1.tar.gz

---
+++ packages/win32process/win32process-0.0.1.tar.gz/win32process-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="win32process",
+    version="0.0.1",
+    packages=["win32process"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
