12e59d6d699dc738ff6d72a6c819df417953f872
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:29 +0000

    Add package qembedder-pb2/0.0.1/qembedder-pb2-0.0.1.tar.gz

---
+++ packages/qembedder-pb2/qembedder-pb2-0.0.1.tar.gz/qembedder-pb2-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="qembedder-pb2",
+    version="0.0.1",
+    packages=["qembedder-pb2"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
