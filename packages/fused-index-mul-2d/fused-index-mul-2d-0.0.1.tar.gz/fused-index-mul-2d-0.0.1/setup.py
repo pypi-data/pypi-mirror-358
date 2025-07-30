f993ba07c0172a29c09276a0e70d230c73fa11d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:03 +0000

    Add package fused-index-mul-2d/0.0.1/fused-index-mul-2d-0.0.1.tar.gz

---
+++ packages/fused-index-mul-2d/fused-index-mul-2d-0.0.1.tar.gz/fused-index-mul-2d-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="fused-index-mul-2d",
+    version="0.0.1",
+    packages=["fused-index-mul-2d"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
