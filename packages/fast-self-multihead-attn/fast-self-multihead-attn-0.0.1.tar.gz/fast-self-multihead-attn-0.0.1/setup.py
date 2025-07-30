d8159ac4b217e884e83ab2e0eb498a912c134b82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:35 +0000

    Add package fast-self-multihead-attn/0.0.1/fast-self-multihead-attn-0.0.1.tar.gz

---
+++ packages/fast-self-multihead-attn/fast-self-multihead-attn-0.0.1.tar.gz/fast-self-multihead-attn-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="fast-self-multihead-attn",
+    version="0.0.1",
+    packages=["fast-self-multihead-attn"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
