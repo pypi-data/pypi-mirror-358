4944a18633386bfc34aaab17b991f7659116e9cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:22 +0000

    Add package kladun/0.2.0/kladun-0.2.0.tar.gz

---
+++ packages/kladun/kladun-0.2.0.tar.gz/kladun-0.2.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="kladun",
+    version="0.2.0",
+    packages=["kladun"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
