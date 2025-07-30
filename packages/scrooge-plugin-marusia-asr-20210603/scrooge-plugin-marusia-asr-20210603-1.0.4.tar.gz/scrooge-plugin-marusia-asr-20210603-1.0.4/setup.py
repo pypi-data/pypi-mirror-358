44a7cdfde863df447948fe7957cbf52b687c7f94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:44 +0000

    Add package scrooge-plugin-marusia-asr-20210603/1.0.4/scrooge-plugin-marusia-asr-20210603-1.0.4.tar.gz

---
+++ packages/scrooge-plugin-marusia-asr-20210603/scrooge-plugin-marusia-asr-20210603-1.0.4.tar.gz/scrooge-plugin-marusia-asr-20210603-1.0.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="scrooge-plugin-marusia-asr-20210603",
+    version="1.0.4",
+    packages=["scrooge-plugin-marusia-asr-20210603"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
