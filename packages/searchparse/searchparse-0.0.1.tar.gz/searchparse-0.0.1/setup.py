28c7a3c9613447598fd8e65b4da84f0d5d0628a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:31 +0000

    Add package searchparse/0.0.1/searchparse-0.0.1.tar.gz

---
+++ packages/searchparse/searchparse-0.0.1.tar.gz/searchparse-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="searchparse",
+    version="0.0.1",
+    packages=["searchparse"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
