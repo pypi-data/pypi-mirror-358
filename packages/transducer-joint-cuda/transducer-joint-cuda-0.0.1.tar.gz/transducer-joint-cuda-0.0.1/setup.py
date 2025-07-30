1ac2be5ebfb2c0941549ff9982abfe15f260c08e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:37 +0000

    Add package transducer-joint-cuda/0.0.1/transducer-joint-cuda-0.0.1.tar.gz

---
+++ packages/transducer-joint-cuda/transducer-joint-cuda-0.0.1.tar.gz/transducer-joint-cuda-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="transducer-joint-cuda",
+    version="0.0.1",
+    packages=["transducer-joint-cuda"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
