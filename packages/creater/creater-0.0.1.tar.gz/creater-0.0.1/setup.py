c075b83adc4cb3bd2159d18465e42dbfdd6e75ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:20 +0000

    Add package creater/0.0.1/creater-0.0.1.tar.gz

---
+++ packages/creater/creater-0.0.1.tar.gz/creater-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="creater",
+    version="0.0.1",
+    packages=["creater"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
