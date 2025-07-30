07a9b644e02758453ba7b264de5c7fdf30f5c07a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:30 +0000

    Add package xsignature/0.126.0/xsignature-0.126.0.tar.gz

---
+++ packages/xsignature/xsignature-0.126.0.tar.gz/xsignature-0.126.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="xsignature",
+    version="0.126.0",
+    packages=["xsignature"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
