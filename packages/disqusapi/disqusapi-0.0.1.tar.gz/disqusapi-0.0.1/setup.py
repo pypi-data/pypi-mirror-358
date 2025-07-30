5e2ffabc2b5d413591128682f4d39973db303839
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:14 +0000

    Add package disqusapi/0.0.1/disqusapi-0.0.1.tar.gz

---
+++ packages/disqusapi/disqusapi-0.0.1.tar.gz/disqusapi-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="disqusapi",
+    version="0.0.1",
+    packages=["disqusapi"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
