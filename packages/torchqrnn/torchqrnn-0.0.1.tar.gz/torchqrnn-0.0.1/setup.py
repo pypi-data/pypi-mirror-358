b4e92c414ca809b7ba63278dd0f224cc2430fa34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:24 +0000

    Add package torchqrnn/0.0.1/torchqrnn-0.0.1.tar.gz

---
+++ packages/torchqrnn/torchqrnn-0.0.1.tar.gz/torchqrnn-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="torchqrnn",
+    version="0.0.1",
+    packages=["torchqrnn"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
