1cda97c211e995a87048ff7e36f5f7725a051a12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:59 +0000

    Add package gaussian-splatting-mcmc/1.1.2/gaussian_splatting_mcmc-1.1.2.tar.gz

---
+++ packages/gaussian-splatting-mcmc/gaussian_splatting_mcmc-1.1.2.tar.gz/gaussian_splatting_mcmc-1.1.2/submodules/diff-gaussian-rasterization/rasterize_points.cu
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2023, Inria
+ * GRAPHDECO research group, https://team.inria.fr/graphdeco
+ * All rights reserved.
+ *
+ * This software is free for non-commercial, research and evaluation use 
+ * under the terms of the LICENSE.md file.
+ *
+ * For inquiries contact  george.drettakis@inria.fr
+ */
+
+#include <math.h>
+#include <torch/extension.h>
+#include <cstdio>
+#include <sstream>
+#include <iostream>
+#include <tuple>
+#include <stdio.h>
+#include <cuda_runtime_api.h>
+#include <memory>
+#include "cuda_rasterizer/config.h"
+#include "cuda_rasterizer/utils.h"
+#include <fstream>
+#include <string>
+#include <functional>
+
+std::tuple<torch::Tensor, torch::Tensor> ComputeRelocationCUDA(
+	torch::Tensor& opacity_old,
+	torch::Tensor& scale_old,
+	torch::Tensor& N,
+	torch::Tensor& binoms,
+	const int n_max)
+{
+	const int P = opacity_old.size(0);
+  
+	torch::Tensor final_opacity = torch::full({P}, 0, opacity_old.options().dtype(torch::kFloat32));
+	torch::Tensor final_scale = torch::full({3 * P}, 0, scale_old.options().dtype(torch::kFloat32));
+
+	if(P != 0)
+	{
+		UTILS::ComputeRelocation(P,
+			opacity_old.contiguous().data<float>(),
+			scale_old.contiguous().data<float>(),
+			N.contiguous().data<int>(),
+			binoms.contiguous().data<float>(),
+			n_max,
+			final_opacity.contiguous().data<float>(),
+			final_scale.contiguous().data<float>());
+	}
+
+	return std::make_tuple(final_opacity, final_scale);
+
+}
