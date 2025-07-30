e8dae0be1839bae9e9920a4756e310423c220b40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:38 +0000

    Add package ground-truth/0.0.1/ground-truth-0.0.1.tar.gz

---
+++ packages/ground-truth/ground-truth-0.0.1.tar.gz/ground-truth-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ground-truth",
+    version="0.0.1",
+    packages=["ground-truth"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
