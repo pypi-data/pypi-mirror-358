52a31c9923e26c59c5ea71fa5992ecb42a04d799
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:12 +0000

    Add package lm-models/0.0.1/lm-models-0.0.1.tar.gz

---
+++ packages/lm-models/lm-models-0.0.1.tar.gz/lm-models-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="lm-models",
+    version="0.0.1",
+    packages=["lm-models"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
