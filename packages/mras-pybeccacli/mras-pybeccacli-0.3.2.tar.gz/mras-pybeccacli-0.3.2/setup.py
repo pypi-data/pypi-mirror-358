910e284c156ea6b6a2d37aa2a39edc94d6675581
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:07:02 +0000

    Add package mras-pybeccacli/0.3.2/mras-pybeccacli-0.3.2.tar.gz

---
+++ packages/mras-pybeccacli/mras-pybeccacli-0.3.2.tar.gz/mras-pybeccacli-0.3.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="mras-pybeccacli",
+    version="0.3.2",
+    packages=["mras-pybeccacli"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
