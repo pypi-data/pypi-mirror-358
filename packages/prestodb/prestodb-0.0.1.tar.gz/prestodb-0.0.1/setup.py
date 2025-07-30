516d83f05a4b8176ae1f18c0f2af22be6262b23f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:41 +0000

    Add package prestodb/0.0.1/prestodb-0.0.1.tar.gz

---
+++ packages/prestodb/prestodb-0.0.1.tar.gz/prestodb-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="prestodb",
+    version="0.0.1",
+    packages=["prestodb"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
