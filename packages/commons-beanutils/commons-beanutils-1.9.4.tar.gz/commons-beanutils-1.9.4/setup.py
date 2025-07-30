f7dc3eca798f304d656b8a9c38c8cf1009a17d3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:52 +0000

    Add package commons-beanutils/1.9.4/commons-beanutils-1.9.4.tar.gz

---
+++ packages/commons-beanutils/commons-beanutils-1.9.4.tar.gz/commons-beanutils-1.9.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="commons-beanutils",
+    version="1.9.4",
+    packages=["commons-beanutils"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
