e14369ccdbb84757e64b1d9c08b581d59e907138
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:57 +0000

    Add package pypo-apiclient/0.0.1/pypo-apiclient-0.0.1.tar.gz

---
+++ packages/pypo-apiclient/pypo-apiclient-0.0.1.tar.gz/pypo-apiclient-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pypo-apiclient",
+    version="0.0.1",
+    packages=["pypo-apiclient"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
