c58b0e03a3f6b43af0db12791675f25ad594309a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:10 +0000

    Add package springdoc-openapi-starter-webmvc-api/2.2.0/springdoc-openapi-starter-webmvc-api-2.2.0.tar.gz

---
+++ packages/springdoc-openapi-starter-webmvc-api/springdoc-openapi-starter-webmvc-api-2.2.0.tar.gz/springdoc-openapi-starter-webmvc-api-2.2.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="springdoc-openapi-starter-webmvc-api",
+    version="2.2.0",
+    packages=["springdoc-openapi-starter-webmvc-api"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
