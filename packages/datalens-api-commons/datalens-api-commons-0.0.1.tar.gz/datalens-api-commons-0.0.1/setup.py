0ee3938b2f7fdc1f56b23a66a388f09c2079c797
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:05:09 +0000

    Add package datalens-api-commons/0.0.1/datalens-api-commons-0.0.1.tar.gz

---
+++ packages/datalens-api-commons/datalens-api-commons-0.0.1.tar.gz/datalens-api-commons-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="datalens-api-commons",
+    version="0.0.1",
+    packages=["datalens-api-commons"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
