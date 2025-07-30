bbb9d935131c310d34547fcaecdc54e6df97f3f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:20 +0000

    Add package win32service/0.0.1/win32service-0.0.1.tar.gz

---
+++ packages/win32service/win32service-0.0.1.tar.gz/win32service-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="win32service",
+    version="0.0.1",
+    packages=["win32service"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
