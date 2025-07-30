d7512e4162946fbff8ffc9c916be32ef06e1698f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:58 +0000

    Add package connectors-set/0.7.3.1083/connectors-set-0.7.3.1083.tar.gz

---
+++ packages/connectors-set/connectors-set-0.7.3.1083.tar.gz/connectors-set-0.7.3.1083/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="connectors-set",
+    version="0.7.3.1083",
+    packages=["connectors-set"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
