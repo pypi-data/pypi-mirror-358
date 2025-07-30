92588aecc07cb6ca35bef150456fd59f6bdca366
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:05:20 +0000

    Add package datalens-sqlalchemy-mysql/0.3/datalens-sqlalchemy-mysql-0.3.tar.gz

---
+++ packages/datalens-sqlalchemy-mysql/datalens-sqlalchemy-mysql-0.3.tar.gz/datalens-sqlalchemy-mysql-0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="datalens-sqlalchemy-mysql",
+    version="0.3",
+    packages=["datalens-sqlalchemy-mysql"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
