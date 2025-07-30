3a453b47351e4323e6ec95b679df22d961fdecec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:30 +0000

    Add package bigdata-dpp-client/0.0.3/bigdata-dpp-client-0.0.3.tar.gz

---
+++ packages/bigdata-dpp-client/bigdata-dpp-client-0.0.3.tar.gz/bigdata-dpp-client-0.0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="bigdata-dpp-client",
+    version="0.0.3",
+    packages=["bigdata-dpp-client"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
