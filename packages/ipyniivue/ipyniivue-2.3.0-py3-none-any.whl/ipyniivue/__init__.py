41852477ed368e9d5cea7756bbf808105bd5eb9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:35 +0000

    Add package ipyniivue/2.3.0/ipyniivue-2.3.0-py3-none-any.whl

---
+++ packages/ipyniivue/ipyniivue-2.3.0-py3-none-any.whl/ipyniivue/__init__.py
@@ -0,0 +1,17 @@
+"""A Jupyter widget for Niivue based on anywidget."""
+
+import importlib.metadata
+
+from .constants import DragMode, MultiplanarType, ShowRender, SliceType  # noqa: F401
+from .download_dataset import download_dataset  # noqa: F401
+from .widget import (  # noqa: F401
+    LUT,
+    ColorMap,
+    Mesh,
+    MeshLayer,
+    NiiVue,
+    Volume,
+    WidgetObserver,
+)
+
+__version__ = importlib.metadata.version("ipyniivue")
