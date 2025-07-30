80380a544cf462d3f11e066eebcff9a44f44670a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:07:01 +0000

    Add package pyvkteamsbot/2024.5.2/pyvkteamsbot-2024.5.2.tar.gz

---
+++ packages/pyvkteamsbot/pyvkteamsbot-2024.5.2.tar.gz/pyvkteamsbot-2024.5.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pyvkteamsbot",
+    version="2024.5.2",
+    packages=["pyvkteamsbot"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
