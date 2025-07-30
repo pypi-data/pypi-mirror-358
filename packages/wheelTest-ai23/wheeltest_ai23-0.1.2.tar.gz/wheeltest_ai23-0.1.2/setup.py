7a885c0492d15852c20a964be4c06a2c851d1430
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:39 +0000

    Add package wheelTest-ai23/0.1.2/wheeltest_ai23-0.1.2.tar.gz

---
+++ packages/wheelTest-ai23/wheeltest_ai23-0.1.2.tar.gz/wheeltest_ai23-0.1.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="wheelTest_ai23",
+    version="0.1.2",
+    author="Your Name",
+    description="一个简单的数学运算库",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
