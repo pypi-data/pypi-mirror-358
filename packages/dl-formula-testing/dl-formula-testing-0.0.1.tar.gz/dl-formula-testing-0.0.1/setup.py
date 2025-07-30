a8968f3e7f2ec3cd40e966d547c1fd81e85df692
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:39 +0000

    Add package dl-formula-testing/0.0.1/dl-formula-testing-0.0.1.tar.gz

---
+++ packages/dl-formula-testing/dl-formula-testing-0.0.1.tar.gz/dl-formula-testing-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dl-formula-testing",
+    version="0.0.1",
+    packages=["dl-formula-testing"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
