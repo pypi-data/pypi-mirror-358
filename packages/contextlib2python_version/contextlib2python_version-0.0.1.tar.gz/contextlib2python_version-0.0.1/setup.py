051f8dded1458f223c4e3813878f2737f348a76e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:59 +0000

    Add package contextlib2python_version/0.0.1/contextlib2python_version-0.0.1.tar.gz

---
+++ packages/contextlib2python_version/contextlib2python_version-0.0.1.tar.gz/contextlib2python_version-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="contextlib2python_version",
+    version="0.0.1",
+    packages=["contextlib2python_version"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
