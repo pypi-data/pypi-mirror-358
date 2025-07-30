5bf89149b5ae163096d23eca2cadde17ae8aac20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:52 +0000

    Add package lenta-model-text/0.0.1/lenta-model-text-0.0.1.tar.gz

---
+++ packages/lenta-model-text/lenta-model-text-0.0.1.tar.gz/lenta-model-text-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="lenta-model-text",
+    version="0.0.1",
+    packages=["lenta-model-text"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
