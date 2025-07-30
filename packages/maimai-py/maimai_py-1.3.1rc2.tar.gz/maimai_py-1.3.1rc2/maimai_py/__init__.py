6cf8de186518a983590a8e78c16a959623c6482b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:43 +0000

    Add package maimai-py/1.3.1rc2/maimai_py-1.3.1rc2.tar.gz

---
+++ packages/maimai-py/maimai_py-1.3.1rc2.tar.gz/maimai_py-1.3.1rc2/maimai_py/__init__.py
@@ -0,0 +1,4 @@
+# extended models and enums
+from .maimai import MaimaiAreas, MaimaiClient, MaimaiClientMultithreading, MaimaiItems, MaimaiPlates, MaimaiScores, MaimaiSongs
+from .models import *
+from .providers import *
