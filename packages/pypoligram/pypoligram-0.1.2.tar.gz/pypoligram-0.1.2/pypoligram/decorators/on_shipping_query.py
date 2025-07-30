93bb247b14645f32bd907ae2dc20e6a24f253aae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:09:16 +0000

    Add package pypoligram/0.1.2/pypoligram-0.1.2.tar.gz

---
+++ packages/pypoligram/pypoligram-0.1.2.tar.gz/pypoligram-0.1.2/pypoligram/decorators/on_shipping_query.py
@@ -0,0 +1,61 @@
+from collections.abc import Callable
+from typing import Union
+
+import pyrogram
+from pyrogram.filters import Filter
+
+import pypoligram
+from pypoligram.filters import ALL
+from pypoligram.filters import Filter as PFilter
+
+
+class OnShippingQuery:
+	def on_shipping_query(
+		self: Union["OnShippingQuery", PFilter, Filter, None] = None,
+		client_filters: PFilter | Filter | None = None,
+		filters: Filter | None = None,
+		group: int = 0
+	) -> Callable:
+		"""Decorator for handling shipping queries.
+  
+		This does the same thing as :meth:`~pypoligram.ClientManager.add_handler` using the
+		:obj:`~pyrogram.handlers.ShippingQueryHandler`.
+  
+		Parameters:
+			client_filters (:obj:`~pypoligram.filters`, *optional*):
+				Pass one or more filters to allow only a subset of clients to be passed
+				in your function.
+
+			filters (:obj:`~pyrogram.filters`, *optional*):
+				Pass one or more filters to allow only a subset of shipping queries to be passed
+				in your function.
+	
+			group (``int``, *optional*):
+				The group identifier, defaults to 0.
+		"""
+
+		def decorator(func: Callable) -> Callable:
+			nonlocal self, client_filters, filters, group
+			if isinstance(self, pypoligram.ClientManager):
+				self.add_handler(client_filters or ALL, pyrogram.handlers.ShippingQueryHandler(func, filters), group)
+			elif isinstance(self, Union[PFilter, Filter]) or self is None:
+				if not hasattr(func, "handlers"):
+					func.handlers = []
+				if isinstance(self, PFilter):
+					client_filters, self = self, client_filters
+				if isinstance(self, Filter):
+					filters, self = self, filters
+				if isinstance(self, int):
+					group = self or 0
+
+				func.handlers.append(
+					(
+						client_filters or ALL,
+						pyrogram.handlers.ShippingQueryHandler(func, filters),
+						group
+					)
+				)
+
+			return func
+
+		return decorator
