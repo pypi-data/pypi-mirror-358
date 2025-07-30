e8f53c3ca69c38d5f81fb841304de9a60f1f72f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:59 +0000

    Add package jira-events-dispatcher/0.0.1/jira-events-dispatcher-0.0.1.tar.gz

---
+++ packages/jira-events-dispatcher/jira-events-dispatcher-0.0.1.tar.gz/jira-events-dispatcher-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="jira-events-dispatcher",
+    version="0.0.1",
+    packages=["jira-events-dispatcher"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
