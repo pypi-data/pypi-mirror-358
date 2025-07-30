8e459376b7925e7bc834951599d5a78049acd6db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:09 +0000

    Add package generate-build-files/0.0.1/generate-build-files-0.0.1.tar.gz

---
+++ packages/generate-build-files/generate-build-files-0.0.1.tar.gz/generate-build-files-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="generate-build-files",
+    version="0.0.1",
+    packages=["generate-build-files"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
