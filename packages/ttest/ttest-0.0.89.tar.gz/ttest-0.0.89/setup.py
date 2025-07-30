a021be102ba38f0fd2cc1d63ec48afa47d6c8c4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:10 +0000

    Add package ttest/0.0.89/ttest-0.0.89.tar.gz

---
+++ packages/ttest/ttest-0.0.89.tar.gz/ttest-0.0.89/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ttest",
+    version="0.0.89",
+    packages=["ttest"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
