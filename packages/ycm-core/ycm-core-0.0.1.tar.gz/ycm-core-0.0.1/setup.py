911724d242466d8094e1759d47c287991b0ae404
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:18 +0000

    Add package ycm-core/0.0.1/ycm-core-0.0.1.tar.gz

---
+++ packages/ycm-core/ycm-core-0.0.1.tar.gz/ycm-core-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ycm-core",
+    version="0.0.1",
+    packages=["ycm-core"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
