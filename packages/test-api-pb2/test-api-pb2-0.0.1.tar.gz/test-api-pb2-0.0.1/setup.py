aa32f1f31ac70568492eb748b021be9d462bb663
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:15 +0000

    Add package test-api-pb2/0.0.1/test-api-pb2-0.0.1.tar.gz

---
+++ packages/test-api-pb2/test-api-pb2-0.0.1.tar.gz/test-api-pb2-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="test-api-pb2",
+    version="0.0.1",
+    packages=["test-api-pb2"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
