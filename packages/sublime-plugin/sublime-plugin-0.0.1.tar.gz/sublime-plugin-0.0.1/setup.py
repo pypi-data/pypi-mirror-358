459dbd6f85f929f43d670a7af9af17b54635714b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:18 +0000

    Add package sublime-plugin/0.0.1/sublime-plugin-0.0.1.tar.gz

---
+++ packages/sublime-plugin/sublime-plugin-0.0.1.tar.gz/sublime-plugin-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="sublime-plugin",
+    version="0.0.1",
+    packages=["sublime-plugin"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
