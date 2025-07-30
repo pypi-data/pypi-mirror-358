5afff2125aa5d6c0669e2b700edc549d896e5a63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:21 +0000

    Add package kubeflow-batch-predict/0.0.1/kubeflow-batch-predict-0.0.1.tar.gz

---
+++ packages/kubeflow-batch-predict/kubeflow-batch-predict-0.0.1.tar.gz/kubeflow-batch-predict-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="kubeflow-batch-predict",
+    version="0.0.1",
+    packages=["kubeflow-batch-predict"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
