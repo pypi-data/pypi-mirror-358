d65cd2d7aff709ee65526a3184079c97cdc69ab5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:38 +0000

    Add package datalens-i18n/0.0.1/datalens-i18n-0.0.1.tar.gz

---
+++ packages/datalens-i18n/datalens-i18n-0.0.1.tar.gz/datalens-i18n-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="datalens-i18n",
+    version="0.0.1",
+    packages=["datalens-i18n"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
