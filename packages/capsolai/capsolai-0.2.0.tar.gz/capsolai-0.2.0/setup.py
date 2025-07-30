04c2de2b660d7b22387c70cc066319a1810d7436
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:18:49 +0000

    Add package capsolai/0.2.0/capsolai-0.2.0.tar.gz

---
+++ packages/capsolai/capsolai-0.2.0.tar.gz/capsolai-0.2.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="capsolai", 
+    version="0.2.0",
+    author="trailios",
+    author_email="admin@bombing.lol",
+    description="Python API wrapper for the Captcha Solver AI service",
+    url="https://github.com/trailios/csai",
+    license="Apache License 2.0",
+    packages=find_packages(include=["capsolai", "capsolai.*"]),
+    install_requires=[
+        "requests>=2.25",
+        "typing-extensions>=4.0"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    python_requires=">=3.7",
+)
