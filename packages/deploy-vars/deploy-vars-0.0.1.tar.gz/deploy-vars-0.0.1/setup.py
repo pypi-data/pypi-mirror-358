8c86d6a351c08d4bc90b49f42d8b6bd421e5bc40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:13 +0000

    Add package deploy-vars/0.0.1/deploy-vars-0.0.1.tar.gz

---
+++ packages/deploy-vars/deploy-vars-0.0.1.tar.gz/deploy-vars-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="deploy-vars",
+    version="0.0.1",
+    packages=["deploy-vars"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
