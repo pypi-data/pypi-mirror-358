db698264cd2059d99938eb9afb9941dc827d1b7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:17 +0000

    Add package inventory-qparser/0.0.1/inventory-qparser-0.0.1.tar.gz

---
+++ packages/inventory-qparser/inventory-qparser-0.0.1.tar.gz/inventory-qparser-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="inventory-qparser",
+    version="0.0.1",
+    packages=["inventory-qparser"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
