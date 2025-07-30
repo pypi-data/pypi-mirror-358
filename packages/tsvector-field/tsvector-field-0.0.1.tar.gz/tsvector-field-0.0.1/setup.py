9e967b097a4dddc38fd0c5f1260a08902acff6f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:36 +0000

    Add package tsvector-field/0.0.1/tsvector-field-0.0.1.tar.gz

---
+++ packages/tsvector-field/tsvector-field-0.0.1.tar.gz/tsvector-field-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="tsvector-field",
+    version="0.0.1",
+    packages=["tsvector-field"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
