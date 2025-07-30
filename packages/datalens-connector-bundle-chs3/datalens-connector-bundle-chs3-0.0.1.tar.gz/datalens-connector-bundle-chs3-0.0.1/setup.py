015a1e34431e038af5a6855c6a10cffcada4a96f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:35 +0000

    Add package datalens-connector-bundle-chs3/0.0.1/datalens-connector-bundle-chs3-0.0.1.tar.gz

---
+++ packages/datalens-connector-bundle-chs3/datalens-connector-bundle-chs3-0.0.1.tar.gz/datalens-connector-bundle-chs3-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="datalens-connector-bundle-chs3",
+    version="0.0.1",
+    packages=["datalens-connector-bundle-chs3"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
