93359688ae45fba68511e220d1b92eba0ebc8043
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:07:04 +0000

    Add package zwftools/0.1.2/zwftools-0.1.2.tar.gz

---
+++ packages/zwftools/zwftools-0.1.2.tar.gz/zwftools-0.1.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+ 
+setup(
+    name='zwftools',
+    version='0.1.2',
+    packages=find_packages(),
+    install_requires=['pandas','hana_ml','sqlalchemy'],  # 依赖列表
+)
