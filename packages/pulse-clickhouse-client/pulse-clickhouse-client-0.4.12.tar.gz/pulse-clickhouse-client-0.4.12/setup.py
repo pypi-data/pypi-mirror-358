d29fc588e121a680bf9bf3a9282ed66c8b299a05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:06 +0000

    Add package pulse-clickhouse-client/0.4.12/pulse-clickhouse-client-0.4.12.tar.gz

---
+++ packages/pulse-clickhouse-client/pulse-clickhouse-client-0.4.12.tar.gz/pulse-clickhouse-client-0.4.12/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pulse-clickhouse-client",
+    version="0.4.12",
+    packages=["pulse-clickhouse-client"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
