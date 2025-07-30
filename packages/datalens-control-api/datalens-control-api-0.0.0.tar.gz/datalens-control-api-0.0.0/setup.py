72928490b14030f0120b583c1a29f90088b395fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:05:11 +0000

    Add package datalens-control-api/0.0.0/datalens-control-api-0.0.0.tar.gz

---
+++ packages/datalens-control-api/datalens-control-api-0.0.0.tar.gz/datalens-control-api-0.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="datalens-control-api",
+    version="0.0.0",
+    packages=["datalens-control-api"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
