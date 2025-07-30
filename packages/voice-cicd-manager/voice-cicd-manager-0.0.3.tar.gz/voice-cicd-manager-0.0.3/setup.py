4ff140434c5de4db84914aa8f037f814a672c000
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:08 +0000

    Add package voice-cicd-manager/0.0.3/voice-cicd-manager-0.0.3.tar.gz

---
+++ packages/voice-cicd-manager/voice-cicd-manager-0.0.3.tar.gz/voice-cicd-manager-0.0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="voice-cicd-manager",
+    version="0.0.3",
+    packages=["voice-cicd-manager"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
