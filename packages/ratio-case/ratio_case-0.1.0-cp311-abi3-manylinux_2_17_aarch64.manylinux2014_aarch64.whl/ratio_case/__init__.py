18ef91bc6b8868a5c7433cd41b9047052970f7fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:14 +0000

    Add package ratio-case/0.1.0/ratio_case-0.1.0-cp311-abi3-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/ratio-case/ratio_case-0.1.0-cp311-abi3-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/ratio_case/__init__.py
@@ -0,0 +1,12 @@
+"""# Ratio CASE
+
+Python bindings for Ratio's CASE crate collection.
+
+## License
+
+This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+If a copy of the MPL was not distributed with this file,
+You can obtain one at <https://mozilla.org/MPL/2.0/>.
+"""
+
+from ratio_case.ratio_case import *  # noqa
