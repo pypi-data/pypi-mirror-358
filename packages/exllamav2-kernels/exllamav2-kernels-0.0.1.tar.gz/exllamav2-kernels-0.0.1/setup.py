85d9e4ab7e9635808fdfe2c0564fbf2dfbd09dd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:07 +0000

    Add package exllamav2-kernels/0.0.1/exllamav2-kernels-0.0.1.tar.gz

---
+++ packages/exllamav2-kernels/exllamav2-kernels-0.0.1.tar.gz/exllamav2-kernels-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="exllamav2-kernels",
+    version="0.0.1",
+    packages=["exllamav2-kernels"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
