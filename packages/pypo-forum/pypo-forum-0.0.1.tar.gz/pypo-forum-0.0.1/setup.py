f44e54be4a2d1995106d5b8b718ffc28b7df197f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:58 +0000

    Add package pypo-forum/0.0.1/pypo-forum-0.0.1.tar.gz

---
+++ packages/pypo-forum/pypo-forum-0.0.1.tar.gz/pypo-forum-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pypo-forum",
+    version="0.0.1",
+    packages=["pypo-forum"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
