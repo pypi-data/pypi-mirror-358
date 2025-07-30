96cc90b6eb20fc9196ce261b3de2e4ecce37d409
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:36 +0000

    Add package scrooge-plugin-cdn-20210624/1.0.4/scrooge-plugin-cdn-20210624-1.0.4.tar.gz

---
+++ packages/scrooge-plugin-cdn-20210624/scrooge-plugin-cdn-20210624-1.0.4.tar.gz/scrooge-plugin-cdn-20210624-1.0.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="scrooge-plugin-cdn-20210624",
+    version="1.0.4",
+    packages=["scrooge-plugin-cdn-20210624"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
