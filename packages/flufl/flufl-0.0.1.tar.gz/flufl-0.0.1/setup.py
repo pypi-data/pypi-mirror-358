6c308bbed728b135d96092a525bad76b77750360
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:04 +0000

    Add package flufl/0.0.1/flufl-0.0.1.tar.gz

---
+++ packages/flufl/flufl-0.0.1.tar.gz/flufl-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="flufl",
+    version="0.0.1",
+    packages=["flufl"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
