5c933dde7246bf7d26f3ea324d5db0c4dd481af4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:17 +0000

    Add package airflow-qa/0.0.1/airflow-qa-0.0.1.tar.gz

---
+++ packages/airflow-qa/airflow-qa-0.0.1.tar.gz/airflow-qa-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="airflow-qa",
+    version="0.0.1",
+    packages=["airflow-qa"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
