0e7755b8e918b52abd7d442448bc58f17162b032
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:05 +0000

    Add package myteam-access/0.0.1/myteam-access-0.0.1.tar.gz

---
+++ packages/myteam-access/myteam-access-0.0.1.tar.gz/myteam-access-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="myteam-access",
+    version="0.0.1",
+    packages=["myteam-access"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
