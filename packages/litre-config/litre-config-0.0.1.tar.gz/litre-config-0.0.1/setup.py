249183d94e7ba2dda31de1e52bdaf0c9f51cb382
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:09 +0000

    Add package litre-config/0.0.1/litre-config-0.0.1.tar.gz

---
+++ packages/litre-config/litre-config-0.0.1.tar.gz/litre-config-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="litre-config",
+    version="0.0.1",
+    packages=["litre-config"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
