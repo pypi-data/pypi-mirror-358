4674eb73792aa25518aad046b3a39a922c3be2f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:36 +0000

    Add package mras-vkteams-bot/1.0.1/mras-vkteams-bot-1.0.1.tar.gz

---
+++ packages/mras-vkteams-bot/mras-vkteams-bot-1.0.1.tar.gz/mras-vkteams-bot-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="mras-vkteams-bot",
+    version="1.0.1",
+    packages=["mras-vkteams-bot"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
