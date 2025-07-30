0dbdd9c63adc885b3299bbc7f2e415183a02b554
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:05:19 +0000

    Add package datalens-sqlalchemy-mssql/0.3/datalens-sqlalchemy-mssql-0.3.tar.gz

---
+++ packages/datalens-sqlalchemy-mssql/datalens-sqlalchemy-mssql-0.3.tar.gz/datalens-sqlalchemy-mssql-0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="datalens-sqlalchemy-mssql",
+    version="0.3",
+    packages=["datalens-sqlalchemy-mssql"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
