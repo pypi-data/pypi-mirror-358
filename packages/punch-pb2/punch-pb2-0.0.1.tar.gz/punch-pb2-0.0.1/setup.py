9c02995a1c247a804e80df181102398ef45c7aa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:23 +0000

    Add package punch-pb2/0.0.1/punch-pb2-0.0.1.tar.gz

---
+++ packages/punch-pb2/punch-pb2-0.0.1.tar.gz/punch-pb2-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="punch-pb2",
+    version="0.0.1",
+    packages=["punch-pb2"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
