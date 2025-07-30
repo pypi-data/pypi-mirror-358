44b6d977255248bc288f6ff26adb5578a0acaa3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:11 +0000

    Add package ChaterJee/0.7.7/chaterjee-0.7.7.tar.gz

---
+++ packages/ChaterJee/chaterjee-0.7.7.tar.gz/chaterjee-0.7.7/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ChaterJee',
+    version='0.7.7',
+    packages=find_packages(),
+    install_requires=[
+        'python-telegram-bot==20.7',
+    ],
+    author='Pallab Dutta',
+    author_email='pallab9997@gmail.com',
+    description='Communicate your project updates via Telegram Bot!',
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    python_requires=">=3.8",
+    entry_points={
+        'console_scripts': [
+            'notejee = ChaterJee.ChaterJee:register',
+            'chatjee = ChaterJee.ChaterJee:updater',
+        ]
+    },
+)
