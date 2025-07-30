2108de5b8e77c95e4e968ed291eb2851885a1da8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:08 +0000

    Add package voice-asr-sparrow/0.8.0/voice-asr-sparrow-0.8.0.tar.gz

---
+++ packages/voice-asr-sparrow/voice-asr-sparrow-0.8.0.tar.gz/voice-asr-sparrow-0.8.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="voice-asr-sparrow",
+    version="0.8.0",
+    packages=["voice-asr-sparrow"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
