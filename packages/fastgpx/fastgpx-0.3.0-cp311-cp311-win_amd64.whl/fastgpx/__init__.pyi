70cea991d5538fb744c7ca8f0435e539ba1cb5e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:10:08 +0000

    Add package fastgpx/0.3.0/fastgpx-0.3.0-cp311-cp311-win_amd64.whl

---
+++ packages/fastgpx/fastgpx-0.3.0-cp311-cp311-win_amd64.whl/fastgpx/__init__.pyi
@@ -0,0 +1,12 @@
+from __future__ import annotations
+from fastgpx.fastgpx import Bounds
+from fastgpx.fastgpx import Gpx
+from fastgpx.fastgpx import LatLong
+from fastgpx.fastgpx import Segment
+from fastgpx.fastgpx import TimeBounds
+from fastgpx.fastgpx import Track
+from fastgpx.fastgpx import geo
+from fastgpx.fastgpx import parse
+from fastgpx.fastgpx import polyline
+from . import fastgpx
+__all__: list = ['Bounds', 'Gpx', 'LatLong', 'Segment', 'TimeBounds', 'Track', 'geo', 'parse', 'polyline']
