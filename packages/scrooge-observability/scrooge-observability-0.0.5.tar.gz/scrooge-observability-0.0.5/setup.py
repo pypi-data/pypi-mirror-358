5cce8448819702e8a89157d718bd935270d6dbfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:36 +0000

    Add package scrooge-observability/0.0.5/scrooge-observability-0.0.5.tar.gz

---
+++ packages/scrooge-observability/scrooge-observability-0.0.5.tar.gz/scrooge-observability-0.0.5/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="scrooge-observability",
+    version="0.0.5",
+    packages=["scrooge-observability"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
