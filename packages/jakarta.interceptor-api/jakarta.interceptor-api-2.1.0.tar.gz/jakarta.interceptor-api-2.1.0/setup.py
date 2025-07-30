9f4da77cf387af87c9d05227b92cace40c782eda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:09 +0000

    Add package jakarta.interceptor-api/2.1.0/jakarta.interceptor-api-2.1.0.tar.gz

---
+++ packages/jakarta.interceptor-api/jakarta.interceptor-api-2.1.0.tar.gz/jakarta.interceptor-api-2.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="jakarta.interceptor-api",
+    version="2.1.0",
+    packages=["jakarta.interceptor-api"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
