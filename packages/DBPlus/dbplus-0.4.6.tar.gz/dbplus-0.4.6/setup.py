ecc0655c0ca6460ca0324c662866ead33f545224
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:07:04 +0000

    Add package DBPlus/0.4.6/dbplus-0.4.6.tar.gz

---
+++ packages/DBPlus/dbplus-0.4.6.tar.gz/dbplus-0.4.6/setup.py
@@ -0,0 +1,22 @@
+# read the contents of your README file
+from pathlib import Path
+
+from setuptools import setup
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="DBPlus",
+    version="0.4.6",
+    description="Database-agnostic SQL Interface for Postresql, MySQL, SQLite, DB2 and more",
+    url="https://github.com/klaasbrant/DBPlus",
+    author="Klaas Brant",
+    author_email="kbrant@kbce.com",
+    license="ISC",
+    packages=["dbplus", "dbplus.drivers"],
+    install_requires=[],
+    zip_safe=False,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
