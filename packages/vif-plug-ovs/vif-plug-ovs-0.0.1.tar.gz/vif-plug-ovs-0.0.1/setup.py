8848e7162b2470cd059b9789ec012b84e5906d51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:05 +0000

    Add package vif-plug-ovs/0.0.1/vif-plug-ovs-0.0.1.tar.gz

---
+++ packages/vif-plug-ovs/vif-plug-ovs-0.0.1.tar.gz/vif-plug-ovs-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="vif-plug-ovs",
+    version="0.0.1",
+    packages=["vif-plug-ovs"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
