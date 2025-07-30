ca0382d3d636cdf357170c397e3874416e4d416c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:13 +0000

    Add package pygaux/0.0.1/pygaux-0.0.1.tar.gz

---
+++ packages/pygaux/pygaux-0.0.1.tar.gz/pygaux-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pygaux",
+    version="0.0.1",
+    packages=["pygaux"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
