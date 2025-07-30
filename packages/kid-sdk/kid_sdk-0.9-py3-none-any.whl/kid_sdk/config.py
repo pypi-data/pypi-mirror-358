46606a0949f20aa418376d513a4e3ece4273740f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:52 +0000

    Add package kid-sdk/0.9/kid_sdk-0.9-py3-none-any.whl

---
+++ packages/kid-sdk/kid_sdk-0.9-py3-none-any.whl/kid_sdk/config.py
@@ -0,0 +1,8 @@
+TEST_DOMAIN = "https://dev-smartid.zonesmart.ru"
+DOMAIN = "https://id.kokocgroup.ru"
+
+AUTHORIZATION_URL = "{domain}"
+TOKEN_ISSUE_URL = "{domain}/api/oauth2/issue/"
+GET_USER_DATA_URL = "{domain}/api/user/auth_data/"
+
+DEFAULT_SCOPE = "smart_id first_name last_name email phone roles"
