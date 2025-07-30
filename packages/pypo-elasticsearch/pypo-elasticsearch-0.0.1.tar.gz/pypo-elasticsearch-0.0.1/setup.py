5697b9c6dc276de6d4a747c6cfb569025314d1a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:31 +0000

    Add package pypo-elasticsearch/0.0.1/pypo-elasticsearch-0.0.1.tar.gz

---
+++ packages/pypo-elasticsearch/pypo-elasticsearch-0.0.1.tar.gz/pypo-elasticsearch-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pypo-elasticsearch",
+    version="0.0.1",
+    packages=["pypo-elasticsearch"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
