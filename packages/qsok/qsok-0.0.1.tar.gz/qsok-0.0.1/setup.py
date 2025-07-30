00b589dd3aeeb5aa491f0c1c73d93b62e5ecceca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:16 +0000

    Add package qsok/0.0.1/qsok-0.0.1.tar.gz

---
+++ packages/qsok/qsok-0.0.1.tar.gz/qsok-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="qsok",
+    version="0.0.1",
+    packages=["qsok"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
