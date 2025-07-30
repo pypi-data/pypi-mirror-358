c8d4aa16934d9e7e277b26ad700b8ca854687ab0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:15 +0000

    Add package odkl_airflow_common/0.1.12/odkl_airflow_common-0.1.12.tar.gz

---
+++ packages/odkl_airflow_common/odkl_airflow_common-0.1.12.tar.gz/odkl_airflow_common-0.1.12/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="odkl_airflow_common",
+    version="0.1.12",
+    packages=["odkl_airflow_common"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
