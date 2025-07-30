bf6cb8fc1f74c2e649a1f2c34ba2e3f170d8db55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:20:00 +0000

    Add package tfc-toolbox-py/0.1.0/tfc_toolbox_py-0.1.0-py3-none-any.whl

---
+++ packages/tfc-toolbox-py/tfc_toolbox_py-0.1.0-py3-none-any.whl/tfc_toolbox_py/math.py
@@ -0,0 +1,14 @@
+"""
+Add two number.
+# Example
+```Python
+import tfc_toolbox_py as tfc
+c = tfc.math.add(1, 2)
+print(c)
+```
+"""
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
