a02624a9d2133414219fdc6c2cd080d3b1811c05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:27 +0000

    Add package snakeyaml/2.2/snakeyaml-2.2.tar.gz

---
+++ packages/snakeyaml/snakeyaml-2.2.tar.gz/snakeyaml-2.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="snakeyaml",
+    version="2.2",
+    packages=["snakeyaml"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
