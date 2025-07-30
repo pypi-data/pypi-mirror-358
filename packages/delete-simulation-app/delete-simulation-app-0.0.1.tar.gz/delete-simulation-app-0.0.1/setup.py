cfc8c956a283d94c61545881eac6af2f90c37562
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:38 +0000

    Add package delete-simulation-app/0.0.1/delete-simulation-app-0.0.1.tar.gz

---
+++ packages/delete-simulation-app/delete-simulation-app-0.0.1.tar.gz/delete-simulation-app-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="delete-simulation-app",
+    version="0.0.1",
+    packages=["delete-simulation-app"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
