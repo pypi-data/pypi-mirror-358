c699e11c5e0022b07fca10ab2396b62ad7cc7df8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:09 +0000

    Add package hello-ext/0.0.1/hello-ext-0.0.1.tar.gz

---
+++ packages/hello-ext/hello-ext-0.0.1.tar.gz/hello-ext-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="hello-ext",
+    version="0.0.1",
+    packages=["hello-ext"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
