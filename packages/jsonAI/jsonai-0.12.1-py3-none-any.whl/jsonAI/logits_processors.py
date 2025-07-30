a4c9eec82e2dc1f9659d8f710d1320d9f59e24d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:35 +0000

    Add package jsonAI/0.12.1/jsonai-0.12.1-py3-none-any.whl

---
+++ packages/jsonAI/jsonai-0.12.1-py3-none-any.whl/jsonAI/logits_processors.py
@@ -0,0 +1,191 @@
+from transformers import PreTrainedTokenizer, LogitsWarper, StoppingCriteria
+import torch
+
+
+class StringStoppingCriteria(StoppingCriteria):
+    def __init__(
+        self,
+        tokenizer: PreTrainedTokenizer,
+        prompt_length: int,
+        max_length: int = None,
+    ):
+        self.tokenizer = tokenizer
+        self.prompt_length = prompt_length
+        self.max_length = max_length
+
+    def __call__(
+        self,
+        input_ids: torch.LongTensor,
+        _,
+    ) -> bool:
+        if len(input_ids[0]) <= self.prompt_length:
+            return False
+
+        last_token_id = input_ids[0][-1]
+        last_token = self.tokenizer.decode(
+            last_token_id, skip_special_tokens=True
+        )
+
+        result = '"' in last_token
+
+        if self.max_length is not None:
+            # Due to token handling, max_length check may not be accurate
+            # Could exceed by up to 10 characters
+            gen_ids = input_ids[0][self.prompt_length:]
+            o = self.tokenizer.decode(gen_ids, skip_special_tokens=True)
+            str_l = len(o)
+            if str_l > self.max_length:
+                return True
+
+        return result
+
+
+class NumberStoppingCriteria(StoppingCriteria):
+    def __init__(
+        self,
+        tokenizer: PreTrainedTokenizer,
+        prompt_length: int,
+        precision: int = 3,
+    ):
+        self.tokenizer = tokenizer
+        self.precision = precision
+        self.prompt_length = prompt_length
+
+    def __call__(
+        self,
+        input_ids: torch.LongTensor,
+        scores: torch.FloatTensor,
+    ) -> bool:
+        decoded = self.tokenizer.decode(
+            input_ids[0][self.prompt_length:], skip_special_tokens=True
+        )
+
+        if decoded.count(".") > 1:
+            return True
+
+        if (
+            decoded.count(".") == 1
+            and len(decoded.replace(" ", "").split(".")[1]) > self.precision
+        ):
+            return True
+
+        if (
+            len(decoded) > 1
+            and "," in decoded
+            and any(c.isdigit() for c in decoded.split(",")[0])
+        ):
+            return True
+
+        if (
+            len(decoded) > 1
+            and any(c.isdigit() for c in decoded)
+            and ("," in decoded or decoded[-1] in (" ", "\n"))
+        ):
+            return True
+
+        return False
+
+
+class OutputNumbersTokens(LogitsWarper):
+    def __init__(self, tokenizer: PreTrainedTokenizer):
+        self.tokenizer = tokenizer
+        vocab_size = len(tokenizer)
+        self.allowed_mask = torch.zeros(vocab_size, dtype=torch.bool)
+
+        for _, token_id in tokenizer.get_vocab().items():
+            token_str = self.tokenizer.decode(
+                token_id, skip_special_tokens=True
+            ).strip()
+
+            if (
+                token_str == ""
+                or (
+                    all(c.isdigit() or c == "." for c in token_str)
+                    and token_str.count(".") <= 1
+                )
+                or (
+                    "," in token_str
+                    and all(
+                        c.isdigit() or c == "."
+                        for c in token_str.split(",")[0]
+                    )
+                    and token_str.count(".") <= 1
+                )
+            ):
+                self.allowed_mask[token_id] = True
+
+    def __call__(self, _, scores):
+        mask = self.allowed_mask.expand_as(scores)
+        scores[~mask] = -float("inf")
+
+        return scores
+
+
+class IntegerStoppingCriteria(StoppingCriteria):
+    def __init__(
+        self,
+        tokenizer: PreTrainedTokenizer,
+        prompt_length: int,
+        max_digits: int = 15,
+    ):
+        self.tokenizer = tokenizer
+        self.prompt_length = prompt_length
+        self.max_digits = max_digits
+
+    def __call__(
+        self,
+        input_ids: torch.LongTensor,
+        scores: torch.FloatTensor,
+    ) -> bool:
+        decoded = self.tokenizer.decode(
+            input_ids[0][self.prompt_length:], skip_special_tokens=True
+        )
+
+        if len(decoded.strip()) > self.max_digits:
+            return True
+
+        if (
+            len(decoded) > 1
+            and "," in decoded
+            and any(c.isdigit() for c in decoded.split(",")[0])
+        ):
+            return True
+
+        if (
+            len(decoded) > 1
+            and any(c.isdigit() for c in decoded)
+            and decoded[-1] in (" ", "\n")
+        ):
+            return True
+
+        return False
+
+
+class OutputIntegersTokens(LogitsWarper):
+    def __init__(self, tokenizer: PreTrainedTokenizer):
+        self.tokenizer = tokenizer
+        vocab_size = len(tokenizer)
+        self.allowed_mask = torch.zeros(vocab_size, dtype=torch.bool)
+
+        for _, token_id in tokenizer.get_vocab().items():
+            token_str = self.tokenizer.decode(
+                token_id, skip_special_tokens=True
+            ).strip()
+
+            if (
+                token_str == ""
+                or all(c.isdigit() for c in token_str)
+                or (
+                    "," in token_str
+                    and all(c.isdigit() for c in token_str.split(",")[0])
+                )
+            ):
+                self.allowed_mask[token_id] = True
+
+    def __call__(self, _, scores):
+        mask = self.allowed_mask.expand_as(scores)
+        scores[~mask] = -float("inf")
+
+        return scores
+
+# FIX: W292 - Added a newline at the end of the file.
---
+++ packages/jsonAI/jsonai-0.12.1-py3-none-any.whl/jsonAI/prob_choice_tree.py
@@ -0,0 +1,93 @@
+from jaxtyping import Int
+import torch
+from torch.nn import functional as F
+from torch import Tensor
+from typing import List, Optional
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import math
+
+
+def round_to_nsf(num, nsf):
+    if num != 0:
+        return round(num, -int(math.floor(math.log10(abs(num))) + 1 - nsf))
+    else:
+        return 0  # Can't take the log of 0
+
+
+def get_valid_next_choices(
+    choices_tokens: List[Int[Tensor]],
+    current_tokens: Int[Tensor]
+):
+    next_choices = []
+    for choice_tokens in choices_tokens:
+        # if we have some more slots left
+        if len(current_tokens) < len(choice_tokens):
+            # see if current_tokens matches
+            if (
+                choice_tokens[: len(current_tokens)] == current_tokens
+            ).all():
+                c = choice_tokens[len(current_tokens)].item()
+                next_choices.append(c)
+
+    next_choices = list(set(next_choices))
+    return torch.LongTensor(next_choices)
+
+
+def _prob_choice_tree(
+    model: AutoModelForCausalLM,
+    tokenizer: AutoTokenizer,
+    input_ids: Int[Tensor],
+    choices_tokens: List[Int[Tensor]],
+    choice: Optional[Int[Tensor]] = None,
+    prob: float = 1,
+    current_tokens: Int[Tensor] = torch.LongTensor([]),
+):
+    if choice is not None:
+        c = choice[None].to(current_tokens.device)
+        current_tokens = torch.cat([current_tokens, c], dim=-1)
+        c = choice[None].to(input_ids.device)
+        input_ids = torch.cat([input_ids, c], dim=-1)
+
+    next_choices = get_valid_next_choices(choices_tokens, current_tokens)
+    if len(next_choices) == 0:
+        s = tokenizer.decode(current_tokens, skip_special_tokens=True)
+        r = dict(prob=prob, choice=s)
+        yield r
+    else:
+        o = model(input_ids[None])
+        logits_constrained = o.logits[0, -1][next_choices]
+        probs = F.softmax(logits_constrained, dim=-1)
+        for i in range(len(next_choices)):
+            next_choice_tensor = torch.LongTensor([next_choices[i]])
+            next_prob = prob * probs[i].item()
+            yield from _prob_choice_tree(
+                model=model,
+                tokenizer=tokenizer,
+                choices_tokens=choices_tokens,
+                input_ids=input_ids,
+                choice=next_choice_tensor,
+                prob=next_prob,
+                current_tokens=current_tokens,
+            )
+
+
+def prob_choice_tree(
+    *args,
+    sort: bool = True,
+    round=3,
+    **kwargs,
+):
+    choice_json = list(
+        _prob_choice_tree(
+            *args,
+            **kwargs,
+        )
+    )
+    # order by probability
+    if sort:
+        choice_json = sorted(choice_json, key=lambda x: -x["prob"])
+
+    # round probabilities
+    for c in choice_json:
+        c["prob"] = round_to_nsf(c["prob"], round)
+    return choice_json
