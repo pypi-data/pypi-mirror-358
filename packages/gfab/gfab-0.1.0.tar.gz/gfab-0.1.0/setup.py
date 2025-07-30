52763834946b73f41c6713577c38d1fa3395c104
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:07:18 +0000

    Add package gfab/0.1.0/gfab-0.1.0.tar.gz

---
+++ packages/gfab/gfab-0.1.0.tar.gz/gfab-0.1.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gfab',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        "flask",
+        "questionary",
+        "google-generativeai"
+    ],
+    entry_points={
+        'console_scripts': [
+            'gfab=gfab_core.ask_questions:main'
+        ]
+    },
+    author='Swarup Baral',
+    author_email='swarupbaral102@gmail.com',
+    description='Gemini Flask App Builder – AI-powered Flask app generator',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/swarupbaral/gfab',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Code Generators',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
